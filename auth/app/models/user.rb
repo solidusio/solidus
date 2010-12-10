spree/spree

Refactoring: Make Order#create_user more readable

auth/app/models/user.rb
@@ -42,6 +42,10 @@ class User < ActiveRecord::Base
     Role.where(:name => "admin").includes(:users).count > 0
   end
 
+  def anonymous?
+    email =~ /@example.com$/
+  end
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     UserMailer.password_reset_instructions(self).deliver

solidusio/solidus

auth/app/models/user.rb
@@ -33,6 +33,10 @@ class User < ActiveRecord::Base
     Role.where(:name => "admin").includes(:users).count > 0
   end
 
+  def anonymous?
+    email =~ /@example.net$/
+  end
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     UserMailer.password_reset_instructions(self).deliver