bf37ffab64dc1597b15936e083c9f3088b8de42f
Ryan Bigg <radarlistener@gmail.com>
2013-01-14 15:04:38 +1100

    Correct Japanese translation tests
    Rather than using i18n-spec to ensure the completeness of a translation, we will be using localeapp

--- i18n/spec/translations/ja_spec.rb
+++ i18n/spec/translations/ja_spec.rb
@@ -3,43 +3,7 @@
 require 'spec_helper'
 
 describe "Japanese (ja) translations" do
-  describe "spree_api.yml" do
-    subject { "config/locales/ja/spree_api.yml" }
-    it { subject.should be_a_subset_of("default/spree_api.yml") }
-    it { subject.should be_a_complete_translation_of("default/spree_api.yml") }
-    it { subject.should be_a_thorough_translation_of("default/spree_api.yml").except([]) }
-  end
-  
-  describe "spree_auth.yml" do
-    subject { "config/locales/ja/spree_auth.yml" }
-    it { subject.should be_a_subset_of("default/spree_auth.yml") }
-    it { subject.should be_a_complete_translation_of("default/spree_auth.yml") }
-    it { subject.should be_a_thorough_translation_of("default/spree_auth.yml").except([]) }
-  end
-  
   describe "spree_core.yml" do
-    subject { "config/locales/ja/spree_core.yml" }
-    let(:untranslated_keys) do
-      [ 
-        "activerecord.attributes.spree/country.iso",
-        "activerecord.attributes.spree/country.iso3",
-        "backordering_is_allowed",
-        "pagination.truncate",
-        "powered_by",
-        "products_with_zero_inventory_display",
-        "smtp",
-        "spree.date_picker.format",
-        "views.pagination.truncate"
-      ]
-    end
-    
-    it { subject.should be_a_subset_of("default/spree_core.yml") }
-
-    it do
-      subject.should be_a_thorough_translation_of("default/spree_core.yml").
-        except(untranslated_keys)
-    end
-
     it do
       I18n.backend = I18n::Backend::Simple.new
       I18n.backend.load_translations("config/locales/ja/spree_core.rb")
@@ -50,19 +14,5 @@ describe "Japanese (ja) translations" do
       I18n.t("products_with_zero_inventory_display", :not => "").should == "在庫なしの商品が表示されます"
       I18n.t("products_with_zero_inventory_display", :not => I18n.t("not")).should == "在庫なしの商品は表示されません"
     end
-  end
-  
-  describe "spree_dash.yml" do
-    subject { "config/locales/ja/spree_dash.yml" }
-    it { subject.should be_a_subset_of("default/spree_dash.yml") }
-    it { subject.should be_a_complete_translation_of("default/spree_dash.yml") }
-    it { subject.should be_a_thorough_translation_of("default/spree_dash.yml").except([]) }
-  end
-  
-  describe "spree_promo.yml" do
-    subject { "config/locales/ja/spree_promo.yml" }
-    it { subject.should be_a_subset_of("default/spree_promo.yml") }
-    it { subject.should be_a_complete_translation_of("default/spree_promo.yml") }
-    it { subject.should be_a_thorough_translation_of("default/spree_promo.yml").except([]) }
   end
 end
--- i18n/spree_i18n.gemspec
+++ i18n/spree_i18n.gemspec
@@ -17,7 +17,6 @@ Gem::Specification.new do |s|
 
   s.add_dependency('spree',  '~> 1.3')
   s.add_dependency('i18n', '~> 0.6')
-  s.add_development_dependency 'i18n-spec', '~> 0.3.0'
   s.add_development_dependency "rspec-rails", "~> 2.12.0"
   s.add_development_dependency "sqlite3", "~> 1.3.6"
 end
