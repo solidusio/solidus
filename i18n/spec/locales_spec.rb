3718018dabe25e0c9ddc05f25dc770f4795aec8b
Ryan Bigg <radarlistener@gmail.com>
2013-01-14 15:17:17 +1100

    Remove locales_spec
    This was causing false positives regarding match_choices keys within translations, and isn't testing much at all

--- i18n/spec/locales_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "locale files" do
-  Dir.glob('config/locales/*.yml') do |locale_file|
-    describe "a locale file" do
-      it_behaves_like 'a valid locale file', locale_file
-    end
-  end
-end
