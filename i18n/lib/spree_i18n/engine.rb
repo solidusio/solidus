26d3a940b7efb5d79e232db18eadc4c9e8bc8649
Washington Luiz <huoxito@gmail.com>
2013-06-23 01:02:49 -0300

    Account for rails default locale on initialization
    Users setting rails i18n.default_locale were getting errors because
    spree_i18n didn't change I18n.locale accordingly and globalize3 depends
    on I18n.locale to set the language when creating a translated record
    
    Fixes #248

--- i18n/lib/spree_i18n/engine.rb
+++ i18n/lib/spree_i18n/engine.rb
@@ -17,6 +17,7 @@ module SpreeI18n
 
     initializer "spree_i18n.environment", :before => :load_config_initializers do |app|
       app.config.i18n.fallbacks = true
+      I18n.locale = app.config.i18n.default_locale
       SpreeI18n::Config = SpreeI18n::Configuration.new
     end
 
