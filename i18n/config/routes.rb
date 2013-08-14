f91bf7821c7f2dc6f19df5725e0f019529394699
Washington Luiz <huoxito@gmail.com>
2013-08-14 15:47:27 -0300

    Make routes.rb rails 4 compatible

--- i18n/config/routes.rb
+++ i18n/config/routes.rb
@@ -1,5 +1,5 @@
-Spree::Core::Engine.routes.prepend do
-  match '/locale/set', :to => 'locale#set', :defaults => { :format => :json }, :as => :set_locale
+Spree::Core::Engine.routes.draw do
+  post '/locale/set', :to => 'locale#set', :defaults => { :format => :json }, :as => :set_locale
 
   namespace :admin do
     get '/:resource/:resource_id/translations' => 'translations#index', as: :translations
