81f7cf22690e3ed9084223a930b703e8e99eaf5a
Ryan Bigg <radarlistener@gmail.com>
2013-08-14 14:30:57 +1000

    Remove attr_accessible call in spree_i18n/translatable.rb

--- i18n/app/models/spree_i18n/translatable.rb
+++ i18n/app/models/spree_i18n/translatable.rb
@@ -2,7 +2,6 @@ module SpreeI18n
   module Translatable
     extend ActiveSupport::Concern
     included do
-      attr_accessible :translations_attributes
       accepts_nested_attributes_for :translations
     end
   end
