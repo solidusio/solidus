379a2bd76048a20da7d416f7ae628219e07a6f0c
Washington Luiz <huoxito@gmail.com>
2013-12-17 08:54:58 -0300

    Name assets same as engine_name
    Helps with building the test_app which will require the assets by the
    engine name

--- i18n/lib/generators/spree_i18n/install/install_generator.rb
+++ i18n/lib/generators/spree_i18n/install/install_generator.rb
@@ -4,12 +4,12 @@ module SpreeI18n
       class_option :auto_run_migrations, :type => :boolean, :default => true
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/i18n\n"
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/i18n\n"
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_i18n\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_i18n\n"
       end
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/i18n\n",
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/spree_i18n\n",
           :before => /\*\//, :verbose => true
       end
 
