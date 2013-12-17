32437825f108ee779f72372d9ccfa06894aa51eb
Washington Luiz <huoxito@gmail.com>
2013-12-17 08:44:01 -0300

    Require spree/testing_support/controller_requests
    Spree master no longer adds lib to autoload paths

--- i18n/spec/spec_helper.rb
+++ i18n/spec/spec_helper.rb
@@ -16,6 +16,7 @@ require 'spree/testing_support/factories'
 require 'spree/testing_support/preferences'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/capybara_ext'
+require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 
 RSpec.configure do |config|
