56b1bf8da8fd1ab2d91a35c3150e0afd1ebf6c7f
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2014-06-03 21:29:39 -0700

    The result call is checked earlier
    It does nothing inside this function. and its all private. so I can
    change what I want.

--- core/app/models/spree/promotion_handler/coupon.rb
+++ core/app/models/spree/promotion_handler/coupon.rb
@@ -43,7 +43,7 @@ module Spree
         # then result here will also be `true`.
         result = promotion.activate(:order => order)
         if result
-          determine_promotion_application_result(result)
+          determine_promotion_application_result
         else
           self.error = Spree.t(:coupon_code_unknown_error)
         end
@@ -76,7 +76,7 @@ module Spree
         discount ||= order.shipment_adjustments.promotion.detect(&detector)
         discount ||= order.adjustments.promotion.detect(&detector)
 
-        if result and discount.eligible
+        if discount.eligible
           order.update_totals
           order.persist_totals
           self.success = Spree.t(:coupon_code_applied)
