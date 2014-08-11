d4c4fa4aac2318ddd4cf5672dc5c9c5c6bf58bd0
Jordan Brough <git.j@brgh.net>
2014-08-11 15:10:02 -0600

    2-2-dev fix for ReimbursementMailer
    Spree::Store isn't a part of stock Spree 2.2.
    peterberkenbosch says to make this change since we're going to overwrite this email in our app anyway.

--- core/app/mailers/spree/reimbursement_mailer.rb
+++ core/app/mailers/spree/reimbursement_mailer.rb
@@ -3,7 +3,7 @@ module Spree
       def reimbursement_email(reimbursement, resend = false)
         @reimbursement = reimbursement.respond_to?(:id) ? reimbursement : Spree::Reimbursement.find(reimbursement)
         subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-        subject += "#{Spree::Store.current.name} #{Spree.t('reimbursement_mailer.reimbursement_email.subject')} ##{@reimbursement.order.number}"
+        subject += "#{Spree.t('reimbursement_mailer.reimbursement_email.subject')} ##{@reimbursement.order.number}"
         mail(to: @reimbursement.order.email, from: from_address, subject: subject)
       end
   end
