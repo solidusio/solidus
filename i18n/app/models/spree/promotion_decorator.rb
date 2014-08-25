3c7151ed131d036d1132ecba46319bc0eef4327e
Kurokawa <black.princess.w@gmail.com>
2014-08-25 17:34:33 +0900

    Added default_scope to avoid N+1 problem fixes #444

--- i18n/app/models/spree/promotion_decorator.rb
+++ i18n/app/models/spree/promotion_decorator.rb
@@ -2,5 +2,8 @@ module Spree
   Promotion.class_eval do
     translates :name, :description, :fallbacks_for_empty_translations => true
     include SpreeI18n::Translatable
+
+    # N+1 problem
+    default_scope -> { includes(:translations).references(:translations) } if method_defined?(:translations) && connection.table_exists?(self.translations_table_name)
   end
 end
--- i18n/app/models/spree/property_decorator.rb
+++ i18n/app/models/spree/property_decorator.rb
@@ -2,5 +2,8 @@ module Spree
   Property.class_eval do
     translates :name, :presentation, :fallbacks_for_empty_translations => true
     include SpreeI18n::Translatable
+
+    # N+1 problem
+    default_scope -> { includes(:translations).references(:translations) } if method_defined?(:translations) && connection.table_exists?(self.translations_table_name)
   end
 end
