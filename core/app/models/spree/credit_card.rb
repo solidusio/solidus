70f018cf206152cacf38d0f8f37d76cdcaf1ec4f
Andrew Thal <athal7@me.com>
2014-09-11 16:14:16 -0400

    Don't raise an error on credit card default setting of another card is expired

--- core/app/models/spree/credit_card.rb
+++ core/app/models/spree/credit_card.rb
@@ -136,8 +136,7 @@ module Spree
     def ensure_one_default
       if self.user_id && self.default
         CreditCard.where(default: true).where.not(id: self.id).where(user_id: self.user_id).each do |ucc|
-          ucc.default = false
-          ucc.save!
+          ucc.update_columns(default: false, updated_at: Time.now)
         end
       end
     end
