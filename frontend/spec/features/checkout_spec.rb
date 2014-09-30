3028967a62d194f0f4bc6c1ad9dc432fe415601a
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-29 20:09:17 -0400

    Fix specs broken by 09ccf8a

--- frontend/spec/features/checkout_spec.rb
+++ frontend/spec/features/checkout_spec.rb
@@ -112,7 +112,7 @@ describe "Checkout", inaccessible: true do
 
   context "and likes to double click buttons" do
     let!(:user) { create(:user) }
-    
+
     let!(:order) do
       order = OrderWalkthrough.up_to(:delivery)
 
@@ -200,6 +200,7 @@ describe "Checkout", inaccessible: true do
 
       Spree::CheckoutController.any_instance.stub(current_order: order)
       Spree::CheckoutController.any_instance.stub(try_spree_current_user: user)
+      Spree::OrdersController.any_instance.stub(try_spree_current_user: user)
 
       visit spree.checkout_state_path(:payment)
     end
