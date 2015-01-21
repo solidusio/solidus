649c7c7fb96a62f4242ed5100ad9ff6b5a8d1a23
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2015-01-21 14:55:41 -0800

    Remove current_spree_page
    This isn't used anywhere, doesn't have any tests, and is pretty crazy.
    
    fixes spree/spree#5921

--- core/app/helpers/spree/base_helper.rb
+++ core/app/helpers/spree/base_helper.rb
@@ -1,17 +1,6 @@
 module Spree
   module BaseHelper
 
-    # Defined because Rails' current_page? helper is not working when Spree is mounted at root.
-    def current_spree_page?(url)
-      path = request.fullpath.gsub(/^\/\//, '/')
-      if url.is_a?(String)
-        return path == url
-      elsif url.is_a?(Hash)
-        return path == spree.url_for(url)
-      end
-      return false
-    end
-
     def link_to_cart(text = nil)
       text = text ? h(text) : Spree.t(:cart)
       css_class = nil
