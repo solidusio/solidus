ca64324e7fa3177282fef30f05706e8239ce8163
Jordan Brough <git.j@brgh.net>
2015-01-27 14:52:05 -0700

    remove undesired code

--- api/app/controllers/spree/api/orders_controller.rb
+++ api/app/controllers/spree/api/orders_controller.rb
@@ -55,10 +55,6 @@ module Spree
         authorize! :update, @order, order_token
 
         if @order.contents.update_cart(order_params)
-          user_id = params[:order][:user_id]
-          if current_api_user.has_spree_role?('admin') && user_id
-            @order.associate_user!(Spree.user_class.find(user_id))
-          end
           respond_with(@order, default_template: :show)
         else
           invalid_resource!(@order)
