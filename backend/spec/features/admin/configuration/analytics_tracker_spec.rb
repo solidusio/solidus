3daabe988d5eec692ff1508d5bc55151ec32e4bb
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2015-01-27 19:33:51 -0800

    Remove environment form Spree::Tracker
    Much like Spree::PaymentMethod's environment was killed
    spree/spree#5822, this removes the environment settings from the
    tracker. Its a poor anti-feature that we've decided to kill.

--- backend/spec/features/admin/configuration/analytics_tracker_spec.rb
+++ backend/spec/features/admin/configuration/analytics_tracker_spec.rb
@@ -5,7 +5,7 @@ describe "Analytics Tracker", :type => :feature do
 
   context "index" do
     before(:each) do
-      2.times { create(:tracker, :environment => "test") }
+      2.times { create(:tracker) }
       visit spree.admin_path
       click_link "Settings"
       click_link "Analytics Tracker"
@@ -18,14 +18,12 @@ describe "Analytics Tracker", :type => :feature do
     it "should have the right tabular values displayed" do
       within_row(1) do
         expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Test")
-        expect(column_text(3)).to eq("Yes")
+        expect(column_text(2)).to eq("Yes")
       end
 
       within_row(2) do
         expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Test")
-        expect(column_text(3)).to eq("Yes")
+        expect(column_text(2)).to eq("Yes")
       end
     end
    end
@@ -40,14 +38,12 @@ describe "Analytics Tracker", :type => :feature do
     it "should be able to create a new analytics tracker" do
       click_link "admin_new_tracker_link"
       fill_in "tracker_analytics_id", :with => "A100"
-      select "Test", :from => "tracker-env"
       click_button "Create"
 
       expect(page).to have_content("successfully created!")
       within_row(1) do
         expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Test")
-        expect(column_text(3)).to eq("Yes")
+        expect(column_text(2)).to eq("Yes")
       end
     end
   end
