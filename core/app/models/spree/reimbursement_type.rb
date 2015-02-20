spree/spree

Refund preferences & Reimbursement Items
- Allow admins and customers to specify refund types
  - Provide a field for customers to specify a refund method preference at RMA time
  - Allow admins to specify an override refund method at reimbursement time
- Break out reimbursement_items from return_items into a separate DB table
  - Paves the way for non-return reimbursements. It also makes ReturnItem a bit simpler.

Minor:

- Fix the x icon on select2 selectboxes.
- Convert CustomerReturnsController into a ResourceController

Generated by @jordan-brough and @richardnuno

core/app/models/spree/reimbursement_type.rb
@@ -1,0 +1,9 @@
+module Spree
+  class ReimbursementType < Spree::Base
+    include Spree::NamedType
+
+    ORIGINAL = 'original'
+
+    has_many :return_items
+  end
+end

solidusio/solidus

Conflicts:
	backend/app/views/spree/admin/customer_returns/_return_item_decision.html.erb
	backend/app/views/spree/admin/customer_returns/edit.html.erb
	core/app/models/spree/refund_reason.rb
	core/app/models/spree/return_authorization_reason.rb
	core/spec/models/spree/reimbursement_spec.rb
2-2-dev:
- Changed "class ReimbursementType < Spree::Base" to " < ActiveRecord::Base"
cherry-picked from 834a24a695ed23462983696250effddbbcd79f74 in spree master

core/app/models/spree/reimbursement_type.rb
@@ -1,0 +1,9 @@
+module Spree
+  class ReimbursementType < ActiveRecord::Base
+    include Spree::NamedType
+
+    ORIGINAL = 'original'
+
+    has_many :return_items
+  end
+end