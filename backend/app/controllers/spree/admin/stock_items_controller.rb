986557abfbf8e2beed1336e036153d096cd37ebd
Andrew Thal <athal7@me.com>
2015-04-03 11:30:34 -0400

    Make admin/stock_items controller use resource controller to take advantage of authorization handling.
    Conflicts:
    	backend/app/controllers/spree/admin/stock_items_controller.rb

--- backend/app/controllers/spree/admin/stock_items_controller.rb
+++ backend/app/controllers/spree/admin/stock_items_controller.rb
@@ -1,54 +1,37 @@
 module Spree
   module Admin
-    class StockItemsController < Spree::Admin::BaseController
-      before_action :determine_backorderable, only: :update
-
-      def update
-        stock_item.save
-        respond_to do |format|
-          format.js { head :ok }
-        end
-      end
-
-      def create
-        variant = Variant.find(params[:variant_id])
-        stock_location = StockLocation.find(params[:stock_location_id])
-        stock_movement = stock_location.stock_movements.build(stock_movement_params.merge(originator: try_spree_current_user))
-        stock_movement.stock_item = stock_location.set_up_stock_item(variant)
-
-        if stock_movement.save
-          flash[:success] = flash_message_for(stock_movement, :successfully_created)
-        else
-          flash[:error] = stock_movement_and_item_errors(stock_movement)
-        end
-
-        redirect_to :back
-      end
-
-      def destroy
-        stock_item.destroy
-
-        respond_with(@stock_item) do |format|
-          format.html { redirect_to :back }
-          format.js
-        end
-      end
+    class StockItemsController < ResourceController
+      update.before :determine_backorderable
 
       private
-        def stock_movement_and_item_errors(stock_movement)
-          (stock_movement.errors.full_messages + stock_movement.stock_item.errors.full_messages).join(', ')
+
+        def location_after_destroy
+          :back
+        end
+
+        def location_after_save
+          :back
+        end
+
+        def build_resource
+          variant = Variant.accessible_by(current_ability, :read).find(params[:variant_id])
+          stock_location = StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+          stock_location.stock_movements.build(stock_movement_params).tap do |stock_movement|
+            stock_movement.originator = try_spree_current_user
+            stock_movement.stock_item = stock_location.set_up_stock_item(variant)
+          end
+        end
+
+        def permitted_resource_params
+          {}
         end
 
         def stock_movement_params
           params.require(:stock_movement).permit(permitted_stock_movement_attributes)
         end
 
-        def stock_item
-          @stock_item ||= StockItem.find(params[:id])
-        end
-
         def determine_backorderable
-          stock_item.backorderable = params[:stock_item].present? && params[:stock_item][:backorderable].present?
+          @stock_item.backorderable = params[:stock_item].present? && params[:stock_item][:backorderable].present?
         end
     end
   end
--- backend/spec/controllers/spree/admin/stock_items_controller_spec.rb
+++ backend/spec/controllers/spree/admin/stock_items_controller_spec.rb
@@ -5,16 +5,6 @@ module Spree
     describe StockItemsController, :type => :controller do
       stub_authorization!
 
-      context "formats" do
-        let!(:stock_item) { create(:variant).stock_items.first }
-
-        it "destroy stock item via js" do
-          expect {
-            spree_delete :destroy, format: :js, id: stock_item
-          }.to change{ StockItem.count }.by(-1)
-        end
-      end
-
       context "create" do
         let!(:variant) { create(:variant) }
         let!(:stock_location) { variant.stock_locations.first }
