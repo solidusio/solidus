a581e758fd1312e761c0e01563766b5dd60a92b1
Andrew Thal <athal7@me.com>
2015-04-03 09:38:27 -0400

    Make admin/stock_movements_controller be a resource controller.
    It previously inherited from Admin::BaseController, which, in addition
    to requiring unnecessary code, did not make use of cancan checks that
    are helpful from ResourceController.

--- backend/app/controllers/spree/admin/stock_movements_controller.rb
+++ backend/app/controllers/spree/admin/stock_movements_controller.rb
@@ -1,38 +1,20 @@
 module Spree
   module Admin
-    class StockMovementsController < Spree::Admin::BaseController
-      respond_to :html
-      helper_method :stock_location
-
-      def index
-        @stock_movements = stock_location.stock_movements.recent.
-          includes(:stock_item => { :variant => :product }).
-          page(params[:page])
-      end
-
-      def new
-        @stock_movement = stock_location.stock_movements.build
-      end
-
-      def create
-        @stock_movement = stock_location.stock_movements.build(stock_movement_params)
-        @stock_movement.save
-        flash[:success] = flash_message_for(@stock_movement, :successfully_created)
-        redirect_to admin_stock_location_stock_movements_path(stock_location)
-      end
-
-      def edit
-        @stock_movement = StockMovement.find(params[:id])
-      end
+    class StockMovementsController < ResourceController
+      belongs_to 'spree/stock_location'
+      before_action :parent
 
       private
 
-      def stock_location
-        @stock_location ||= StockLocation.find(params[:stock_location_id])
+      def permitted_resource_params
+        params.require(:stock_movement).permit(:quantity, :stock_item_id, :action)
       end
 
-      def stock_movement_params
-        params.require(:stock_movement).permit(:quantity, :stock_item_id, :action)
+      def collection
+        super.
+          recent.
+          includes(:stock_item => { :variant => :product }).
+          page(params[:page])
       end
     end
   end
