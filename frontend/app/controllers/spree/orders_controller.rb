spree/spree

Remove OrderPopulator class
Help consolidate order update logic by removing an extraneous service object.

Nearly all the logic in OrderPopulator has been moved to OrderContents over time
and there isn't much left that OrderPopulator does except to introduce extra
indirection for the one frontend controller endpoint that uses it.

The "options" hash has also now been added to OrderContents which helps provide
some of the customization that OrderPopulator could theoretically have been used
for.

frontend/app/controllers/spree/orders_controller.rb
@@ -39,14 +39,29 @@ module Spree
 
     # Adds a new item to the order (creating a new order if none already exists)
     def populate
-      populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
-      if populator.populate(params[:variant_id], params[:quantity], params[:options])
-        respond_with(@order) do |format|
-          format.html { redirect_to cart_path }
+      order    = current_order(create_order_if_necessary: true)
+      variant  = Spree::Variant.find(params[:variant_id])
+      quantity = params[:quantity].to_i
+      options  = (params[:options] || {}).merge(currency: current_currency)
+
+      # 2,147,483,647 is crazy. See issue #2695.
+      if quantity.between?(1, 2_147_483_647)
+        begin
+          order.contents.add(variant, quantity, options)
+        rescue ActiveRecord::RecordInvalid => e
+          error = e.record.errors.full_messages.join(", ")
         end
       else
-        flash[:error] = populator.errors.full_messages.join(" ")
+        error = Spree.t(:please_enter_reasonable_quantity)
+      end
+
+      if error
+        flash[:error] = error
         redirect_back_or_default(spree.root_path)
+      else
+        respond_with(order) do |format|
+          format.html { redirect_to cart_path }
+        end
       end
     end
 

solidusio/solidus

Fixes #5941
(cherry picked from commit c2870cfa2fa22e91e8529edb0136c21cfa1b4e2a in spree/spree/master)
Conflicts:
	core/app/models/spree/order_populator.rb
	core/config/locales/en.yml
	core/spec/models/spree/order_populator_spec.rb
	frontend/app/controllers/spree/orders_controller.rb
	frontend/spec/controllers/spree/orders_controller_spec.rb
	guides/content/developer/core/orders.md

frontend/app/controllers/spree/orders_controller.rb
@@ -41,14 +41,28 @@ module Spree
 
     # Adds a new item to the order (creating a new order if none already exists)
     def populate
-      populator = Spree::OrderPopulator.new(current_order(create_order_if_necessary: true), current_currency)
-      if populator.populate(params[:variant_id], params[:quantity], params[:options])
-        respond_with(@order) do |format|
-          format.html { redirect_to cart_path }
+      order    = current_order(create_order_if_necessary: true)
+      variant  = Spree::Variant.find(params[:variant_id])
+      quantity = params[:quantity].to_i
+
+      # 2,147,483,647 is crazy. See issue #2695.
+      if quantity.between?(1, 2_147_483_647)
+        begin
+          order.contents.add(variant, quantity, current_currency)
+        rescue ActiveRecord::RecordInvalid => e
+          error = e.record.errors.full_messages.join(", ")
         end
       else
-        flash[:error] = populator.errors.full_messages.join(" ")
+        error = Spree.t(:please_enter_reasonable_quantity)
+      end
+
+      if error
+        flash[:error] = error
         redirect_back_or_default(spree.root_path)
+      else
+        respond_with(order) do |format|
+          format.html { redirect_to cart_path }
+        end
       end
     end
 