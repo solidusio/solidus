f8ede4a7d5859d73862baeb46f9ff98ff074e00c
John Hawthorn <john@freerunningtechnologies.com>
2015-04-29 14:00:26 -0700

    site_name preference is now Store.site_name

--- core/app/mailers/spree/carton_mailer.rb
+++ core/app/mailers/spree/carton_mailer.rb
@@ -5,7 +5,7 @@ module Spree
     def shipped_email(carton_id, resend: false)
       @carton = Spree::Carton.find(carton_id)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Config[:site_name]} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@carton.order_numbers.join(', ')}"
+      subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@carton.order_numbers.join(', ')}"
       mail(to: @carton.order_emails, from: from_address, subject: subject)
     end
   end
