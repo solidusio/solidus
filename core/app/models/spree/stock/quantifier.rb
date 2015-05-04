5977f760704fd24cb5865bfe2a4aac32a95449f6
Jared Norman <jared@freerunningtechnologies.com>
2015-05-04 16:52:31 -0700

    Document Spree::Stock::Quantifier

--- core/app/models/spree/stock/quantifier.rb
+++ core/app/models/spree/stock/quantifier.rb
@@ -8,6 +8,10 @@ module Spree
         @stock_items = Spree::StockItem.joins(:stock_location).where(:variant_id => @variant, Spree::StockLocation.table_name =>{ :active => true})
       end
 
+      # Returns the total number of inventory units on hand for the variant.
+      #
+      # @return [Fixnum] number of inventory units on hand, or infinity if
+      #   inventory is not tracked on the variant.
       def total_on_hand
         if @variant.should_track_inventory?
           stock_items.sum(:count_on_hand)
@@ -16,10 +20,18 @@ module Spree
         end
       end
 
+      # Checks if any of its stock items are backorderable.
+      #
+      # @return [Boolean] true if any stock items are backorderable
       def backorderable?
         stock_items.any?(&:backorderable)
       end
 
+      # Checks if it is possible to supply a given number of units.
+      #
+      # @param required [Fixnum] the number of required stock units
+      # @return [Boolean] true if we have the required amount on hand or the
+      #   variant is backorderable, otherwise false
       def can_supply?(required)
         total_on_hand >= required || backorderable?
       end
