aedd1e401a07a82c202fa6dbfab762cc7da9252c
John Hawthorn <john@freerunningtechnologies.com>
2015-05-04 05:17:22 -0700

    Another promotion code spec fix

--- core/spec/models/spree/promotion_handler/coupon_spec.rb
+++ core/spec/models/spree/promotion_handler/coupon_spec.rb
@@ -271,7 +271,8 @@ module Spree
             context "when the a new coupon is less good" do
               let!(:action_5) { Promotion::Actions::CreateAdjustment.create(promotion: promotion_5, calculator: calculator_5) }
               let(:calculator_5) { Calculator::FlatRate.new(preferred_amount: 5) }
-              let!(:promotion_5) { Promotion.create name: "promo", :code => "5off"  }
+              let!(:promotion_5) { Promotion.create name: "promo" }
+              let!(:promotion_code_5) { promotion_5.codes.create! value: "5off"  }
 
               it 'notifies of better deal' do
                 subject.apply
