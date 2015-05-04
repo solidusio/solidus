7bcfc5148485e927b5e067d3027631dcc4b9f1f4
John Hawthorn <john@freerunningtechnologies.com>
2015-05-03 23:37:30 -0700

    Restore line_item_options functionality

--- api/app/controllers/spree/api/line_items_controller.rb
+++ api/app/controllers/spree/api/line_items_controller.rb
@@ -13,7 +13,9 @@ module Spree
         @line_item = @order.contents.add(
           variant,
           params[:line_item][:quantity] || 1,
-          stock_location_quantities: params[:line_item][:stock_location_quantities]
+          {
+            stock_location_quantities: params[:line_item][:stock_location_quantities]
+          }.merge(line_item_params[:options] || {})
         )
 
         if @line_item.errors.empty?
