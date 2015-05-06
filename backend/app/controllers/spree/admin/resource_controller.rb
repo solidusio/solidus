b94b0e9ec2b23f34f8d6f68655befc1dcf3501aa
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2015-05-06 14:46:56 -0700

    Only set the flash if we're responding to html.

--- backend/app/controllers/spree/admin/resource_controller.rb
+++ backend/app/controllers/spree/admin/resource_controller.rb
@@ -30,10 +30,12 @@ class Spree::Admin::ResourceController < Spree::Admin::BaseController
     invoke_callbacks(:update, :before)
     if @object.update_attributes(permitted_resource_params)
       invoke_callbacks(:update, :after)
-      flash[:success] = flash_message_for(@object, :successfully_updated)
       respond_with(@object) do |format|
-        format.html { redirect_to location_after_save }
-        format.js   { render :layout => false }
+        format.html do
+          flash[:success] = flash_message_for(@object, :successfully_updated)
+          redirect_to location_after_save
+        end
+        format.js { render :layout => false }
       end
     else
       invoke_callbacks(:update, :fails)
