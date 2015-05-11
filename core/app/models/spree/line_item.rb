6717b5d0c2f5e633e791fb4abd7b8ab265c0daf0
Magnus von Koeller <hello@magnusvk.com>
2015-05-11 18:00:02 -0400

    Remove duplicate association
    It looks as if this was a merge conflict.

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -13,10 +13,8 @@ module Spree
     has_many :adjustments, as: :adjustable, inverse_of: :adjustable, dependent: :destroy
     has_many :inventory_units, inverse_of: :line_item
 
-    has_many :line_item_actions
-    has_many :actions, through: :line_item_actions
-
     has_many :line_item_actions, dependent: :destroy
+    has_many :actions, through: :line_item_actions
 
     before_validation :copy_price
     before_validation :copy_tax_category
