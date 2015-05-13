e77515086bd764107d7d9ae2ddf5f2a59a7ce082
Phillip Birtcher <pbirtcher@bonobos.com>
2015-05-13 18:15:40 -0400

    OrderCapturing for processing payments for an order in a sequence
    Conflicts:
    	core/app/models/spree/payment.rb

---
+++ core/app/models/spree/order_capturing.rb
@@ -0,0 +1,35 @@
+class Spree::OrderCapturing
+  # Allows for prioritizing payment methods in the order to be captured
+  class_attribute :sorted_payment_method_classes
+  self.sorted_payment_method_classes = []
+
+  def initialize(order, sorted_payment_method_classes = nil)
+    @order = order
+    @sorted_payment_method_classes = sorted_payment_method_classes || Spree::OrderCapturing.sorted_payment_method_classes
+  end
+
+  def capture_payments
+    return if @order.paid?
+
+    Spree::OrderMutex.with_lock!(@order) do
+      uncaptured_amount = @order.display_total.cents
+
+      begin
+        sorted_payments(@order).each do |payment|
+          amount = [uncaptured_amount, payment.money.cents].min
+          payment.capture!(amount)
+          uncaptured_amount -= amount
+        end
+      ensure
+        @order.update!
+      end
+    end
+  end
+
+  private
+
+  def sorted_payments(order)
+    payments = order.pending_payments
+    payments = payments.sort_by { |p| [@sorted_payment_method_classes.index(p.payment_method.class), p.id] }
+  end
+end
---
+++ core/spec/models/spree/order_capturing_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+
+describe Spree::OrderCapturing do
+  describe '#capture_payments' do
+    subject { Spree::OrderCapturing.new(order, payment_methods).capture_payments }
+
+    context "payment methods specified" do
+      let!(:order) { create(:order, ship_address: create(:address)) }
+
+      let!(:product) { create(:product, price: 10.00) }
+      let!(:variant) do
+        create(:variant, price: 10, product: product, track_inventory: false, tax_category: tax_rate.tax_category)
+      end
+      let!(:shipping_method) { create(:free_shipping_method) }
+      let(:tax_rate) { create(:tax_rate, amount: 0.1, zone: create(:global_zone, name: "Some Tax Zone")) }
+      let(:secondary_total) { 10.0 }
+      let(:bogus_total) { order.total }
+
+      before do
+        order.contents.add(variant, 3)
+        order.update!
+        @secondary_bogus_payment = create(:payment, order: order, amount: secondary_total, payment_method: secondary_payment_method.create!(name: 'So bogus', environment: 'test'))
+        @bogus_payment = create(:payment, order: order, amount: bogus_total)
+        order.contents.advance
+        order.complete!
+        order.reload
+      end
+
+      context "payment method ordering" do
+        let(:secondary_payment_method) { SecondaryBogusPaymentMethod }
+
+        class SecondaryBogusPaymentMethod < Spree::Gateway::Bogus; end
+
+        context "SecondaryBogusPaymentMethod payments are prioritized" do
+          let(:payment_methods) { [SecondaryBogusPaymentMethod, Spree::Gateway::Bogus] }
+
+          it "captures SecondaryBogusPaymentMethod payments first" do
+            subject
+            expect(@secondary_bogus_payment.reload.capture_events.sum(:amount)).to eq(10.0)
+            expect(@bogus_payment.reload.capture_events.sum(:amount)).to eq(order.total - 10.0)
+          end
+        end
+
+        context "Bogus payments are prioritized" do
+          let(:payment_methods) { [Spree::Gateway::Bogus, SecondaryBogusPaymentMethod] }
+
+          it "captures Bogus payments first" do
+            subject
+            expect(@secondary_bogus_payment.reload.capture_events.sum(:amount)).to eq(0.0)
+            expect(@bogus_payment.reload.capture_events.sum(:amount)).to eq(order.total)
+          end
+        end
+
+        context "when the payment method ordering is configured" do
+          subject { Spree::OrderCapturing.new(order, payment_methods).capture_payments }
+
+          let(:payment_methods) { nil }
+
+          before do
+            allow(Spree::OrderCapturing).to receive(:sorted_payment_method_classes).and_return(
+              [SecondaryBogusPaymentMethod, Spree::Gateway::Bogus]
+            )
+          end
+
+          it "captures in the order specified" do
+            subject
+            expect(@secondary_bogus_payment.reload.capture_events.sum(:amount)).to eq(10.0)
+            expect(@bogus_payment.reload.capture_events.sum(:amount)).to eq(order.total - 10.0)
+          end
+        end
+      end
+
+      context "when there is an error processing a payment" do
+        let(:secondary_payment_method) { ExceptionallyBogusPaymentMethod }
+        let(:bogus_total) { order.total - 1 }
+        let(:secondary_total) { 1 }
+        let(:payment_methods) { [Spree::Gateway::Bogus, ExceptionallyBogusPaymentMethod] }
+
+        class ExceptionallyBogusPaymentMethod < Spree::Gateway::Bogus
+          def capture(*args)
+            raise ActiveMerchant::ConnectionError
+          end
+        end
+
+        it "raises an error and leaves the order in a reasonable state" do
+          expect { subject }.to raise_error(Spree::Core::GatewayError)
+          expect(order.payments.to_a.sum(&:uncaptured_amount)).to eq 1.0
+        end
+      end
+    end
+  end
+end
