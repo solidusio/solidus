4957111ce789953e5fcb8000465e882a06b1a04c
Phillip Birtcher <pbirtcher@bonobos.com>
2015-05-14 12:00:02 -0400

    Use OrderCapturing for exchanges rake

--- core/lib/spree/core/unreturned_item_charger.rb
+++ core/lib/spree/core/unreturned_item_charger.rb
@@ -34,7 +34,7 @@ module Spree
 
       new_order.contents.approve(name: self.class.name)
       new_order.reload.complete!
-      new_order.payments.map(&:capture!) if (Spree::Config[:auto_capture_exchanges] && !Spree::Config[:auto_capture])
+      Spree::OrderCapturing.new(new_order).capture_payments if (Spree::Config[:auto_capture_exchanges] && !Spree::Config[:auto_capture])
 
       @return_items.each(&:expired!)
       create_new_rma if Spree::Config[:create_rma_for_unreturned_exchange]
