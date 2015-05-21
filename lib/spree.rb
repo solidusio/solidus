48d6dcf10906adc1895bd32819fe758f0c8488b1
John Hawthorn <john@freerunningtechnologies.com>
2015-05-21 11:05:13 -0700

    Add lib/solidus.rb

--- lib/spree.rb
+++ lib/spree.rb
@@ -1,15 +1,1 @@
-require 'spree_core'
-require 'spree_api'
-require 'spree_backend'
-require 'spree_frontend'
-require 'spree_sample'
-
-begin
-  require 'protected_attributes'
-  puts "*" * 75
-  puts "[FATAL] Spree does not work with the protected_attributes gem installed!"
-  puts "You MUST remove this gem from your Gemfile. It is incompatible with Spree."
-  puts "*" * 75
-  exit
-rescue LoadError
-end
+require 'solidus'
