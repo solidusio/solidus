08d509c84824d0215a80e9fdf0d70f5b66407577
Phillip Birtcher <pbirtcher@bonobos.com>
2015-05-28 13:32:27 -0400

    Refactor order mailer for code reuse
    Conflicts:
    	core/app/mailers/spree/order_mailer.rb

--- core/app/mailers/spree/order_mailer.rb
+++ core/app/mailers/spree/order_mailer.rb
@@ -1,26 +1,35 @@
 module Spree
   class OrderMailer < BaseMailer
     def confirm_email(order, resend = false)
-      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+      @order = find_order(order)
+      subject = build_subject(Spree.t('order_mailer.confirm_email.subject'), resend)
+
       mail(to: @order.email, from: from_address, subject: subject)
     end
 
     def cancel_email(order, resend = false)
-      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+      @order = find_order(order)
+      subject = build_subject(Spree.t('order_mailer.cancel_email.subject'), resend)
+
       mail(to: @order.email, from: from_address, subject: subject)
     end
 
     def inventory_cancellation_email(order, inventory_units, resend = false)
-      @order = order
-      @inventory_units = inventory_units
+      @order, @inventory_units = find_order(order), inventory_units
+      subject = build_subject(Spree.t('order_mailer.inventory_cancellation.subject'), resend)
 
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{Spree::Config[:site_name]} #{Spree.t('order_mailer.inventory_cancellation.subject')} ##{@order.number}"
       mail(to: @order.email, from: from_address, subject: subject)
+    end
+
+    private
+
+    def find_order(order)
+      @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    end
+
+    def build_subject(subject_text, resend)
+      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+      subject += "#{Spree::Config[:site_name]} #{subject_text} ##{@order.number}"
     end
   end
 end
