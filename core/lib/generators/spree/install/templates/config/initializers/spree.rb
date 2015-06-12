fdeb22f2f74f6c41188928ea25c6871c714a6012
John Hawthorn <john@freerunningtechnologies.com>
2015-06-12 13:31:57 -0700

    Update template config/initializers/spree.rb
    Changes the default to use_static_preferences! for any new stores.
    
    Removes the old leading comment explaining the gotchas of changing
    preferences in the admin. As there are no longer any preferences
    changeable from the admin.
    
    Adds a small selection of useful preferences, which new sites are most
    likely to use.

--- core/lib/generators/spree/install/templates/config/initializers/spree.rb
+++ core/lib/generators/spree/install/templates/config/initializers/spree.rb
@@ -1,14 +1,40 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
+# Configure Solidus Preferences
+
 Spree.config do |config|
-  # Example:
+  # Without this preferences are loaded and persisted to the database. This
+  # changes them to be stored in memory.
+  # This will be the default in a future version.
+  config.use_static_preferences!
+
+  # Core:
+
+  # Default currency for new sites
+  config.currency = "USD"
+
+  # from address for transactional emails
+  config.mails_from = "store@example.com"
+
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+
+  # When true, product caches are only invalidated when they come in or out of
+  # stock. Default is to invalidate cache on any inventory changes.
+  # config.binary_inventory_cache = true
+
+
+  # Frontend:
+
+  # Custom logo for the frontend
+  # config.logo = "logo/spree_50.png"
+
+  # Template to use when rendering layout
+  # config.layout = "spree/layouts/spree_application"
+
+
+  # Admin:
+
+  # Custom logo for the admin
+  # config.admin_interface_logo = "logo/spree_50.png"
 end
 
 Spree.user_class = <%= (options[:user_class].blank? ? "Spree::LegacyUser" : options[:user_class]).inspect %>
