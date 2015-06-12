62181c6e30bd580919747d55cde6df239acd1b44
John Hawthorn <john.hawthorn@gmail.com>
2015-06-12 00:16:36 -0700

    More Preferences::Configuration docs

--- core/app/models/spree/preferences/configuration.rb
+++ core/app/models/spree/preferences/configuration.rb
@@ -25,21 +25,34 @@ module Spree::Preferences
   class Configuration
     include Spree::Preferences::Preferable
 
+    # @yield [config] Yields this configuration object to a block
     def configure
-      yield(self) if block_given?
+      yield(self)
     end
 
+    # @!attribute preference_store
+    # Storage method for preferences. Default is {ScopedStore}
     attr_writer :preference_store
     def preference_store
       @preference_store ||= ScopedStore.new(self.class.name.underscore)
     end
 
+    # Replace the default legacy preference store, which stores preferences in
+    # the spree_preferences table, with a plain in memory hash. This is faster
+    # and less error prone.
+    #
+    # This will set all preferences to their default values.
+    #
+    # These won't be loaded from or persisted to the database, so any desired
+    # changes must be made each time the application is started, such as in an
+    # initializer.
     def use_static_preferences!
       @preference_store = default_preferences
     end
 
     alias_method :preferences, :preference_store
 
+    # Reset all preferences to their default values.
     def reset
       set(default_preferences)
     end
@@ -49,8 +62,9 @@ module Spree::Preferences
 
     alias :get :get_preference
 
-    def set(options)
-      options.each do |name, value|
+    # @param preferences [Hash] a hash of preferences to set
+    def set(preferences)
+      preferences.each do |name, value|
         set_preference name, value
       end
     end
