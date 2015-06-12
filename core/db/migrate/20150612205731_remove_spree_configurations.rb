dfe4b5e45c2c8ddf1077840ad3dcb5ee60248c88
John Hawthorn <john@freerunningtechnologies.com>
2015-06-12 13:55:48 -0700

    Remove Spree::Configuration model and table
    NB: This is not Spree::Preferences::Configuartion
    NB: This is not Spree::Preference
    
    This is a model which has never been used for anything. I believe it is
    still around only due to confusion with the rest of the preference
    system.

---
+++ core/db/migrate/20150612205731_remove_spree_configurations.rb
@@ -0,0 +1,15 @@
+class RemoveSpreeConfigurations < ActiveRecord::Migration
+  def up
+    drop_table :spree_configurations
+  end
+
+  def down
+    create_table :spree_configurations do |t|
+      t.string     :name
+      t.string     :type, :limit => 50
+      t.timestamps null: true
+    end
+
+    add_index :spree_configurations, [:name, :type], :name => 'index_spree_configurations_on_name_and_type'
+  end
+end
