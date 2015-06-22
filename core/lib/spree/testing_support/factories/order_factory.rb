95a0e8756bac65d9fe42f6461063e29a75924cd0
John Hawthorn <john.hawthorn@gmail.com>
2015-06-22 01:06:54 -0700

    Use Time.current for completed_at in factory
    This fixes a test that fails in the evening when Time.now.to_date
    doesn't match Time.current.to_date.

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -47,7 +47,7 @@ FactoryGirl.define do
 
         after(:create) do |order|
           order.refresh_shipment_rates
-          order.update_column(:completed_at, Time.now)
+          order.update_column(:completed_at, Time.current)
         end
 
         factory :completed_order_with_pending_payment do
