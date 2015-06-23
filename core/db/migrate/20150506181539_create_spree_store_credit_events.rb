a99a8e606464ab189c09f0b22607b18adfdddb0f
John Hawthorn <john.hawthorn@gmail.com>
2015-06-23 13:14:22 -0700

    Fix duplicated key warning

--- core/db/migrate/20150506181539_create_spree_store_credit_events.rb
+++ core/db/migrate/20150506181539_create_spree_store_credit_events.rb
@@ -4,7 +4,7 @@ class CreateSpreeStoreCreditEvents < ActiveRecord::Migration
       t.integer :store_credit_id,    null: false
       t.string  :action,             null: false
       t.decimal :amount,             precision: 8,  scale: 2
-      t.decimal :user_total_amount,  precision: 8,  scale: 2, default: 0.0, null: false, null: false
+      t.decimal :user_total_amount,  precision: 8,  scale: 2, default: 0.0, null: false
       t.string  :authorization_code, null: false
       t.datetime :deleted_at
       t.references :originator, polymorphic: true
