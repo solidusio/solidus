e0bbb886ab9dc6c82f966c6f4a2efdab08eaea71
John Hawthorn <john@freerunningtechnologies.com>
2015-06-24 21:19:18 -0700

    Fix issues with stock_transfer factory
    A stock location was being created at load time, outside of running
    specs. Times were also being read at load time, instead of in a spec.
    
    With this fix stock_transfers_controller can run with random ordering.

--- core/lib/spree/testing_support/factories/stock_transfer_factory.rb
+++ core/lib/spree/testing_support/factories/stock_transfer_factory.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :stock_transfer, class: Spree::StockTransfer do
-    source_location Spree::StockLocation.new(name: "Source Location", code: "SRC", admin_name: "Source")
+    source_location { Spree::StockLocation.create!(name: "Source Location", code: "SRC", admin_name: "Source") }
 
     factory :stock_transfer_with_items do
+      destination_location { Spree::StockLocation.create!(name: "Destination Location", code: "DEST", admin_name: "Destination") }
+
       after(:create) do |stock_transfer, evaluator|
         variant_1 = create(:variant)
         variant_2 = create(:variant)
-
-        stock_transfer.destination_location = Spree::StockLocation.new(name: "Destination Location", code: "DEST", admin_name: "Destination")
 
         variant_1.stock_items.find_by(stock_location: stock_transfer.source_location).set_count_on_hand(10)
         variant_2.stock_items.find_by(stock_location: stock_transfer.source_location).set_count_on_hand(10)
@@ -20,9 +20,8 @@ FactoryGirl.define do
       end
 
       factory :receivable_stock_transfer_with_items do
-        destination_location Spree::StockLocation.new(name: "Destination Location", code: "DEST", admin_name: "Destination")
-        finalized_at   Time.now
-        shipped_at     Time.now
+        finalized_at  { Time.now }
+        shipped_at    { Time.now }
       end
     end
   end
