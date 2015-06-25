a1479f49e10d32543367b4c06918756b0a9ffc7a
Richard Nuno <me@richardnuno.com>
2015-06-25 09:40:34 -0400

    Add newlines to the rendered response

--- api/app/controllers/spree/api/base_controller.rb
+++ api/app/controllers/spree/api/base_controller.rb
@@ -185,7 +185,13 @@ module Spree
       end
 
       def insufficient_stock_error(exception)
-        render json: { errors: [I18n.t(:quantity_is_not_available, :scope => "spree.api.order")], type: 'insufficient_stock' }, status: 422
+        render(
+          json: {
+            errors: [I18n.t(:quantity_is_not_available, :scope => "spree.api.order")],
+            type: 'insufficient_stock',
+          },
+          status: 422,
+        )
       end
 
     end
