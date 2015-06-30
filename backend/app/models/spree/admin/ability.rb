54ee7c9f54c4700a7729b76570832ca6ba96afc6
Richard Wilson <richard@freerunningtech.com>
2015-06-30 16:14:02 -0700

    Create an order managment and order display role.
    Combined with the view changes preceding this commit, these roles are
    the minimum set of permissions required for a user to view or edit
    orders.

--- backend/app/models/spree/admin/ability.rb
+++ backend/app/models/spree/admin/ability.rb
@@ -17,6 +17,26 @@ module Spree
           can [:display, :admin], Spree::PromotionAction
           can [:display, :admin], Spree::PromotionCategory
         end
+
+        if user.has_spree_role? :order_management
+          can :manage, Spree::Order
+          can :manage, Spree::Payment
+          can :manage, Spree::Shipment
+          can :manage, Spree::Adjustment
+          can :manage, Spree::LineItem
+          can :manage, Spree::ReturnAuthorization
+          can :manage, Spree::CustomerReturn
+        end
+
+        if user.has_spree_role? :order_display
+          can [:display, :admin, :edit, :cart], Spree::Order
+          can [:display, :admin], Spree::Payment
+          can [:display, :admin], Spree::Shipment
+          can [:display, :admin], Spree::Adjustment
+          can [:display, :admin], Spree::LineItem
+          can [:display, :admin], Spree::ReturnAuthorization
+          can [:display, :admin], Spree::CustomerReturn
+        end
       end
     end
   end
--- backend/spec/models/spree/admin/ability_spec.rb
+++ backend/spec/models/spree/admin/ability_spec.rb
@@ -66,5 +66,79 @@ describe Spree::Admin::Ability do
         it { should_not be_able_to(:admin, Spree::PromotionCategory) }
       end
     end
+
+    context "managing orders" do
+      let(:role) { :order_management }
+
+      context "when the user has the order_management role" do
+        let(:has_role) { true }
+
+        it { should be_able_to(:manage, Spree::Order) }
+        it { should be_able_to(:manage, Spree::Payment) }
+        it { should be_able_to(:manage, Spree::Shipment) }
+        it { should be_able_to(:manage, Spree::Adjustment) }
+        it { should be_able_to(:manage, Spree::LineItem) }
+        it { should be_able_to(:manage, Spree::ReturnAuthorization) }
+        it { should be_able_to(:manage, Spree::CustomerReturn) }
+      end
+
+      context "when the user does not have the order_management role" do
+        let(:has_role) { false }
+
+        it { should_not be_able_to(:manage, Spree::Order) }
+        it { should_not be_able_to(:manage, Spree::Payment) }
+        it { should_not be_able_to(:manage, Spree::Shipment) }
+        it { should_not be_able_to(:manage, Spree::Adjustment) }
+        it { should_not be_able_to(:manage, Spree::LineItem) }
+        it { should_not be_able_to(:manage, Spree::ReturnAuthorization) }
+        it { should_not be_able_to(:manage, Spree::CustomerReturn) }
+      end
+    end
+
+    context "displaying orders" do
+      let(:role) { :order_display }
+
+      context "when the user has the order_display role" do
+        let(:has_role) { true }
+
+        it { should be_able_to(:display, Spree::Order) }
+        it { should be_able_to(:display, Spree::Payment) }
+        it { should be_able_to(:display, Spree::Shipment) }
+        it { should be_able_to(:display, Spree::Adjustment) }
+        it { should be_able_to(:display, Spree::LineItem) }
+        it { should be_able_to(:display, Spree::ReturnAuthorization) }
+        it { should be_able_to(:display, Spree::CustomerReturn) }
+        it { should be_able_to(:admin, Spree::Order) }
+        it { should be_able_to(:admin, Spree::Payment) }
+        it { should be_able_to(:admin, Spree::Shipment) }
+        it { should be_able_to(:admin, Spree::Adjustment) }
+        it { should be_able_to(:admin, Spree::LineItem) }
+        it { should be_able_to(:admin, Spree::ReturnAuthorization) }
+        it { should be_able_to(:admin, Spree::CustomerReturn) }
+        it { should be_able_to(:edit, Spree::Order) }
+        it { should be_able_to(:cart, Spree::Order) }
+      end
+
+      context "when the user does not have the order_display role" do
+        let(:has_role) { false }
+
+        it { should_not be_able_to(:display, Spree::Order) }
+        it { should_not be_able_to(:display, Spree::Payment) }
+        it { should_not be_able_to(:display, Spree::Shipment) }
+        it { should_not be_able_to(:display, Spree::Adjustment) }
+        it { should_not be_able_to(:display, Spree::LineItem) }
+        it { should_not be_able_to(:display, Spree::ReturnAuthorization) }
+        it { should_not be_able_to(:display, Spree::CustomerReturn) }
+        it { should_not be_able_to(:admin, Spree::Order) }
+        it { should_not be_able_to(:admin, Spree::Payment) }
+        it { should_not be_able_to(:admin, Spree::Shipment) }
+        it { should_not be_able_to(:admin, Spree::Adjustment) }
+        it { should_not be_able_to(:admin, Spree::LineItem) }
+        it { should_not be_able_to(:admin, Spree::ReturnAuthorization) }
+        it { should_not be_able_to(:admin, Spree::CustomerReturn) }
+        it { should_not be_able_to(:edit, Spree::Order) }
+        it { should_not be_able_to(:cart, Spree::Order) }
+      end
+    end
   end
 end
