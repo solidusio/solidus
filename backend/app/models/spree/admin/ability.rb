1005c7b504e9b6d4ddb43ca5a15b0c9159cf9c4e
Richard Wilson <richard@freerunningtech.com>
2015-07-01 15:30:51 -0700

    Add roles for product management.
    Add sample roles for the minimal amount of permissions to manage
    products in their entirety. As well as for read only acccess.

--- backend/app/models/spree/admin/ability.rb
+++ backend/app/models/spree/admin/ability.rb
@@ -52,6 +52,33 @@ module Spree
           can [:display, :admin], Spree::StockItem
           can [:display, :admin], Spree::StockTransfer
         end
+
+        if user.has_spree_role? :product_management
+          can :manage, Spree::Product
+          can :manage, Spree::Image
+          can :manage, Spree::Variant
+          can :manage, Spree::OptionValue
+          can :manage, Spree::ProductProperty
+          can :manage, Spree::OptionType
+          can :manage, Spree::Property
+          can :manage, Spree::Prototype
+          can :manage, Spree::Taxonomy
+          can :manage, Spree::Taxon
+          can :manage, Spree::Classification
+        end
+
+        if user.has_spree_role? :product_display
+          can [:display, :admin, :edit], Spree::Product
+          can [:display, :admin], Spree::Image
+          can [:display, :admin], Spree::Variant
+          can [:display, :admin], Spree::OptionValue
+          can [:display, :admin], Spree::ProductProperty
+          can [:display, :admin], Spree::OptionType
+          can [:display, :admin], Spree::Property
+          can [:display, :admin], Spree::Prototype
+          can [:display, :admin], Spree::Taxonomy
+          can [:display, :admin], Spree::Taxon
+        end
       end
     end
   end
