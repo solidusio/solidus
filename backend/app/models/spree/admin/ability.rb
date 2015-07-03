f83d9dbc0586d460c7e5f04cf94ff017b37f40ed
Richard Wilson <richard@freerunningtech.com>
2015-07-02 17:10:10 -0700

    Add basic roles for displaying and managing configuration.
    Used for testing, and demonstrating the minimum amount of permissions to
    access all of the resources for application configuration.

--- backend/app/models/spree/admin/ability.rb
+++ backend/app/models/spree/admin/ability.rb
@@ -94,6 +94,44 @@ module Spree
           can [:display, :admin, :edit, :addresses, :orders, :items], Spree.user_class
           can [:display, :admin], Spree::StoreCredit
         end
+
+        if user.has_spree_role? :configuration_management
+          can :manage, :general_settings
+          can :manage, Spree::TaxCategory
+          can :manage, Spree::TaxRate
+          can :manage, Spree::Zone
+          can :manage, Spree::Country
+          can :manage, Spree::State
+          can :manage, Spree::PaymentMethod
+          can :manage, Spree::Taxonomy
+          can :manage, Spree::ShippingMethod
+          can :manage, Spree::ShippingCategory
+          can :manage, Spree::StockLocation
+          can :manage, Spree::StockMovement
+          can :manage, Spree::Tracker
+          can :manage, Spree::RefundReason
+          can :manage, Spree::ReimbursementType
+          can :manage, Spree::ReturnReason
+        end
+
+        if user.has_spree_role? :configuration_display
+          can [:edit, :admin], :general_settings
+          can [:display, :admin], Spree::TaxCategory
+          can [:display, :admin], Spree::TaxRate
+          can [:display, :admin], Spree::Zone
+          can [:display, :admin], Spree::Country
+          can [:display, :admin], Spree::State
+          can [:display, :admin], Spree::PaymentMethod
+          can [:display, :admin], Spree::Taxonomy
+          can [:display, :admin], Spree::ShippingMethod
+          can [:display, :admin], Spree::ShippingCategory
+          can [:display, :admin], Spree::StockLocation
+          can [:display, :admin], Spree::StockMovement
+          can [:display, :admin], Spree::Tracker
+          can [:display, :admin], Spree::RefundReason
+          can [:display, :admin], Spree::ReimbursementType
+          can [:display, :admin], Spree::ReturnReason
+        end
       end
     end
   end
