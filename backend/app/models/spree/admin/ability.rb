744dae276e90b7986ddd96c180e28e915d442f1f
Richard Wilson <richard@freerunningtech.com>
2015-07-08 15:58:31 -0700

    Move configuration display to a permission set

--- backend/app/models/spree/admin/ability.rb
+++ backend/app/models/spree/admin/ability.rb
@@ -8,25 +8,6 @@ module Spree
         if user.has_spree_role?(:dashboard_display)
           can [:admin, :home], :dashboards
         end
-
-        if user.has_spree_role? :configuration_display
-          can [:edit, :admin], :general_settings
-          can [:display, :admin], Spree::TaxCategory
-          can [:display, :admin], Spree::TaxRate
-          can [:display, :admin], Spree::Zone
-          can [:display, :admin], Spree::Country
-          can [:display, :admin], Spree::State
-          can [:display, :admin], Spree::PaymentMethod
-          can [:display, :admin], Spree::Taxonomy
-          can [:display, :admin], Spree::ShippingMethod
-          can [:display, :admin], Spree::ShippingCategory
-          can [:display, :admin], Spree::StockLocation
-          can [:display, :admin], Spree::StockMovement
-          can [:display, :admin], Spree::Tracker
-          can [:display, :admin], Spree::RefundReason
-          can [:display, :admin], Spree::ReimbursementType
-          can [:display, :admin], Spree::ReturnReason
-        end
       end
     end
   end
---
+++ core/app/models/spree/permission_sets/configuration_display.rb
@@ -0,0 +1,24 @@
+module Spree
+  module PermissionSets
+    class ConfigurationDisplay < PermissionSets::Base
+      def activate!
+          can [:edit, :admin], :general_settings
+          can [:display, :admin], Spree::TaxCategory
+          can [:display, :admin], Spree::TaxRate
+          can [:display, :admin], Spree::Zone
+          can [:display, :admin], Spree::Country
+          can [:display, :admin], Spree::State
+          can [:display, :admin], Spree::PaymentMethod
+          can [:display, :admin], Spree::Taxonomy
+          can [:display, :admin], Spree::ShippingMethod
+          can [:display, :admin], Spree::ShippingCategory
+          can [:display, :admin], Spree::StockLocation
+          can [:display, :admin], Spree::StockMovement
+          can [:display, :admin], Spree::Tracker
+          can [:display, :admin], Spree::RefundReason
+          can [:display, :admin], Spree::ReimbursementType
+          can [:display, :admin], Spree::ReturnReason
+      end
+    end
+  end
+end
