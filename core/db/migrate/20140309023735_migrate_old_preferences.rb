ebaba00302404a098cb01c78ecfee18d207808b1
Adam Mueller <adam@freerunningtech.com>
2015-07-23 13:29:38 -0700

    Optimize some migrations
    Moving lots of preferences can take a really long time, so I optimized
    that one. Also, adding tons of unnecessary indexes can take a long time.
    Both when you add them and later when using the table. So lets just not
    add random bad indexes.

--- core/db/migrate/20140309023735_migrate_old_preferences.rb
+++ core/db/migrate/20140309023735_migrate_old_preferences.rb
@@ -1,8 +1,10 @@
 class MigrateOldPreferences < ActiveRecord::Migration
+  disable_ddl_transaction!
+
   def up
     migrate_preferences(Spree::Calculator)
-    migrate_preferences(Spree::PaymentMethod)
     migrate_preferences(Spree::PromotionRule)
+    migrate_preferences(Spree::PaymentMethod)
   end
 
   def down
@@ -11,13 +13,39 @@ class MigrateOldPreferences < ActiveRecord::Migration
   private
   def migrate_preferences klass
     klass.reset_column_information
-    klass.find_each do |record|
-      store = Spree::Preferences::ScopedStore.new(record.class.name.underscore, record.id)
-      record.defined_preferences.each do |key|
-        value = store.fetch(key){}
-        record.preferences[key] = value unless value.nil?
+    klass.find_in_batches do |batch|
+      ActiveRecord::Base.transaction do
+        batch.each do |record|
+          keys = record.class.defined_preferences
+
+          # Batch load preferences for this record.
+          preferences = Hash[Spree::Preference.where(
+            key: keys.map{ |k| cache_key(record, k) }
+          ).pluck(:key, :value)]
+
+          # Copy preferences to the record.
+          keys.each do |key|
+            value = preferences[cache_key(record, key)]
+            record.preferences[key] = value unless value.nil?
+          end
+
+          # Persist the preferences.
+          record.update_column(:preferences, record.preferences)
+        end
       end
-      record.save!
     end
+  end
+
+  def cache_key model, key
+    [
+      ENV["RAILS_CACHE_ID"],
+      class_underscore_cache[model.type],
+      key,
+      model.id
+    ].compact.join("/")
+  end
+
+  def class_underscore_cache
+    @class_underscore_cache ||= Hash.new{ |h,k| h[k] = k.underscore }
   end
 end
