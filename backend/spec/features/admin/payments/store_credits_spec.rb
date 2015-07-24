9674282269609a2fecfc34df65c23b285e9e977c
Adam Mueller <adam@freerunningtech.com>
2015-07-24 13:36:27 -0700

    Add test for store credit source view

---
+++ backend/spec/features/admin/payments/store_credits_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe 'Store credits', type: :feature do
+  stub_authorization!
+
+  let(:order) { FactoryGirl.create(:completed_order_with_totals) }
+  let(:payment) do
+    FactoryGirl.create(
+      :store_credit_payment,
+      order: order,
+      amount: 20
+    )
+  end
+
+  it "viewing a store credit payment" do
+    visit spree.admin_order_payment_path(order, payment)
+
+    expect(page).to have_content "Store Credit"
+    expect(page).to have_content "Amount: $20.00"
+  end
+end
