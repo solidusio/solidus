110eba00afa670a517a6aa5a451b430377b0a7f3
Ben A. Morgan <ben@benmorgan.io>
2015-07-26 06:49:19 -0700

    setup HABTM between promotion rule and user

---
+++ core/app/models/spree/promotion_rule_user.rb
@@ -0,0 +1,8 @@
+module Spree
+  class PromotionRuleUser < Spree::Base
+    self.table_name = 'spree_promotion_rules_users'
+
+    belongs_to :promotion_rule, class_name: 'Spree::PromotionRule'
+    belongs_to :user, class_name: Spree::UserClassHandle.new
+  end
+end
