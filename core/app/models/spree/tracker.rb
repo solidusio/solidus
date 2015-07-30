a72f1857958f1eaef689c5bb381d4136b61380f0
Ben A. Morgan <ben@benmorgan.io>
2015-07-30 02:54:13 -0700

    remove environment for tracker

--- core/app/models/spree/tracker.rb
+++ core/app/models/spree/tracker.rb
@@ -1,8 +1,8 @@
 module Spree
   class Tracker < Spree::Base
     def self.current
-      tracker = where(active: true, environment: Rails.env).first
-      tracker.analytics_id.present? ? tracker : nil if tracker
+      tracker = find_by(active: true)
+      tracker if tracker && tracker.analytics_id.present?
     end
   end
 end
