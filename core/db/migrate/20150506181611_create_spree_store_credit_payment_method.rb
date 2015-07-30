7698ce436e8feb6aa00891a0fcf7943ad355a270
Ben A. Morgan <ben@benmorgan.io>
2015-07-30 02:25:22 -0700

    remove environment key

--- core/db/migrate/20150506181611_create_spree_store_credit_payment_method.rb
+++ core/db/migrate/20150506181611_create_spree_store_credit_payment_method.rb
@@ -5,27 +5,11 @@ class CreateSpreeStoreCreditPaymentMethod < ActiveRecord::Migration
   end
 
   def up
-    # Check if there is an environment column for users
-    # that are migrating from Spree 3.0.
-    if PaymentMethod.column_names.include? 'environment'
-      PaymentMethod.create_with(
-        name: Spree.t("store_credit.store_credit"),
-        description: Spree.t("store_credit.store_credit"),
-        active: true,
-        display_on: 'none',
-      ).find_or_create_by!(
-        type: "Spree::PaymentMethod::StoreCredit",
-        environment: Rails.env,
-      )
-    else
-      PaymentMethod.create_with(
-        name: Spree.t("store_credit.store_credit"),
-        description: Spree.t("store_credit.store_credit"),
-        active: true,
-        display_on: 'none',
-      ).find_or_create_by!(
-        type: "Spree::PaymentMethod::StoreCredit"
-      )
-    end
+    PaymentMethod.create_with(
+      name: Spree.t("store_credit.store_credit"),
+      description: Spree.t("store_credit.store_credit"),
+      active: true,
+      display_on: 'none',
+    ).find_or_create_by!(type: "Spree::PaymentMethod::StoreCredit")
   end
 end
