fa78dc228c8a0bd294a2d5221275ee7293947a72
John Hawthorn <john.hawthorn@gmail.com>
2015-07-30 11:22:02 -0700

    Standardize rake test_app warning
    This should always print the same warning to STDERR, and then exit with
    a failure code.

--- frontend/spec/spec_helper.rb
+++ frontend/spec/spec_helper.rb
@@ -18,8 +18,8 @@ ENV["RAILS_ENV"] ||= 'test'
 begin
   require File.expand_path("../dummy/config/environment", __FILE__)
 rescue LoadError
-  puts "Could not load dummy application. Please ensure you have run `bundle exec rake test_app`"
-  exit
+  $stderr.puts "Could not load dummy application. Please ensure you have run `bundle exec rake test_app`"
+  exit 1
 end
 
 require 'rspec/rails'
