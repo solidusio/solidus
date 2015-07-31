c1d723497f54aff7db9926205ab4cfc8f79ca1b8
Jordan Brough <git.j@brgh.net>
2015-07-31 14:45:34 -0600

    Use DbMaximumLengthValidator on product_properties.value
    This was reverted in 31352dae44d1db7ce09cf195e2f8ce2168bce1ca but it
    seems to actually work.  Currently only MySQL seems to have a length
    restriction on the "value" field here.

--- core/spec/models/spree/product_property_spec.rb
+++ core/spec/models/spree/product_property_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe Spree::ProductProperty, :type => :model do
 
   context "validations" do
-    it "should validate length of value" do
-      pp = create(:product_property)
-      pp.value = "x" * 256
-      expect(pp).not_to be_valid
+    # Only MySQL and stores that were migrated prior to the Rails 4.1(?) upgrade
+    # have length limitations on "value".
+    if Spree::ProductProperty.columns_hash['value'].limit
+      it "should validate length of value" do
+        pp = create(:product_property)
+        overflow_length = Spree::ProductProperty.columns_hash['value'].limit + 1
+        pp.value = "x" * overflow_length
+        expect(pp).not_to be_valid
+      end
     end
   end
 
