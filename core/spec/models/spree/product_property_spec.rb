c695eae155f4d1da10c42fb7a556448d02ac70b1
Jordan Brough <git.j@brgh.net>
2015-08-03 08:52:59 -0600

    Improve comment

--- core/spec/models/spree/product_property_spec.rb
+++ core/spec/models/spree/product_property_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Spree::ProductProperty, :type => :model do
 
   context "validations" do
-    # Only MySQL and stores that were migrated prior to the Rails 4.1(?) upgrade
-    # have length limitations on "value".
+    # Only MySQL stores or stores that were migrated prior to the Rails 4.2
+    # upgrade have length limitations on "value":
+    # > The PostgreSQL and SQLite adapters no longer add a default limit of 255
+    # > characters on string columns.
+    # http://guides.rubyonrails.org/4_2_release_notes.html#active-record-notable-changes
+    # https://github.com/rails/rails/pull/14579
     if Spree::ProductProperty.columns_hash['value'].limit
       it "should validate length of value" do
         pp = create(:product_property)
