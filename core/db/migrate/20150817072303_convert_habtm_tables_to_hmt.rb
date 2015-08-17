1ea45f5f69421572c848b1ee406f2fc475567793
Ben A. Morgan <ben@benmorgan.io>
2015-08-17 00:44:03 -0700

    update columns and table names for hmt tables

---
+++ core/db/migrate/20150817072303_convert_habtm_tables_to_hmt.rb
@@ -0,0 +1,47 @@
+class RenameHabtmTablesToHmtConvention < ActiveRecord::Migration
+  def up
+    add_columns :spree_option_types_prototypes
+    add_columns :spree_products_promotion_rules
+    add_columns :spree_taxons_promotion_rules, add_id: false
+    add_columns :spree_properties_prototypes
+    add_columns :spree_taxons_prototypes, add_id: false
+    add_columns :spree_shipping_methods_zones
+
+    rename_table :spree_option_types_prototypes, :spree_option_type_prototypes
+    rename_table :spree_products_promotion_rules, :spree_product_promotion_rules
+    rename_table :spree_taxons_promotion_rules, :spree_promotion_rule_taxons
+    rename_table :spree_properties_prototypes, :spree_property_prototypes
+    rename_table :spree_taxons_prototypes, :spree_prototype_taxons
+    rename_table :spree_shipping_methods_zones, :spree_shipping_method_zones
+  end
+
+  def down
+    remove_columns :spree_option_types_prototypes
+    remove_columns :spree_products_promotion_rules
+    remove_columns :spree_taxons_promotion_rules, remove_id: false
+    remove_columns :spree_properties_prototypes
+    remove_columns :spree_taxons_prototypes, remove_id: false
+    remove_columns :spree_shipping_methods_zones
+
+    rename_table :spree_option_type_prototypes, :spree_option_types_prototypes,
+    rename_table :spree_product_promotion_rules, :spree_products_promotion_rules
+    rename_table :spree_promotion_rule_taxons, :spree_taxons_promotion_rules
+    rename_table :spree_property_prototypes, :spree_properties_prototypes
+    rename_table :spree_prototype_taxons, :spree_taxons_prototypes
+    rename_table :spree_shipping_method_zones, :spree_shipping_methods_zones
+  end
+
+  private
+
+  def add_columns(table, add_id: true)
+    add_column table, :id, :primary_key if add_id
+    add_column table, :created_at, :datetime
+    add_column table, :updated_at, :datetime
+  end
+
+  def remove_columns(table, remove_id: true)
+    remove_column table, :updated_at
+    remove_column table, :created_at
+    remove_column table, :id if remove_id
+  end
+end
