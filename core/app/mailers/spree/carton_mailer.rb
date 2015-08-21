b7457f7aa2224be5e689f645bcaa1d7fe63c292b
John Hawthorn <john@freerunningtechnologies.com>
2015-08-21 15:43:54 -0700

    Add support for legacy shipped_email signature

--- core/app/mailers/spree/carton_mailer.rb
+++ core/app/mailers/spree/carton_mailer.rb
@@ -2,11 +2,17 @@ module Spree
   class CartonMailer < BaseMailer
     # Send an email to customers to notify that an individual carton has been
     # shipped.
-    def shipped_email(order, carton, resend: false)
-      @order = order
-      @store = order.store
-      @carton = carton
-      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    def shipped_email(*args)
+      options = {resend: false}.merge(args.extract_options!)
+      if args.length == 1
+        ActiveSupport::Deprecation.warn "Calling shipped_email with a carton_id is DEPRECATED. Instead use CartonMailer.shipped_email(order, carton)"
+        @carton = Carton.find(args[0])
+        @order = @carton.orders.first # assume first order
+      else
+        @order, @carton = args
+      end
+      @store = @order.store
+      subject = (options[:resend] ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "#{@store.name} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@order.number}"
       mail(to: @order.email, from: from_address(@store), subject: subject)
     end
