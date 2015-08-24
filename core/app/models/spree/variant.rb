df0abafd9e3ef0e405a0e1b10781a6aea9f6ee15
John Hawthorn <john@freerunningtechnologies.com>
2015-08-24 14:54:27 -0700

    Remove delegate_belongs_to from Variant

--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -21,9 +21,9 @@ module Spree
     belongs_to :product, touch: true, class_name: 'Spree::Product', inverse_of: :variants
     belongs_to :tax_category, class_name: 'Spree::TaxCategory'
 
-    delegate_belongs_to :product, :name, :description, :slug, :available_on,
-                        :shipping_category_id, :meta_description, :meta_keywords,
-                        :shipping_category
+    delegate :name, :description, :slug, :available_on, :shipping_category_id,
+             :meta_description, :meta_keywords, :shipping_category,
+             to: :product
 
     has_many :inventory_units, inverse_of: :variant
     has_many :line_items, inverse_of: :variant
