d7028da64d5aa1588fb2c4bad0f72688c8af995d
John Hawthorn <john@freerunningtechnologies.com>
2015-09-02 11:31:43 -0700

    Create payments manually

--- core/app/models/spree/order_update_attributes.rb
+++ core/app/models/spree/order_update_attributes.rb
@@ -1,21 +1,18 @@
 module Spree
   class OrderUpdateAttributes
-    attr_reader :attributes, :order, :request
+    attr_reader :attributes, :payments_attributes, :order, :request
     def initialize(order, attributes, request_env: nil, request: nil)
       @order = order
-      @attributes = attributes
+      @attributes = attributes.dup
+      @payments_attributes = @attributes.delete(:payments_attributes) || []
       @request = request
       @request_env = request_env
     end
 
     def update
-      if attributes[:payments_attributes]
-        attributes[:payments_attributes].each do |payment_attributes|
-          payment_attributes[:request_env] = request_env
-        end
-      end
+      assign_payments_attributes
+      assign_order_attributes
 
-      order.attributes = attributes
       if order.save
         order.set_shipments_cost if order.shipments.any?
         true
@@ -25,6 +22,16 @@ module Spree
     end
 
     private
+    def assign_order_attributes
+      order.attributes = attributes
+    end
+
+    def assign_payments_attributes
+      @payments_attributes.each do |payment_attributes|
+        payment_attributes = payment_attributes.merge(request_env: request_env)
+        order.payments.new(payment_attributes)
+      end
+    end
 
     def request_env
       @request_env ||= request ? request.headers.env : {}
