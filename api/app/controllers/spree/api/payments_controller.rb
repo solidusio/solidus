0486c55cc4112a19925576cc8168bd30c4ba6ff2
John Hawthorn <john@freerunningtechnologies.com>
2015-09-04 11:11:21 -0700

    Always use PaymentCreate when creating payments

--- api/app/controllers/spree/api/payments_controller.rb
+++ api/app/controllers/spree/api/payments_controller.rb
@@ -17,7 +17,7 @@ module Spree
       end
 
       def create
-        @payment = @order.payments.build(payment_params)
+        @payment = PaymentCreate.new(@order, payment_params).build
         if @payment.save
           respond_with(@payment, status: 201, default_template: :show)
         else
