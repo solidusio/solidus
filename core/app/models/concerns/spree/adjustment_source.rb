788e1dc690660a714185e687244e4e59862e50df
Grzegorz Łuszczek <grzegorz@piklus.pl>
2015-09-11 01:42:25 +0200

    Move all calculations to DB in AdjustmentSource
    Closes #364

--- core/app/models/concerns/spree/adjustment_source.rb
+++ core/app/models/concerns/spree/adjustment_source.rb
@@ -4,20 +4,19 @@ module Spree
 
     included do
       def deals_with_adjustments_for_deleted_source
-        adjustment_scope = self.adjustments.includes(:order).references(:spree_orders)
+        adjustment_scope = self.adjustments.joins(:order)
 
         # For incomplete orders, remove the adjustment completely.
-        adjustment_scope.where("spree_orders.completed_at IS NULL").destroy_all
+        adjustment_scope.where(spree_orders: { completed_at: nil }).destroy_all
 
         # For complete orders, the source will be invalid.
         # Therefore we nullify the source_id, leaving the adjustment in place.
         # This would mean that the order's total is not altered at all.
-        adjustment_scope.where("spree_orders.completed_at IS NOT NULL").each do |adjustment|
-          adjustment.update_columns(
-            source_id: nil,
-            updated_at: Time.now,
-          )
-        end
+        attrs = {
+          source_id: nil,
+          updated_at: Time.now
+        }
+        adjustment_scope.where.not(spree_orders: { completed_at: nil }).update_all(attrs)
       end
     end
   end
