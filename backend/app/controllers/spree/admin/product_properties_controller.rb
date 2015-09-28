7d402026570184d3e0ff896875da487a9e27bb75
Richard Nuno <me@richardnuno.com>
2015-09-28 15:05:08 -0400

    Join performed in the view rather than in the controller

--- backend/app/controllers/spree/admin/product_properties_controller.rb
+++ backend/app/controllers/spree/admin/product_properties_controller.rb
@@ -18,9 +18,8 @@ module Spree
 
         def setup_variant_property_rules
           @option_types = @product.variant_option_values_by_option_type
-          ovis = (params[:ovi] || []).reject(&:blank?).map(&:to_i)
-          @option_value_ids = ovis.join(',')
-          @variant_property_rule = @product.find_variant_property_rule(ovis) || @product.variant_property_rules.build
+          @option_value_ids = (params[:ovi] || []).reject(&:blank?).map(&:to_i)
+          @variant_property_rule = @product.find_variant_property_rule(@option_value_ids) || @product.variant_property_rules.build
           @variant_property_rule.values.build if can?(:create, Spree::VariantPropertyRuleValue)
         end
     end
