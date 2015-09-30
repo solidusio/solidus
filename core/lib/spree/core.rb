4400e54dd602e56c0034b8e1543d1757e6cf2850
John Hawthorn <john@stembolt.com>
2015-09-30 14:32:29 -0700

    Move PermissionSets into lib

--- core/lib/spree/core.rb
+++ core/lib/spree/core.rb
@@ -92,6 +92,7 @@ require 'spree/core/controller_helpers/store'
 require 'spree/core/controller_helpers/strong_parameters'
 require 'spree/core/unreturned_item_charger'
 require 'spree/core/role_configuration'
+require 'spree/permission_sets'
 
 require 'spree/mailer_previews/order_preview'
 require 'spree/mailer_previews/carton_preview'
