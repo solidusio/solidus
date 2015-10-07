665720e406e2d776d361344012eca6913ada482a
Jordan Brough <git.j@brgh.net>
2015-10-07 10:53:50 -0600

    Add update_target flag to api/address_books_controller#update response
    So that we can identify the address that was added or updated.
    Details in the added yardocs.

--- api/app/controllers/spree/api/address_books_controller.rb
+++ api/app/controllers/spree/api/address_books_controller.rb
@@ -10,16 +10,27 @@ module Spree
         render :show, status: :ok
       end
 
+      # Update a user's address book by adding an address to it or by updating
+      # the associated UserAddress (e.g. making it the default).
+      #
+      # @param user_id [String] the user id of the address book we're updating.
+      # @param address_book [Hash] any key-values permitted by
+      #   permitted_address_book_attributes
+      # @return [Array] *All* of the user's addresses, since the resource here
+      #   is the address book and since we may have mutated other UserAddresses
+      #   (e.g. changed the 'default' flag).  The user's default address will be
+      #   flagged with default=true and the target address from this update will
+      #   be flagged with update_target=true.
       def update
         authorize! :save_in_address_book, address_book_user
 
         address_params = address_book_params
         default_flag = address_params.delete(:default)
-        address = address_book_user.save_in_address_book(address_params, default_flag)
-        if address.valid?
+        @address = address_book_user.save_in_address_book(address_params, default_flag)
+        if @address.valid?
           render :show, status: :ok
         else
-          invalid_resource!(address)
+          invalid_resource!(@address)
         end
       end
 
