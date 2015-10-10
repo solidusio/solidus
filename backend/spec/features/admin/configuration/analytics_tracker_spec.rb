b72f67026e72b754c56b2b9ae053e4466e98c231
Ben A. Morgan <ben@benmorgan.io>
2015-10-09 17:11:39 -0700

    remove test select from spec

--- backend/spec/features/admin/configuration/analytics_tracker_spec.rb
+++ backend/spec/features/admin/configuration/analytics_tracker_spec.rb
@@ -40,7 +40,6 @@ describe "Analytics Tracker", :type => :feature do
     it "should be able to create a new analytics tracker" do
       click_link "admin_new_tracker_link"
       fill_in "tracker_analytics_id", :with => "A100"
-      select "Test", :from => "tracker-env"
       click_button "Create"
 
       expect(page).to have_content("successfully created!")
