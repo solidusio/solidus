5f3dd3f84a255e3510b111544817ee4fad274a0a
Ben A. Morgan <ben@benmorgan.io>
2015-10-15 13:30:45 -0700

    update readability of add foreign keys to taxons prototypes

--- core/db/migrate/20151010003252_add_foreign_keys_for_taxons_prototypes.rb
+++ core/db/migrate/20151010003252_add_foreign_keys_for_taxons_prototypes.rb
@@ -1,7 +1,9 @@
 class AddForeignKeysForTaxonsPrototypes < ActiveRecord::Migration
   def change
-    { spree_taxons: :taxon_id, spree_prototypes: :prototype_id }.each do |to_table, column|
-      add_foreign_key :spree_prototype_taxons, to_table, column: column, on_delete: :cascade
-    end
+    add_foreign_key :spree_prototype_taxons, :spree_taxons,
+                    column: :taxon_id, on_delete: :cascade
+
+    add_foreign_key :spree_prototype_taxons, :spree_prototypes,
+                    column: :prototype_id, on_delete: :cascade
   end
 end
