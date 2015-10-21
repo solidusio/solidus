4b4b60c6d4fae3ce7643fbb56ffbca2cfa32e3db
Patrick Sinclair <metade@gmail.com>
2015-10-21 16:26:17 +0100

    Adding index to the sale column

--- core/db/migrate/20151021113730_add_sale_to_spree_promotions.rb
+++ core/db/migrate/20151021113730_add_sale_to_spree_promotions.rb
@@ -1,5 +1,6 @@
 class AddSaleToSpreePromotions < ActiveRecord::Migration
   def change
     add_column :spree_promotions, :sale, :boolean, default: false
+    add_index :spree_promotions, :sale
   end
 end
