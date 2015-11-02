solidusio/solidus

commit 01a887207170d39b6a8b8a8f82c2f3ed7afd90ac

Restrict who has the ability to update a user's email address.
There is a possibility for privelege escalation whereby a lower-level
admin changes the email address of a higher-level admin to an email
address they have access to, thereby gaining access to more admin
permissions. To prevent this vulnerability, this code restricts the
ability for admins with roles below SuperUser only to update the email
address for themselves and for users with no roles.

* Refactor permitted_user_attributes, remove email, add nested address attributes that were duplicated
* Permit email attribute on create and when the user has the ability
* Restrict UserManagement permission to not update email for users with roles
* Disable the admin user email field when not authorized to update it

core/lib/spree/permission_sets/user_management.rb
@@ -2,7 +2,16 @@ module Spree
   module PermissionSets
     class UserManagement < PermissionSets::Base
       def activate!
-        can :manage, Spree.user_class
+        can [:admin, :display, :create, :update], Spree.user_class
+
+        # due to how cancancan filters by associations,
+        # we have to define this twice, once for `accessible_by`
+        can :update_email, Spree.user_class, spree_roles: { id: nil }
+        # and once for `can?`
+        can :update_email, Spree.user_class do |user|
+          user.spree_roles.none?
+        end
+
         cannot [:delete, :destroy], Spree.user_class
         can :manage, Spree::StoreCredit
         can :display, Spree::Role

api/app/controllers/spree/api/users_controller.rb
@@ -15,6 +15,10 @@ class Spree::Api::UsersController < Spree::Api::ResourceController
   end
 
   def permitted_resource_attributes
-    super | [bill_address_attributes: permitted_address_attributes, ship_address_attributes: permitted_address_attributes]
+    if action_name == "create" || can?(:update_email, user)
+      super | [:email]
+    else
+      super
+    end
   end
 end

backend/app/controllers/spree/admin/users_controller.rb
@@ -109,10 +109,11 @@ module Spree
         end
 
         def user_params
-          attributes = permitted_user_attributes | [
-            ship_address_attributes: permitted_address_attributes,
-            bill_address_attributes: permitted_address_attributes
-          ]
+          attributes = permitted_user_attributes
+
+          if action_name == "create" || can?(:update_email, @user)
+            attributes |= [:email]
+          end
 
           if can? :manage, Spree::Role
             attributes += [{ spree_role_ids: [] }]

backend/app/views/spree/admin/users/_form.html.erb
@@ -2,7 +2,7 @@
   <div class="alpha six columns">
     <%= f.field_container :email do %>
       <%= f.label :email, Spree.t(:email) %>
-      <%= f.email_field :email, :class => 'fullwidth' %>
+      <%= f.email_field :email, :class => 'fullwidth', disabled: cannot?(:update_email, @user) %>
       <%= error_message_on :user, :email %>
     <% end %>
 

backend/spec/controllers/spree/admin/users_controller_spec.rb
@@ -131,13 +131,17 @@ describe Spree::Admin::UsersController, :type => :controller do
 
   describe "#update" do
     let(:dummy_role) { Spree::Role.create(name: "dummyrole") }
+    let(:ability) { Spree::Ability.new(user) }
     before do
       use_mock_user
       allow(mock_user).to receive_messages(:spree_roles= => true, :stock_locations= => true)
-      user.spree_roles << Spree::Role.find_or_create_by(name: 'admin')
+      allow(controller).to receive(:current_ability) { ability }
+      Spree::PermissionSets::UserManagement.new(ability).activate!
     end
 
-    context "when the user can manage roles" do
+    context "as a superuser" do
+      before { Spree::PermissionSets::SuperUser.new(ability).activate! }
+
       it "can set roles" do
         expect(mock_user).to receive(:spree_roles=).with([dummy_role])
         spree_put :update, { id: mock_user.id, user: { first_name: "Bob", spree_role_ids: [dummy_role.id] } }
@@ -145,24 +149,40 @@ describe Spree::Admin::UsersController, :type => :controller do
 
       it "can clear roles" do
         expect(mock_user).to receive(:spree_roles=).with([])
-        spree_put :update, { id: mock_user.id, user: { first_name: "Bob"}  }
+        spree_put :update, { id: mock_user.id, user: { first_name: "Bob" }  }
+      end
+
+      it "can change email of a user with no roles" do
+        expect(mock_user).to receive(:update_attributes).with({ email: "bob@example.com" })
+        spree_put :update, { id: mock_user.id, user: { email: "bob@example.com" } }
+      end
+
+      it "can change email of a user with roles" do
+        allow(mock_user).to receive(:spree_roles) { [dummy_role] }
+        expect(mock_user).to receive(:update_attributes).with({ email: "bob@example.com" })
+        spree_put :update, { id: mock_user.id, user: { email: "bob@example.com" } }
       end
     end
 
-    context "when the user cannot manage roles" do
-      before do
-        user.spree_roles = [Spree::Role.find_or_create_by(name: "user_management")]
-      end
+    it "cannot set roles" do
+      expect(mock_user).to_not receive(:spree_roles=)
+      spree_put :update, { id: mock_user.id, user: { first_name: "Bob", spree_role_ids: [dummy_role.id] } }
+    end
 
-      it "cannot set roles" do
-        expect(mock_user).to_not receive(:spree_roles=)
-        spree_put :update, { id: mock_user.id, user: { first_name: "Bob", spree_role_ids: [dummy_role.id] } }
-      end
+    it "cannot clear roles" do
+      expect(mock_user).to_not receive(:spree_roles=)
+      spree_put :update, { id: mock_user.id, user: { first_name: "Bob" }  }
+    end
 
-      it "cannot clear roles" do
-        expect(mock_user).to_not receive(:spree_roles=)
-        spree_put :update, { id: mock_user.id, user: { first_name: "Bob" }  }
-      end
+    it "can change email of a user with no roles" do
+      expect(mock_user).to receive(:update_attributes).with({ email: "bob@example.com" })
+      spree_put :update, { id: mock_user.id, user: { email: "bob@example.com" } }
+    end
+
+    it "cannot change email of a user with roles" do
+      allow(mock_user).to receive(:spree_roles) { [dummy_role] }
+      expect(mock_user).to receive(:update_attributes).with({})
+      spree_put :update, { id: mock_user.id, user: { email: "bob@example.com" } }
     end
 
     it "allows shipping address attributes through" do
@@ -231,6 +251,7 @@ end
 def use_mock_user
   allow(mock_user).to receive(:save).and_return(true)
   allow(mock_user).to receive(:update_attributes).and_return(true)
+  allow(mock_user).to receive(:spree_roles).and_return([])
   allow(Spree.user_class).to receive(:find).with(mock_user.id.to_s).and_return(mock_user)
   allow(Spree.user_class).to receive(:new).and_return(mock_user)
 end

core/lib/spree/core/controller_helpers/strong_parameters.rb
@@ -48,6 +48,13 @@ module Spree
             product_properties_attributes: permitted_product_properties_attributes
           ]
         end
+
+        def permitted_user_attributes
+          permitted_attributes.user_attributes + [
+            bill_address_attributes: permitted_address_attributes,
+            ship_address_attributes: permitted_address_attributes
+          ]
+        end
       end
     end
   end

core/lib/spree/permission_sets/default_customer.rb
@@ -17,7 +17,7 @@ module Spree
         can :display, ProductProperty
         can :display, Property
         can :create, Spree.user_class
-        can [:read, :update], Spree.user_class, id: user.id
+        can [:read, :update, :update_email], Spree.user_class, id: user.id
         can :display, State
         can :display, StockItem, stock_location: { active: true }
         can :display, StockLocation, active: true

core/lib/spree/permitted_attributes.rb
@@ -110,8 +110,11 @@ module Spree
 
     @@transfer_item_attributes = [:variant_id, :expected_quantity, :received_quantity]
 
-    # TODO Should probably use something like Spree.user_class.attributes
-    @@user_attributes = [:email, :password, :password_confirmation]
+    # intentionally leaving off email here to prevent privilege escalation
+    # by changing a user with higher priveleges' email to one a lower-priveleged
+    # admin owns. creating a user with an email is handled separate at the
+    # controller level
+    @@user_attributes = [:password, :password_confirmation]
 
     @@variant_attributes = [
       :name, :presentation, :cost_price, :lock_version,

commit f0248e228fbfb9964734cade192fc5d6a6e44a11

Fix UserManagement permissions issue.
https://github.com/solidusio/solidus/commit/01a887207170d39b6a8b8a8f82c2f3ed7afd90ac
introduced a bug whereby admins with the UserManagement permission (but
no SuperUser permission) lost the ability to view the orders, addresses,
and items tabs in user admin. This code puts back that ability, plus
updates the specs so that we would catch this in the future by asserting
on the user feature specs using an admin with the UserManagement role
instead of assuming that the admin is always a SuperUser.

This makes use of the spec authorization helpers introduced in
https://github.com/solidusio/solidus/pull/449 to be able to better test
against the UserManagement role.

core/lib/spree/permission_sets/user_management.rb
@@ -2,7 +2,7 @@ module Spree
   module PermissionSets
     class UserManagement < PermissionSets::Base
       def activate!
-        can [:admin, :display, :create, :update, :save_in_address_book, :remove_from_address_book], Spree.user_class
+        can [:admin, :display, :create, :update, :save_in_address_book, :remove_from_address_book, :addresses, :orders, :items], Spree.user_class
 
         # due to how cancancan filters by associations,
         # we have to define this twice, once for `accessible_by`

backend/spec/features/admin/users_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Users', :type => :feature do
-  stub_authorization!
+  stub_authorization!(permission_sets: [Spree::PermissionSets::UserManagement, Spree::PermissionSets::OrderManagement])
   let!(:country) { create(:country) }
   let!(:user_a) { create(:user_with_addreses, email: 'a@example.com') }
   let!(:user_b) { create(:user_with_addreses, email: 'b@example.com') }
@@ -119,16 +119,6 @@ describe 'Users', :type => :feature do
       expect(page).to have_text 'Account updated'
     end
 
-    it 'can edit user roles' do
-      Spree::Role.create name: "admin"
-      click_link user_a.email
-
-      check 'user_spree_role_admin'
-      click_button 'Update'
-      expect(page).to have_text 'Account updated'
-      expect(find_field('user_spree_role_admin')['checked']).to be true
-    end
-
     it 'can edit user shipping address' do
       click_link "Addresses"
 
@@ -153,42 +143,56 @@ describe 'Users', :type => :feature do
       expect(user_a.reload.bill_address.address1).to eq "1313 Mockingbird Ln"
     end
 
-    context 'no api key exists' do
-      it 'can generate a new api key' do
-        within("#admin_user_edit_api_key") do
-          expect(user_a.spree_api_key).to be_blank
-          click_button Spree.t('generate_key', :scope => 'api')
-        end
 
-        expect(user_a.reload.spree_api_key).to be_present
+    context "superuser" do
+      stub_authorization!
+
+      it 'can edit user roles' do
+        Spree::Role.create name: "admin"
+        click_link user_a.email
+
+        check 'user_spree_role_admin'
+        click_button 'Update'
+        expect(page).to have_text 'Account updated'
+        expect(find_field('user_spree_role_admin')['checked']).to be true
       end
-    end
+      context 'no api key exists' do
+        it 'can generate a new api key' do
+          within("#admin_user_edit_api_key") do
+            expect(user_a.spree_api_key).to be_blank
+            click_button Spree.t('generate_key', :scope => 'api')
+          end
 
-    context 'an api key exists' do
-      before do
-        user_a.generate_spree_api_key!
-        expect(user_a.reload.spree_api_key).to be_present
-        visit current_path
-      end
-
-      it 'can clear an api key' do
-        within("#admin_user_edit_api_key") do
-          click_button Spree.t('clear_key', :scope => 'api')
+          expect(user_a.reload.spree_api_key).to be_present
         end
-
-        expect(user_a.reload.spree_api_key).to be_blank
-        expect { find("#current-api-key") }.to raise_error Capybara::ElementNotFound
       end
 
-      it 'can regenerate an api key' do
-        old_key = user_a.spree_api_key
-
-        within("#admin_user_edit_api_key") do
-          click_button Spree.t('regenerate_key', :scope => 'api')
+      context 'an api key exists' do
+        before do
+          user_a.generate_spree_api_key!
+          expect(user_a.reload.spree_api_key).to be_present
+          visit current_path
         end
 
-        expect(user_a.reload.spree_api_key).to be_present
-        expect(user_a.reload.spree_api_key).not_to eq old_key
+        it 'can clear an api key' do
+          within("#admin_user_edit_api_key") do
+            click_button Spree.t('clear_key', :scope => 'api')
+          end
+
+          expect(user_a.reload.spree_api_key).to be_blank
+          expect { find("#current-api-key") }.to raise_error Capybara::ElementNotFound
+        end
+
+        it 'can regenerate an api key' do
+          old_key = user_a.spree_api_key
+
+          within("#admin_user_edit_api_key") do
+            click_button Spree.t('regenerate_key', :scope => 'api')
+          end
+
+          expect(user_a.reload.spree_api_key).to be_present
+          expect(user_a.reload.spree_api_key).not_to eq old_key
+        end
       end
     end
   end