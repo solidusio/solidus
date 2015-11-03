e2f6c0bc31c660ca58ce1ed1f628ea140805f65d
Gregor MacDougall <gregor@freerunningtechnologies.com>
2015-11-03 08:11:52 -0500

    Add documentation to the OrderMerger
    As part of this, I additionally noted that several methods should be
    private as they are not used outside of this new class.

--- core/app/models/spree/order_merger.rb
+++ core/app/models/spree/order_merger.rb
@@ -1,12 +1,50 @@
 module Spree
+  # Spree::OrderMerger is responsible for taking two orders and merging them
+  # together by adding the line items from additional orders to the order
+  # that the OrderMerger is initialized with.
+  #
+  # Orders that are merged should be incomplete orders which should belong to
+  # the same user. They should also be in the same currency.
   class OrderMerger
+    # @!attribute order
+    #   @api public
+    #   @return [Spree::Order] The order which items wll be merged into.
     attr_accessor :order
+
     delegate :updater, to: :order
 
+    # Create the OrderMerger
+    #
+    # @api public
+    # @param [Spree::Order] order The order which line items will be merged
+    # into.
     def initialize(order)
       @order = order
     end
 
+    # Merge a second order in to the order the OrderMerger was initialized with
+    #
+    # The line items from `other_order` will be merged in to the `order` for
+    # this OrderMerger object. If the line items are for the same variant, it
+    # will the quantity of the incoming line item to the existing line item.
+    # Otherwise, it will assign the line item to the new order.
+    #
+    # After the orders have been merged the `other_order` will be destroyed.
+    #
+    # @example
+    #   initial_order = Spree::Order.find(1)
+    #   order_to_merge = Spree::Order.find(2)
+    #   merger = Spree::OrderMerger.new(initial_order)
+    #   merger.merge!(order_to_merge)
+    #   # order_to_merge is destroyed, initial order now contains the line items
+    #   # of order_to_merge
+    #
+    # @api public
+    # @param [Spree::Order] other_order An order which will be merged in to the
+    # order the OrderMerger was initialized with.
+    # @param [Spree::User] user Associate the order the user specified. If not
+    # specified, the order user association will not be changed.
+    # @return [void]
     def merge!(other_order, user = nil)
       other_order.line_items.each do |other_order_line_item|
         next unless other_order_line_item.currency == order.currency
@@ -23,9 +61,18 @@ module Spree
       other_order.destroy
     end
 
-    # Compare the line item of the other order with mine.
-    # Make sure you allow any extensions to chime in on whether or
-    # not the extension-specific parts of the line item match
+    private
+
+    # Retreive a matching line item from the existing order
+    #
+    # It will compare line items based on variants, and all line item
+    # comparison hooks on the order.
+    #
+    # @api private
+    # @param [Spree::LineItem] other_order_line_item The line item from
+    # `other_order` we are attempting to merge in.
+    # @return [Spree::LineItem] A matching line item from the order. nil if none
+    # exist.
     def find_matching_line_item(other_order_line_item)
       order.line_items.detect do |my_li|
         my_li.variant == other_order_line_item.variant &&
@@ -35,12 +82,30 @@ module Spree
       end
     end
 
-    def set_user(user = nil)
-      order.associate_user!(user) if !order.user && !user.blank?
+    # Associate the user with the order
+    #
+    # @api private
+    # @param [Spree::User] user The user to associate with the order. If nil
+    # the order user association will remain the same. If the order is already
+    # associated with a user, it will not be changed.
+    # @return [void]
+    def set_user(user)
+      order.associate_user!(user) if !order.user && user
     end
 
-    # The idea is the end developer can choose to override the merge
-    # to their own choosing. Default is merge with errors.
+    # Merge the `other_order_line_item` into `current_line_item`
+    #
+    # If `current_line_item` is nil, the `other_order_line_item` will be
+    # re-assigned to the `order`.
+    #
+    # If the merged line item can not be saved, an error will be added to
+    # `order`.
+    #
+    # @api private
+    # @param [Spree::LineItem] current_line_item The line item to be merged
+    # into. If nil, the `other_order_line_item` will be re-assigned.
+    # @param [Spree::LineItem] other_order_line_item The line item to merge in.
+    # @return [void]
     def handle_merge(current_line_item, other_order_line_item)
       if current_line_item
         current_line_item.quantity += other_order_line_item.quantity
@@ -51,11 +116,24 @@ module Spree
       end
     end
 
-    # Change the error messages as you choose.
+    # Handle an error from saving the `line_item`
+    #
+    # This adds errors from the line item to the `errors[:base]` of the order.
+    #
+    # @api private
+    # @param [Spree::LineItem] line_item The line item which could not be saved
+    # @return [void]
     def handle_error(line_item)
       order.errors[:base] << line_item.errors.full_messages
     end
 
+    # Save the order totals after merge
+    #
+    # It triggers the order updater to ensure that item counts and totals are
+    # up to date.
+    #
+    # @api private
+    # @return [void]
     def persist_merge
       updater.update_item_count
       updater.update_item_total
