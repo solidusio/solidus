fd5cac10c69ba8b651245a3e0008af523e474d21
John Hawthorn <john.hawthorn@gmail.com>
2015-11-05 15:47:26 -0800

    Use empty AR model for store migrations
    Fixes an issue the solidus_globalize port is having because of the
    globalize table not existing at the time the migration is run.

--- core/db/migrate/20141009204607_add_store_id_to_orders.rb
+++ core/db/migrate/20141009204607_add_store_id_to_orders.rb
@@ -1,8 +1,12 @@
 class AddStoreIdToOrders < ActiveRecord::Migration
+  class Store < ActiveRecord::Base
+    self.table_name = 'spree_stores'
+  end
   def change
     add_column :spree_orders, :store_id, :integer
-    if Spree::Store.default.persisted?
-      Spree::Order.where(store_id: nil).update_all(store_id: Spree::Store.default.id)
+    default_store = Store.where(default: true).first
+    if default_store
+      Spree::Order.where(store_id: nil).update_all(store_id: default_store.id)
     end
   end
 end
