be74e5e32f707605866404d9ecb31bec62ec8c4e
Sinetheta <k.attfield@gmail.com>
2015-11-24 16:26:11 -0800

    Add custom layout for style guide

--- backend/app/controllers/spree/admin/style_guide_controller.rb
+++ backend/app/controllers/spree/admin/style_guide_controller.rb
@@ -2,6 +2,7 @@ module Spree
   module Admin
     class StyleGuideController < Spree::Admin::BaseController
       respond_to :html
+      layout '/spree/layouts/admin_style_guide'
 
       def index
       end
