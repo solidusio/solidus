26bb7e47f243099ab37634098e2218cfd0879a7a
Frédéric Boivin <fboivin63@gmail.com>
2015-11-26 18:05:13 -0500

    Override Solidus Locale controller to surcharge behavior

--- i18n/app/controllers/spree/locale_controller_decorator.rb
+++ i18n/app/controllers/spree/locale_controller_decorator.rb
@@ -1,7 +1,6 @@
-module Spree
-  class LocaleController < Spree::StoreController
+Spree::LocaleController.class_eval do
     def set
       redirect_to root_path(locale: params[:switch_to_locale])
     end
-  end
 end
+
