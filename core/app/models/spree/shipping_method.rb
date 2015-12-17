d8efb983413a9d9c04863618093e623d56cedcad
John Hawthorn <john@stembolt.com>
2015-12-16 16:22:15 -0800

    Remove duplicated default_scope
    paranoia already adds this scope for us.

--- core/app/models/spree/shipping_method.rb
+++ core/app/models/spree/shipping_method.rb
@@ -4,8 +4,6 @@ module Spree
     include Spree::CalculatedAdjustments
     DISPLAY = [:both, :front_end, :back_end]
 
-    default_scope -> { where(deleted_at: nil) }
-
     has_many :shipping_method_categories, :dependent => :destroy
     has_many :shipping_categories, through: :shipping_method_categories
     has_many :shipping_rates, inverse_of: :shipping_method
