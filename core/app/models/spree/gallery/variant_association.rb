506676682ba5e8dbf1d5533d8cfbdca5f1c987f4
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2015-12-21 18:06:22 -0800

    Create a VariantAssociation Gallery
    This implements the traditional Solidus behavior of using the
    ActiveRecord association between variants and the polymorphic "viewable"
    on images.
    
    It is a simple one-to-many relationship, but will fallback to a product
    to find a "best" image if no images exist on the association.

---
+++ core/app/models/spree/gallery/variant_association.rb
@@ -0,0 +1,40 @@
+module Spree
+  # Uses the ActiveRecord association between Spree::Variant and
+  #   Spree::Image
+  module Gallery
+    class VariantAssociation < Gallery::Base
+
+      # Creates a gallery for the given variant
+      # @param [Spree::Variant] variant variant to build the gallery for
+      def initialize(variant)
+        @variant = variant
+      end
+
+      # (see Spree::Gallery#images)
+      def images
+        variant.images
+      end
+
+      # (see Spree::Gallery#primary_image)
+      def primary_image
+        variant.images.first
+      end
+
+      # Will fall back to the first image on the variant's product if
+      # the variant has no associated images.
+      # @return [Spree::Image, nil] The variant's primary image, if it exists,
+      #   or the variant's products first image, if it exists, nil otherwise
+      def best_image
+        primary_image || products_images.first
+      end
+
+      private
+      attr_reader :variant
+
+      def products_images
+        variant.product.try!(:variant_images) || []
+      end
+
+    end
+  end
+end
---
+++ core/spec/models/spree/gallery/variant_association.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+RSpec.describe Spree::Gallery::VariantAssociation, type: :model do
+  let(:gallery) { described_class.new(variant) }
+
+  let(:variant) { Spree::Variant.new }
+
+  shared_context 'has multiple images' do
+    let(:first_image) { Spree::Image.new }
+    let(:second_image) { Spree::Image.new }
+
+    before do
+      variant.images << first_image
+      variant.images << second_image
+    end
+  end
+
+  describe '#images' do
+    subject { gallery.images }
+
+    it 'is empty' do
+      expect(subject).to be_empty
+    end
+
+    context 'has multiple images' do
+
+      include_context 'has multiple images'
+
+      it 'has the images of the association' do
+        expect(subject).to eq [first_image, second_image]
+      end
+
+    end
+  end
+
+  describe '#primary_image' do
+    subject { gallery.primary_image }
+
+    it 'is nil' do
+      expect(subject).to be_nil
+    end
+
+    context 'has images' do
+      include_context 'has multiple images'
+
+      it 'uses the first image' do
+        expect(subject).to eq first_image
+      end
+    end
+  end
+
+  describe '#best_image' do
+    subject { gallery.best_image }
+
+    it 'is nil' do
+      expect(subject).to be_nil
+    end
+
+    context 'has images' do
+      include_context 'has multiple images'
+
+      it 'uses the first image' do
+        expect(subject).to eq first_image
+      end
+    end
+
+    context 'variant has a product' do
+      let(:product) { Spree::Product.new }
+      let(:variant) { Spree::Variant.new product: product }
+
+      it 'is nil' do
+        expect(subject).to be_nil
+      end
+
+      context 'that has images' do
+        let(:product) { create :product }
+        let(:variant) { create :variant, product: product }
+        let!(:image) { create :image, viewable: product.master }
+
+        it 'falls back to the products image' do
+          expect(subject).to eq image
+        end
+      end
+    end
+  end
+end
