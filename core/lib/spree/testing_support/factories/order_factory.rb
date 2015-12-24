044da78fd0769145e39964a11d344d7fd465022c
John Hawthorn <john@stembolt.com>
2015-12-23 16:45:03 -0800

    Avoid extra stock location in order factory
    Previously, create(:order_with_line_items) would create two stock
    locations. One would be created as part of the product factory, which
    creates a stock location if none exists, and another would be created as
    part of the shipment factory.
    
    This commit moves the creation of the stock location up a level, into
    the order factory. This allows it to be created before the line_item and
    product factory.
    
    This also allows doing:
    
        create(:shipped_order, stock_location: location)
    
    which should be helpful in testing.

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -27,16 +27,19 @@ FactoryGirl.define do
         line_items_attributes { [{}] * line_items_count }
         shipment_cost 100
         shipping_method nil
+        stock_location { create(:stock_location) }
       end
 
       after(:create) do |order, evaluator|
+        evaluator.stock_location # must evaluate before creating line items
+
         evaluator.line_items_attributes.each do |attributes|
           attributes = {order: order, price: evaluator.line_items_price}.merge(attributes)
           create(:line_item, attributes)
         end
         order.line_items.reload
 
-        create(:shipment, order: order, cost: evaluator.shipment_cost, shipping_method: evaluator.shipping_method, address: evaluator.ship_address)
+        create(:shipment, order: order, cost: evaluator.shipment_cost, shipping_method: evaluator.shipping_method, address: evaluator.ship_address, stock_location: evaluator.stock_location)
         order.shipments.reload
 
         order.update!
