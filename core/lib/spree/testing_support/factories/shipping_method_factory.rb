4ca4d8f280df32e75957b850a41edde8dc4563b8
Dylan Kendal <dylan@stembolt.com>
2015-12-23 16:56:01 -0800

    Convert base_shipping_method factory into an alias
    Base_shipping_method was invalid because it doesn't have a calculator.
    
    base_shipping_method isn't used anywhere within core, but it could be
    used other applications.
    
    For both of these reasons, base_shipping_method is now an alias for
    shipping_method, which is valid.

--- core/lib/spree/testing_support/factories/shipping_method_factory.rb
+++ core/lib/spree/testing_support/factories/shipping_method_factory.rb
@@ -1,8 +1,20 @@
 FactoryGirl.define do
-  factory :base_shipping_method, class: Spree::ShippingMethod do
+  factory(
+    :shipping_method,
+    aliases: [
+      :base_shipping_method
+    ],
+    class: Spree::ShippingMethod
+  ) do
     zones { |a| [Spree::Zone.global] }
     name 'UPS Ground'
     code 'UPS_GROUND'
+
+    calculator { |s| s.association(:shipping_calculator, strategy: :build, preferred_amount: s.cost) }
+
+    transient do
+      cost 10.0
+    end
 
     before(:create) do |shipping_method, evaluator|
       if shipping_method.shipping_categories.empty?
@@ -10,15 +22,8 @@ FactoryGirl.define do
       end
     end
 
-    factory :shipping_method, class: Spree::ShippingMethod do
-      transient do
-        cost 10.0
-      end
-
-      calculator { |s| s.association(:shipping_calculator, strategy: :build, preferred_amount: s.cost) }
-    end
-
     factory :free_shipping_method, class: Spree::ShippingMethod do
+      cost nil
       association(:calculator, factory: :shipping_no_amount_calculator, strategy: :build)
     end
   end
