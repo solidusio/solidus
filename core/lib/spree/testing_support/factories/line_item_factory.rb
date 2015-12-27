96a6621cf15101907c1d4571f4d1ac8a2cde6ffd
Sinetheta <k.attfield@gmail.com>
2015-12-26 17:51:02 -0800

    Require dependencies in line_item factory

--- core/lib/spree/testing_support/factories/line_item_factory.rb
+++ core/lib/spree/testing_support/factories/line_item_factory.rb
@@ -1,3 +1,6 @@
+require 'spree/testing_support/factories/order_factory'
+require 'spree/testing_support/factories/product_factory'
+
 FactoryGirl.define do
   factory :line_item, class: Spree::LineItem do
     quantity 1
