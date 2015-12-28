c30ab833b9f1caa604dc1440bb56f7418af498d7
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:17:42 +0100

    Test and fix prototype factory

--- core/lib/spree/testing_support/factories/prototype_factory.rb
+++ core/lib/spree/testing_support/factories/prototype_factory.rb
@@ -1,3 +1,5 @@
+require 'spree/testing_support/factories/property_factory'
+
 FactoryGirl.define do
   factory :prototype, class: Spree::Prototype do
     name 'Baseball Cap'
---
+++ core/spec/lib/spree/core/testing_support/factories/prototype_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/prototype_factory'
+
+RSpec.describe 'prototype factory' do
+  let(:factory_class) { Spree::Prototype }
+
+  describe 'plain prototype' do
+    let(:factory) { :prototype }
+
+    it_behaves_like 'a working factory'
+  end
+end
