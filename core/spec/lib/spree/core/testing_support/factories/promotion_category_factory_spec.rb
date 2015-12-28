86c00abc7d0fce1d94af2076221cd576900be254
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:04:04 +0100

    Test promotion category factory

---
+++ core/spec/lib/spree/core/testing_support/factories/promotion_category_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/promotion_category_factory'
+
+RSpec.describe 'promotion category factory' do
+  let(:factory_class) { Spree::PromotionCategory }
+
+  describe 'plain promotion category' do
+    let(:factory) { :promotion_category }
+
+    it_behaves_like 'a working factory'
+  end
+end
