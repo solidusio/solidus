2f68bee73ad8ebb523bdd31605f4f93a13176d8f
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 19:54:47 +0100

    Test image factory

---
+++ core/spec/lib/spree/core/testing_support/factories/image_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/image_factory'
+
+RSpec.describe 'image factory' do
+  let(:factory_class) { Spree::Image }
+
+  describe 'plain adjustment' do
+    let(:factory) { :image }
+
+    it_behaves_like 'a working factory'
+  end
+end
