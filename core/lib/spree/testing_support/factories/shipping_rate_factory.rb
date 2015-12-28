ae7ed367c2edc92aa8263246b851a54f671ba2b5
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:59:46 +0100

    Test and fix shipping rate factory

--- core/lib/spree/testing_support/factories/shipping_rate_factory.rb
+++ core/lib/spree/testing_support/factories/shipping_rate_factory.rb
@@ -1,3 +1,6 @@
+require 'spree/testing_support/factories/shipping_method_factory'
+require 'spree/testing_support/factories/shipment_factory'
+
 FactoryGirl.define do
   factory :shipping_rate, class: Spree::ShippingRate do
     shipping_method
---
+++ core/spec/lib/spree/core/testing_support/factories/shipping_rate_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/shipping_rate_factory'
+
+RSpec.describe 'shipping rate factory' do
+  let(:factory_class) { Spree::ShippingRate }
+
+  describe 'plain shipping rate' do
+    let(:factory) { :shipping_rate }
+
+    it_behaves_like 'a working factory'
+  end
+end
