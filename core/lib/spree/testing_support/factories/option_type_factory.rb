d1c73fdd729a876b50fc89acadf7a3df4648e211
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 20:00:05 +0100

    Split up options factory file

--- core/lib/spree/testing_support/factories/option_type_factory.rb
+++ core/lib/spree/testing_support/factories/option_type_factory.rb
@@ -1,11 +1,4 @@
 FactoryGirl.define do
-  factory :option_value, class: Spree::OptionValue do
-    sequence(:name) { |n| "Size-#{n}" }
-
-    presentation 'S'
-    option_type
-  end
-
   factory :option_type, class: Spree::OptionType do
     sequence(:name) { |n| "foo-size-#{n}" }
     presentation 'Size'
