35e3e3efe784e6f98d8b0e8d82e3a38e45356052
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 19:38:08 +0100

    Test credit card factory

---
+++ core/spec/lib/spree/core/testing_support/factories/credit_card_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/credit_card_factory'
+
+RSpec.describe 'credit card factory' do
+  let(:factory_class) { Spree::CreditCard }
+
+  describe 'plain credit card' do
+    let(:factory) { :credit_card }
+
+    it_behaves_like 'a working factory'
+  end
+end
