31eb4bcfe35449baeeca5c040c8f2cf8620d16d0
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:21:06 +0100

    Test and fix refund factory

--- core/lib/spree/testing_support/factories/refund_factory.rb
+++ core/lib/spree/testing_support/factories/refund_factory.rb
@@ -1,3 +1,6 @@
+require 'spree/testing_support/factories/payment_factory'
+require 'spree/testing_support/factories/refund_reason_factory'
+
 FactoryGirl.define do
   sequence(:refund_transaction_id) { |n| "fake-refund-transaction-#{n}"}
 
---
+++ core/spec/lib/spree/core/testing_support/factories/refund_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/refund_factory'
+
+RSpec.describe 'refund factory' do
+  let(:factory_class) { Spree::Refund }
+
+  describe 'plain refund' do
+    let(:factory) { :refund }
+
+    it_behaves_like 'a working factory'
+  end
+end
