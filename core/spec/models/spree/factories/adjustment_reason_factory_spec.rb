fbd070b84cd6e066e3e5071af7d1e4ad228faf1b
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 18:40:23 +0100

    Test adjustment reason factory

---
+++ core/spec/models/spree/factories/adjustment_reason_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/adjustment_reason_factory'
+
+RSpec.describe 'adjustment reason factory' do
+  let(:factory_class) { Spree::AdjustmentReason }
+
+  describe 'adjustment reason' do
+    let(:factory) { :adjustment_reason }
+
+    it_behaves_like 'a working factory'
+  end
+end
