ce4126b5ce8170c468dc18349bcba3527fb98b1d
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 18:23:22 +0100

    Test last order factory

--- core/spec/models/spree/factories/order_factory_spec.rb
+++ core/spec/models/spree/factories/order_factory_spec.rb
@@ -3,7 +3,7 @@ ENV['NO_FACTORIES'] = "NO FACTORIES"
 require 'spec_helper'
 require 'spree/testing_support/factories/order_factory'
 
-RSpec.shared_examples_for 'an order factory' do
+RSpec.shared_examples_for 'a working factory' do
   it "builds" do
     expect(build factory).to be_a(factory_class)
   end
@@ -19,31 +19,43 @@ RSpec.describe 'order factory', type: :model do
   describe 'plain order' do
     let(:factory) { :order }
 
-    it_behaves_like 'an order factory'
+    it_behaves_like 'a working factory'
   end
 
 
   describe 'order with totals' do
     let(:factory) { :order_with_totals }
 
-    it_behaves_like 'an order factory'
+    it_behaves_like 'a working factory'
   end
 
   describe 'order with line items' do
     let(:factory) { :order_with_line_items }
 
-    it_behaves_like 'an order factory'
+    it_behaves_like 'a working factory'
   end
 
   describe 'completed order with totals' do
     let(:factory) { :completed_order_with_totals }
 
-    it_behaves_like 'an order factory'
+    it_behaves_like 'a working factory'
   end
 
   describe 'completed order with pending payment' do
     let(:factory) { :completed_order_with_pending_payment }
 
-    it_behaves_like 'an order factory'
+    it_behaves_like 'a working factory'
+  end
+
+  describe 'order ready to ship' do
+    let(:factory) { :order_ready_to_ship }
+
+    it_behaves_like 'a working factory'
+  end
+
+  describe 'shipped order' do
+    let(:factory) { :shipped_order }
+
+    it_behaves_like 'a working factory'
   end
 end
