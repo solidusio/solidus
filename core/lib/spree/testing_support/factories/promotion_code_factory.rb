98a93d73e3db03171a19fe572454cd8e09ed7304
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:06:26 +0100

    Test and fix promotion code factory

--- core/lib/spree/testing_support/factories/promotion_code_factory.rb
+++ core/lib/spree/testing_support/factories/promotion_code_factory.rb
@@ -1,4 +1,5 @@
 require 'spree/testing_support/sequences'
+require 'spree/testing_support/factories/promotion_factory'
 
 FactoryGirl.define do
   factory :promotion_code, class: 'Spree::PromotionCode' do
