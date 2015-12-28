80a8fd1c63a7112c32138cc49a88801fb18b1f32
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 22:14:43 +0100

    Test property factory

---
+++ core/spec/lib/spree/core/testing_support/factories/property_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/property_factory'
+
+RSpec.describe 'property factory' do
+  let(:factory_class) { Spree::Property }
+
+  describe 'plain property' do
+    let(:factory) { :property }
+
+    it_behaves_like 'a working factory'
+  end
+end
