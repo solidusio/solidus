4c81ecf4819bad27ec6beedb423140b7fd6892a8
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-28 21:35:42 +0100

    Test option value and option type factories

---
+++ core/spec/lib/spree/core/testing_support/factories/option_value_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/option_value_factory'
+
+RSpec.describe 'option value factory' do
+  let(:factory_class) { Spree::OptionValue }
+
+  describe 'plain option value' do
+    let(:factory) { :option_value }
+
+    it_behaves_like 'a working factory'
+  end
+end
