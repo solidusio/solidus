6b82a16dea5e04f1dbc53acb6efd0a0251b755ca
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 14:55:38 +0100

    Test tax rate factory

---
+++ core/spec/lib/spree/core/testing_support/factories/tax_rate_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/tax_rate_factory'
+
+RSpec.describe 'tax rate factory' do
+  let(:factory_class) { Spree::TaxRate }
+
+  describe 'tax rate' do
+    let(:factory) { :tax_rate }
+
+    it_behaves_like 'a working factory'
+  end
+end
