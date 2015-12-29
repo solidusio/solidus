7485453b5bf85af34412b7c9ad7a64bc7da260e0
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 14:59:59 +0100

    Test tracker factory

---
+++ core/spec/lib/spree/core/testing_support/factories/tracker_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/tracker_factory'
+
+RSpec.describe 'tracker factory' do
+  let(:factory_class) { Spree::Tracker }
+
+  describe 'tracker' do
+    let(:factory) { :tracker }
+
+    it_behaves_like 'a working factory'
+  end
+end
