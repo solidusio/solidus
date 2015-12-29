f1bca4fa5b5494bc9dc533368a88586957eb85ad
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 14:25:24 +0100

    Test and fix stock package factory

--- core/lib/spree/testing_support/factories/stock_package_factory.rb
+++ core/lib/spree/testing_support/factories/stock_package_factory.rb
@@ -1,3 +1,6 @@
+require 'spree/testing_support/factories/inventory_unit_factory'
+require 'spree/testing_support/factories/variant_factory'
+
 FactoryGirl.define do
   factory :stock_package, class: Spree::Stock::Package do
     skip_create
