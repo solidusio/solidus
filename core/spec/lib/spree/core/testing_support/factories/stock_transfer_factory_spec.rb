a539c48a1c00beb4db33ef9c84a71cfd9de4b4dd
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 14:30:31 +0100

    Test stock transfer factory

---
+++ core/spec/lib/spree/core/testing_support/factories/stock_transfer_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/stock_transfer_factory'
+
+RSpec.describe 'stock transfer factory' do
+  let(:factory_class) { Spree::StockTransfer }
+
+  describe 'plain stock transfer' do
+    let(:factory) { :stock_transfer }
+
+    it_behaves_like 'a working factory'
+  end
+end
