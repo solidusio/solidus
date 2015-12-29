85470bf664fd4c973cb0736df19f2466b1fdd265
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 15:05:19 +0100

    Test and fix user factory

--- core/lib/spree/testing_support/factories/user_factory.rb
+++ core/lib/spree/testing_support/factories/user_factory.rb
@@ -1,5 +1,6 @@
 require 'spree/testing_support/sequences'
 require 'spree/testing_support/factories/role_factory'
+require 'spree/testing_support/factories/address_factory'
 
 FactoryGirl.define do
   sequence :user_authentication_token do |n|
@@ -23,7 +24,7 @@ FactoryGirl.define do
       spree_roles { [Spree::Role.find_by(name: 'admin') || create(:role, name: 'admin')] }
     end
 
-    factory :user_with_addreses do |u|
+    factory :user_with_addresses do |u|
       bill_address
       ship_address
     end
