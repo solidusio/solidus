8e13663ad4a28302475242e8d0b607b48f6675a2
Martin Meyerhoff <mamhoff@googlemail.com>
2015-12-29 15:17:35 +0100

    Test variant property rule factory

---
+++ core/spec/lib/spree/core/testing_support/factories/variant_property_rule_factory_spec.rb
@@ -0,0 +1,14 @@
+ENV['NO_FACTORIES'] = "NO FACTORIES"
+
+require 'spec_helper'
+require 'spree/testing_support/factories/variant_property_rule_factory'
+
+RSpec.describe 'variant property rule factory' do
+  let(:factory_class) { Spree::VariantPropertyRule }
+
+  describe 'variant property rule' do
+    let(:factory) { :variant_property_rule }
+
+    it_behaves_like 'a working factory'
+  end
+end
