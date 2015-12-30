c02ff1be18b5af5eafe15010eb4c409388137094
John Hawthorn <john@stembolt.com>
2015-12-30 14:58:11 -0800

    Add all image assets to the precompile list
    I have no idea why this is required, but after switching to
    sprockets-rails 3.0 it is required to declare image assets in the
    precompile.

--- backend/config/initializers/assets.rb
+++ backend/config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( jquery-ui/* logo/solidus_logo.png )
+Rails.application.config.assets.precompile += %w( jquery-ui/* )
---
+++ core/config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( logo/solidus_logo.png noimage/*.png )
