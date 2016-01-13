31023294ac87e581a478ef2c28f49bcabf402f3c
John Hawthorn <john@stembolt.com>
2016-01-13 14:59:34 -0800

    country_factory: Build attributes using carmen
    Previously we would always build countries which were basically the USA,
    but had whatever attributes were specified different. This PR changes
    the country factory to build attributes based on an ISO code provided.

--- core/lib/spree/testing_support/factories/country_factory.rb
+++ core/lib/spree/testing_support/factories/country_factory.rb
@@ -1,9 +1,19 @@
+require 'carmen'
+
 FactoryGirl.define do
   factory :country, class: Spree::Country do
-    iso_name 'UNITED STATES'
-    name 'United States of America'
     iso 'US'
-    iso3 'USA'
-    numcode 840
+
+    transient do
+      carmen_country { Carmen::Country.coded(iso) || fail("Unknown country iso code: #{iso.inspect}") }
+    end
+
+    iso_name { carmen_country.name.upcase }
+    name { carmen_country.name }
+    iso3 { carmen_country.alpha_3_code }
+    numcode { carmen_country.numeric_code }
+
+    # FIXME: We should set states required, but it causes failing tests
+    # states_required { carmen_country.subregions? }
   end
 end
