d03b46cc29f59b5f9b17fba958eea4b5096a5983
Jordan Brough <git.j@brgh.net>
2016-01-20 05:24:43 -0700

    Allow newer versions of mysql2 gem
    See PR 359. The previous issued was fixed in Rails 4.2.5:
    https://github.com/rails/rails/commit/5da5e37#diff-e7dead35794529e0cc5d0b2d788f8235L3
    which was released on 2015-11-12.

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -4,9 +4,7 @@
 source 'https://rubygems.org'
 
 platforms :ruby do
-  # Version restriction because AR will not use mysql2 0.4.0
-  # This can be removed when a future version of rails is released
-  gem 'mysql2', '~> 0.3.20'
+  gem 'mysql2'
   gem 'pg'
   gem 'sqlite3'
   gem 'fast_sqlite'
