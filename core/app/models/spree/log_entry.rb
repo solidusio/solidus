bf3e2f89e250b5bb3c461943af58d19fed8428ee
John Hawthorn <john.hawthorn@gmail.com>
2016-01-21 09:58:13 -0800

    Remove after_rollback from LogEntry

--- core/app/models/spree/log_entry.rb
+++ core/app/models/spree/log_entry.rb
@@ -2,17 +2,6 @@ module Spree
   class LogEntry < Spree::Base
     belongs_to :source, polymorphic: true
 
-    # Fix for https://github.com/spree/spree/issues/1767
-    # If a payment fails, we want to make sure we keep the record of it failing
-    after_rollback :save_anyway
-
-    def save_anyway
-      log = Spree::LogEntry.new
-      log.source  = source
-      log.details = details
-      log.save!
-    end
-
     def parsed_details
       @details ||= YAML.load(details)
     end
