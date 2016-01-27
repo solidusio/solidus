055499c9574f02995880f6a35dd4a7cfa1c40fc7
John Hawthorn <john@stembolt.com>
2016-01-27 13:42:05 -0800

    Fix alignment of def with end

--- core/db/migrate/20130319064308_change_spree_return_authorization_amount_precision.rb
+++ core/db/migrate/20130319064308_change_spree_return_authorization_amount_precision.rb
@@ -1,5 +1,5 @@
 class ChangeSpreeReturnAuthorizationAmountPrecision < ActiveRecord::Migration
-   def change
+  def change
     change_column :spree_return_authorizations, :amount, :decimal, precision: 10, scale: 2, default: 0.0, null: false
   end
 end
