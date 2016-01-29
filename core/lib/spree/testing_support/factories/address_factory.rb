f949c134fa7ec0a99b78ea942164751edaea8183
Martin Meyerhoff <mamhoff@gmail.com>
2016-01-29 11:16:10 -0600

    Address factory: Provide an easy way for valid non-US Addresses
    Now, the address factory makes you states and (using the `twitter_cldr` library)
    zip codes that actually match the country.

--- core/lib/spree/testing_support/factories/address_factory.rb
+++ core/lib/spree/testing_support/factories/address_factory.rb
@@ -1,24 +1,30 @@
 require 'spree/testing_support/factories/state_factory'
 require 'spree/testing_support/factories/country_factory'
+require 'twitter_cldr'
 
 FactoryGirl.define do
   factory :address, class: Spree::Address do
+    transient do
+      # There's `Spree::Address#country_iso=`, prohibiting me from using `country_iso` here
+      country_iso_code 'US'
+    end
+
     firstname 'John'
     lastname 'Doe'
     company 'Company'
     address1 '10 Lovely Street'
     address2 'Northwest'
     city 'Herndon'
-    zipcode '35005'
+    zipcode { TwitterCldr::Shared::PostalCodes.for_territory(country_iso_code).sample.first }
     phone '555-555-0199'
     alternative_phone '555-555-0199'
 
-    state { |address| address.association(:state) }
+    state { |address| address.association(:state, country_iso: country_iso_code) }
     country do |address|
       if address.state
         address.state.country
       else
-        address.association(:country)
+        address.association(:country, iso: country_iso_code)
       end
     end
   end
--- core/spec/lib/spree/core/testing_support/factories/address_factory_spec.rb
+++ core/spec/lib/spree/core/testing_support/factories/address_factory_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe 'address factory' do
 
     it_behaves_like 'a working factory'
   end
+
+  describe 'when passing in a country iso code' do
+    subject { build(:address, country_iso_code: "RO") }
+
+    it 'creates a valid address with actually valid data' do
+      expect(subject).to be_valid
+    end
+  end
 end
