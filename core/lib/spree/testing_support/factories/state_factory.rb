434ff17b497d59dc017bff9dba2b03cb139860ce
Martin Meyerhoff <mamhoff@gmail.com>
2016-01-29 10:38:49 -0600

    Make state factory use Carmen
    Allow the state factory to use a `country_iso` flag to be passed in
    and then select the name and abbreviation as the first subregion
    from that state.

--- core/lib/spree/testing_support/factories/state_factory.rb
+++ core/lib/spree/testing_support/factories/state_factory.rb
@@ -2,13 +2,22 @@ require 'spree/testing_support/factories/country_factory'
 
 FactoryGirl.define do
   factory :state, class: Spree::State do
-    name 'Alabama'
-    abbr 'AL'
+    transient do
+      country_iso 'US'
+      carmen_subregion do
+        Carmen::Country.coded(country_iso).subregions.sort_by(&:name).first ||
+          fail("Unknown country iso code or no Country has no subregions: #{country_iso.inspect}")
+      end
+    end
+
+    abbr { carmen_subregion.code }
+    name { carmen_subregion.name }
+
     country do |country|
-      if usa = Spree::Country.find_by_numcode(840)
+      if usa = Spree::Country.find_by(iso: country_iso)
         country = usa
       else
-        country.association(:country)
+        country.association(:country, iso: country_iso)
       end
     end
   end
