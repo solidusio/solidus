60a583183fb8e4b82319565a499901cf43c3b8d1
Martin Meyerhoff <mamhoff@gmail.com>
2016-02-13 00:05:20 +0100

    Adapt line item spec to tax_address instead of tax_zone
    This really just fixes some descriptions and some spec setup.

--- core/spec/models/spree/line_item_spec.rb
+++ core/spec/models/spree/line_item_spec.rb
@@ -61,9 +61,9 @@ describe Spree::LineItem, type: :model do
       create(:tax_rate, zone: order.tax_zone, tax_category: variant.tax_category)
     end
 
-    context "when order has a tax zone" do
+    context "when order has a tax address" do
       before do
-        expect(order.tax_zone).to be_present
+        expect(order.tax_address).to be_present
       end
 
       it "creates a tax adjustment" do
@@ -73,12 +73,9 @@ describe Spree::LineItem, type: :model do
       end
     end
 
-    context "when order does not have a tax zone" do
+    context "when order does not have a tax address" do
       before do
-        order.bill_address = nil
-        order.ship_address = nil
-        order.save
-        expect(order.reload.tax_zone).to be_nil
+        expect(order).to receive(:tax_address).and_return(nil).at_least(:once)
       end
 
       it "does not create a tax adjustment" do
