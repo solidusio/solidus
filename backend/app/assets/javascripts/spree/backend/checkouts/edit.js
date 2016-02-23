e10b18dbd25d240117bb2331eb761b0dfb68cfdb
John Hawthorn <john@stembolt.com>
2016-02-23 10:12:29 -0800

    Move selecting action out of formatSelection
    We should not be performing actions inside a function intended for
    formatting results.

--- backend/app/assets/javascripts/spree/backend/checkouts/edit.js
+++ backend/app/assets/javascripts/spree/backend/checkouts/edit.js
@@ -30,31 +30,35 @@ $(document).ready(function() {
       dropdownCssClass: 'customer_search',
       formatResult: formatCustomerResult,
       formatSelection: function (customer) {
-        $('#order_email').val(customer.email);
-        $('#user_id').val(customer.id);
-        $('#guest_checkout_true').prop("checked", false);
-        $('#guest_checkout_false').prop("checked", true);
-        $('#guest_checkout_false').prop("disabled", false);
-
-        var billAddress = customer.bill_address;
-        if(billAddress) {
-          $('#order_bill_address_attributes_firstname').val(billAddress.firstname);
-          $('#order_bill_address_attributes_lastname').val(billAddress.lastname);
-          $('#order_bill_address_attributes_address1').val(billAddress.address1);
-          $('#order_bill_address_attributes_address2').val(billAddress.address2);
-          $('#order_bill_address_attributes_city').val(billAddress.city);
-          $('#order_bill_address_attributes_zipcode').val(billAddress.zipcode);
-          $('#order_bill_address_attributes_phone').val(billAddress.phone);
-
-          $('#order_bill_address_attributes_country_id').select2("val", billAddress.country_id).promise().done(function () {
-            update_state('b', function () {
-              $('#order_bill_address_attributes_state_id').select2("val", billAddress.state_id);
-            });
-          });
-        }
         return Select2.util.escapeMarkup(customer.email);
       }
     })
+
+    $("#customer_search").on("select2-selecting", function(e) {
+      var customer = e.choice;
+      $('#order_email').val(customer.email);
+      $('#user_id').val(customer.id);
+      $('#guest_checkout_true').prop("checked", false);
+      $('#guest_checkout_false').prop("checked", true);
+      $('#guest_checkout_false').prop("disabled", false);
+
+      var billAddress = customer.bill_address;
+      if (billAddress) {
+        $('#order_bill_address_attributes_firstname').val(billAddress.firstname);
+        $('#order_bill_address_attributes_lastname').val(billAddress.lastname);
+        $('#order_bill_address_attributes_address1').val(billAddress.address1);
+        $('#order_bill_address_attributes_address2').val(billAddress.address2);
+        $('#order_bill_address_attributes_city').val(billAddress.city);
+        $('#order_bill_address_attributes_zipcode').val(billAddress.zipcode);
+        $('#order_bill_address_attributes_phone').val(billAddress.phone);
+
+        $('#order_bill_address_attributes_country_id').select2("val", billAddress.country_id).promise().done(function () {
+          update_state('b', function () {
+            $('#order_bill_address_attributes_state_id').select2("val", billAddress.state_id);
+          });
+        });
+      }
+    });
   }
 
   var order_use_billing_input = $('input#order_use_billing');
