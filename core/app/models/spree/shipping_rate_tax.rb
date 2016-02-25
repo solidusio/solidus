5cea3dd98444cc072fbaa62060ddda73c9581e0c
Martin Meyerhoff <martin@stembolt.com>
2016-02-25 12:49:48 +0100

    Add absolute_amount method on shipping_rate

--- core/app/models/spree/shipping_rate_tax.rb
+++ core/app/models/spree/shipping_rate_tax.rb
@@ -2,5 +2,9 @@ module Spree
   class ShippingRateTax < ActiveRecord::Base
     belongs_to :shipping_rate, class_name: "Spree::ShippingRate"
     belongs_to :tax_rate, class_name: "Spree::TaxRate"
+
+    def absolute_amount
+      amount.abs
+    end
   end
 end
--- core/spec/models/spree/shipping_rate_tax_spec.rb
+++ core/spec/models/spree/shipping_rate_tax_spec.rb
@@ -7,5 +7,20 @@ module Spree
     it { is_expected.to respond_to(:amount) }
     it { is_expected.to respond_to(:tax_rate) }
     it { is_expected.to respond_to(:shipping_rate) }
+
+    describe 'absolute_amount' do
+      subject(:shipping_rate_tax) { described_class.new(amount: amount).absolute_amount }
+
+      context 'with a negative amount' do
+        let(:amount) { -19 }
+
+        it { is_expected.to eq(19) }
+      end
+
+      context 'with a positive amount' do
+        let(:amount) { 19 }
+        it { is_expected.to eq(19) }
+      end
+    end
   end
 end
