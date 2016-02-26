a9e05468bf10cadc1bdea089f4a2fbc77b461e10
Martin Meyerhoff <martin@stembolt.com>
2016-02-26 19:26:11 +0100

    Make TaxHelper method accept params
    While reviewing the PR that would add a ShippingRateTaxer object,
    we found that having these method implicitly rely on an `order` method
    defined in the including code is not so desirable.
    
    This commit changes the signatures of the tax helper methods so they always
    explicitly take an order object.

--- core/app/models/spree/tax/item_adjuster.rb
+++ core/app/models/spree/tax/item_adjuster.rb
@@ -12,7 +12,8 @@ module Spree
         @item = item
         @order = @item.order
         # set instance variable so `TaxRate.match` is only called when necessary
-        @rates_for_order_zone = options[:rates_for_order_zone]
+        @applicable_rates = options[:applicable_rates]
+        @order_tax_zone = options[:order_tax_zone]
       end
 
       # Deletes all existing tax adjustments and creates new adjustments for all
@@ -23,19 +24,19 @@ module Spree
       #
       # @return [Array<Spree::Adjustment>] newly created adjustments
       def adjust!
-        return unless order_tax_zone
+        return unless order_tax_zone(order)
         # Using .destroy_all to make sure callbacks fire
         item.adjustments.tax.destroy_all
 
         TaxRate.store_pre_tax_amount(item, rates_for_item)
 
-        rates_for_item.map { |rate| rate.adjust(order_tax_zone, item) }
+        rates_for_item.map { |rate| rate.adjust(order_tax_zone(order), item) }
       end
 
       private
 
       def rates_for_item
-        @rates_for_item ||= applicable_rates.select { |rate| rate.tax_category == item.tax_category }
+        @rates_for_item ||= applicable_rates(order).select { |rate| rate.tax_category == item.tax_category }
       end
     end
   end
--- core/app/models/spree/tax/order_adjuster.rb
+++ core/app/models/spree/tax/order_adjuster.rb
@@ -14,10 +14,10 @@ module Spree
       # Creates tax adjustments for all taxable items (shipments and line items)
       # in the given order.
       def adjust!
-        return unless order_tax_zone
+        return unless order_tax_zone(order)
 
         (order.line_items + order.shipments).each do |item|
-          ItemAdjuster.new(item, rates_for_order_zone: applicable_rates).adjust!
+          ItemAdjuster.new(item, rates_for_order_zone: applicable_rates(order)).adjust!
         end
       end
     end
