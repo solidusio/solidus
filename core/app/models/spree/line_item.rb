23d93eff414b0dc65832e8f6daf0e43c6b1d48cc
John Hawthorn <john@stembolt.com>
2016-02-29 11:25:19 -0800

    Order always has method #currency. don't use try

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -102,7 +102,7 @@ module Spree
 
       opts = options.dup # we will be deleting from the hash, so leave the caller's copy intact
 
-      currency = opts.delete(:currency) || order.try(:currency)
+      currency = opts.delete(:currency) || order.currency
 
       self.currency = currency
       self.price    = variant.price_in(currency).amount +
