e22eb6675d883fe103e9c5c636fa9dc1b11b2914
yeonhoyoon <yeonhoyoon@gmail.com>
2016-03-02 15:27:18 +0900

    Get available calculators from configuration.
    Currently, admin page pulls the list of available calculators from calculators that are already present in the shipping methods in the database. If you delete all the shipping methods in the database, you cannot select calculator when trying to add a new shipping method.

--- backend/app/controllers/spree/admin/shipping_methods_controller.rb
+++ backend/app/controllers/spree/admin/shipping_methods_controller.rb
@@ -39,7 +39,7 @@ module Spree
       def load_data
         @available_zones = Zone.order(:name)
         @tax_categories = Spree::TaxCategory.order(:name)
-        @calculators = ShippingMethod.calculators.sort_by(&:name)
+        @calculators = Rails.application.config.spree.calculators.shipping_methods
       end
     end
   end
