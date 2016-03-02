de36aad6914d4a7b8825677ceab1e508518f9ecb
John Hawthorn <john@stembolt.com>
2016-03-02 14:21:01 -0800

    Remove references to defunct .edit-item
    This was removed with the split between cart and shipment admin areas.

--- backend/app/assets/javascripts/spree/backend/shipments.js
+++ backend/app/assets/javascripts/spree/backend/shipments.js
@@ -24,9 +24,6 @@ $(document).ready(function () {
 
   });
 
-  //handle edit click
-  $('a.edit-item').click(toggleItemEdit);
-
   //handle cancel click
   $('a.cancel-item').click(toggleItemEdit);
 
@@ -195,7 +192,6 @@ toggleMethodEdit = function(){
 
 toggleItemEdit = function(){
   var link = $(this);
-  link.parent().find('a.edit-item').toggle();
   link.parent().find('a.cancel-item').toggle();
   link.parent().find('a.split-item').toggle();
   link.parent().find('a.save-item').toggle();
@@ -209,7 +205,6 @@ toggleItemEdit = function(){
 startItemSplit = function(event){
   event.preventDefault();
   var link = $(this);
-  link.parent().find('a.edit-item').toggle();
   link.parent().find('a.split-item').toggle();
   link.parent().find('a.delete-item').toggle();
   var variant_id = link.data('variant-id');
@@ -313,7 +308,6 @@ cancelItemSplit = function(event) {
   var link = $(this);
   var prev_row = link.closest('tr').prev();
   link.closest('tr').remove();
-  prev_row.find('a.edit-item').toggle();
   prev_row.find('a.split-item').toggle();
   prev_row.find('a.delete-item').toggle();
 }
