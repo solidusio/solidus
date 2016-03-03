61629fd7905007cea1d4d6e487407df54feec660
John Hawthorn <john@stembolt.com>
2016-03-02 16:58:18 -0800

    Backbonify toggleMethodEdit

--- backend/app/assets/javascripts/spree/backend/shipments.js
+++ backend/app/assets/javascripts/spree/backend/shipments.js
@@ -133,14 +133,6 @@ deleteLineItem = function(line_item_id){
   });
 }
 
-toggleMethodEdit = function(){
-  var link = $(this);
-  link.parents('tbody').find('tr.edit-method').toggle();
-  link.parents('tbody').find('tr.show-method').toggle();
-
-  return false;
-}
-
 startItemSplit = function(event){
   event.preventDefault();
   var link = $(this);
@@ -274,8 +266,9 @@ var ShipmentEditView = Backbone.View.extend({
     startItemSplit.bind(e.currentTarget)(e);
   },
 
-  toggleMethodEdit: function(e){
-    toggleMethodEdit.bind(e.currentTarget)(e);
+  toggleMethodEdit: function(){
+    this.$('tr.edit-method').toggle();
+    this.$('tr.show-method').toggle();
   },
 
   cancelItemSplit: function(e){
