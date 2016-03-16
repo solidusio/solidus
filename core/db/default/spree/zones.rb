6e2676af3d763cc5c40fbea95ae6e1d223bb8055
Davor Ocelic <docelic@spinlocksolutions.com>
2016-03-16 16:36:57 +0100

    Add Croatia, Czech Republic and Greece to EU_VAT
    This change was already merged on Sep 17, 2015:
    e645b8c6d318b639bb3bbc5d507c6212565fb1a
    
    But was overwritten/lost by commit on Dec 3, 2015:
    b08f40eefe670ad07dc0019f8ba7c49d6ab4dcc4

--- core/db/default/spree/zones.rb
+++ core/db/default/spree/zones.rb
@@ -1,7 +1,7 @@
 eu_vat = Spree::Zone.find_or_create_by!(name: "EU_VAT", description: "Countries that make up the EU VAT zone.")
 north_america = Spree::Zone.find_or_create_by!(name: "North America", description: "USA + Canada")
 
-%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE).
+%w(PL FI PT RO DE FR SK HU SI IE AT ES IT BE SE LV BG GB LT CY LU MT DK NL EE HR CZ GR).
 each do |symbol|
   eu_vat.zone_members.create!(zoneable: Spree::Country.find_by!(iso: symbol))
 end
