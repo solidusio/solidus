1778b9d184466031b1d8d6c2c026ef7d53ca9618
Martin Meyerhoff <martin@stembolt.com>
2016-03-24 21:19:48 +0100

    Add YARD documentation to the conservative line item pricer

--- core/app/models/spree/line_item/pricers/conservative.rb
+++ core/app/models/spree/line_item/pricers/conservative.rb
@@ -1,7 +1,19 @@
 module Spree
   class LineItem
     module Pricers
+      # A pricer that returns a line items price if it has one, otherwise chooses a fitting
+      # one in the line item's currency.
+      # This pricer's behaviour mimicks pricing behaviour in Solidus 1.2 and earlier (Spree 2.4 and
+      # earlier). The standard pricer in Solidus 1.3 and above will choose the price depending on
+      # the country, and will not necessarily be as conservative about a line item's current price.
+      #
+      # @note If you use price modifiers, you need to use this conservative pricer.
+      #
       class Conservative < Abstract
+        # The new price of the line item.
+        #
+        # @return Spree::Money
+        #
         def price
           raise CurrencyMissing if line_item.currency.blank?
           return in_line_item_currency(line_item.price) if line_item.price
