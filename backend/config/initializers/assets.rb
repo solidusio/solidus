2fc28d464fb35964f8be587426dcea63a983f7b9
John Hawthorn <john@stembolt.com>
2016-04-07 14:20:44 -0700

    Only precompile jquery ui images
    Previously, this precompiled all of the jquery ui assets (including CSS
    and JS). We only need images, as the others will be bundled with other
    assets.

--- backend/config/initializers/assets.rb
+++ backend/config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( jquery-ui/* solidus-style-guide-logo.png )
+Rails.application.config.assets.precompile += %w( jquery-ui/*.png solidus-style-guide-logo.png )
