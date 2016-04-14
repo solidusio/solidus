dfb93def37ce32373282046a206776977c684cc3
Martin Meyerhoff <martin@stembolt.com>
2016-04-14 10:11:50 +0200

    Implement Pricer class
    The pricer class knows its related pricing options class, and gets
    default options from there.
    
    Here it also becomes apparent how the pricer's options hash has
    to always coincide with column names on the Spree::Price model.

--- core/app/models/spree/variant/pricer.rb
+++ core/app/models/spree/variant/pricer.rb
@@ -1,12 +1,21 @@
 module Spree
   class Variant
     class Pricer
+      class PriceNotFound < ActiveRecord::RecordNotFound; end
+
+      def self.pricing_options_class
+        Spree::Variant::Pricer::PricingOptions
+      end
+
       attr_reader :variant
       def initialize(variant)
         @variant = variant
       end
 
-      def price_for(options)
+      def price_for(price_options)
+        price = variant.prices.where(price_options.desired_attributes).first
+        raise PriceNotFound unless price
+        price.money
       end
     end
   end
--- core/spec/models/spree/variant/pricer_spec.rb
+++ core/spec/models/spree/variant/pricer_spec.rb
@@ -8,5 +8,43 @@ describe Spree::Variant::Pricer do
   it { is_expected.to respond_to(:variant) }
   it { is_expected.to respond_to(:price_for) }
 
-  describe "#price_for(options)"
+  describe ".pricing_options_class" do
+    it "returns the standard pricing options class" do
+      expect(described_class.pricing_options_class).to eq(Spree::Variant::Pricer::PricingOptions)
+    end
+  end
+
+  describe "#price_for(options)" do
+    let(:variant) { create(:variant) }
+
+    context "with the default currency" do
+      let(:pricing_options) { described_class.pricing_options_class.new(currency: "USD") }
+
+      it "returns the correct (default) price as a Spree::Money object" do
+        expect(subject.price_for(pricing_options)).to eq(variant.default_price.money)
+      end
+    end
+
+    context "with a different currency" do
+      let(:pricing_options) { described_class.pricing_options_class.new(currency: "EUR") }
+
+      context "when there is a price for that currency" do
+        before do
+          variant.prices.create(amount: 99.00, currency: "EUR")
+        end
+
+        it "returns the price in the correct currency as a Spree::Money object" do
+          expect(subject.price_for(pricing_options)).to eq(Spree::Money.new(99.00, currency: "EUR"))
+        end
+      end
+
+      context "when there is no price for that currency" do
+        it "errors out with a Price Not Found error" do
+          expect do
+            subject.price_for(pricing_options)
+          end.to raise_error(described_class::PriceNotFound)
+        end
+      end
+    end
+  end
 end
