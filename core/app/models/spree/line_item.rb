33cdb8b4af9aebe0528ee2baff5ea8f0e037d082
Martin Meyerhoff <martin@stembolt.com>
2016-04-20 22:43:41 +0200

    Remove Price-Modifier related code
    All of this code is deprecated and has been extracted to the
    `solidus_price_modifier` gem.

--- core/app/models/spree/line_item.rb
+++ core/app/models/spree/line_item.rb
@@ -104,18 +104,12 @@ module Spree
 
     # Sets options on the line item.
     #
-    # The option can be arbitrary attributes on the LineItem. If no price is given in the options,
-    # this will call the legacy `PriceModifier` line item pricer.
+    # The options can be arbitrary attributes on the LineItem.
     #
     # @param options [Hash] options for this line item
     def options=(options = {})
       return unless options.present?
       assign_attributes options
-
-      # There's no need to call a pricer if we've set the price directly.
-      unless options.key?(:price)
-        self.money_price = variant.price_for(pricing_options)
-      end
     end
 
     def pricing_options
--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -266,46 +266,6 @@ module Spree
       price_in(currency).try(:amount)
     end
 
-    # Calculates the sum of the specified price modifiers in the specified
-    # currency.
-    #
-    # @deprecated This is a very complex way of modifying prices, please write a pricer instead
-    # @param currency [String] (see #price)
-    # @param options (see #price_modifier_amount)
-    # @return (see #price_modifier_amount)
-    def price_modifier_amount_in(currency, options = {})
-      return 0 unless options.present?
-
-      options.keys.map { |key|
-        m = "#{key}_price_modifier_amount_in".to_sym
-        if respond_to? m
-          send(m, currency, options[key])
-        else
-          0
-        end
-      }.sum
-    end
-    deprecate :price_modifier_amount_in, deprecator: Spree::Deprecation
-
-    # Calculates the sum of the specified price modifiers.
-    #
-    # @deprecated This method does not handle currencies
-    # @param options [Hash] for specifying keys, eg: `{keys: ['key_1', 'key_2']}`
-    # @return [Fixnum] the sum
-    def price_modifier_amount(options = {})
-      return 0 unless options.present?
-
-      options.keys.map { |key|
-        m = "#{options[key]}_price_modifier_amount".to_sym
-        if respond_to? m
-          send(m, options[key])
-        else
-          0
-        end
-      }.sum
-    end
-    deprecate :price_modifier_amount, deprecator: Spree::Deprecation
-
     # Generates a friendly name and sku string.
     #
     # @return [String]
