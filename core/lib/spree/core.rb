f3de1247f7a190de07c90b84de85df98ca99a81a
John Hawthorn <john@stembolt.com>
2016-04-20 15:14:41 -0700

    Extract safe_{add,remove}_index to MigrationHelper

--- core/lib/spree/core.rb
+++ core/lib/spree/core.rb
@@ -70,6 +70,7 @@ require 'spree/core/environment/calculators'
 require 'spree/core/environment'
 require 'spree/promo/environment'
 require 'spree/migrations'
+require 'spree/migration_helpers'
 require 'spree/core/engine'
 
 require 'spree/i18n'
