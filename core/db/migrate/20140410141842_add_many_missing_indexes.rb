8d5c8ab87851795a9cacdbe66a33d32b434a5cf7
John Hawthorn <john@stembolt.com>
2016-04-20 15:15:10 -0700

    Use safe_add_index and safe_remove_index
    Some Spree stores migrating to Solidus may have backported these indexes
    or used custom names to define these indexes. No need to stop the
    migrations from running if they already exist or don't exist.

--- core/db/migrate/20140410141842_add_many_missing_indexes.rb
+++ core/db/migrate/20140410141842_add_many_missing_indexes.rb
@@ -1,17 +1,19 @@
 class AddManyMissingIndexes < ActiveRecord::Migration
+  include Spree::MigrationHelpers
+
   def change
-    add_index :spree_adjustments, [:adjustable_id, :adjustable_type]
-    add_index :spree_adjustments, :eligible
-    add_index :spree_adjustments, :order_id
-    add_index :spree_promotions, :code
-    add_index :spree_promotions, :expires_at
-    add_index :spree_states, :country_id
-    add_index :spree_stock_items, :deleted_at
-    add_index :spree_option_types, :position
-    add_index :spree_option_values, :position
-    add_index :spree_product_option_types, :option_type_id
-    add_index :spree_product_option_types, :product_id
-    add_index :spree_products_taxons, :position
-    add_index :spree_promotions, :starts_at
+    safe_add_index :spree_adjustments, [:adjustable_id, :adjustable_type]
+    safe_add_index :spree_adjustments, :eligible
+    safe_add_index :spree_adjustments, :order_id
+    safe_add_index :spree_promotions, :code
+    safe_add_index :spree_promotions, :expires_at
+    safe_add_index :spree_states, :country_id
+    safe_add_index :spree_stock_items, :deleted_at
+    safe_add_index :spree_option_types, :position
+    safe_add_index :spree_option_values, :position
+    safe_add_index :spree_product_option_types, :option_type_id
+    safe_add_index :spree_product_option_types, :product_id
+    safe_add_index :spree_products_taxons, :position
+    safe_add_index :spree_promotions, :starts_at
   end
 end
