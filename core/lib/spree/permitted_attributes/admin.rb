87c56498e075524b79fbe1f506310162776ac05e
Anthony D'Addeo <amdaddeo@gmail.com>
2016-05-02 15:02:56 -0500

    Expand PermittedAttributes with admin module
    There are multiple places where additional attributes
    are being combined with attributes defined in PermittedAttributes
    to allow updates based on other permissions. This commit
    moves that logic into a new PermittedAttributes module.
    
    This will simplify controllers that are currently tracking these
    additional attributes with class attribtues and will also
    make those definitions easier to resuse.

---
+++ core/lib/spree/permitted_attributes/admin.rb
@@ -0,0 +1,21 @@
+module Spree
+  module PermittedAttributes
+    module Admin
+      ATTRIBUTES = [
+        :line_item_attributes,
+        :order_attributes,
+        :shipment_attributes
+      ]
+
+      mattr_reader(*ATTRIBUTES)
+
+
+      @@line_item_attributes = [:price, :variant_id, :sku]
+
+      @@order_attributes = [:import, :number, :completed_at, :locked_at, :channel, :user_id, :created_at]
+
+      @@shipment_attributes = [:shipping_method, :stock_location, inventory_units: [:variant_id, :sku]]
+
+    end
+  end
+end
