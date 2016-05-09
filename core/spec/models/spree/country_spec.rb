6da186cacd5b10aabfee262352b497f71dc47600
Martin Meyerhoff <martin@stembolt.com>
2016-05-09 14:30:23 -0400

    Add reverse association Country#prices

--- core/spec/models/spree/country_spec.rb
+++ core/spec/models/spree/country_spec.rb
@@ -49,4 +49,20 @@ describe Spree::Country, type: :model do
       end
     end
   end
+
+  describe '#prices' do
+    let(:country) { create(:country) }
+    subject { country.prices }
+
+    it { is_expected.to be_a(ActiveRecord::Associations::CollectionProxy) }
+
+    context "if the country has associated prices" do
+      let!(:price_one) { create(:price) }
+      let!(:price_two) { create(:price) }
+      let!(:price_three) { create(:price) }
+      let(:country) { create(:country, prices: [price_one, price_two]) }
+
+      it { is_expected.to contain_exactly(price_one, price_two) }
+    end
+  end
 end
