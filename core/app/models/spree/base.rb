fe0e54d543371abfc98fd8c8190f2f9ab79b45dd
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2016-05-09 08:11:55 -0700

    Document display_includes

--- core/app/models/spree/base.rb
+++ core/app/models/spree/base.rb
@@ -27,6 +27,11 @@ class Spree::Base < ActiveRecord::Base
 
   self.abstract_class = true
 
+  # Provides a scope that should be included any time products
+  # are fetched with the intention of displaying to the user.
+  #
+  # Allows individual stores to include any active record scopes or joins
+  # when products are displayed.
   def self.display_includes
     where(nil)
   end
