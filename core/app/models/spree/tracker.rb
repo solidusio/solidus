6b376ebcc583144fd04fd27604665e2eb34a1520
Adam Mueller <adam.mathys@gmail.com>
2016-05-10 15:48:03 -0400

    Find current tracker based on store

--- core/app/models/spree/tracker.rb
+++ core/app/models/spree/tracker.rb
@@ -2,9 +2,19 @@ module Spree
   class Tracker < Spree::Base
     belongs_to :store
 
-    def self.current
-      tracker = where(active: true).first
-      tracker.analytics_id.present? ? tracker : nil if tracker
+    def self.current(store = nil)
+      tracker =
+        if store
+          find_by(active: true, store_id: store.id)
+        else
+          ActiveSupport::Deprecation.warn <<-EOS.squish, caller
+            Calling Spree::Tracker.current without a store is DEPRECATED.
+            Instead, please provide a Spree::Store.
+          EOS
+          find_by(active: true)
+        end
+
+      tracker.try(:analytics_id).blank? ? nil : tracker
     end
   end
 end
