11190efddc439d6d5cef8bee6ec8e0f60b38c242
Alberto Vena <kennyadsl@gmail.com>
2016-05-11 11:32:54 -0400

    refactor reimbursement sample

--- sample/db/samples/reimbursements.rb
+++ sample/db/samples/reimbursements.rb
@@ -4,9 +4,7 @@ order          = Spree::Order.last
 inventory_unit = order.inventory_units.first
 stock_location = inventory_unit.find_stock_item.stock_location
 
-return_item = Spree::ReturnItem.create(
-  inventory_unit: inventory_unit,
-)
+return_item = Spree::ReturnItem.create(inventory_unit: inventory_unit)
 
 return_item.exchange_variant = return_item.eligible_exchange_variants.last
 return_item.build_exchange_inventory_unit
