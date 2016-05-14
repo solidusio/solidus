714a19c46714c613c156b4417e675ba9fff6aaed
Alberto Vena <kennyadsl@gmail.com>
2016-05-14 00:32:58 -0400

    fix reimbursement mailer indentation

--- core/app/mailers/spree/reimbursement_mailer.rb
+++ core/app/mailers/spree/reimbursement_mailer.rb
@@ -1,11 +1,11 @@
 module Spree
   class ReimbursementMailer < BaseMailer
-      def reimbursement_email(reimbursement, resend = false)
-        @reimbursement = reimbursement.respond_to?(:id) ? reimbursement : Spree::Reimbursement.find(reimbursement)
-        store = @reimbursement.order.store
-        subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-        subject += "#{store.name} #{Spree.t('reimbursement_mailer.reimbursement_email.subject')} ##{@reimbursement.order.number}"
-        mail(to: @reimbursement.order.email, from: from_address(store), subject: subject)
-      end
+    def reimbursement_email(reimbursement, resend = false)
+      @reimbursement = reimbursement.respond_to?(:id) ? reimbursement : Spree::Reimbursement.find(reimbursement)
+      store = @reimbursement.order.store
+      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+      subject += "#{store.name} #{Spree.t('reimbursement_mailer.reimbursement_email.subject')} ##{@reimbursement.order.number}"
+      mail(to: @reimbursement.order.email, from: from_address(store), subject: subject)
+    end
   end
 end
