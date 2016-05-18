203c5dc45e918108eec29c03369eedbaa94970d2
Martin Meyerhoff <martin@stembolt.com>
2016-05-18 09:53:27 -0400

    Add comments to PriceGenerator

--- core/app/models/spree/variant/price_generator.rb
+++ core/app/models/spree/variant/price_generator.rb
@@ -1,5 +1,16 @@
 module Spree
   class Variant
+    # This class generates prices for all countries that have VAT configured.
+    # The prices will include their respective VAT rates. It will also generate an
+    # export (net) price for any country that doesn't have VAT.
+    # @example
+    #   The admin views German gross prices (Spree::Config.admin_vat_country_iso == "DE")
+    #   There is VATs configured for Germany (19%) and Finland (24%).
+    #   The price generator is run on a variant with a base (German) price of 10.00.
+    #   The Price Generator will generate:
+    #     - A price for Finland (country_id == "FI"): 10.42
+    #     - A price for export (country_id == nil): 8.40
+    #
     class PriceGenerator
       attr_reader :variant
 
@@ -9,7 +20,7 @@ module Spree
 
       def run
         country_isos_requiring_price.each do |country_iso|
-          # No double prices
+          # If there is a price for this country/variant combination already, don't create a new one
           next if variant.prices.map(&:country_iso).include?(country_iso)
           # Also don't re-create the default price
           next if variant.default_price && variant.default_price.country_iso == country_iso
@@ -24,6 +35,7 @@ module Spree
 
       private
 
+      # nil is added to the array so we always have an export price.
       def country_isos_requiring_price
         return [nil] unless variant.tax_category
         [nil] + variant_vat_rates.map(&:zone).flat_map(&:countries).flat_map(&:iso)
