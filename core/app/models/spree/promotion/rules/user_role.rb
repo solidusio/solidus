7f3619be80be6dc9504cd03a4e770f934d9e7c71
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-05-19 12:56:10 +0200

    Use preference instead of relation table to save user roles

--- core/app/models/spree/promotion/rules/user_role.rb
+++ core/app/models/spree/promotion/rules/user_role.rb
@@ -2,23 +2,14 @@ module Spree
   class Promotion
     module Rules
       class UserRole < PromotionRule
-        has_many :promotion_rule_roles,
-          class_name: 'Spree::PromotionRuleRole',
-          foreign_key: :promotion_rule_id,
-          dependent: :destroy
-
-        has_many :roles,
-          through: :promotion_rule_roles,
-          class_name: 'Spree::Role'
-
-        accepts_nested_attributes_for :roles
+        preference :role_ids, :array, default: []
 
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
         end
 
         def eligible?(order, options = {})
-          (roles & order.user.spree_roles).any? if order.user
+          order.user.spree_roles.exists?(id: preferred_role_ids) if order.user
         end
       end
     end
