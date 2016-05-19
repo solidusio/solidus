4f459b4c92976d95ed8db8ce4f9f8e5ae6b48a26
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-05-19 16:01:17 +0200

    Add match policy to user role promotion rule

--- core/spec/models/spree/promotion/rules/user_role_spec.rb
+++ core/spec/models/spree/promotion/rules/user_role_spec.rb
@@ -8,6 +8,38 @@ describe Spree::Promotion::Rules::UserRole, type: :model do
 
   subject { rule }
 
+  shared_examples 'eligibility' do
+    context 'no roles on rule' do
+      let(:roles_for_user) { [create(:role)] }
+      it 'should not be eligible' do
+        expect(rule).to_not be_eligible(order)
+      end
+    end
+
+    context 'no roles on user' do
+      let(:roles_for_rule) { [create(:role)] }
+      it 'should not be eligible' do
+        expect(rule).to_not be_eligible(order)
+      end
+    end
+
+    context 'mismatched roles' do
+      let(:roles_for_user) { [create(:role)] }
+      let(:roles_for_rule) { [create(:role)] }
+      it 'should not be eligible' do
+        expect(rule).to_not be_eligible(order)
+      end
+    end
+
+    context 'matching all roles' do
+      let(:roles_for_user) { [create(:role), create(:role)] }
+      let(:roles_for_rule) { roles_for_user }
+      it 'should be eligible' do
+        expect(rule).to be_eligible(order)
+      end
+    end
+  end
+
   context '#eligible?(order)' do
     context 'order with no user' do
       let(:order) { Spree::Order.new }
@@ -19,42 +51,34 @@ describe Spree::Promotion::Rules::UserRole, type: :model do
 
     context 'order with user' do
       let(:order) { Spree::Order.new(user: user) }
-      context 'no roles on rule' do
-        let(:roles_for_user) { [create(:role)] }
-        it 'should not be eligible' do
-          expect(rule).to_not be_eligible(order)
+
+      context 'with any match policy' do
+        before { rule.preferred_match_policy = 'any' }
+
+        include_examples 'eligibility'
+
+        context 'one shared role' do
+          let(:shared_role) { create(:role) }
+          let(:roles_for_user) { [create(:role), shared_role] }
+          let(:roles_for_rule) { [create(:role), shared_role] }
+          it 'should be eligible' do
+            expect(rule).to be_eligible(order)
+          end
         end
       end
 
-      context 'no roles on user' do
-        let(:roles_for_rule) { [create(:role)] }
-        it 'should not be eligible' do
-          expect(rule).to_not be_eligible(order)
-        end
-      end
+      context 'with all match policy' do
+        before { rule.preferred_match_policy = 'all' }
 
-      context 'mismatched roles' do
-        let(:roles_for_user) { [create(:role)] }
-        let(:roles_for_rule) { [create(:role)] }
-        it 'should not be eligible' do
-          expect(rule).to_not be_eligible(order)
-        end
-      end
+        include_examples 'eligibility'
 
-      context 'matching roles' do
-        let(:roles_for_user) { [create(:role)] }
-        let(:roles_for_rule) { roles_for_user }
-        it 'should be eligible' do
-          expect(rule).to be_eligible(order)
-        end
-      end
-
-      context 'one shared role' do
-        let(:shared_role) { create(:role) }
-        let(:roles_for_user) { [create(:role), shared_role] }
-        let(:roles_for_rule) { [create(:role), shared_role] }
-        it 'should be eligible' do
-          expect(rule).to be_eligible(order)
+        context 'one shared role' do
+          let(:shared_role) { create(:role) }
+          let(:roles_for_user) { [create(:role), shared_role] }
+          let(:roles_for_rule) { [create(:role), shared_role] }
+          it 'should not be eligible' do
+            expect(rule).to_not be_eligible(order)
+          end
         end
       end
     end
