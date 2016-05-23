6514a3357c039435b89ceaa08243b0fcc5b06f9d
Martin Meyerhoff <martin@stembolt.com>
2016-05-23 12:30:06 +0200

    Add country fields to admin pricing interface
    This adds a country field to the form for new prices, allows searching by country,
    and adds a country column to the prices index table.
    
    The country field is disabled for persisted records, as with the currency.

--- backend/spec/features/admin/products/pricing_spec.rb
+++ backend/spec/features/admin/products/pricing_spec.rb
@@ -16,8 +16,9 @@ describe 'Pricing' do
   end
 
   context "in the prices tab" do
+    let!(:country) { create :country, iso: "DE" }
     let(:master_price) { product.master.default_price }
-    let!(:other_price) { product.master.prices.create(amount: 34.56, currency: "RUB") }
+    let!(:other_price) { product.master.prices.create(amount: 34.56, currency: "RUB", country_iso: "DE") }
 
     before do
       visit spree.admin_product_prices_path(product)
@@ -35,6 +36,8 @@ describe 'Pricing' do
         expect(page).to have_content("34.56 ₽")
         expect(page).to have_content("RUB")
         expect(page).to have_content("Master")
+        expect(page).to have_content("Any Country")
+        expect(page).to have_content("Germany")
       end
     end
 
