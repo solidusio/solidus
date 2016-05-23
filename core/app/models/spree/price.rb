7b7a81993cfed08fd2e036b3dff09386759489fa
Martin Meyerhoff <martin@stembolt.com>
2016-05-23 12:30:06 +0200

    Add country fields to admin pricing interface
    This adds a country field to the form for new prices, allows searching by country,
    and adds a country column to the prices index table.
    
    The country field is disabled for persisted records, as with the currency.

--- core/app/models/spree/price.rb
+++ core/app/models/spree/price.rb
@@ -27,7 +27,7 @@ module Spree
     money_methods :amount, :price
     alias_method :money, :display_amount
 
-    self.whitelisted_ransackable_attributes = %w( amount variant_id currency )
+    self.whitelisted_ransackable_attributes = %w( amount variant_id currency country_iso )
 
     # An alias for #amount
     def price
