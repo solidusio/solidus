33e0d2e940d0a2018788c324762cafeed59b8c34
dangerdogz <fboivin63@gmail.com>
2016-05-24 17:07:21 -0400

    Add I18n_generic_plural constant
    Add a constant to be used as a value for the
    count: parameter in the Model.model_name.human
    pluralization context to the i18n file

--- core/lib/spree/i18n.rb
+++ core/lib/spree/i18n.rb
@@ -6,9 +6,16 @@ module Spree
     include ActionView::Helpers::TranslationHelper
   end
 
+  # This value is used as a count for the pluralization helpers related to I18n
+  # ex: Spree::Order.model_name.human(count: Spree::I18N_GENERIC_PLURAL)
+  # Related to Solidus issue #1164, this is needed to avoid problems with
+  # some pluralization calculators
+  I18N_GENERIC_PLURAL = 2.1
+
   class << self
     # Add spree namespace and delegate to Rails TranslationHelper for some nice
     # extra functionality. e.g return reasonable strings for missing translations
+
     def translate(key, options = {})
       options[:scope] = [:spree, *options[:scope]]
       TranslationHelperWrapper.new.translate(key, options)
