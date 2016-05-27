e329a5607e1bf9f77ac0bfbf4b6e7aa51593b496
John Hawthorn <john@stembolt.com>
2016-05-27 14:54:19 -0700

    Move Env::Calculators declaration into class

--- core/lib/spree/core/environment/calculators.rb
+++ core/lib/spree/core/environment/calculators.rb
@@ -6,6 +6,10 @@ module Spree
 
         add_class_set :shipping_methods
         add_class_set :tax_rates
+
+        add_class_set :promotion_actions_create_adjustments
+        add_class_set :promotion_actions_create_item_adjustments
+        add_class_set :promotion_actions_create_quantity_adjustments
       end
     end
   end
