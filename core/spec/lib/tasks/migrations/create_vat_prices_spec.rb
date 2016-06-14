b0ea829553d4d619cc3524198162704926337f3c
John Hawthorn <john.hawthorn@gmail.com>
2016-06-14 15:52:59 -0700

    Skip creating VAT prices if no default_tax

--- core/spec/lib/tasks/migrations/create_vat_prices_spec.rb
+++ core/spec/lib/tasks/migrations/create_vat_prices_spec.rb
@@ -8,13 +8,22 @@ describe 'solidus:migrations:create_vat_prices' do
       task_name: 'solidus:migrations:create_vat_prices:up',
     )
 
-    # TODO: Update to work if this is missing, and add more specs
-    let!(:zone) { create(:zone, :with_country, default_tax: true) }
+    context "without a zone" do
+      it 'skips' do
+        expect { task.invoke }.to output(
+          "Creating differentiated prices for VAT countries ... No Zone set as default_tax. Skipping.\n"
+        ).to_stdout
+      end
+    end
 
-    it 'runs' do
-      expect { task.invoke }.to output(
-        "Creating differentiated prices for VAT countries ... Success.\n"
-      ).to_stdout
+    context "with a zone" do
+      let!(:zone) { create(:zone, :with_country, default_tax: true) }
+
+      it 'runs' do
+        expect { task.invoke }.to output(
+          "Creating differentiated prices for VAT countries ... Success.\n"
+        ).to_stdout
+      end
     end
   end
 end
