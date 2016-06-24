5099ce69c4272c5170fbccb5a5c1a2df6dbbecd7
John Hawthorn <john@stembolt.com>
2016-06-24 13:05:59 -0700

    Reduce stubbing in api/orders_controller_spec

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -30,6 +30,7 @@ Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
 require 'spree/testing_support/factories'
 require 'spree/testing_support/preferences'
+require 'spree/testing_support/authorization_helpers'
 
 require 'spree/api/testing_support/caching'
 require 'spree/api/testing_support/helpers'
