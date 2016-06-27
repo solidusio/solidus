24a5bcce3ef8eb63f4de9ee90c20481110e16013
John Hawthorn <john@stembolt.com>
2016-06-27 16:33:42 -0700

    Add spec on completed_order_with_promotion
    Also ensure that the factory has the promotion attached as an
    OrderPromotion.

--- core/spec/lib/spree/core/testing_support/factories/order_factory_spec.rb
+++ core/spec/lib/spree/core/testing_support/factories/order_factory_spec.rb
@@ -194,5 +194,36 @@ RSpec.describe 'order factory' do
     let(:factory) { :completed_order_with_promotion }
 
     it_behaves_like 'a working factory'
+
+    it "has the expected attributes" do
+      order = create(factory)
+      aggregate_failures do
+        expect(order).to be_completed
+        expect(order).to be_complete
+
+        expect(order.order_promotions.count).to eq(1)
+        order_promotion = order.order_promotions[0]
+        expect(order_promotion.promotion_code.promotion).to eq order_promotion.promotion
+      end
+    end
+
+    context 'with a promotion with an action' do
+      let(:promotion) { create(:promotion, :with_line_item_adjustment) }
+      it "has the expected attributes" do
+        order = create(factory, promotion: promotion)
+        aggregate_failures do
+          expect(order).to be_completed
+          expect(order).to be_complete
+
+          expect(order.line_items[0].adjustments.count).to eq 1
+          adjustment = order.line_items[0].adjustments[0]
+          expect(adjustment).to have_attributes(
+            amount: -10,
+            eligible: true,
+            order_id: order.id
+          )
+        end
+      end
+    end
   end
 end
