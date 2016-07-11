68c4a9517183565b107a1b30e86691be6aa91880
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-07-11 13:26:03 +1200

    Moves deleted variant hint to a helper
    - Deleted variant hint code moved from view to helper
    - Changed locale symbols so that complete text for each case is at a separate reference. This causes some duplication in English, but makes fewer assumptions about sentence structure.

--- backend/app/helpers/spree/admin/stock_transfers_helper.rb
+++ backend/app/helpers/spree/admin/stock_transfers_helper.rb
@@ -20,6 +20,20 @@ module Spree
       def stock_transfer_status(stock_transfer)
         stock_transfer.closed? ? Spree.t(:closed) : Spree.t(:open)
       end
+
+      def deleted_variant_admin_hint(variant)
+        newer_variant_with_same_sku = Spree::Variant.find_by(sku: variant.sku)
+        hint_type = newer_variant_with_same_sku ? :deleted_explanation_with_replacement : :deleted_explanation
+        hint_text = I18n.t(
+          hint_type,
+          scope: [:spree, :hints, "spree/variant"],
+          date: variant.deleted_at
+        )
+        admin_hint(
+          I18n.t(:deleted, scope: [:spree, :hints, "spree/variant"]),
+          hint_text
+        )
+      end
     end
   end
 end
