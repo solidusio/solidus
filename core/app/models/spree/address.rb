7fe6670d07b2ba4e9b40768ada9fca94a398a870
Jordan Brough <git.j@brgh.net>
2016-08-03 11:28:19 -0600

    Make last names optional on Spree::Address
    For example, we're integrating with Amazon Payments right now and
    Amazon doesn't require two names, which breaks this validation. And
    it's hard to remove an ActiveRecord validation at the app level.

--- core/app/models/spree/address.rb
+++ core/app/models/spree/address.rb
@@ -5,7 +5,7 @@ module Spree
     belongs_to :country, class_name: "Spree::Country"
     belongs_to :state, class_name: "Spree::State"
 
-    validates :firstname, :lastname, :address1, :city, :country_id, presence: true
+    validates :firstname, :address1, :city, :country_id, presence: true
     validates :zipcode, presence: true, if: :require_zipcode?
     validates :phone, presence: true, if: :require_phone?
 
