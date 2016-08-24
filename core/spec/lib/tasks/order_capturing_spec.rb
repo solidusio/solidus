1df939e2165f44dd3576e47f2fcd9084626555b1
Jordan Brough <git.j@brgh.net>
2016-08-24 06:34:32 -0600

    Update spec to use line_item.inventory_units
    Using order.inventory_units bypasses `line_item.adjustments` which
    makes them outdated for further use.

--- core/spec/lib/tasks/order_capturing_spec.rb
+++ core/spec/lib/tasks/order_capturing_spec.rb
@@ -18,7 +18,7 @@ describe "order_capturing:capture_payments" do
 
   context "with a mix of canceled and shipped inventory" do
     before do
-      Spree::OrderCancellations.new(order).short_ship([order.inventory_units.first])
+      Spree::OrderCancellations.new(order).short_ship([order.line_items.first.inventory_units.first])
       order.shipping.ship_shipment(order.shipments.first)
       order.update_attributes!(payment_state: 'balance_due')
     end
