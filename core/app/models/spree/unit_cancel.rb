925dc69f1f39031f387b099a7fd690720d426466
Jordan Brough <git.j@brgh.net>
2016-08-30 16:30:32 -0600

    Alternate solution for UnitCancel adjustment creation
    Per PR feedback. Avoid changing the `has_many`/`has_one` relationship
    while still keeping in-memory objects up to date.

--- core/app/models/spree/unit_cancel.rb
+++ core/app/models/spree/unit_cancel.rb
@@ -6,17 +6,17 @@ class Spree::UnitCancel < Spree::Base
   DEFAULT_REASON = 'Cancel'
 
   belongs_to :inventory_unit
-  has_many :adjustments, as: :source, dependent: :destroy
+  has_one :adjustment, as: :source, dependent: :destroy
 
   validates :inventory_unit, presence: true
 
   # Creates necessary cancel adjustments for the line item.
   def adjust!
-    raise "Adjustment is already created" if adjustments.exists?
+    raise "Adjustment is already created" if adjustment
 
     amount = compute_amount(inventory_unit.line_item)
 
-    inventory_unit.line_item.adjustments.create!(
+    self.adjustment = inventory_unit.line_item.adjustments.create!(
       source: self,
       amount: amount,
       order: inventory_unit.order,
