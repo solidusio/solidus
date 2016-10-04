a39a9bd717729c6faad59592fe019783102444ff
Graeme Nathan <graeme@stembolt.com>
2016-10-04 16:13:01 -0700

    Remove prototype sections of spec

--- backend/spec/features/admin/products/products_spec.rb
+++ backend/spec/features/admin/products/products_spec.rb
@@ -109,80 +109,6 @@ describe "Products", type: :feature do
       end
     end
 
-    context "creating a new product from a prototype" do
-      def build_option_type_with_values(name, values)
-        ot = FactoryGirl.create(:option_type, name: name)
-        values.each do |val|
-          ot.option_values.create(name: val.downcase, presentation: val)
-        end
-        ot
-      end
-
-      let(:product_attributes) do
-        # FactoryGirl.attributes_for is un-deprecated!
-        #   https://github.com/thoughtbot/factory_girl/issues/274#issuecomment-3592054
-        FactoryGirl.attributes_for(:simple_product)
-      end
-
-      let(:prototype) do
-        size = build_option_type_with_values("size", %w(Small Medium Large))
-        FactoryGirl.create(:prototype, name: "Size", option_types: [size])
-      end
-
-      let(:option_values_hash) do
-        hash = {}
-        prototype.option_types.each do |i|
-          hash[i.id.to_s] = i.option_value_ids
-        end
-        hash
-      end
-
-      before(:each) do
-        @option_type_prototype = prototype
-        @property_prototype = create(:prototype, name: "Random")
-        @shipping_category = create(:shipping_category)
-        click_nav "Products"
-        click_link "admin_new_product"
-        within('#new_product') do
-          expect(page).to have_content("SKU")
-        end
-      end
-
-      it "should allow an admin to create a new product and variants from a prototype", js: true do
-        fill_in "product_name", with: "Baseball Cap"
-        fill_in "product_sku", with: "B100"
-        fill_in "product_price", with: "100"
-        fill_in "product_available_on", with: "2012/01/24"
-        select "Size", from: "Prototype"
-        check "Large"
-        select @shipping_category.name, from: "product_shipping_category_id"
-        click_button "Create"
-        expect(page).to have_content("successfully created!")
-        expect(Spree::Product.last.variants.length).to eq(1)
-      end
-
-      it "should not display variants when prototype does not contain option types", js: true do
-        select "Random", from: "Prototype"
-
-        fill_in "product_name", with: "Baseball Cap"
-
-        expect(page).not_to have_content("Variants")
-      end
-
-      it "should keep option values selected if validation fails", js: true do
-        fill_in "product_name", with: "Baseball Cap"
-        fill_in "product_sku", with: "B100"
-        fill_in "product_price", with: "100"
-        select "Size", from: "Prototype"
-        check "Large"
-        click_button "Create"
-        expect(page).to have_content("Shipping category can't be blank")
-        expect(field_labeled("Size")).to be_checked
-        expect(field_labeled("Large")).to be_checked
-        expect(field_labeled("Small")).not_to be_checked
-      end
-    end
-
     context "creating a new product" do
       before(:each) do
         @shipping_category = create(:shipping_category)
@@ -296,41 +222,12 @@ describe "Products", type: :feature do
     context 'updating a product', js: true do
       let(:product) { create(:product) }
 
-      let(:prototype) do
-        size = build_option_type_with_values("size", %w(Small Medium Large))
-        FactoryGirl.create(:prototype, name: "Size", option_types: [size])
-      end
-
-      before(:each) do
-        @option_type_prototype = prototype
-        @property_prototype = create(:prototype, name: "Random")
-      end
-
       it 'should parse correctly available_on' do
         visit spree.admin_product_path(product)
         fill_in "product_available_on", with: "2012/12/25"
         click_button "Update"
         expect(page).to have_content("successfully updated!")
         expect(Spree::Product.last.available_on).to eq('Tue, 25 Dec 2012 00:00:00 UTC +00:00')
-      end
-
-      it 'should add option_types when selecting a prototype' do
-        visit spree.admin_product_path(product)
-        click_link 'Product Properties'
-        expect(page).to have_content("Select From Prototype")
-        click_link "Select From Prototype"
-
-        row = find('#prototypes tr', text: 'Size')
-        row.click_link 'Select'
-
-        # The following is unfortunate.
-        # It is tough to distinguish between the different fields, so we assert
-        # that there are two rows (ensuring one has been added) and then
-        # inspect the first one.
-        expect(page).to have_css('#product_properties .product_property', count: 2)
-        within('#product_properties .product_property:nth-child(1)') do
-          expect(find('input[type=text]', match: :first).value).to eq('baseball_cap_color')
-        end
       end
     end
 
@@ -371,7 +268,6 @@ describe "Products", type: :feature do
       expect(page).to have_link('Products')
       expect(page).not_to have_link('Option Types')
       expect(page).not_to have_link('Properties')
-      expect(page).not_to have_link('Prototypes')
 
       expect(page).not_to have_link('New Product')
       expect(page).not_to have_css('a.clone')
