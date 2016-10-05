923a7262ebb91523d7b37c953c9ded8f09b6afdc
Graeme Nathan <graeme@stembolt.com>
2016-10-05 12:59:50 -0700

    Remove prototype factory

--- core/lib/spree/testing_support/factories/prototype_factory.rb
+++
@@ -1,8 +0,0 @@
-require 'spree/testing_support/factories/property_factory'
-
-FactoryGirl.define do
-  factory :prototype, class: Spree::Prototype do
-    name 'Baseball Cap'
-    properties { [create(:property)] }
-  end
-end
