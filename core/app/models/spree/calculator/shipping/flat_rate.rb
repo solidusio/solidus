fceb8fb18623c634d43d11d7ee342fbd6496fffc
Graeme Nathan <graeme@stembolt.com>
2016-10-20 13:36:57 -0700

    Use ActiveModel::Naming for Shipping::FlatRate

--- core/app/models/spree/calculator/shipping/flat_rate.rb
+++ core/app/models/spree/calculator/shipping/flat_rate.rb
@@ -6,10 +6,6 @@ module Spree
       preference :amount, :decimal, default: 0
       preference :currency, :string, default: ->{ Spree::Config[:currency] }
 
-      def self.description
-        Spree.t(:shipping_flat_rate_per_order)
-      end
-
       def compute_package(_package)
         preferred_amount
       end
--- core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
+++ core/spec/models/spree/calculator/shipping/flat_rate_spec.rb
@@ -5,6 +5,11 @@ module Spree
     describe FlatRate, type: :model do
       subject { Calculator::Shipping::FlatRate.new(preferred_amount: 4.00) }
 
+      describe ".description" do
+        subject { described_class.description }
+        it { is_expected.to eq("Flat rate") }
+      end
+
       it 'always returns the same rate' do
         expect(subject.compute(build(:stock_package_fulfilled))).to eql 4.00
       end
