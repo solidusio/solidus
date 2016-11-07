e4611c056da7a7e0287cf542fa24cca91bba091e
Peter Berkenbosch <peter@stembolt.com>
2016-11-07 13:11:21 -0800

    Update comment and fix issue on payment builder
    - Update the comment on the payment builder so it's clear that this will
      use the default source in the user's wallet.
    - Fix issue with more then 1 payment being created. Added back the
      check on payments that exist on the order with the same payment source

--- core/app/models/spree/wallet/default_payment_builder.rb
+++ core/app/models/spree/wallet/default_payment_builder.rb
@@ -1,5 +1,6 @@
 # This class is responsible for building a default payment on an order, using a
-# payment source that is already in the user's "wallet".
+# payment source that is already in the user's "wallet" and is marked
+# as being the default payment source.
 class Spree::Wallet::DefaultPaymentBuilder
   def initialize(order)
     @order = order
@@ -10,7 +11,8 @@ class Spree::Wallet::DefaultPaymentBuilder
   #
   # @return [Payment] the unsaved payment to be added, or nil if none.
   def build
-    if default = order.user.try!(:wallet).try!(:default)
+    default = order.user.try!(:wallet).try!(:default)
+    if default && order.payments.where(source_type: default.payment_source_type).none?
       Spree::Payment.new(
         payment_method: default.payment_source.payment_method,
         source: default.payment_source,
