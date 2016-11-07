0f04e1cc097e6404705e5f7f19b8100c5885dde9
Peter Berkenbosch <peter@stembolt.com>
2016-11-07 14:14:25 -0800

    fix payment source query for payment creation from default wallet source

--- core/app/models/spree/wallet/default_payment_builder.rb
+++ core/app/models/spree/wallet/default_payment_builder.rb
@@ -11,7 +11,8 @@ class Spree::Wallet::DefaultPaymentBuilder
   #
   # @return [Payment] the unsaved payment to be added, or nil if none.
   def build
-    if default = order.user.try!(:wallet).try!(:default) && order.payments.where(payment_source: default.payment_source).count ==0
+    default = order.user.try!(:wallet).try!(:default)
+    if default && order.payments.where(source_type: default.payment_source_type).count == 0
       Spree::Payment.new(
         payment_method: default.payment_source.payment_method,
         source: default.payment_source,
