932757efc394fd83dc66a2ae1057a0d2a3eb00ea
Niklas Hofer <niklas+dev@lanpartei.de>
2016-12-09 16:17:19 +0100

    Avoid break of Validator if DB column is missing

--- core/app/models/spree/validations/db_maximum_length_validator.rb
+++ core/app/models/spree/validations/db_maximum_length_validator.rb
@@ -10,7 +10,13 @@ module Spree
       end
 
       def validate(record)
-        limit = record.class.columns_hash[@field].limit
+        field = record.class.columns_hash[@field]
+        # solidus_globalize want its translated fields to be removed from the
+        # DB. Even if they are kept, they will not appear in `.columns_hash`
+        # and friends because they are added to `ignored_columns`.
+        return unless field
+
+        limit = field.limit
         value = record[@field.to_sym]
         if value && limit && value.to_s.length > limit
           record.errors.add(@field.to_sym, :too_long, count: limit)
