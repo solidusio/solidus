7b95cf570800153b0b1fa0a1a6f9651aaa78f93f
John Hawthorn <john@stembolt.com>
2016-12-09 15:49:18 -0800

    Avoid Float math in Calculator::FlexiRate

--- core/app/models/spree/calculator/flexi_rate.rb
+++ core/app/models/spree/calculator/flexi_rate.rb
@@ -2,8 +2,8 @@ require_dependency 'spree/calculator'
 
 module Spree
   class Calculator::FlexiRate < Calculator
-    preference :first_item,      :decimal, default: 0.0
-    preference :additional_item, :decimal, default: 0.0
+    preference :first_item,      :decimal, default: 0
+    preference :additional_item, :decimal, default: 0
     preference :max_items,       :integer, default: 0
     preference :currency,        :string,  default: ->{ Spree::Config[:currency] }
 
@@ -12,18 +12,13 @@ module Spree
     end
 
     def compute(object)
-      sum = 0
-      max = preferred_max_items.to_i
       items_count = object.quantity
-      items_count.times do |i|
-        if i == 0
-          sum += preferred_first_item.to_f
-        elsif ((max > 0) && (i <= (max - 1))) || (max == 0)
-          sum += preferred_additional_item.to_f
-        end
-      end
+      items_count = [items_count, preferred_max_items].min unless preferred_max_items.zero?
 
-      sum
+      return BigDecimal.new(0) if items_count == 0
+
+      additional_items_count = items_count - 1
+      preferred_first_item + preferred_additional_item * additional_items_count
     end
   end
 end
