ebae69ae2c185005e23bc5f355b85a3457ea3b40
John Hawthorn <john@stembolt.com>
2016-12-20 15:38:52 -0800

    Remove deprecated Order#by_number

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -127,13 +127,6 @@ module Spree
     class_attribute :line_item_comparison_hooks
     self.line_item_comparison_hooks = Set.new
 
-    class << self
-      def by_number(number)
-        where(number: number)
-      end
-      deprecate :by_number, deprecator: Spree::Deprecation
-    end
-
     scope :created_between, ->(start_date, end_date) { where(created_at: start_date..end_date) }
     scope :completed_between, ->(start_date, end_date) { where(completed_at: start_date..end_date) }
 
