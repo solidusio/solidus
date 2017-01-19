e28668145c67285b64d0aebcdb96c1c29293de47
John Hawthorn <john@stembolt.com>
2017-01-19 13:21:51 -0800

    Replace cache_key_for_taxons with cache
    Rails 5.0 supports generating a cache key from a scope, using a similar
    technique to what cache_key_for_taxons was using. This commit
    deprecates cache_key_for_taxons favour of using cache with a scope
    directly.
    
    Passing a scope or collection to `cache` will generate a hash based on
    the scope and MAX(updated_at) and COUNT(*). This is better than the
    existing helper because it takes the full scope into account and finds
    the max_updated_at and count values in a single query.

--- core/app/helpers/spree/store_helper.rb
+++ core/app/helpers/spree/store_helper.rb
@@ -8,6 +8,11 @@ module Spree
     end
 
     def cache_key_for_taxons
+      Spree::Deprecation.warn <<-WARN.strip_heredoc
+        cache_key_for_taxons is deprecated. Rails >= 5 has built-in support for collection cache keys.
+        Instead in your view use:
+        cache [I18n.locale, @taxons] do
+      WARN
       max_updated_at = @taxons.maximum(:updated_at).to_i
       parts = [@taxon.try(:id), max_updated_at].compact.join("-")
       "#{I18n.locale}/taxons/#{parts}"
