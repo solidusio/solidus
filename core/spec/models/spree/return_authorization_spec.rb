c1f9c906ec743464ca282c7a6bab1faca90172da
John Hawthorn <john@stembolt.com>
2017-01-25 11:03:46 -0800

    Remove expedited exchanges spec

--- core/spec/models/spree/return_authorization_spec.rb
+++ core/spec/models/spree/return_authorization_spec.rb
@@ -44,65 +44,6 @@ describe Spree::ReturnAuthorization, type: :model do
         expect(return_authorization.errors['base'].size).to eq 0
       end
     end
-
-    context "expedited exchanges are configured" do
-      let(:order)                { create(:shipped_order, line_items_count: 2) }
-      let(:exchange_return_item) { build(:exchange_return_item, inventory_unit: order.inventory_units.first) }
-      let(:return_item)          { build(:return_item, inventory_unit: order.inventory_units.last) }
-      subject                    { create(:return_authorization, order: order, return_items: [exchange_return_item, return_item]) }
-
-      before do
-        Spree::Config[:expedited_exchanges] = true
-        @pre_exchange_hooks = subject.class.pre_expedited_exchange_hooks
-      end
-
-      after do
-        subject.class.pre_expedited_exchange_hooks = @pre_exchange_hooks
-      end
-
-      context "no items to exchange" do
-        subject { create(:return_authorization, order: order) }
-
-        it "does not create a reimbursement" do
-          expect{ subject.save }.to_not change { Spree::Reimbursement.count }
-        end
-      end
-
-      context "items to exchange" do
-        it "calls pre_expedited_exchange hooks with the return items to exchange" do
-          hook = double(:as_null_object)
-          expect(hook).to receive(:call).with [exchange_return_item]
-          subject.class.pre_expedited_exchange_hooks = [hook]
-          subject.save
-        end
-
-        it "attempts to accept all return items requiring exchange" do
-          expect(exchange_return_item).to receive :attempt_accept
-          expect(return_item).not_to receive :attempt_accept
-          subject.save
-        end
-
-        it "performs an exchange reimbursement for the exchange return items" do
-          subject.save
-          reimbursement = Spree::Reimbursement.last
-          expect(reimbursement.order).to eq subject.order
-          expect(reimbursement.return_items).to eq [exchange_return_item]
-          expect(exchange_return_item.reload.exchange_shipment).to be_present
-        end
-
-        context "the reimbursement fails" do
-          before do
-            allow_any_instance_of(Spree::Reimbursement).to receive(:save) { false }
-            allow_any_instance_of(Spree::Reimbursement).to receive(:errors) { double(full_messages: "foo") }
-          end
-
-          it "puts errors on the return authorization" do
-            subject.save
-            expect(subject.errors[:base]).to include "foo"
-          end
-        end
-      end
-    end
   end
 
   describe ".before_create" do
