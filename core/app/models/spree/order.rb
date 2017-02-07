e53986b5b8ad2863391290ad9a4fa545b2cc946b
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-07 07:10:52 +0700

    Validate `Order.guest_token` is never an empty string

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -111,6 +111,7 @@ module Spree
 
     validates :email, presence: true, if: :require_email
     validates :email, email: true, allow_blank: true
+    validates :guest_token, presence: { allow_nil: true }
     validates :number, presence: true, uniqueness: { allow_blank: true }
     validates :store_id, presence: true
 
