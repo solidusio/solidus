5680d12fb3cae172d6da73b0122383ffddd7fd3c
John Hawthorn <john@stembolt.com>
2017-02-23 14:01:53 -0800

    Use Spree::UserClassHandle on credit card
    Using Spree::UserClassHandle is preferred to using Spree.user_class
    directly as it avoids issues if CreditCard is autoloaded before
    Spree.user_class is set in an initializer.

--- core/app/models/spree/credit_card.rb
+++ core/app/models/spree/credit_card.rb
@@ -3,7 +3,7 @@ module Spree
   #
   class CreditCard < Spree::Base
     belongs_to :payment_method
-    belongs_to :user, class_name: Spree.user_class, foreign_key: 'user_id'
+    belongs_to :user, class_name: Spree::UserClassHandle.new, foreign_key: 'user_id'
     belongs_to :address
     has_many :payments, as: :source
 
