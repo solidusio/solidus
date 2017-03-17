45ea284b6356fb9b0f7dad94fb203a837ab6aef3
John Hawthorn <john@stembolt.com>
2017-03-17 13:09:45 -0700

    Simplify link_to_with_icon

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -109,11 +109,14 @@ module Spree
 
       def link_to_with_icon(icon_name, text, url, options = {})
         options[:class] = (options[:class].to_s + " fa fa-#{icon_name} icon_link with-tip").strip
-        options[:class] += ' no-text' if options[:no_text]
-        options[:title] = text if options[:no_text]
-        text = options[:no_text] ? '' : content_tag(:span, text, class: 'text')
-        options.delete(:no_text)
-        link_to(text, url, options)
+        if options.delete(:no_text)
+          options[:class] += ' no-text'
+          options[:title] = text
+          contents = ''
+        else
+          contents = content_tag(:span, text, class: 'text')
+        end
+        link_to(contents, url, options)
       end
 
       def icon(icon_name)
