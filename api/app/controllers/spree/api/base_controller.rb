5edd4b6a5f0fac4b09eda614bd2eb20962fb336d
Nishio Takuya <littlecub240@gmail.com>
2017-03-18 11:57:50 +0900

    improve api/products taxons children n+1 query

--- api/app/controllers/spree/api/base_controller.rb
+++ api/app/controllers/spree/api/base_controller.rb
@@ -133,7 +133,7 @@ module Spree
       end
 
       def product_includes
-        [:option_types, :taxons, product_properties: :property, variants: variants_associations, master: variants_associations]
+        [:option_types, taxons: [:children], product_properties: :property, variants: variants_associations, master: variants_associations]
       end
 
       def order_id
