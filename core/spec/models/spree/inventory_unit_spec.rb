35b73d62cf2478df5b81cbec1d9c91ba55e49bf0
Martin Meyerhoff <martin@stembolt.com>
2017-03-19 21:32:32 +0100

    Remove order reference from inventory unit table
    We do not need this column as the same data is represented
    by the `order_id` column on the `spree_shipments` table.
    
    I needed to change some stubbing in the manifest spec.

--- core/spec/models/spree/inventory_unit_spec.rb
+++ core/spec/models/spree/inventory_unit_spec.rb
@@ -37,7 +37,6 @@ RSpec.describe Spree::InventoryUnit, type: :model do
       unit = shipment.inventory_units.first
       unit.state = 'backordered'
       unit.variant_id = stock_item.variant.id
-      unit.order_id = order.id
       unit.line_item = line_item
       unit.tap(&:save!)
     end
@@ -59,7 +58,6 @@ RSpec.describe Spree::InventoryUnit, type: :model do
     it "does not find inventory units that aren't backordered" do
       on_hand_unit = shipment.inventory_units.build
       on_hand_unit.state = 'on_hand'
-      on_hand_unit.order_id = order.id
       on_hand_unit.line_item = line_item
       on_hand_unit.variant = stock_item.variant
       on_hand_unit.save!
@@ -70,7 +68,6 @@ RSpec.describe Spree::InventoryUnit, type: :model do
     it "does not find inventory units that don't match the stock item's variant" do
       other_variant_unit = shipment.inventory_units.build
       other_variant_unit.state = 'backordered'
-      other_variant_unit.order_id = order.id
       other_variant_unit.line_item = line_item
       other_variant_unit.variant = create(:variant)
       other_variant_unit.save!
@@ -107,7 +104,6 @@ RSpec.describe Spree::InventoryUnit, type: :model do
         unit = other_shipment.inventory_units.build
         unit.state = 'backordered'
         unit.variant_id = stock_item.variant.id
-        unit.order_id = other_order.id
         unit.line_item = line_item
         unit.tap(&:save!)
       end
