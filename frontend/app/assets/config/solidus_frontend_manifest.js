fdfc528e15b417d861199c3c2eed2d8f24a5d51b
Mihalis Fthenos <mihalisfthenos@gmail.com>
2017-03-26 08:39:06 -0400

    remove jquery.validate plugin

--- frontend/app/assets/config/solidus_frontend_manifest.js
+++ frontend/app/assets/config/solidus_frontend_manifest.js
@@ -1,5 +1,3 @@
-//= link_tree ../../../vendor/assets/javascripts/jquery.validate/localization/ .js
-//
 //= link_tree ../images
 //
 //= link spree/frontend/all.js
--- frontend/app/assets/javascripts/spree/frontend.js
+++ frontend/app/assets/javascripts/spree/frontend.js
@@ -1,4 +1,3 @@
-//= require jquery.validate/jquery.validate.min
 //= require spree
 //= require spree/frontend/checkout
 //= require spree/frontend/product
