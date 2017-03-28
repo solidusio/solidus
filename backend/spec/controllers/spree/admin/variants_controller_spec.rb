e04069fa777ae4106911cc0d09a56004ebf8fa25
Brian Buchalter <bal711@gmail.com>
2017-03-28 04:11:47 -0400

    Use subject in Admin::VariantsController spec
    By using subject we can eliminate duplication in our specs.

--- backend/spec/controllers/spree/admin/variants_controller_spec.rb
+++ backend/spec/controllers/spree/admin/variants_controller_spec.rb
@@ -9,20 +9,24 @@ module Spree
         let(:product) { create(:product) }
         let!(:variant_1) { create(:variant, product: product) }
         let!(:variant_2) { create(:variant, product: product) }
+        let(:params) { { product_id: product.slug } }
 
         before { variant_2.destroy }
 
+        subject { get :index, params: params }
+
         context "deleted is not requested" do
           it "does not assign deleted variants for a requested product" do
-            get :index, params: { product_id: product.slug }
+            subject
             expect(assigns(:collection)).to include variant_1
             expect(assigns(:collection)).not_to include variant_2
           end
         end
 
         context "deleted is requested" do
+          let(:params) { { product_id: product.slug, deleted: "on" } }
           it "assigns deleted along with non-deleted variants for a requested product" do
-            get :index, params: { product_id: product.slug, deleted: "on" }
+            subject
             expect(assigns(:collection)).to include variant_1
             expect(assigns(:collection)).to include variant_2
           end
