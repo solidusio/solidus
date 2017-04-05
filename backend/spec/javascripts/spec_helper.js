d5e9457603b231dafdacbc1419c6b2b08a1ac53e
John Hawthorn <john.hawthorn@gmail.com>
2017-04-05 10:51:30 -0700

    Add teaspoon javascript test runner to backend

---
+++ backend/spec/javascripts/spec_helper.js
@@ -0,0 +1,9 @@
+//= require support/sinon
+//= require support/chai
+
+//= require_self
+//= require spree/backend
+//= require_tree ./support
+
+window.support = {};
+window.expect = chai.expect;
---
+++ backend/spec/teaspoon_env.rb
@@ -0,0 +1,14 @@
+Teaspoon.configure do |config|
+  config.mount_at = "/teaspoon"
+  config.root = Spree::Backend::Engine.root
+  config.asset_paths = ["spec/javascripts", "spec/javascripts/stylesheets"]
+  config.fixture_paths = ["spec/javascripts/fixtures"]
+
+  config.suite do |suite|
+    suite.use_framework :mocha, "2.3.3"
+    suite.matcher = "{spec/javascripts,app/assets}/**/*_spec.{js,js.coffee,coffee}"
+    suite.helper = "spec_helper"
+    suite.boot_partial = "/boot"
+    suite.expand_assets = true
+  end
+end
