14157caca3c00a29bc94a54f686f3f697dc51c4a
Jordan Brough <git.j@brgh.net>
2017-04-22 07:49:26 -0600

    Ensure roles and roles_users are unique
    - Role should be unique by name
    - RoleUser should be unique by role per user

--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -3,6 +3,8 @@ module Spree
     has_many :role_users, class_name: "Spree::RoleUser", dependent: :destroy
     has_many :users, through: :role_users
 
+    validates_uniqueness_of :name
+
     def admin?
       name == "admin"
     end
---
+++ core/db/migrate/20170422134804_add_roles_unique_constraints.rb
@@ -0,0 +1,6 @@
+class AddRolesUniqueConstraints < ActiveRecord::Migration[5.0]
+  def change
+    add_index :spree_roles, :name, unique: true
+    add_index :spree_roles_users, [:user_id, :role_id], unique: true
+  end
+end
