1a6a037ab66f074c0fa223ca00b2be6b697e9646
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-05-22 16:02:56 +0100

    Skip stock_packer factory lint, it's not a Active Record model

--- core/spec/support/lint_factories.rb
+++ core/spec/support/lint_factories.rb
@@ -1,7 +1,11 @@
 RSpec.configure do |config|
   config.before(:suite) do |example|
     DatabaseCleaner.start
-    FactoryGirl.lint
+    factories_to_lint = FactoryGirl.factories.reject do |factory|
+      factory.name =~ /^stock_packer$/
+    end
+
+    FactoryGirl.lint factories_to_lint
     DatabaseCleaner.clean
   end
 end
