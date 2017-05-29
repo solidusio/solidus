12aa79ae461626cd19df4dcce55f17606a0f5c3f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-05-29 14:27:11 +0200

    Skip customer_return_without_return_items factory linter

--- core/spec/support/lint_factories.rb
+++ core/spec/support/lint_factories.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
   config.before(:suite) do |example|
     DatabaseCleaner.start
     factories_to_lint = FactoryGirl.factories.reject do |factory|
-      factory.name =~ /^stock_packer$/
+     [:stock_packer, :customer_return_without_return_items].include?(factory.name)
     end
 
     FactoryGirl.lint factories_to_lint
