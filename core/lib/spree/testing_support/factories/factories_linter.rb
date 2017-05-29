397fe19f0b6c6ecc3c2252f3d1cfe27bbeaaf1cb
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-05-29 14:49:19 +0200

    Move factory linter into factories folder

--- core/lib/spree/testing_support/factories/factories_linter.rb
+++ core/lib/spree/testing_support/factories/factories_linter.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
   config.before(:suite) do |example|
     DatabaseCleaner.start
     factories_to_lint = FactoryGirl.factories.reject do |factory|
-     [:stock_packer, :customer_return_without_return_items].include?(factory.name)
+      [:stock_packer, :customer_return_without_return_items].include?(factory.name)
     end
 
     FactoryGirl.lint factories_to_lint
