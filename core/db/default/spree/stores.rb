41ad1d8d02dea3f1150f08288d0987c867755991
Simone Bravo <simonebravo96@gmail.com>
2017-06-05 22:40:11 +0200

    Conform store seed with migration default

--- core/db/default/spree/stores.rb
+++ core/db/default/spree/stores.rb
@@ -2,9 +2,8 @@
 unless Spree::Store.where(code: 'spree').exists?
   Spree::Store.new do |s|
     s.code              = 'spree'
-    s.name              = 'Spree Demo Site'
-    s.url               = 'demo.spreecommerce.com'
-    s.mail_from_address = 'spree@example.com'
-    s.cart_tax_country_iso = 'US'
+    s.name              = 'Sample Store'
+    s.url               = 'example.com'
+    s.mail_from_address = 'store@example.com'
   end.save!
 end
