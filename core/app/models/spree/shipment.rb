49dea5215421460021b91c4d65ed31029ba366fb
John Hawthorn <john@stembolt.com>
2017-06-13 15:28:00 -0700

    Deprecate Shipment#add_shipping_method
    This was a convenience method which was used in specs to add a shipping
    rate with a given shipping method. However it isn't suitable for use
    outside of specs it doesn't handle having an existing selected rate and
    doesn't calculate the correct cost.
    
    In specs it would be better just to create the shipping_rate instead of
    using this method, as it will be more obvious what is going on.

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -95,6 +95,7 @@ module Spree
     def add_shipping_method(shipping_method, selected = false)
       shipping_rates.create(shipping_method: shipping_method, selected: selected, cost: cost)
     end
+    deprecate :add_shipping_method, deprecator: Spree::Deprecation
 
     def after_cancel
       manifest.each { |item| manifest_restock(item) }
