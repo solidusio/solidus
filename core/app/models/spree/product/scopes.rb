ba2572d154d461ef153bd5854dbf7fbcbabdf722
John Hawthorn <john@stembolt.com>
2017-06-15 13:36:48 -0700

    Fix admin SQL issues with DISTINCT products
    MySQL 5.7+ previously errored when trying to sort products by price.
    
    ORDER BY clause is not in SELECT list,
    references column 'spree_prices.amount' which is not in SELECT list;
    this is incompatible with DISTINCT
    
    Additionally, under PostgreSQL, searching for an SKU which matches
    multiple variants of the same product and then sorting by price,
    multiple of the same Product would be returned in the search.
    
    This commit removes the Product.distinct_by_product_ids hack replaces
    the join search on products->variants with a WHERE EXISTS subquery.

--- core/app/models/spree/product/scopes.rb
+++ core/app/models/spree/product/scopes.rb
@@ -181,6 +181,13 @@ module Spree
       group("spree_products.id").joins(:taxons).where(Spree::Taxon.arel_table[:name].eq(name))
     end
 
+    def self.with_variant_sku_cont(sku)
+      sku_match = "%#{sku}%"
+      variant_table = Spree::Variant.arel_table
+      subquery = Spree::Variant.where(variant_table[:sku].matches(sku_match).and(variant_table[:product_id].eq(arel_table[:id])))
+      where(subquery.exists)
+    end
+
     def self.distinct_by_product_ids(sort_order = nil)
       sort_column = sort_order.split(" ").first
 
