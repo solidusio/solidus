8273e20d2d5103796e1c55b6b7babf8147c8164e
Daniele Palombo <daniele.palombo@gmail.com>
2017-06-23 13:00:10 +0200

    Helpers use multi store configuration

--- i18n/app/helpers/solidus_i18n/locale_helper.rb
+++ i18n/app/helpers/solidus_i18n/locale_helper.rb
@@ -1,19 +1,25 @@
 module SolidusI18n
   module LocaleHelper
-
-    def select_available_locales
-      select_tag('available_locales[]', options_for_select(
-        all_locales_options,
-        Config.available_locales
-      ), common_options)
+    def select_available_locales(store = nil)
+      select_tag('store[preferred_available_locales][]',
+                 options_for_select(
+                   all_locales_options,
+                   store.preferred_available_locales
+                 ), common_options)
     end
 
     def available_locales_options
-      Config.available_locales.map { |locale| locale_presentation(locale) }
+      current_store.preferred_available_locales.map { |locale| locale_presentation(locale) }
     end
 
     def all_locales_options
       SolidusI18n::Locale.all.map { |locale| locale_presentation(locale) }
+    end
+
+    def available_locales_presentation(store)
+      store.preferred_available_locales.map do |locale|
+        Spree.t(:'i18n.this_file_language', locale: locale)
+      end.join(', ')
     end
 
     private
