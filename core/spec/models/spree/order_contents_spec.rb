c3820f04da810a60bee3b63c4a72fa78e7f7607d
Martin Meyerhoff <martin@stembolt.com>
2017-06-29 22:27:41 +0200

    Fix OrderContents#add spec when shipment is given
    This shipment in this spec was not associated to the same order,
    which is unrealistic. Fixed by adding the necessary association.

--- core/spec/models/spree/order_contents_spec.rb
+++ core/spec/models/spree/order_contents_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Spree::OrderContents, type: :model do
     end
 
     context 'given a shipment' do
-      let!(:shipment) { create(:shipment) }
+      let!(:shipment) { create(:shipment, order: order) }
 
       it "ensure shipment calls update_amounts instead of order calling ensure_updated_shipments" do
         expect(subject.order).to_not receive(:ensure_updated_shipments)
