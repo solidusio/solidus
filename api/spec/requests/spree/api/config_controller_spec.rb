71051429cfa1e786b2323e410642a1240f807357
John Hawthorn <john@stembolt.com>
2017-06-28 17:09:41 -0700

    Convert config_controller_spec to request

--- api/spec/requests/spree/api/config_controller_spec.rb
+++ api/spec/requests/spree/api/config_controller_spec.rb
@@ -9,13 +9,13 @@ module Spree
     end
 
     it "returns Spree::Money settings" do
-      get :money
+      get '/api/config/money'
       expect(response).to be_success
       expect(json_response["symbol"]).to eq("$")
     end
 
     it "returns some configuration settings" do
-      get :show
+      get '/api/config'
       expect(response).to be_success
       expect(json_response["default_country_iso"]).to eq("US")
       expect(json_response["default_country_id"]).to eq(default_country.id)
