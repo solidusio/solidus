5df67a3109614fb263dd8593eb088544f01e7021
John Hawthorn <john@stembolt.com>
2017-06-29 12:30:44 -0700

    Convert properties_controller_spec to request spec

--- api/spec/requests/spree/api/properties_controller_spec.rb
+++ api/spec/requests/spree/api/properties_controller_spec.rb
@@ -12,65 +12,65 @@ module Spree
     end
 
     it "can see a list of all properties" do
-      get :index
+      get spree.api_properties_path
       expect(json_response["properties"].count).to eq(2)
       expect(json_response["properties"].first).to have_attributes(attributes)
     end
 
     it "can control the page size through a parameter" do
-      get :index, params: { per_page: 1 }
+      get spree.api_properties_path, params: { per_page: 1 }
       expect(json_response['properties'].count).to eq(1)
       expect(json_response['current_page']).to eq(1)
       expect(json_response['pages']).to eq(2)
     end
 
     it 'can query the results through a parameter' do
-      get :index, params: { q: { name_cont: 'ba' } }
+      get spree.api_properties_path, params: { q: { name_cont: 'ba' } }
       expect(json_response['count']).to eq(1)
       expect(json_response['properties'].first['presentation']).to eq property_2.presentation
     end
 
     it "retrieves a list of properties by id" do
-      get :index, params: { ids: [property_1.id] }
+      get spree.api_properties_path, params: { ids: [property_1.id] }
       expect(json_response["properties"].first).to have_attributes(attributes)
       expect(json_response["count"]).to eq(1)
     end
 
     it "retrieves a list of properties by ids string" do
-      get :index, params: { ids: [property_1.id, property_2.id].join(",") }
+      get spree.api_properties_path, params: { ids: [property_1.id, property_2.id].join(",") }
       expect(json_response["properties"].first).to have_attributes(attributes)
       expect(json_response["properties"][1]).to have_attributes(attributes)
       expect(json_response["count"]).to eq(2)
     end
 
     it "can see a single property" do
-      get :show, params: { id: property_1.id }
+      get spree.api_property_path(property_1.id)
       expect(json_response).to have_attributes(attributes)
     end
 
     it "can see a property by name" do
-      get :show, params: { id: property_1.name }
+      get spree.api_property_path(property_1.name)
       expect(json_response).to have_attributes(attributes)
     end
 
     it "can learn how to create a new property" do
-      get :new
+      get spree.new_api_property_path
       expect(json_response["attributes"]).to eq(attributes.map(&:to_s))
       expect(json_response["required_attributes"]).to be_empty
     end
 
     it "cannot create a new property if not an admin" do
-      post :create, params: { property: { name: "My Property 3" } }
+      post spree.api_properties_path, params: { property: { name: "My Property 3" } }
       assert_unauthorized!
     end
 
     it "cannot update a property" do
-      put :update, params: { id: property_1.name, property: { presentation: "my value 456" } }
+      put spree.api_property_path(property_1.name), params: { property: { presentation: "my value 456" } }
       assert_unauthorized!
     end
 
     it "cannot delete a property" do
-      delete :destroy, params: { id: property_1.id }
+      delete spree.api_property_path(property_1.name)
       assert_unauthorized!
       expect { property_1.reload }.not_to raise_error
     end
@@ -80,19 +80,19 @@ module Spree
 
       it "can create a new property" do
         expect(Spree::Property.count).to eq(2)
-        post :create, params: { property: { name: "My Property 3", presentation: "my value 3" } }
+        post spree.api_properties_path, params: { property: { name: "My Property 3", presentation: "my value 3" } }
         expect(json_response).to have_attributes(attributes)
         expect(response.status).to eq(201)
         expect(Spree::Property.count).to eq(3)
       end
 
       it "can update a property" do
-        put :update, params: { id: property_1.name, property: { presentation: "my value 456" } }
+        put spree.api_property_path(property_1.name), params: { property: { presentation: "my value 456" } }
         expect(response.status).to eq(200)
       end
 
       it "can delete a property" do
-        delete :destroy, params: { id: property_1.name }
+        delete spree.api_property_path(property_1.name)
         expect(response.status).to eq(204)
         expect { property_1.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
