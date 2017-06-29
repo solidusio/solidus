a1a583808508bce58aa2dbd601bfc375f14477ad
John Hawthorn <john@stembolt.com>
2017-06-29 12:28:13 -0700

    Convert stock_movements_controller to api spec

--- api/spec/requests/spree/api/stock_movements_controller_spec.rb
+++ api/spec/requests/spree/api/stock_movements_controller_spec.rb
@@ -14,24 +14,23 @@ module Spree
 
     context 'as a user' do
       it 'cannot see a list of stock movements' do
-        get :index, params: { stock_location_id: stock_location.to_param }
+        get spree.api_stock_location_stock_movements_path(stock_location)
         expect(response.status).to eq(401)
       end
 
       it 'cannot see a stock movement' do
-        get :show, params: { stock_location_id: stock_location.to_param, id: stock_movement.id }
+        get spree.api_stock_location_stock_movement_path(stock_location, stock_movement)
         expect(response.status).to eq(404)
       end
 
       it 'cannot create a stock movement' do
         params = {
-          stock_location_id: stock_location.to_param,
           stock_movement: {
             stock_item_id: stock_item.to_param
           }
         }
 
-        post :create, params: params
+        post spree.api_stock_location_stock_movements_path(stock_location), params: params
         expect(response.status).to eq(401)
       end
     end
@@ -40,14 +39,14 @@ module Spree
       sign_in_as_admin!
 
       it 'gets list of stock movements' do
-        get :index, params: { stock_location_id: stock_location.to_param }
+        get spree.api_stock_location_stock_movements_path(stock_location)
         expect(json_response['stock_movements'].first).to have_attributes(attributes)
         expect(json_response['stock_movements'].first['stock_item']['count_on_hand']).to eq 11
       end
 
       it 'can control the page size through a parameter' do
         create(:stock_movement, stock_item: stock_item)
-        get :index, params: { stock_location_id: stock_location.to_param, per_page: 1 }
+        get spree.api_stock_location_stock_movements_path(stock_location), params: { per_page: 1 }
         expect(json_response['count']).to eq(1)
         expect(json_response['current_page']).to eq(1)
         expect(json_response['pages']).to eq(2)
@@ -55,25 +54,24 @@ module Spree
 
       it 'can query the results through a paramter' do
         create(:stock_movement, :received, quantity: 10, stock_item: stock_item)
-        get :index, params: { stock_location_id: stock_location.to_param, q: { quantity_eq: '10' } }
+        get spree.api_stock_location_stock_movements_path(stock_location), params: { q: { quantity_eq: '10' } }
         expect(json_response['count']).to eq(1)
       end
 
       it 'gets a stock movement' do
-        get :show, params: { stock_location_id: stock_location.to_param, id: stock_movement.to_param }
+        get spree.api_stock_location_stock_movement_path(stock_location, stock_movement)
         expect(json_response).to have_attributes(attributes)
         expect(json_response['stock_item_id']).to eq stock_movement.stock_item_id
       end
 
       it 'can create a new stock movement' do
         params = {
-          stock_location_id: stock_location.to_param,
           stock_movement: {
             stock_item_id: stock_item.to_param
           }
         }
 
-        post :create, params: params
+        post spree.api_stock_location_stock_movements_path(stock_location), params: params
         expect(response.status).to eq(201)
         expect(json_response).to have_attributes(attributes)
       end
