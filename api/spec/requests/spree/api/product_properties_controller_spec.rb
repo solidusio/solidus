917463362a42339df2d7b01de8de575ce607e46c
John Hawthorn <john@stembolt.com>
2017-06-30 12:56:29 -0700

    Convert product_properties_controller_spec

--- api/spec/requests/spree/api/product_properties_controller_spec.rb
+++ api/spec/requests/spree/api/product_properties_controller_spec.rb
@@ -21,19 +21,19 @@ module Spree
       end
 
       it "can not see a list of product properties" do
-        get :index
+        get spree.api_product_product_properties_path(product)
         expect(response.status).to eq(404)
       end
     end
 
     it "can see a list of all product properties" do
-      get :index
+      get spree.api_product_product_properties_path(product)
       expect(json_response["product_properties"].count).to eq 2
       expect(json_response["product_properties"].first).to have_attributes(attributes)
     end
 
     it "can control the page size through a parameter" do
-      get :index, params: { per_page: 1 }
+      get spree.api_product_product_properties_path(product), params: { per_page: 1 }
       expect(json_response['product_properties'].count).to eq(1)
       expect(json_response['current_page']).to eq(1)
       expect(json_response['pages']).to eq(2)
@@ -42,34 +42,34 @@ module Spree
     it 'can query the results through a parameter' do
       Spree::ProductProperty.last.update_attribute(:value, 'loose')
       property = Spree::ProductProperty.last
-      get :index, params: { q: { value_cont: 'loose' } }
+      get spree.api_product_product_properties_path(product), params: { q: { value_cont: 'loose' } }
       expect(json_response['count']).to eq(1)
       expect(json_response['product_properties'].first['value']).to eq property.value
     end
 
     it "can see a single product_property" do
-      get :show, params: { id: property_1.property_name }
+      get spree.api_product_product_property_path(product, property_1.property_name)
       expect(json_response).to have_attributes(attributes)
     end
 
     it "can learn how to create a new product property" do
-      get :new
+      get spree.new_api_product_product_property_path(product)
       expect(json_response["attributes"]).to eq(attributes.map(&:to_s))
       expect(json_response["required_attributes"]).to be_empty
     end
 
     it "cannot create a new product property if not an admin" do
-      post :create, params: { product_property: { property_name: "My Property 3" } }
+      post spree.api_product_product_properties_path(product), params: { product_property: { property_name: "My Property 3" } }
       assert_unauthorized!
     end
 
     it "cannot update a product property" do
-      put :update, params: { id: property_1.property_name, product_property: { value: "my value 456" } }
+      put spree.api_product_product_property_path(product, property_1.property_name), params: { product_property: { value: "my value 456" } }
       assert_unauthorized!
     end
 
     it "cannot delete a product property" do
-      delete :destroy, params: { id: property_1.property_name }
+      delete spree.api_product_product_property_path(product, property_1.property_name)
       assert_unauthorized!
       expect { property_1.reload }.not_to raise_error
     end
@@ -79,34 +79,33 @@ module Spree
 
       it "can create a new product property" do
         expect do
-          post :create, params: { product_property: { property_name: "My Property 3", value: "my value 3" } }
+          post spree.api_product_product_properties_path(product), params: { product_property: { property_name: "My Property 3", value: "my value 3" } }
         end.to change(product.product_properties, :count).by(1)
         expect(json_response).to have_attributes(attributes)
         expect(response.status).to eq(201)
       end
 
       it "can update a product property" do
-        put :update, params: { id: property_1.property_name, product_property: { value: "my value 456" } }
+        put spree.api_product_product_property_path(product, property_1.property_name), params: { product_property: { value: "my value 456" } }
         expect(response.status).to eq(200)
       end
 
       it "can delete a product property" do
-        delete :destroy, params: { id: property_1.property_name }
+        delete spree.api_product_product_property_path(product, property_1.property_name)
         expect(response.status).to eq(204)
         expect { property_1.reload }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
 
     context "with product identified by id" do
-      let(:resource_scoping) { { product_id: product.id } }
       it "can see a list of all product properties" do
-        get :index
+        get spree.api_product_product_properties_path(product)
         expect(json_response["product_properties"].count).to eq 2
         expect(json_response["product_properties"].first).to have_attributes(attributes)
       end
 
       it "can see a single product_property by id" do
-        get :show, params: { id: property_1.id }
+        get spree.api_product_product_property_path(product, property_1.id)
         expect(json_response).to have_attributes(attributes)
       end
     end
