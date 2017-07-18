bb3a54847916d4996d20cd82a0ed7b1ffcafc8ee
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2017-07-18 14:49:24 -0700

    Define Spree::ReturnItem in modules
    Previously it was unclear when these were evaluated that
    Spree::ReturnItem is actually an active record class (inherited from
    Spree::Base). This makes it clear that we're actually nesting a class in
    a module in a class in a module.

--- core/app/models/spree/return_item/eligibility_validator/no_reimbursements.rb
+++ core/app/models/spree/return_item/eligibility_validator/no_reimbursements.rb
@@ -1,16 +1,21 @@
 module Spree
-  class ReturnItem::EligibilityValidator::NoReimbursements < Spree::ReturnItem::EligibilityValidator::BaseValidator
-    def eligible_for_return?
-      if @return_item.inventory_unit.return_items.reimbursed.valid.any?
-        add_error(:inventory_unit_reimbursed, Spree.t('return_item_inventory_unit_reimbursed'))
-        return false
-      else
-        return true
-      end
-    end
+  class ReturnItem < Spree::Base
+    module EligibilityValidator
+      class NoReimbursements < Spree::ReturnItem::EligibilityValidator::BaseValidator
 
-    def requires_manual_intervention?
-      @errors.present?
+        def eligible_for_return?
+          if @return_item.inventory_unit.return_items.reimbursed.valid.any?
+            add_error(:inventory_unit_reimbursed, Spree.t('return_item_inventory_unit_reimbursed'))
+            return false
+          else
+            return true
+          end
+        end
+
+        def requires_manual_intervention?
+          @errors.present?
+        end
+      end
     end
   end
 end
