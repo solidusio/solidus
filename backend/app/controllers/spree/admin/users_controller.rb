fb851688bf284ce82312ed936744b88f0f90e47e
Thomas von Deyen <thomas@vondeyen.com>
2017-07-19 15:24:26 +0200

    Show orders count on users table

--- backend/app/controllers/spree/admin/users_controller.rb
+++ backend/app/controllers/spree/admin/users_controller.rb
@@ -110,6 +110,7 @@ module Spree
         else
           @search = Spree.user_class.ransack(params[:q])
           @collection = @search.result.includes(:spree_roles)
+          @collection = @collection.includes(:spree_orders)
           @collection = @collection.page(params[:page]).per(Spree::Config[:admin_products_per_page])
         end
       end
