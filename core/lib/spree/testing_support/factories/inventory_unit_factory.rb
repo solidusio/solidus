25ffb768215290f9aceee680e917ea7d41982506
John Hawthorn <john@stembolt.com>
2017-08-01 12:15:54 -0700

    Allow passing order to inventory_unit factory

--- core/lib/spree/testing_support/factories/inventory_unit_factory.rb
+++ core/lib/spree/testing_support/factories/inventory_unit_factory.rb
@@ -5,8 +5,18 @@ require 'spree/testing_support/factories/shipment_factory'
 
 FactoryBot.define do
   factory :inventory_unit, class: 'Spree::InventoryUnit' do
+    transient do
+      order nil
+    end
+
     variant
-    line_item { build(:line_item, variant: variant) }
+    line_item do
+      if order
+        build(:line_item, variant: variant, order: order)
+      else
+        build(:line_item, variant: variant)
+      end
+    end
     state 'on_hand'
     shipment { build(:shipment, state: 'pending', order: line_item.order) }
     # return_authorization
