4d9b3ea3957ee5660ee75734a8d68f458ed2a752
swcraig <sebastian@stembolt.com>
2017-08-18 09:36:29 -0700

    Fix typo in `store_credit_category_factory_spec`

--- core/spec/lib/spree/core/testing_support/factories/store_credit_category_factory_spec.rb
+++ core/spec/lib/spree/core/testing_support/factories/store_credit_category_factory_spec.rb
@@ -4,7 +4,7 @@ require 'spree/testing_support/factories/store_credit_category_factory'
 RSpec.describe 'store credit category factory' do
   let(:factory_class) { Spree::StoreCreditCategory }
 
-  describe 'plain store credi category' do
+  describe 'plain store credit category' do
     let(:factory) { :store_credit_category }
 
     it_behaves_like 'a working factory'
