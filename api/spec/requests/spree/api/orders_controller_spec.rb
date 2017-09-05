533aea2eb7328560bb5067fd57f2470442dca1eb
John Hawthorn <john@stembolt.com>
2017-09-05 11:21:28 -0700

    Fix spec dependant on database ordering
    This spec previously created two line_items, and relied on the order
    they came back from the DB (and only testing the first). I believe the
    second line item was not intended by the original author of this spec.
    
    This commit removes the second line item and makes the spec consistent
    regardless of DB ordering.

--- api/spec/requests/spree/api/orders_controller_spec.rb
+++ api/spec/requests/spree/api/orders_controller_spec.rb
@@ -506,6 +506,7 @@ module Spree
 
       context "with a line item" do
         let(:order) { create(:order_with_line_items) }
+        let(:line_item) { order.line_items.first }
 
         it "can empty an order" do
           create(:adjustment, order: order, adjustable: order)
