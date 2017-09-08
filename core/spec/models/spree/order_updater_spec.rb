73494303607fa17188eac46012057e86a69fd6e5
Martin Meyerhoff <martin@stembolt.com>
2017-09-08 11:01:24 +0200

    Fix Order Updater spec
    This spec would start with an invalid order, which should not happen
    in an actual sitation.

--- core/spec/models/spree/order_updater_spec.rb
+++ core/spec/models/spree/order_updater_spec.rb
@@ -372,7 +372,7 @@ module Spree
     end
 
     context "updating payment state" do
-      let(:order) { Order.new }
+      let(:order) { build(:order) }
       let(:updater) { order.updater }
       before { allow(order).to receive(:refund_total).and_return(0) }
 
