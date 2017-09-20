0f1100be5c67449590b8e57d18b87782fcf1bec7
Martin Meyerhoff <martin@stembolt.com>
2017-09-20 20:22:37 +0200

    Use FFaker instead of Twitter CLDr in Address factory
    In the address factory, we were using Twitter CLDR for generating
    sample zip codes. We can just as well use FFaker, which conveniently
    comes with an address faking module for different countries.
    
    Our default address is in the USA, so we can use the FFaker::AddressUS
    module.

--- core/lib/spree/testing_support/factories/address_factory.rb
+++ core/lib/spree/testing_support/factories/address_factory.rb
@@ -1,6 +1,5 @@
 require 'spree/testing_support/factories/state_factory'
 require 'spree/testing_support/factories/country_factory'
-require 'twitter_cldr'
 
 FactoryGirl.define do
   factory :address, class: Spree::Address do
@@ -16,7 +15,7 @@ FactoryGirl.define do
     address1 '10 Lovely Street'
     address2 'Northwest'
     city 'Herndon'
-    zipcode { TwitterCldr::Shared::PostalCodes.for_territory(country_iso_code).sample.first }
+    zipcode { FFaker::AddressUS.zip_code }
     phone '555-555-0199'
     alternative_phone '555-555-0199'
 
