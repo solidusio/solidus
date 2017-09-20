728466f52c00f8b4c1f26976334e47b52db4f99e
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2017-09-20 16:18:02 -0700

    Remove monkey patching from solidus specs
    We don't to monkeypatch rspec in anymore (since its > 2009 and we've now
    updated our specs)

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -17,6 +17,7 @@ require 'spree/config'
 require 'with_model'
 
 RSpec.configure do |config|
+  config.disable_monkey_patching!
   config.color = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
