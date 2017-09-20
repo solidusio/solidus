e63a3d8dc708bbb04a1db108e49a9a09981f69fb
swcraig <sebastian@stembolt.com>
2017-09-20 13:46:27 -0700

    Remove Paperclip attachment from Spree::Taxon

--- core/app/models/spree/taxon.rb
+++ core/app/models/spree/taxon.rb
@@ -25,16 +25,6 @@ module Spree
     after_save :touch_ancestors_and_taxonomy
     after_touch :touch_ancestors_and_taxonomy
 
-    has_attached_file :icon,
-      styles: { mini: '32x32>', normal: '128x128>' },
-      default_style: :mini,
-      url: '/spree/taxons/:id/:style/:basename.:extension',
-      path: ':rails_root/public/spree/taxons/:id/:style/:basename.:extension',
-      default_url: '/assets/default_taxon.png'
-
-    validates_attachment :icon,
-      content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-
     # @note This method is meant to be overridden on a store by store basis.
     # @return [Array] filters that should be used for a taxon
     def applicable_filters
