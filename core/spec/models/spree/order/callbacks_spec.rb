eb107b618a62a57a99449c0c2393637f9021e7d6
John Hawthorn <john.hawthorn@gmail.com>
2017-10-04 14:55:07 -0700

    Better validation specs

--- core/spec/models/spree/order/callbacks_spec.rb
+++ core/spec/models/spree/order/callbacks_spec.rb
@@ -4,17 +4,6 @@ RSpec.describe Spree::Order, type: :model do
   let(:store) { create(:store) }
   let(:order) { Spree::Order.new(store: store) }
 
-  context "validations" do
-    context "email validation" do
-      # Regression test for https://github.com/spree/spree/issues/1238
-      it "o'brien@gmail.com is a valid email address" do
-        order.state = 'address'
-        order.email = "o'brien@gmail.com"
-        expect(order.error_on(:email).size).to eq(0)
-      end
-    end
-  end
-
   context "#save" do
     context "when associated with a registered user" do
       let(:user) { create(:user, email: "test@example.com") }
@@ -24,15 +13,6 @@ RSpec.describe Spree::Order, type: :model do
         order.save!
         expect(order.email).to eq(user.email)
       end
-    end
-  end
-
-  context "in the cart state" do
-    it "should not validate email address" do
-      order.state = "cart"
-      order.email = nil
-      order.save!
-      expect(order.error_on(:email).size).to eq(0)
     end
   end
 end
--- core/spec/models/spree/order/validations_spec.rb
+++ core/spec/models/spree/order/validations_spec.rb
@@ -2,13 +2,58 @@ require 'rails_helper'
 
 module Spree
   RSpec.describe Spree::Order, type: :model do
-    context "validations" do
-      # Regression test for https://github.com/spree/spree/issues/2214
-      it "does not return two error messages when email is blank" do
-        order = Spree::Order.new
-        allow(order).to receive_messages(require_email: true)
-        order.valid?
-        expect(order.errors[:email]).to eq(["can't be blank"])
+    context "email validation" do
+      let(:store) { create(:store) }
+      let(:order) { Spree::Order.create!(store: store, state: state, email: email) }
+
+      shared_examples "email validated" do
+        # Regression test for https://github.com/spree/spree/issues/1238
+        context "with quote in email" do
+          let(:email) { "o'brien@gmail.com" }
+          it "o'brien@gmail.com is a valid email address" do
+            expect(order).to be_valid
+          end
+        end
+
+        context "with blank email" do
+          let(:email) { nil }
+
+          # Regression test for https://github.com/spree/spree/issues/2214
+          it "Returns a single error message" do
+            expect(order).not_to be_valid
+            expect(order.errors[:email]).to eq(["can't be blank"])
+          end
+        end
+      end
+
+      shared_examples "email not validated" do
+        context "with blank email" do
+          let(:email) { nil }
+          it "is valid" do
+            order.email = nil
+            expect(order).to be_valid
+            expect(order.error_on(:email).size).to eq(0)
+          end
+        end
+      end
+
+      context "new record" do
+        let(:order) { Spree::Order.new(store: store) }
+        it_behaves_like "email not validated"
+      end
+
+      %w[cart address].each do |state_name|
+        context "#{state_name} state" do
+          let(:state) { state_name }
+          it_behaves_like "email not validated"
+        end
+      end
+
+      %w[payment confirm complete].each do |state_name|
+        context "#{state_name} state" do
+          let(:state) { state_name }
+          it_behaves_like "email validated"
+        end
       end
     end
   end
