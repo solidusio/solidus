f4dc9c59f54e31ca0f5f7bafa46c2b12b9fb91fe
swcraig <sebastian@stembolt.com>
2017-10-06 10:58:33 -0700

    Remove images from backend stock items controller

--- backend/app/controllers/spree/admin/stock_items_controller.rb
+++ backend/app/controllers/spree/admin/stock_items_controller.rb
@@ -44,7 +44,7 @@ module Spree
         @stock_item_stock_locations = params[:stock_location_id].present? ? @stock_locations.where(id: params[:stock_location_id]) : @stock_locations
         @variant_display_attributes = self.class.variant_display_attributes
         @variants = Spree::Config.variant_search_class.new(params[:variant_search_term], scope: variant_scope).results
-        @variants = @variants.includes(:images, stock_items: :stock_location, product: :variant_images)
+        @variants = @variants.includes(stock_items: :stock_location)
         @variants = @variants.includes(option_values: :option_type)
         @variants = @variants.order(id: :desc).page(params[:page]).per(params[:per_page] || Spree::Config[:orders_per_page])
       end
