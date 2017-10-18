38ce1a7238b60e9c515d37e0f7a889d43bff4885
John Hawthorn <john.hawthorn@gmail.com>
2017-10-18 12:52:51 -0700

    Remove versioncake
    We stopped using versioncake (which we were never really using) since
    the replacement of RABL with JBuilder. We should remove the dependency
    entirely.

--- api/lib/spree/api/engine.rb
+++ api/lib/spree/api/engine.rb
@@ -9,16 +9,6 @@ module Spree
       initializer "spree.api.environment", before: :load_config_initializers do |_app|
         Spree::Api::Config = Spree::ApiConfiguration.new
       end
-
-      initializer "spree.api.versioncake" do |_app|
-        VersionCake.setup do |config|
-          config.resources do |r|
-            r.resource %r{.*}, [], [], [1]
-          end
-          config.missing_version = 1
-          config.extraction_strategy = :http_header
-        end
-      end
     end
   end
 end
--- api/lib/spree_api.rb
+++ api/lib/spree_api.rb
@@ -1,4 +1,3 @@
 require 'spree/api'
 require 'spree/api/responders'
-require 'versioncake'
 require 'jbuilder'
