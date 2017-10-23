7fb65bd5a01faf13697e07acfe89fd74705d2ee2
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2017-10-23 07:37:34 -0700

    Require the 'spree' model when we boot up core

--- core/lib/solidus_core.rb
+++ core/lib/solidus_core.rb
@@ -1,1 +1,4 @@
+# we need to require_relative here as there is a lib/spree.rb in the root of solidus.
+# If someone is using solidus instead of individual gems, we can't simply require 'spree'
+require_relative 'spree'
 require 'spree_core'
