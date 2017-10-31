b205fb109197c1e28f9841baff76f3af80dafd07
John Hawthorn <john@stembolt.com>
2017-10-31 12:54:57 -0700

    Remove unnecessary random factory descriptions

--- core/lib/spree/testing_support/factories/zone_factory.rb
+++ core/lib/spree/testing_support/factories/zone_factory.rb
@@ -4,7 +4,6 @@ require 'spree/testing_support/factories/country_factory'
 FactoryBot.define do
   factory :global_zone, class: 'Spree::Zone' do
     name 'GlobalZone'
-    description { generate(:random_string) }
     zone_members do |proxy|
       zone = proxy.instance_eval { @instance }
       Spree::Country.all.map do |c|
@@ -15,7 +14,6 @@ FactoryBot.define do
 
   factory :zone, class: 'Spree::Zone' do
     name { generate(:random_string) }
-    description { generate(:random_string) }
 
     trait :with_country do
       countries { [create(:country)] }
