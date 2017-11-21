0e3ecda654bd3be53b709a94f40ea5bd9051d9f5
John Hawthorn <john.hawthorn@gmail.com>
2017-11-21 12:35:54 -0800

    Don't generate stock transfer tables in new stores

--- core/db/migrate/20160101010000_solidus_one_four.rb
+++ core/db/migrate/20160101010000_solidus_one_four.rb
@@ -857,29 +857,6 @@ class SolidusOneFour < ActiveRecord::Migration[5.0]
       t.index ["stock_item_id"], name: "index_spree_stock_movements_on_stock_item_id"
     end
 
-    create_table "spree_stock_transfers", force: :cascade do |t|
-      t.string "description"
-      t.integer "source_location_id"
-      t.integer "destination_location_id"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.string "number"
-      t.datetime "shipped_at"
-      t.datetime "closed_at"
-      t.string "tracking_number"
-      t.integer "created_by_id"
-      t.integer "closed_by_id"
-      t.datetime "finalized_at"
-      t.integer "finalized_by_id"
-      t.datetime "deleted_at"
-      t.index ["closed_at"], name: "index_spree_stock_transfers_on_closed_at"
-      t.index ["destination_location_id"], name: "index_spree_stock_transfers_on_destination_location_id"
-      t.index ["finalized_at"], name: "index_spree_stock_transfers_on_finalized_at"
-      t.index ["number"], name: "index_spree_stock_transfers_on_number"
-      t.index ["shipped_at"], name: "index_spree_stock_transfers_on_shipped_at"
-      t.index ["source_location_id"], name: "index_spree_stock_transfers_on_source_location_id"
-    end
-
     create_table "spree_store_credit_categories", force: :cascade do |t|
       t.string "name"
       t.datetime "created_at"
@@ -1018,18 +995,6 @@ class SolidusOneFour < ActiveRecord::Migration[5.0]
       t.index ["permalink"], name: "index_taxons_on_permalink"
       t.index ["position"], name: "index_spree_taxons_on_position"
       t.index ["taxonomy_id"], name: "index_taxons_on_taxonomy_id"
-    end
-
-    create_table "spree_transfer_items", force: :cascade do |t|
-      t.integer "variant_id", null: false
-      t.integer "stock_transfer_id", null: false
-      t.integer "expected_quantity", default: 0, null: false
-      t.integer "received_quantity", default: 0, null: false
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.datetime "deleted_at"
-      t.index ["stock_transfer_id"], name: "index_spree_transfer_items_on_stock_transfer_id"
-      t.index ["variant_id"], name: "index_spree_transfer_items_on_variant_id"
     end
 
     create_table "spree_unit_cancels", force: :cascade do |t|
