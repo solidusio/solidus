8fa3f8cdaff8e87b9042c5851375ec5ebd0f5068
John Hawthorn <john.hawthorn@gmail.com>
2017-11-21 15:55:46 -0800

    Simplify new image "inline" form
    There's no need to do ajax here, we can render the form when we load the
    page.

--- backend/app/assets/javascripts/spree/backend/images/index.js
+++ backend/app/assets/javascripts/spree/backend/images/index.js
@@ -2,16 +2,7 @@ Spree.ready(function() {
   $('#new_image_link').click(function(event) {
     event.preventDefault();
 
-    $('.no-objects-found').hide();
-
     $(this).hide();
-    Spree.ajax({
-      type: 'GET',
-      url: this.href,
-      success: function(r) {
-        $('#images').html(r);
-        $('select.select2').select2();
-      }
-    });
+    $('#new_image').show();
   });
 });
