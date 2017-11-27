227faf37524bce59ef75c66037cac8a15fbe312a
John Hawthorn <john@stembolt.com>
2017-11-27 11:19:08 -0800

    Add missing requires to frontend.rb

--- frontend/lib/spree/frontend.rb
+++ frontend/lib/spree/frontend.rb
@@ -3,8 +3,10 @@ require 'jquery-rails'
 require 'canonical-rails'
 require 'sass-rails'
 require 'font-awesome-rails'
+require 'responders'
 
-require 'spree/core'
+require 'solidus_core'
+require 'solidus_api'
 
 require 'spree/frontend/middleware/seo_assist'
 require 'spree/frontend/engine'
