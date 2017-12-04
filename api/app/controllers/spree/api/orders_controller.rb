334d3a980d5e63390dc9281fff079b9c755a4b2a
John Hawthorn <john@stembolt.com>
2017-12-04 15:26:41 -0800

    security: Only allow Importer::Order for admins
    Any user with an API key has access to the Api::Orders#create endpoint.
    This exposed some functionality from Importer::Order.import that should
    not be exposed to regular users.
    
    This commit changes Api::Order#create to only use Importer::Order.import
    on requests from admin users. For users without the :admin permission on
    orders, it will use OrderUpdateAttributes, and take the same parameters
    as the Api::Order#update endpoint.

--- api/app/controllers/spree/api/orders_controller.rb
+++ api/app/controllers/spree/api/orders_controller.rb
@@ -27,8 +27,18 @@ module Spree
 
       def create
         authorize! :create, Order
-        @order = Spree::Core::Importer::Order.import(determine_order_user, order_params)
-        respond_with(@order, default_template: :show, status: 201)
+
+        if can?(:admin, Order)
+          @order = Spree::Core::Importer::Order.import(determine_order_user, order_params)
+          respond_with(@order, default_template: :show, status: 201)
+        else
+          @order = Spree::Order.create!(user: current_api_user, store: current_store)
+          if OrderUpdateAttributes.new(@order, order_params).apply
+            respond_with(@order, default_template: :show, status: 201)
+          else
+            invalid_resource!(@order)
+          end
+        end
       end
 
       def empty
