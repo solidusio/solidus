b1a913c33b19770109074b8f64a424fe86b79b59
John Hawthorn <john.hawthorn@gmail.com>
2017-12-04 16:26:18 -0800

    Simplify build.sh for single Gemfile

--- build.sh
+++ build.sh
@@ -2,45 +2,45 @@
 
 set -e
 
-# Switching Gemfile
-set_gemfile(){
-  echo "Switching Gemfile..."
-  export BUNDLE_GEMFILE="`pwd`/Gemfile"
-}
-
 # Target postgresql. Override with: `DB=sqlite bash build.sh`
 export DB=${DB:-postgresql}
 
 # Solidus defaults
-echo "Setup Solidus defaults"
+echo "Installing Solidus test dependencies"
 bundle check || bundle update --quiet
 
 # Solidus API
-echo "**************************************"
-echo "* Setup Solidus API and running RSpec..."
-echo "**************************************"
-cd api; set_gemfile; bundle update --quiet; bundle exec rspec spec
+echo "***********************"
+echo "* Testing Solidus API *"
+echo "***********************"
+cd api
+bundle exec rspec spec
 
 # Solidus Backend
-echo "******************************************"
-echo "* Setup Solidus Backend and running RSpec..."
-echo "******************************************"
-cd ../backend; set_gemfile; bundle update --quiet; bundle exec rspec spec; bundle exec teaspoon
+echo "***************************"
+echo "* Testing Solidus Backend *"
+echo "***************************"
+cd ../backend
+bundle exec rspec spec
+bundle exec teaspoon
 
 # Solidus Core
-echo "***************************************"
-echo "* Setup Solidus Core and running RSpec..."
-echo "***************************************"
-cd ../core; set_gemfile; bundle update --quiet; bundle exec rspec spec
+echo "************************"
+echo "* Testing Solidus Core *"
+echo "************************"
+cd ../core
+bundle exec rspec spec
 
 # Solidus Frontend
-echo "*******************************************"
-echo "* Setup Solidus Frontend and running RSpec..."
-echo "*******************************************"
-cd ../frontend; set_gemfile; bundle update --quiet; bundle exec rspec spec
+echo "****************************"
+echo "* Testing Solidus Frontend *"
+echo "****************************"
+cd ../frontend
+bundle exec rspec spec
 
 # Solidus Sample
-echo "*****************************************"
-echo "* Setup Solidus Sample and running RSpec..."
-echo "*****************************************"
-cd ../sample; set_gemfile; bundle update --quiet; bundle exec rspec spec
+echo "**************************"
+echo "* Testing Solidus Sample *"
+echo "**************************"
+cd ../sample
+bundle exec rspec spec
