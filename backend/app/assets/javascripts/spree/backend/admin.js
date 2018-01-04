e62a906d3044980e410d626286d0e303f70329fa
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-01-04 18:49:30 +0100

    datepicker: move related code to dedicated file

--- backend/app/assets/javascripts/spree/backend/admin.js
+++ backend/app/assets/javascripts/spree/backend/admin.js
@@ -52,53 +52,7 @@ $.fn.radioControlsVisibilityOfElement = function(dependentElementSelector){
   });
 }
 
-var handle_date_picker_fields = function(){
-  $('.datepicker').flatpickr({
-    allowInput: true
-  });
-
-  // Handle range dates
-  if ($('.date-range-filter .datepicker-from, .date-range-filter .datepicker-to').length) {
-    // Returns a callback for flatpickr onChange event which updates the
-    // respective range extremity flatpickr instance in order to have
-    // the left range extremity lower or equal to the right range extremity.
-    // This is accomplished swapping the range extremities when they are in the
-    // wrong order.
-    var swapDates = function($other, otherInstance, compareDates) {
-      return function(selectedDates, dateStr, instance) {
-        var date = instance.parseDate(dateStr);
-        var otherDateStr = $other.val();
-        var otherDate = instance.parseDate(otherDateStr);
-
-        if (date && otherDate && compareDates(date, otherDate)) {
-          instance.setDate(otherDate);
-          otherInstance.setDate(date);
-        }
-      }
-    }
-
-    var $left = $('.date-range-filter .datepicker-from');
-    var $right = $('.date-range-filter .datepicker-to');
-    var leftInstance = $left[0]._flatpickr;
-    var rightInstance = $right[0]._flatpickr;
-    var leftSwapDates = swapDates($right, rightInstance, function(date, otherDate) {
-      return date > otherDate
-    })
-    var rightSwapDates = swapDates($left, leftInstance, function(date, otherDate) {
-      return date < otherDate
-    })
-
-    leftInstance.config.onChange.push(leftSwapDates);
-    rightInstance.config.onChange.push(rightSwapDates);
-
-    // Execute swap dates check in order to correct possible wrong order at page
-    // load
-    leftSwapDates(null, $left.val(), leftInstance);
-  }
-}
-
 Spree.ready(function(){
-  handle_date_picker_fields();
   var uniqueId = 1;
   $('.spree_add_fields').click(function() {
     var target = $(this).data("target");
