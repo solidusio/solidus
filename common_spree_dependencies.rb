85c9741bb8a7a60efa5f1ab03c6cef3c68ef5716
John Hawthorn <john.hawthorn@gmail.com>
2018-01-10 14:55:22 -0800

    Use pg ~> 0.21
    pg 1.0.0 causes activerecord to error as it tries to require "~> 0.18"

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -7,7 +7,7 @@ platforms :ruby do
   # Version restriction because AR will not use mysql2 0.4.0
   # This can be removed when a future version of rails is released
   gem 'mysql2', '~> 0.3.20'
-  gem 'pg'
+  gem 'pg', '~> 0.21'
   gem 'sqlite3'
   gem 'fast_sqlite'
 end
