c9adadee760368bd5add91b529a6b24333e2c423
John Hawthorn <john@stembolt.com>
2018-01-09 16:17:46 -0800

    Fix admin product creation specs

--- backend/spec/features/admin/products/products_spec.rb
+++ backend/spec/features/admin/products/products_spec.rb
@@ -156,10 +156,7 @@ describe "Products", type: :feature do
       before(:each) do
         @shipping_category = create(:shipping_category)
         click_nav "Products"
-        click_link "admin_new_product"
-        within('#new_product') do
-          expect(page).to have_content("SKU")
-        end
+        click_on "New Product"
       end
 
       it "should allow an admin to create a new product", js: true do
@@ -174,7 +171,7 @@ describe "Products", type: :feature do
         expect(page).to have_content("successfully updated!")
       end
 
-      it "should show validation errors", js: true do
+      it "should show validation errors", js: false do
         fill_in "product_name", with: "Baseball Cap"
         fill_in "product_sku", with: "B100"
         fill_in "product_price", with: "100"
