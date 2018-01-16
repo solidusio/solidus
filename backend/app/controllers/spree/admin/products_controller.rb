9bcca73b6d54b38a711011e619c523775dc3d3e0
John Hawthorn <john@stembolt.com>
2018-01-16 12:34:03 -0800

    Remove unused extra data in products controller

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -89,8 +89,6 @@ module Spree
       end
 
       def load_data
-        @taxons = Spree::Taxon.order(:name)
-        @option_types = Spree::OptionType.order(:name)
         @tax_categories = Spree::TaxCategory.order(:name)
         @shipping_categories = Spree::ShippingCategory.order(:name)
       end
