e70cd8a19ecdabdca9bc755ef2cbffded1aa07df
John Hawthorn <john.hawthorn@gmail.com>
2018-01-18 12:42:42 -0800

    Use SimpleCov 'rails' preset
    The "rails" helper is built into SimpleCov. It adds all the groups we
    previously had (except for views, which can't be measured using
    simplecov anyways). It also declares track_files on app/ and lib/, so it
    will show files which are never even loaded, which previously there was
    no indication of.

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -1,15 +1,6 @@
 if ENV["COVERAGE"]
-  # Run Coverage report
   require 'simplecov'
-  SimpleCov.start do
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Helpers', 'app/helpers'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
-    add_group 'Jobs', 'app/jobs'
-    add_group 'Libraries', 'lib'
-  end
+  SimpleCov.start('rails')
 end
 
 require 'rspec/core'
