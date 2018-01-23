9be3db2f2b9b3d233ecc638d48a04cfcac97402f
John Hawthorn <john.hawthorn@gmail.com>
2018-01-23 15:53:00 -0800

    Remove jquery ui's datepicker

--- backend/app/assets/javascripts/spree/backend.js
+++ backend/app/assets/javascripts/spree/backend.js
@@ -2,7 +2,6 @@
 //= require handlebars
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/datepicker
 //= require jquery-ui/sortable
 //= require jquery.sticky-kit.min
 //= require solidus_admin/select2
