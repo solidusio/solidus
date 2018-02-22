12887850ab88b5aa9b97c18812aee5f130039957
John Hawthorn <john@stembolt.com>
2018-02-22 13:28:48 -0800

    Remove foreign key from store_shipping_methods
    This didn't work under MySQL because there was a mismatch between the
    type of the tables' PK and the reference because of Rails 5.1's move to
    bigint.
    
    It would be nice to have a foreign key here, but it must work.

--- core/db/migrate/20180202222641_create_store_shipping_methods.rb
+++ core/db/migrate/20180202222641_create_store_shipping_methods.rb
@@ -1,8 +1,8 @@
 class CreateStoreShippingMethods < ActiveRecord::Migration[5.1]
   def change
     create_table :spree_store_shipping_methods do |t|
-      t.references :store, foreign_key: { to_table: "spree_stores" }
-      t.references :shipping_method, foreign_key: { to_table: "spree_shipping_methods" }
+      t.references :store, null: false
+      t.references :shipping_method, null: false
 
       t.timestamps
     end
