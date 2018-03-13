cec90403c0c0927d95855d28b478f25527d8dda3
John Hawthorn <john@stembolt.com>
2018-03-12 17:30:00 -0700

    Don't set user_return_to in LocaleController
    This no longer has any effect (we now use spree_user_return_to) and we
    should not be using it.

--- frontend/app/controllers/spree/locale_controller.rb
+++ frontend/app/controllers/spree/locale_controller.rb
@@ -3,9 +3,6 @@
 module Spree
   class LocaleController < Spree::StoreController
     def set
-      if request.referer && request.referer.starts_with?('http://' + request.host)
-        session['user_return_to'] = request.referer
-      end
       if params[:locale] && I18n.available_locales.map(&:to_s).include?(params[:locale])
         session[:locale] = I18n.locale = params[:locale]
         flash.notice = t('spree.locale_changed')
