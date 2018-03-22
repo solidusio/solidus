5b640463469e9c0a80e65a107851cdf67cdb7f65
Gianluca Rizzo <gianluca.rizzo@keypartner.it>
2018-03-22 21:50:47 +0100

    Disable backend footer profile edit link if role cannot edit users

--- backend/spec/views/spree/admin/shared/navigation_footer_spec.rb
+++ backend/spec/views/spree/admin/shared/navigation_footer_spec.rb
@@ -4,8 +4,10 @@ require 'spec_helper'
 
 describe "spree/admin/shared/_navigation_footer", type: :view do
   let(:user) { FactoryBot.build_stubbed(:admin_user) }
+  let(:ability) { Object.new.extend(CanCan::Ability) }
   before do
     allow(view).to receive(:try_spree_current_user).and_return(user)
+    allow(controller).to receive(:current_ability).and_return(ability)
   end
 
   it "has a a login-nav section" do
@@ -13,9 +15,32 @@ describe "spree/admin/shared/_navigation_footer", type: :view do
     expect(rendered).to have_selector("#login-nav")
   end
 
-  it "has a user-account-link" do
-    render
-    expect(rendered).to have_link(user.email, href: Spree::Core::Engine.routes.url_helpers.edit_admin_user_path(user))
+  context "authorized user" do
+    before do
+      ability.can :admin, user
+    end
+
+    it "has a user-account-link that links to edit_admin_user_path" do
+      render
+      expect(rendered).to have_link(user.email, href: Spree::Core::Engine.routes.url_helpers.edit_admin_user_path(user))
+    end
+
+    it "has not a user-account-link that links to admin_path" do
+      render
+      expect(rendered).to_not have_link(user.email, href: Spree::Core::Engine.routes.url_helpers.admin_path)
+    end
+  end
+
+  context "unauthorized user" do
+    it "has a user-account-link that links to admin_path" do
+      render
+      expect(rendered).to_not have_link(user.email, href: Spree::Core::Engine.routes.url_helpers.admin_path)
+    end
+
+    it "has not a user-account-link that links to edit_admin_user_path" do
+      render
+      expect(rendered).to_not have_link(user.email, href: Spree::Core::Engine.routes.url_helpers.edit_admin_user_path(user))
+    end
   end
 
   context "with a required spree_logout_path helper" do
