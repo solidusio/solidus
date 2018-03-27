9ed30e770b39b0d00561abaa3d9978976d368a34
John Hawthorn <john.hawthorn@gmail.com>
2018-03-27 13:26:54 -0700

    Stop using PaymentMethod.find_with_destroyed
    We were only using it from a helper, which is unnecessary now that the
    payment.payment_method association includes deleted payment methods.

--- backend/app/helpers/spree/admin/payments_helper.rb
+++ backend/app/helpers/spree/admin/payments_helper.rb
@@ -4,9 +4,7 @@ module Spree
   module Admin
     module PaymentsHelper
       def payment_method_name(payment)
-        # HACK: to allow us to retrieve the name of a "deleted" payment method
-        id = payment.payment_method_id
-        Spree::PaymentMethod.find_with_destroyed(id).name
+        payment.payment_method.name
       end
     end
   end
