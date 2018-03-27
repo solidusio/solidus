67307e92c161abdb248fd88618a8b6d738aa823a
John Hawthorn <john.hawthorn@gmail.com>
2018-03-27 13:28:30 -0700

    Deprecate payment_method_name helper
    No reason not to do this explicitly

--- backend/app/helpers/spree/admin/payments_helper.rb
+++ backend/app/helpers/spree/admin/payments_helper.rb
@@ -4,6 +4,7 @@ module Spree
   module Admin
     module PaymentsHelper
       def payment_method_name(payment)
+        Spree::Deprecation.warn "payment_method_name(payment) is deprecated. Instead use payment.payment_method.name"
         payment.payment_method.name
       end
     end
