593951bee69d57ae3b9d72fd372a87926da3747f
John Hawthorn <john.hawthorn@gmail.com>
2018-04-06 14:26:28 -0700

    Remove decorators config in engine
    We no longer have any

--- i18n/lib/solidus_i18n/engine.rb
+++ i18n/lib/solidus_i18n/engine.rb
@@ -5,13 +5,5 @@ module SolidusI18n
     engine_name 'solidus_i18n'
 
     config.autoload_paths += %W(#{config.root}/lib)
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare(&method(:activate).to_proc)
   end
 end
