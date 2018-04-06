51d4bb4af8529e8f76b48fd78149453439b8c892
Graeme Nathan <graeme@stembolt.com>
2018-04-06 16:59:00 -0700

    Pass creator as param when performing reimbursement

--- core/app/models/spree/reimbursement_type/store_credit.rb
+++ core/app/models/spree/reimbursement_type/store_credit.rb
@@ -4,17 +4,33 @@ class Spree::ReimbursementType::StoreCredit < Spree::ReimbursementType
   extend Spree::ReimbursementType::ReimbursementHelpers
 
   class << self
-    def reimburse(reimbursement, return_items, simulate)
+    def reimburse(reimbursement, return_items, simulate, creator: nil)
+      unless creator
+        creator = Spree.user_class.find_by(email: 'spree@example.com')
+        Spree::Deprecation.warn("Calling #reimburse on #{self} without creator is deprecated")
+      end
       unpaid_amount = return_items.sum(&:total).to_d.round(2, :down)
       payments = store_credit_payments(reimbursement)
       reimbursement_list = []
 
       # Credit each store credit that was used on the order
-      reimbursement_list, unpaid_amount = create_refunds(reimbursement, payments, unpaid_amount, simulate, reimbursement_list)
+      reimbursement_list, unpaid_amount = create_refunds(
+        reimbursement,
+        payments,
+        unpaid_amount,
+        simulate,
+        reimbursement_list
+      )
 
       # If there is any amount left to pay out to the customer, then create credit with that amount
       if unpaid_amount > 0.0
-        reimbursement_list, _unpaid_amount = create_credits(reimbursement, unpaid_amount, simulate, reimbursement_list)
+        reimbursement_list, _unpaid_amount = create_credits(
+          reimbursement,
+          unpaid_amount,
+          simulate,
+          reimbursement_list,
+          creator: creator
+        )
       end
 
       reimbursement_list
