8cfd1a2f35920760364f59cff09c51ed8f7fe20a
John Hawthorn <john.hawthorn@gmail.com>
2018-04-06 14:27:52 -0700

    Remove SolidusI18n::Locale helpers
    These will be built-in to Solidus in version 2.6.

--- i18n/lib/solidus_i18n/locale.rb
+++
@@ -1,9 +0,0 @@
-module SolidusI18n
-  class Locale
-    def self.all
-      I18n.available_locales.select do |locale|
-        I18n.t(:spree, locale: locale, fallback: false, default: nil)
-      end
-    end
-  end
-end
