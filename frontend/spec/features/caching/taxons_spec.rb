5f3a775ac1bac4e88bc166bfafac6d6e1e8bc9b4
John Hawthorn <john@stembolt.com>
2018-04-10 16:43:08 -0700

    Remove assert_written_to_cache
    This is error prone and breaks in Rails 5.2, since the key being logged
    is an array.
    
    The key we see from AS::Notifications is now an array and has additional
    hashes on the template names (I assume russian doll caching related).
    It's too hard to predict where these values come from, so we can't
    continue testing this.
    
    We should have most of the benefit and testing of this by just counting
    the number of cache writes (which we were doing already).

--- frontend/spec/features/caching/taxons_spec.rb
+++ frontend/spec/features/caching/taxons_spec.rb
@@ -9,7 +9,6 @@ describe 'taxons', type: :feature, caching: true do
   before do
     # warm up the cache
     visit spree.root_path
-    assert_written_to_cache("views/en/spree/taxonomies/#{taxonomy.id}")
 
     clear_cache_events
   end
@@ -17,7 +16,6 @@ describe 'taxons', type: :feature, caching: true do
   it "busts the cache when max_level_in_taxons_menu conf changes" do
     Spree::Config[:max_level_in_taxons_menu] = 5
     visit spree.root_path
-    assert_written_to_cache("views/en/spree/taxonomies/#{taxonomy.id}")
     expect(cache_writes.count).to eq(1)
   end
 end
