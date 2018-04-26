4454910817c01a32b8bed34c8457d29ec6a78583
James Gayfer <james@stembolt.com>
2018-04-26 10:19:23 -0700

    Add docs site

---
+++ docs_site/config.rb
@@ -0,0 +1,28 @@
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+page "/guides/*", layout: 'guide'
+page "/404.html", directory_index: false
+
+set :css_dir, "assets/stylesheets"
+set :images_dir, "assets/images"
+set :js_dir, "assets/javascripts"
+set :base_url, build? ? "https://solidus.io" : "http://localhost:4567"
+
+activate :directory_indexes
+#
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ?  "npm run production" : "npm run development",
+         source: ".tmp",
+         latency: 1
+
+configure :build do
+  # Append a hash to asset urls (make sure to use the url helpers)
+  activate :asset_hash
+
+  ignore "assets/javascripts/common.js"
+  ignore "assets/stylesheets/site"
+end
+
+set(:port, 4568)
