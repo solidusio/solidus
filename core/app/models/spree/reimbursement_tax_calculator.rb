a03949bb18728405549d653ec43b471b12f3c699
François Koessler <francois@khawa.tech>
2018-06-20 14:23:29 +0200

    Don't try setting tax on a settlement without shipment

--- core/app/models/spree/reimbursement_tax_calculator.rb
+++ core/app/models/spree/reimbursement_tax_calculator.rb
@@ -35,6 +35,7 @@ module Spree
       end
 
       def set_settlement_tax!(settlement)
+        return unless settlement.shipment
         settlement.update_attributes!({
           additional_tax_total: settlement.shipment.additional_tax_total,
           included_tax_total:   settlement.shipment.included_tax_total
