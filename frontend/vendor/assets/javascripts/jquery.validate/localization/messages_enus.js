201631d7ff7e08b1e860264f21e879423550f34f
Antonio Facciolo <antonio.facciolo@ad-notam.com>
2018-07-19 17:26:38 +0200

    add jquery validate locale

---
+++ frontend/vendor/assets/javascripts/jquery.validate/localization/messages_enus.js
@@ -0,0 +1,8 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: EN (English)
+ * Region: US (United States)
+ */
+(function ($) {
+  $.extend($.validator.messages, {});
+}(jQuery));
