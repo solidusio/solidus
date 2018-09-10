f55dfece839d0f795efbc32bf5da277645dee367
jacobherrington <jherrington.personal@gmail.com>
2018-09-10 17:43:38 -0500

    improve test code quality by moving unecessary let

--- backend/spec/controllers/spree/admin/products_controller_spec.rb
+++ backend/spec/controllers/spree/admin/products_controller_spec.rb
@@ -52,18 +52,21 @@ describe Spree::Admin::ProductsController, type: :controller do
 
   # regression test for https://github.com/spree/spree/issues/2791
   context "adding taxons to a product" do
-    let!(:product) { create(:product) }
-    let!(:first_taxon) { create(:taxon) }
-    let!(:second_taxon) { create(:taxon) }
+    let(:product) { create(:product) }
+    let(:first_taxon) { create(:taxon) }
 
     it "adds a single taxon to a product" do
       put :update, params: { id: product.to_param, product: { taxon_ids: first_taxon.id.to_s } }
       expect(flash[:success]).to eq("Product #{product.name.inspect} has been successfully updated!")
     end
 
-    it "adds multiple taxons to a product" do
-      put :update, params: { id: product.to_param, product: { taxon_ids: "#{first_taxon.id}, #{second_taxon.id}" } }
-      expect(flash[:success]).to eq("Product #{product.name.inspect} has been successfully updated!")
+    context "when there are mulitple taxons" do
+      let(:second_taxon) { create(:taxon) }
+
+      it "adds multiple taxons to a product" do
+        put :update, params: { id: product.to_param, product: { taxon_ids: "#{first_taxon.id}, #{second_taxon.id}" } }
+        expect(flash[:success]).to eq("Product #{product.name.inspect} has been successfully updated!")
+      end
     end
   end
 
