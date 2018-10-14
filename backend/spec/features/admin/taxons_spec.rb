dd447b2b1db4c58f2e2c384a3412d0124d2ebbdb
Kudryavtsev Ilya <emmetbraun@gmail.com>
2018-10-14 17:40:01 +0300

    Fix tab selection in sidebar navigation

--- backend/spec/features/admin/taxons_spec.rb
+++ backend/spec/features/admin/taxons_spec.rb
@@ -40,4 +40,39 @@ describe "Taxonomies and taxons", type: :feature do
 
     expect(page).to have_current_path %r{/admin/taxonomies/\d+/taxons/\d+/edit}
   end
+
+  context "inside sidebar menu" do
+    def only_one_selected_tab_inside?(sub_tab_selector, tab_name, tab_path)
+      within(sub_tab_selector) do
+        expect(page).to have_selector('.selected', count: 1)
+
+        within('.selected') { expect(page).to have_link(tab_name, href: tab_path) }
+      end
+    end
+
+    context "on display taxonomies page", js: true do
+      it "should be selected only one tab 'Taxonomies' in product sub tabs" do
+        visit spree.admin_taxonomies_path
+        only_one_selected_tab_inside?('[data-hook=admin_product_sub_tabs]', 'Taxonomies', spree.admin_taxonomies_path)
+      end
+    end
+
+    context "on edit taxonomy page", js: true do
+      it "should be selected only one tab 'Taxonomies' in product sub tabs" do
+        taxonomy = create :taxonomy
+
+        visit spree.edit_admin_taxonomy_path(taxonomy)
+        only_one_selected_tab_inside?('[data-hook=admin_product_sub_tabs]', 'Taxonomies', spree.admin_taxonomies_path)
+      end
+    end
+
+    context "on edit taxonomy's taxon page", js: true do
+      it "should be selected only one tab 'Taxonomies' in product sub tabs" do
+        taxonomy = create :taxonomy
+
+        visit spree.edit_admin_taxonomy_taxon_path(taxonomy, taxonomy.root.id)
+        only_one_selected_tab_inside?('[data-hook=admin_product_sub_tabs]', 'Taxonomies', spree.admin_taxonomies_path)
+      end
+    end
+  end
 end
