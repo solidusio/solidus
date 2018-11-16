a5edd5e319f1de8fe23d5583a26959f2c1f72833
jacobherrington <jacobherringtondeveloper@gmail.com>
2018-11-16 00:46:57 -0600

    Add a button to hide nav

---
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -0,0 +1,23 @@
+Spree.ready(function() {
+  var adminNavToggle = document.querySelector("#admin-nav-toggle");
+  var adminNav =  document.querySelector(".admin-nav");
+
+  function showNav() {
+    adminNavToggle.innerHTML = '<i class=\"fa fa-chevron-left fa-2x\">';
+    adminNav.style.display = "block";
+  }
+
+  function hideNav() {
+    adminNavToggle.innerHTML = '<i class=\"fa fa-chevron-right fa-2x\">';
+    adminNav.style.display = "none";
+  }
+
+  adminNavToggle.addEventListener("click", function() {
+    if (document.body.classList.contains("admin-nav-extended")) {
+      showNav();
+    } else {
+      hideNav();
+    }
+    document.body.classList.toggle("admin-nav-extended");
+  });
+});
