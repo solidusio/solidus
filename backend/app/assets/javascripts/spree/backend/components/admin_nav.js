82e5c283e034e694f68a3df59422030c8dc17542
jacobherrington <jacobherringtondeveloper@gmail.com>
2018-11-16 07:50:21 -0600

    Add transistions and simplify JS

--- backend/app/assets/javascripts/spree/backend/components/admin_nav.js
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -1,24 +1,8 @@
 Spree.ready(function() {
   var adminNavToggle = document.querySelector("#admin-nav-toggle");
-  var adminNav =  document.querySelector(".admin-nav");
-
-  function showNav() {
-    adminNavToggle.innerHTML = '<i class=\"fa fa-chevron-left fa-2x\">';
-    adminNav.style.display = "block";
-  }
-
-  function hideNav() {
-    adminNavToggle.innerHTML = '<i class=\"fa fa-chevron-right fa-2x\">';
-    adminNav.style.display = "none";
-  }
 
   if (adminNavToggle) {
     adminNavToggle.addEventListener("click", function() {
-      if (document.body.classList.contains("admin-nav-extended")) {
-        showNav();
-      } else {
-        hideNav();
-      }
       document.body.classList.toggle("admin-nav-extended");
     });
   }
