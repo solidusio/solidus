bdffc729cf5edc184da7dff6c897496961d01853
Angel Perez <iAngel.p93@gmail.com>
2018-11-20 13:32:35 -0400

    Disable 'count on hand' input when 'track inventory' option is set to false

--- backend/app/assets/javascripts/spree/backend/stock_management.js
+++ backend/app/assets/javascripts/spree/backend/stock_management.js
@@ -2,11 +2,20 @@ Spree.ready(function() {
   $('.js-edit-stock-item').each(function() {
     var $el = $(this);
     var model = new Spree.Models.StockItem($el.data('stock-item'));
+    var trackInventory = $el.data('track-inventory');
     new Spree.Views.Stock.EditStockItemRow({
       el: $el,
       stockLocationName: $el.data('stock-location-name'),
       model: model
     });
+
+    if (trackInventory === false) {
+      $('.js-edit-stock-item input').attr({
+        disabled: true,
+        class: 'with-tip',
+        title: '"Track inventory" option disabled for this variant'
+      });
+    }
   });
 
   $('.js-add-stock-item').each(function() {
