cd3dc205ad1adb62141e578abb6a857026572fff
jacobherrington <jacobherringtondeveloper@gmail.com>
2018-11-20 10:34:36 -0600

    Add a session variable to remember nav state

--- backend/app/assets/javascripts/spree/backend/components/admin_nav.js
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -1,9 +1,14 @@
 Spree.ready(function() {
+  if (JSON.parse(sessionStorage.getItem("navExtended"))) {
+    document.body.classList.add("admin-nav-hidden");
+  }
+
   var adminNavToggle = document.querySelector("#admin-nav-toggle");
 
   if (adminNavToggle) {
     adminNavToggle.addEventListener("click", function() {
-      document.body.classList.toggle("admin-nav-extended");
+      document.body.classList.toggle("admin-nav-hidden");
+      sessionStorage.setItem("navExtended", document.body.classList.contains("admin-nav-hidden"));
     });
   }
 });
