0b8553a208d1f72609b0d5b536976fd1e9fe1bd2
Dumitru Ceban <dumitru@ceban.it>
2018-12-17 16:53:22 +0100

    Refactor test for Shipment assoc. to InventoryUnit
    It was stubbed to return a plain `Array`, instead it must return a
    `ActiveRecord::Associations::CollectionProxy` object.

--- core/spec/models/spree/shipment_spec.rb
+++ core/spec/models/spree/shipment_spec.rb
@@ -267,8 +267,8 @@ RSpec.describe Spree::Shipment, type: :model do
 
         before do
           allow(line_item).to receive(:order) { order }
-          allow(shipment).to receive(:inventory_units) { inventory_units }
-          allow(inventory_units).to receive_message_chain(:includes, :joins).and_return inventory_units
+          shipment.inventory_units = inventory_units
+          allow(shipment.inventory_units).to receive_message_chain(:includes, :joins).and_return inventory_units
         end
 
         it 'should use symbols for states when adding contents to package' do
