f81bebe47954a8959bc13eae2cedb4f962eb08b6
Jared Norman <jared@super.gd>
2018-12-21 17:16:25 -0800

    Remove update_totals/persist_totals delegation
    These are private methods on OrderUpdater, so there's no need for doing
    this. It's been like like this since 2.2ish when these were made
    private, but no one removed this.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -138,7 +138,6 @@ module Spree
       find_by! number: value
     end
 
-    delegate :update_totals, :persist_totals, to: :updater
     delegate :firstname, :lastname, to: :bill_address, prefix: true, allow_nil: true
     alias_method :billing_firstname, :bill_address_firstname
     alias_method :billing_lastname, :bill_address_lastname
