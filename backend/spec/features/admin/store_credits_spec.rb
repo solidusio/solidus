d33cdd1f6a0970b5a78248a9e3dc7bcc9e83bc76
Alberto Vena <kennyadsl@gmail.com>
2019-01-06 16:06:57 +0100

    Add feature specs for admin store credit show page
    This page allows changing memo via an ajax call and this behavior
    should be tested.

--- backend/spec/features/admin/store_credits_spec.rb
+++ backend/spec/features/admin/store_credits_spec.rb
@@ -56,6 +56,55 @@ describe "Store credits admin" do
     end
   end
 
+  describe "displaying a store credit details page" do
+    before do
+      visit spree.admin_path
+      click_link "Users"
+      click_link store_credit.user.email
+      click_link "Store Credit"
+      page.find(".sc-table td.actions a.fa-edit").click
+    end
+
+    it "shows the store credit's information" do
+      within ".content" do
+        expect(page).to have_content "Credited #{store_credit.display_amount}"
+        expect(page).to have_content "Created By #{store_credit.created_by.email}"
+        expect(page).to have_content "Type #{store_credit.category_name}"
+        expect(page).to have_content 'Store credit history'
+      end
+    end
+
+    it "lets edit store credit's memo", js: true do
+      allow_any_instance_of(Spree::Admin::StoreCreditsController)
+        .to receive(:try_spree_current_user)
+        .and_return(admin_user)
+
+      # When there are no errors
+      within '.store-credit-memo-row' do
+        click_button 'Edit'
+        fill_in 'store_credit_memo', with: 'Lottery Won'
+        click_button 'Save'
+        expect(page).to have_content "Memo Lottery Won"
+      end
+      expect(page).to have_content "Store Credit has been successfully updated!"
+
+      # When there are errors
+      allow_any_instance_of(Spree::StoreCredit).to receive(:save) { false }
+      allow_any_instance_of(Spree::StoreCredit)
+        .to receive_message_chain(:errors, :full_messages)
+        .and_return(["Memo is not valid"])
+
+      within '.store-credit-memo-row' do
+        find(:css, ".edit-memo").click
+        fill_in 'store_credit_memo', with: 'Lottery Won Twice'
+        find(:css, ".save-memo").click
+        expect(page).to have_content "Memo Lottery Won"
+        expect(page).not_to have_content "Memo Lottery Won Twice"
+      end
+      expect(page).to have_content "Unable to update store credit [\"Memo is not valid\"]"
+    end
+  end
+
   describe "updating store credit" do
     let(:updated_amount) { "99.0" }
     let!(:update_reason) { create(:store_credit_update_reason) }
