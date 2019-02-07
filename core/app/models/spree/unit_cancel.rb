d822a085e61880a5dcfa355da08457ccfa210de7
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-02-07 09:58:30 +0100

    Refactor Spree::UnitCancel#compute_amount
    Related to #3068. In order to keep code consistency, I made a
    refactoring along the same lines of
    `core/app/models/spree/calculator/returns/default_refund_amount.rb`,
    which performs a similar calculation.

--- core/app/models/spree/unit_cancel.rb
+++ core/app/models/spree/unit_cancel.rb
@@ -35,6 +35,16 @@ class Spree::UnitCancel < Spree::Base
   def compute_amount(line_item)
     raise "Adjustable does not match line item" unless line_item == inventory_unit.line_item
 
-    -((line_item.total - line_item.total_before_tax).to_d / line_item.inventory_units.not_canceled.reject(&:original_return_item).size)
+    -weighted_line_item_amount(line_item)
+  end
+
+  private
+
+  def weighted_line_item_amount(line_item)
+    line_item.total_before_tax / quantity_of_line_item(line_item)
+  end
+
+  def quantity_of_line_item(line_item)
+    BigDecimal(line_item.inventory_units.not_canceled.reject(&:original_return_item).size)
   end
 end
