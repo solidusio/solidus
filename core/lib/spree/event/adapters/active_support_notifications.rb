d73652104d097b3063b3974379ec455aa312d9b4
andrea longhi <andrea@spaghetticode.it>
2019-02-11 14:20:04 +0100

    Extract `Spree::Event#adapter`
    `Extract Spree::Event#adapter` can now be configured in order to easily use a
    custom event adapter.
    
    The default adapter is `Spree::Event::Adapters::ActiveSupportNotifications`.

---
+++ core/lib/spree/event/adapters/active_support_notifications.rb
@@ -0,0 +1,26 @@
+module Spree
+  module Event
+    module Adapters
+      module ActiveSupportNotifications
+        extend self
+
+        def instrument(event_name, opts)
+          ActiveSupport::Notifications.instrument event_name, opts do
+            yield opts if block_given?
+          end
+        end
+
+        def subscribe(event_name)
+          ActiveSupport::Notifications.subscribe event_name do |*args|
+            event = ActiveSupport::Notifications::Event.new(*args)
+            yield event
+          end
+        end
+
+        def unsubscribe(subscriber)
+          ActiveSupport::Notifications.unsubscribe(subscriber)
+        end
+      end
+    end
+  end
+end
