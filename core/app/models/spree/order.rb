95df25fc62760d4718da42048def769b33a3251a
andrea longhi <andrea@spaghetticode.it>
2019-02-12 11:58:01 +0100

    Add `Spree::Order::Interactors::Finalize` interactor
    The interactor is a verbatim extraction of the code in `Spree::Order#finalize!`
    method.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -418,23 +418,7 @@ module Spree
     # Finalizes an in progress order after checkout is complete.
     # Called after transition to complete state when payments will have been processed
     def finalize!
-      # lock all adjustments (coupon promotions, etc.)
-      all_adjustments.each(&:finalize!)
-
-      # update payment and shipment(s) states, and save
-      updater.update_payment_state
-      shipments.each do |shipment|
-        shipment.update_state
-        shipment.finalize!
-      end
-
-      updater.update_shipment_state
-      save!
-      updater.run_hooks
-
-      touch :completed_at
-
-      deliver_order_confirmation_email unless confirmation_delivered?
+      Spree::Order::Interactors::Finalizer.call(order: self)
     end
 
     def fulfill!
---
+++ core/app/models/spree/order/interactors/finalizer.rb
@@ -0,0 +1,38 @@
+module Spree
+  class Order
+    module Interactors
+      class Finalizer
+        include Interactor
+
+        delegate :all_adjustments, :updater, :shipments, :save!, :touch,
+          :deliver_order_confirmation_email, :confirmation_delivered?, to: :order
+
+        def call
+          # lock all adjustments (coupon promotions, etc.)
+          all_adjustments.each(&:finalize!)
+
+          # update payment and shipment(s) states, and save
+          updater.update_payment_state
+          shipments.each do |shipment|
+            shipment.update_state
+            shipment.finalize!
+          end
+
+          updater.update_shipment_state
+          save!
+          updater.run_hooks
+
+          touch :completed_at
+
+          deliver_order_confirmation_email unless confirmation_delivered?
+        end
+
+        private
+
+        def order
+          context.order
+        end
+      end
+    end
+  end
+end
