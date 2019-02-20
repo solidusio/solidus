d87a25a60e81bf7683866f02d6f08c358ed02974
andrea longhi <andrea@spaghetticode.it>
2019-02-20 15:11:38 +0100

    Fix migration `20120411123334` on rollback
    The migration was not reversible, so `change` was replaced by `up`.
    
    Having a `down` method does not make much sense, as changing back the field
    length to 40 characters does not bring any particular benefit.

--- api/db/migrate/20120411123334_resize_api_key_field.rb
+++ api/db/migrate/20120411123334_resize_api_key_field.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ResizeApiKeyField < ActiveRecord::Migration[4.2]
-  def change
+  def up
     unless defined?(User)
       change_column :spree_users, :api_key, :string, limit: 48
     end
