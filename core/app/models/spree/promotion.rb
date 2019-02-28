66d842eca844a3c61f03d2e140f8ffdb90a5ebf7
Alberto Vena <kennyadsl@gmail.com>
2019-02-28 16:38:44 +0100

    Remove code setter/getter from Spree::Promotion
    They have been deprecated long time ago, 'code' column have been
    already removed from the database. It's time to say goodbye.

--- core/app/models/spree/promotion.rb
+++ core/app/models/spree/promotion.rb
@@ -53,14 +53,6 @@ module Spree
       order && !UNACTIVATABLE_ORDER_STATES.include?(order.state)
     end
 
-    def code
-      raise "Attempted to call code on a Spree::Promotion. Promotions are now tied to multiple code records"
-    end
-
-    def code=(_val)
-      raise "Attempted to call code= on a Spree::Promotion. Promotions are now tied to multiple code records"
-    end
-
     def self.with_coupon_code(val)
       joins(:codes).where(
         PromotionCode.arel_table[:value].eq(val.downcase)
