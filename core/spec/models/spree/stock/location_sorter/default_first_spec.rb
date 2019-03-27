5c8fcffdece65466abf209f45dd629aa8afbaf42
Alberto Vena <kennyadsl@gmail.com>
2019-03-27 10:46:31 +0100

    Rewrite Stock Location Sorter specs
    This is not releated with this PR but I think we should change
    this code here to discourage adopting a practice that we don't
    want to see into other specs.

--- core/spec/models/spree/stock/location_sorter/default_first_spec.rb
+++ core/spec/models/spree/stock/location_sorter/default_first_spec.rb
@@ -8,8 +8,10 @@ module Spree
       RSpec.describe DefaultFirst, type: :model do
         subject { described_class.new(stock_locations) }
 
-        let(:stock_locations) { OpenStruct.new(order_default: sorted_stock_locations) }
-        let(:sorted_stock_locations) { instance_double('Spree::StockLocation::ActiveRecord_Relation') }
+        let!(:first_stock_location) { create(:stock_location, default: false) }
+        let!(:second_stock_location) { create(:stock_location, default: true) }
+        let(:stock_locations) { Spree::StockLocation.all }
+        let(:sorted_stock_locations) { stock_locations.reverse }
 
         it 'returns the default stock location first' do
           expect(subject.sort).to eq(sorted_stock_locations)
--- core/spec/models/spree/stock/location_sorter/unsorted_spec.rb
+++ core/spec/models/spree/stock/location_sorter/unsorted_spec.rb
@@ -8,7 +8,9 @@ module Spree
       RSpec.describe Unsorted, type: :model do
         subject { described_class.new(stock_locations) }
 
-        let(:stock_locations) { instance_double('Spree::StockLocation::ActiveRecord_Relation') }
+        let!(:first_stock_location) { create(:stock_location) }
+        let!(:second_stock_location) { create(:stock_location) }
+        let(:stock_locations) { Spree::StockLocation.all }
 
         it 'returns the original stock locations unsorted' do
           expect(subject.sort).to eq(stock_locations)
