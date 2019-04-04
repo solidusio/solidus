5c6c5fefe4381b69efb3ae3d4ea7216da7a3f10b
Sean <seand7565@gmail.com>
2019-04-03 19:16:16 -0500

    Switching to cookies for admin-nav-hidden state storage. This allows for the class to be set on body instantly, rather than having to wait for page load

--- backend/app/assets/javascripts/spree/backend/components/admin_nav.js
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -1,14 +1,11 @@
 Spree.ready(function() {
-  if (JSON.parse(sessionStorage.getItem("navExtended"))) {
-    document.body.classList.add("admin-nav-hidden");
-  }
-
   var adminNavToggle = document.querySelector("#admin-nav-toggle");
 
   if (adminNavToggle) {
     adminNavToggle.addEventListener("click", function() {
       document.body.classList.toggle("admin-nav-hidden");
-      sessionStorage.setItem("navExtended", document.body.classList.contains("admin-nav-hidden"));
+      console.log(document.cookie)
+      document.cookie = "admin_nav_hidden=" + document.body.classList.contains("admin-nav-hidden") + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
     });
   }
 });
