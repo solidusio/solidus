5bf10afa0b0a60716b7394dee89022f68e855847
Sean <seand7565@gmail.com>
2019-04-03 19:45:59 -0500

    Defaulting to closed on small screens

--- backend/app/assets/javascripts/spree/backend/components/admin_nav.js
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -1,10 +1,15 @@
 Spree.ready(function() {
+  if(window.screen.width >= 1024 && !document.cookie.includes("admin_nav_hidden")){
+    //Set default nav to collapse on small screens - but don't override user preference
+    document.body.classList.add("admin-nav-hidden");
+    document.cookie = "admin_nav_hidden=true; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+  }
+
   var adminNavToggle = document.querySelector("#admin-nav-toggle");
 
   if (adminNavToggle) {
     adminNavToggle.addEventListener("click", function() {
       document.body.classList.toggle("admin-nav-hidden");
-      console.log(document.cookie)
       document.cookie = "admin_nav_hidden=" + document.body.classList.contains("admin-nav-hidden") + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
     });
   }
