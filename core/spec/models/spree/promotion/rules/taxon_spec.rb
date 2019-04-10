cfb43ea3af86c3b32b8d837ab4ac9535160b6141
François Koessler <francois@khawa.tech>
2019-04-10 15:58:20 +0200

    Use taxon children when searching classification
    Promotion rule is now actionnable if product has a child
    taxon of taxon rule

--- core/spec/models/spree/promotion/rules/taxon_spec.rb
+++ core/spec/models/spree/promotion/rules/taxon_spec.rb
@@ -151,14 +151,14 @@ RSpec.describe Spree::Promotion::Rules::Taxon, type: :model do
     let(:order) { create :order_with_line_items }
     let(:taxon) { create :taxon, name: 'first' }
 
-    before do
-      rule.preferred_match_policy = 'invalid'
-      rule.save!(validate: false)
-      line_item.product.taxons << taxon
-      rule.taxons << taxon
-    end
-
     context 'with an invalid match policy' do
+      before do
+        rule.preferred_match_policy = 'invalid'
+        rule.save!(validate: false)
+        line_item.product.taxons << taxon
+        rule.taxons << taxon
+      end
+
       it 'logs a warning and uses "any" policy' do
         expect(Spree::Deprecation).to(
           receive(:warn).
@@ -168,6 +168,43 @@ RSpec.describe Spree::Promotion::Rules::Taxon, type: :model do
         expect(
           rule.actionable?(line_item)
         ).to be_truthy
+      end
+    end
+
+    context 'when a product has a taxon of a taxon rule' do
+      before do
+        product.taxons << taxon
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is actionable' do
+        expect(rule).to be_actionable(line_item)
+      end
+    end
+
+    context 'when a product has a taxon child of a taxon rule' do
+      before do
+        taxon.children << taxon2
+        product.taxons << taxon2
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is actionable' do
+        expect(rule).to be_actionable(line_item)
+      end
+    end
+
+    context 'when a product does not have taxon or child taxon of a taxon rule' do
+      before do
+        product.taxons << taxon2
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is not actionable' do
+        expect(rule).not_to be_actionable(line_item)
       end
     end
   end
