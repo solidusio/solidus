cd721553787039aeed6f39e8bee1157c0d631874
andrea longhi <andrea@spaghetticode.it>
2019-04-30 15:46:43 +0200

    Add Spree::Stock::InventoryUnitBuilder#missing_units_for_line_item
    The new method builds missing inventory units for the specified line
    item.
    
    inventory_unit_builder

--- core/app/models/spree/stock/inventory_unit_builder.rb
+++ core/app/models/spree/stock/inventory_unit_builder.rb
@@ -9,13 +9,24 @@ module Spree
 
       def units
         @order.line_items.flat_map do |line_item|
-          Array.new(line_item.quantity) do
-            Spree::InventoryUnit.new(
-              pending: true,
-              variant: line_item.variant,
-              line_item: line_item
-            )
-          end
+          build_units(line_item, line_item.quantity)
+        end
+      end
+
+      def missing_units_for_line_item(line_item)
+        quantity = line_item.quantity - line_item.inventory_units.count
+        build_units(line_item, quantity)
+      end
+
+      private
+
+      def build_units(line_item, quantity)
+        Array.new(quantity) do
+          Spree::InventoryUnit.new(
+            pending: true,
+            variant: line_item.variant,
+            line_item: line_item
+          )
         end
       end
     end
