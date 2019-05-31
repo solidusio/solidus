41d400b986a4c6e0597ec6141a66eaaedf12a2bd
andrea longhi <andrea@spaghetticode.it>
2019-05-31 10:49:11 +0200

    Change `appear_before` matcher to avoid flakes
    The previous implementation for the rspec custom matcher `appear_before` was
    often flaky. The new one, relying on the standard Capybara matcher `#has_text?`
    is supposed to be more reliable, also when JS is involved.

--- backend/spec/support/appear_before_matcher.rb
+++ backend/spec/support/appear_before_matcher.rb
@@ -5,6 +5,6 @@ require 'rspec/expectations'
 RSpec::Matchers.define :appear_before do |expected|
   match do |actual|
     raise "Page instance required to use the appear_before matcher" unless page
-    page.body.index(actual) <= page.body.index(expected)
+    has_text? /#{actual}.*#{expected}/
   end
 end
