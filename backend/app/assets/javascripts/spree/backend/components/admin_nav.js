97db7f10b8d8a25c20e0fa39ba6bdd92d0886c2d
Sean <seand7565@gmail.com>
2019-06-10 12:39:54 -0500

    Formatting nav-collapse to work with new admin layout

--- backend/app/assets/javascripts/spree/backend/components/admin_nav.js
+++ backend/app/assets/javascripts/spree/backend/components/admin_nav.js
@@ -1,9 +1,9 @@
 Spree.ready(function() {
-  toggleTooltips()
-  if(window.screen.width <= 1024 && !document.cookie.includes("admin_nav_hidden")){
-    //Set default nav to collapse on small screens - but don't override user preference
+  toggleTooltips();
+  if (window.screen.width <= 1024 && !document.cookie.includes("admin_nav_hidden")) {
+    // Set default nav to collapse on small screens - but don't override user preference
     document.body.classList.add("admin-nav-hidden");
-    document.cookie = "admin_nav_hidden=true; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+    document.cookie = "admin_nav_hidden=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
   }
 
   var adminNavToggle = document.querySelector("#admin-nav-toggle");
@@ -12,17 +12,16 @@ Spree.ready(function() {
     adminNavToggle.addEventListener("click", function() {
       document.body.classList.toggle("admin-nav-hidden");
       toggleTooltips();
-      document.cookie = "admin_nav_hidden=" + document.body.classList.contains("admin-nav-hidden") + "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+      document.cookie = "admin_nav_hidden=" + document.body.classList.contains("admin-nav-hidden") + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
     });
   }
 
-  function toggleTooltips(){
-    $(".tab-with-icon .text:visible").each(function(){
-      $(this.closest(".tab-with-icon")).attr("data-original-title","").tooltip()
-    })
-    $(".tab-with-icon .text:hidden").each(function(){
-      $(this.closest(".tab-with-icon")).attr("data-original-title",$(this).text()).tooltip()
-    })
+  function toggleTooltips() {
+    $(".tab-with-icon .text:visible").each(function() {
+      $(this.closest(".tab-with-icon")).attr("data-original-title", "").tooltip();
+    });
+    $(".tab-with-icon .text:hidden").each(function() {
+      $(this.closest(".tab-with-icon")).attr("data-original-title", $(this).text()).tooltip();
+    });
   }
-
 });
