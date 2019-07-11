5c0ee20e38b47eb5434ab017c4cef5c35b0a2545
Alberto Vena <kennyadsl@gmail.com>
2019-07-11 16:26:30 +0200

    Move backend_configuration from /app to /lib
    It has nothing to do with Rails anymore and follows the style
    of what we do in core

--- backend/lib/spree/backend_configuration.rb
+++ backend/lib/spree/backend_configuration.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spree/preferences/configuration'
+
 module Spree
   class BackendConfiguration < Preferences::Configuration
     preference :locale, :string, default: Rails.application.config.i18n.default_locale
