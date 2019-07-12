eafbc3c0517984a4337c2b6c39d60e770e87a987
Alberto Vena <kennyadsl@gmail.com>
2019-07-12 12:36:02 +0200

    Move frontend_configuration from /app to /lib
    It has nothing to do with Rails anymore and follows the style
    of what we do in core

--- frontend/lib/spree/frontend/engine.rb
+++ frontend/lib/spree/frontend/engine.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require 'spree/frontend/config'
+
 module Spree
   module Frontend
     class Engine < ::Rails::Engine
       config.middleware.use "Spree::Frontend::Middleware::SeoAssist"
 
-      initializer "spree.frontend.environment", before: :load_config_initializers do |_app|
-        Spree::Frontend::Config = Spree::FrontendConfiguration.new
-      end
+      # Leave initializer empty for backwards-compatability. Other apps
+      # might still rely on this event.
+      initializer "spree.frontend.environment", before: :load_config_initializers do; end
     end
   end
 end
