cd428b08d7e603dafb82c34fff5c5879a2da150b
andrea longhi <andrea@spaghetticode.it>
2019-09-20 14:54:06 +0200

    Disable return authorization creation button after click
    Disabling the button prevents multiple form submissions that would result
    in unwanted duplicated records.

--- backend/spec/features/admin/orders/return_authorizations_spec.rb
+++ backend/spec/features/admin/orders/return_authorizations_spec.rb
@@ -8,20 +8,49 @@ describe "ReturnAuthorizations", type: :feature do
   stub_authorization!
 
   let!(:order) { create(:shipped_order) }
-  let!(:return_authorization) { create(:return_authorization, order: order) }
 
-  it "can visit the return authorizations list page" do
-    visit spree.admin_order_return_authorizations_path(order)
-  end
-
-  describe "edit" do
-    it "can visit the return authorizations edit page" do
-      visit spree.edit_admin_order_return_authorization_path(order, return_authorization)
+  describe "create" do
+    def create_return_authorization
+      find("#select-all").click
+      select "NY Warehouse", from: "Stock Location"
+      click_button "Create"
     end
 
-    it "return authorizations edit page has a data hook for extensions to add content above, below or within the RA form" do
-      visit spree.edit_admin_order_return_authorization_path(order, return_authorization)
-      expect(page).to have_selector("[data-hook=return-authorization-form-wrapper]")
+    before do
+      visit spree.new_admin_order_return_authorization_path(order)
+    end
+
+    it "creates a return authorization" do
+      create_return_authorization
+
+      expect(page).to have_content "Return Authorization has been successfully created!"
+    end
+
+    it "disables the button at submit", :js do
+      page.execute_script "$('form').submit(function(e) { e.preventDefault()})"
+
+      create_return_authorization
+
+      expect(page).to have_button("Create", disabled: true)
+    end
+  end
+
+  describe "when a return authorization exists" do
+    let!(:return_authorization) { create(:return_authorization, order: order) }
+
+    it "can visit the return authorizations list page" do
+      visit spree.admin_order_return_authorizations_path(order)
+    end
+
+    describe "edit" do
+      it "can visit the return authorizations edit page" do
+        visit spree.edit_admin_order_return_authorization_path(order, return_authorization)
+      end
+
+      it "return authorizations edit page has a data hook for extensions to add content above, below or within the RA form" do
+        visit spree.edit_admin_order_return_authorization_path(order, return_authorization)
+        expect(page).to have_selector("[data-hook=return-authorization-form-wrapper]")
+      end
     end
   end
 end
