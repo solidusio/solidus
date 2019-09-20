934223c07e67c906b1b31e573e5d1cb038890280
andrea longhi <andrea@spaghetticode.it>
2019-09-20 12:22:31 +0200

    Disable customer return creation button after click
    Disabling the button prevents multiple form submissions that may result in
    errors, as it generally does not make any sense creating multiple customer
    returns starting from the same data.

--- backend/spec/features/admin/orders/customer_returns_spec.rb
+++ backend/spec/features/admin/orders/customer_returns_spec.rb
@@ -8,21 +8,34 @@ describe 'Customer returns', type: :feature do
   context 'when the order has more than one line item' do
     let(:order) { create :shipped_order, line_items_count: 2 }
 
+    def create_customer_return
+      find('#select-all').click
+      page.execute_script "$('select.add-item').val('receive')"
+      select 'NY Warehouse', from: 'Stock Location'
+      click_button 'Create'
+    end
+
+    before do
+      visit spree.new_admin_order_customer_return_path(order)
+    end
+
     context 'when creating a return with state "Received"' do
       it 'marks the order as returned', :js do
-        visit spree.new_admin_order_customer_return_path(order)
-
-        find('#select-all').click
-        page.execute_script "$('select.add-item').val('receive')"
-        select 'NY Warehouse', from: 'Stock Location'
-        click_button 'Create'
+        create_customer_return
 
         expect(page).to have_content 'Customer Return has been successfully created'
-
         within 'dd.order-state' do
           expect(page).to have_content 'Returned'
         end
       end
+    end
+
+    it 'disables the button at submit', :js do
+      page.execute_script "$('form').submit(function(e) { e.preventDefault()})"
+
+      create_customer_return
+
+      expect(page).to have_button("Create", disabled: true)
     end
   end
 end
