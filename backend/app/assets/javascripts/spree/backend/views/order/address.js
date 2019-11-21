1bf4279fc2b75fbd7a3a02f2e4250eae15691627
andrea longhi <andrea@spaghetticode.it>
2019-11-21 11:43:53 +0100

    Admin new-order form: customer with textual state bugfix
    When creating a new order from the backend, when the admin user changes
    customer using the autocomplete field and the new customer has only a
    textual state, the bill address shown in page should show the expected
    textual state.

--- backend/app/assets/javascripts/spree/backend/views/order/address.js
+++ backend/app/assets/javascripts/spree/backend/views/order/address.js
@@ -24,7 +24,7 @@ Spree.Views.Order.Address = Backbone.View.extend({
   eachField: function(callback){
     var view = this;
     var fields = ["firstname", "lastname", "company", "address1", "address2",
-      "city", "zipcode", "phone", "country_id"];
+      "city", "zipcode", "phone", "country_id", "state_name"];
     _.each(fields, function(field) {
       var el = view.$('[name$="[' + field + ']"]');
       if (el.length) callback(field, el);
