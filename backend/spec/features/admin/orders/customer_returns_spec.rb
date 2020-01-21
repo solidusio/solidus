915d3e5a2f8b30279b89d2f72821de16e1e848dc
Elia Schito <elia@schito.me>
2020-01-21 19:18:32 +0100

    Extract helpers for the Customer Returns feat spec
    This is in preparation to adding more specs and cover more behavior
    of that part of the admin.

--- backend/spec/features/admin/orders/customer_returns_spec.rb
+++ backend/spec/features/admin/orders/customer_returns_spec.rb
@@ -5,35 +5,38 @@ require 'spec_helper'
 describe 'Customer returns', type: :feature do
   stub_authorization!
 
+  def create_customer_return(value)
+    find('#select-all').click
+    page.execute_script "$('select.add-item').val(#{value.to_s.inspect})"
+    select 'NY Warehouse', from: 'Stock Location'
+    click_button 'Create'
+  end
+
+  def order_state_label
+    find('dd.order-state').text
+  end
+
+  before do
+    visit spree.new_admin_order_customer_return_path(order)
+  end
+
   context 'when the order has more than one line item' do
     let(:order) { create :shipped_order, line_items_count: 2 }
 
-    def create_customer_return
-      find('#select-all').click
-      page.execute_script "$('select.add-item').val('receive')"
-      select 'NY Warehouse', from: 'Stock Location'
-      click_button 'Create'
-    end
-
-    before do
-      visit spree.new_admin_order_customer_return_path(order)
-    end
-
     context 'when creating a return with state "Received"' do
       it 'marks the order as returned', :js do
-        create_customer_return
+        create_customer_return('receive')
 
         expect(page).to have_content 'Customer Return has been successfully created'
-        within 'dd.order-state' do
-          expect(page).to have_content 'Returned'
-        end
+
+        expect(order_state_label).to eq('Returned')
       end
     end
 
     it 'disables the button at submit', :js do
       page.execute_script "$('form').submit(function(e) { e.preventDefault()})"
 
-      create_customer_return
+      create_customer_return('receive')
 
       expect(page).to have_button("Create", disabled: true)
     end
