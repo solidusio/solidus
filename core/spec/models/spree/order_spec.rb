a6768ce1145245753d45d18ad2c9a91b095cb415
Martin Meyerhoff <mamhoff@gmail.com>
2020-03-02 15:34:34 +0100

    Use complete order in captured store credits spec
    This spec expects the order to be completed (see two lines below, where
    `order.finalize` is called). However, without this change, the generated order stays in
    `cart` state despite the setup block.

--- core/spec/models/spree/order_spec.rb
+++ core/spec/models/spree/order_spec.rb
@@ -94,11 +94,11 @@ RSpec.describe Spree::Order, type: :model do
       let!(:store_credit_payment_method) { create(:store_credit_payment_method) }
       let(:order_total) { 500.00 }
       let(:store_credit) { create(:store_credit, amount: order_total) }
-      let(:order) { create(:order_with_line_items, user: store_credit.user, line_items_price: order_total) }
+      let(:order) { create(:order_ready_to_complete, user: store_credit.user, line_items_price: order_total) }
 
       before do
         order.add_store_credit_payments
-        order.finalize!
+        order.complete!
         order.capture_payments!
       end
 
