73cd0eff42e7d350e02f44db97c14b0d1bf7f76f
Filippo Liverani <dev@mailvore.com>
2020-04-10 17:45:32 +0200

    Override broken awesome_nested_set set_depth! method
    Skips a breaking model reload before NestedSet update depth logic.
    This issue has already been addressed in:
    collectiveidea/awesome_nested_set#413
    The patch can be removed when a new version of awesome_nested_set will
    be released.

--- core/spec/models/spree/promotion/rules/taxon_spec.rb
+++ core/spec/models/spree/promotion/rules/taxon_spec.rb
@@ -101,6 +101,8 @@ RSpec.describe Spree::Promotion::Rules::Taxon, type: :model do
         before do
           taxon.children << taxon2
           taxon.save!
+          taxon.reload
+
           product.taxons = [taxon2, taxon3]
           rule.taxons = [taxon, taxon3]
         end
