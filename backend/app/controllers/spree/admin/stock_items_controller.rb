7357f86853e426bc6974be6fcbbce62e32033a04
Edwin Cruz <edwin.cruz@magmalabs.io>
2020-05-18 09:45:01 -0500

    Stock items management more flexible performing bulk restocks
    If you filtered variats using selecting a stock location, it
    was the only one available to perform restocks regardless having
    only that stock locations or more, this change makes sure
    all available stock locations per variants are available

--- backend/app/controllers/spree/admin/stock_items_controller.rb
+++ backend/app/controllers/spree/admin/stock_items_controller.rb
@@ -40,8 +40,14 @@ module Spree
       end
 
       def load_stock_management_data
-        @stock_locations = Spree::StockLocation.accessible_by(current_ability)
-        @stock_item_stock_locations = params[:stock_location_id].present? ? @stock_locations.where(id: params[:stock_location_id]) : @stock_locations
+        @stock_locations = Spree::StockLocation.accessible_by(current_ability, :read)
+        @stock_item_stock_locations = Spree::DeprecatedInstanceVariableProxy.new(
+          view_context,
+          :@stock_locations,
+          :stock_item_stock_locations,
+          Spree::Deprecation,
+          "Please, do not use @stock_item_stock_locations anymore in the views, use @stock_locations",
+        )
         @variant_display_attributes = self.class.variant_display_attributes
         @variants = Spree::Config.variant_search_class.new(params[:variant_search_term], scope: variant_scope).results.
             order(id: :desc).page(params[:page]).per(params[:per_page] || Spree::Config[:orders_per_page])
