18992d35bc5cc9ef87516ce2c3e212da0da25229
Halil Özgür <h+github@halil.im>
2020-05-20 18:50:08 +0300

    Remove the duplicated validation
    Similar to the `active` scope, this too is already defined in `Spree::NamedType`

--- core/app/models/spree/store_credit_reason.rb
+++ core/app/models/spree/store_credit_reason.rb
@@ -4,6 +4,4 @@ class Spree::StoreCreditReason < Spree::Base
   include Spree::NamedType
 
   has_many :store_credit_events, inverse_of: :store_credit_reason
-
-  validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
 end
