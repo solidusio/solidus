c8e7c453b4ab4d9d94806adef6b34364407aabc5
George Mendoza <gsmendoza@gmail.com>
2020-07-16 17:30:41 +0800

    Add disable_api_routes API config
    https://github.com/solidusio/solidus/issues/3669

--- api/lib/spree/api_configuration.rb
+++ api/lib/spree/api_configuration.rb
@@ -3,5 +3,6 @@
 module Spree
   class ApiConfiguration < Preferences::Configuration
     preference :requires_authentication, :boolean, default: true
+    preference :disable_api_routes, :boolean, default: false
   end
 end
---
+++ api/spec/routing/api_routes_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'api', type: :routing do
+  context 'when disable_api_routes is enabled' do
+    before do
+      stub_spree_preferences(Spree::Api::Config, disable_api_routes: true)
+      Rails.application.reload_routes!
+    end
+
+    it "does not mount the api routes" do
+      aggregate_failures do
+        expect(get: '/api/orders').to_not be_routable
+        expect(get: '/api/products').to_not be_routable
+      end
+    end
+  end
+
+  context 'when disable_api_routes is disabled' do
+    before do
+      stub_spree_preferences(Spree::Api::Config, disable_api_routes: false)
+      Rails.application.reload_routes!
+    end
+
+    it "mounts the api routes" do
+      aggregate_failures do
+        expect(get: '/api/orders').to be_routable
+        expect(get: '/api/products').to be_routable
+      end
+    end
+  end
+end
