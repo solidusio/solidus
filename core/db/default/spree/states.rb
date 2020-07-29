b343b9649707d9783146dea814bd17e9c24831c5
Sean <seand7565@gmail.com>
2020-07-29 09:12:11 -0500

    Fix Italy state seed generation
    Previously, Italy was using the top-level `subregions` - `regions`
    in Carmen to generate states, however for a mailing address this
    is incorrect, and it instead needs to use `provinces`, which is
    nested as the second level of `subregions` in Carmen. This fixes the
    seed generation, allowing for more countries to make this switch
    as necessary (there are a handful of other countries that need this)
    
    If you want an easy (and possibly destructive) method to update your
    states, see the following gist, which contains a rake task for you to
    run: https://gist.github.com/seand7565/6342d7ce7a89b2412fcb4eab7f2c8a61

--- core/db/default/spree/states.rb
+++ core/db/default/spree/states.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
+def create_states(subregions, country)
+  subregions.each do |subregion|
+    Spree::State.where(abbr: subregion.code, country: country).first_or_create!(
+      name: subregion.name
+    )
+  end
+end
+
 ActiveRecord::Base.transaction do
   Spree::Country.all.each do |country|
     carmen_country = Carmen::Country.coded(country.iso)
     next unless carmen_country.subregions?
 
-    carmen_country.subregions.each do |subregion|
-      Spree::State.where(abbr: subregion.code, country: country).first_or_create!(
-        name: subregion.name
-      )
+    if Spree::Config[:countries_that_use_nested_subregions].include? country.iso
+      create_states(carmen_country.subregions.flat_map(&:subregions), country)
+    else
+      create_states(carmen_country.subregions, country)
     end
   end
 end
