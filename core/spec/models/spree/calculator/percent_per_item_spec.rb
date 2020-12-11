a22602642152b5e63ba8c8f515a6b7db9311fa61
Daniele Palombo <daniele.palombo@gmail.com>
2020-12-11 16:58:16 +0100

    Deprecate Spree::Calculator::PercentPerItem
    This method is deprecated in favour of
    Spree::Calculator::PercentOnLineItem
    
    ref https://github.com/solidusio/solidus/commit/983e5aa529851176fd97017d622cdaf47338cdf3

--- core/spec/models/spree/calculator/percent_per_item_spec.rb
+++ core/spec/models/spree/calculator/percent_per_item_spec.rb
@@ -8,5 +8,17 @@ require_dependency 'spree/calculator'
 module Spree
   RSpec.describe Calculator::PercentPerItem, type: :model do
     it_behaves_like 'a calculator with a description'
+
+    describe '#compute' do
+      let(:order) { stub_model(Spree::Order) }
+
+      before do
+        expect(Spree::Deprecation).to receive(:warn).with(/method is deprecated/)
+      end
+
+      it 'warns about deprecation' do
+        described_class.new.compute(order)
+      end
+    end
   end
 end
