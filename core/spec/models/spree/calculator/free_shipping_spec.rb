f1cda0c8007586957901b8155d8a214f43f668d7
Daniele Palombo <daniele.palombo@gmail.com>
2020-12-11 16:18:06 +0100

    Deprecate Spree::Calculator::FreeShipping
    because it's no longer used
    
    ref https://github.com/solidusio/solidus/commit/57248d31cfe3ebceee0cef9f53697e083387ce6a

--- core/spec/models/spree/calculator/free_shipping_spec.rb
+++ core/spec/models/spree/calculator/free_shipping_spec.rb
@@ -5,4 +5,16 @@ require 'shared_examples/calculator_shared_examples'
 
 RSpec.describe Spree::Calculator::FreeShipping, type: :model do
   it_behaves_like 'a calculator with a description'
+
+  describe '#compute' do
+    let(:order) { stub_model(Spree::Order) }
+
+    before do
+      expect(Spree::Deprecation).to receive(:warn).with(/method is deprecated/)
+    end
+
+    it 'warns about deprecation' do
+      described_class.new.compute(order)
+    end
+  end
 end
