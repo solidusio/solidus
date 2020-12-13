bf9461aaf842f17afd6e961062c8d4c75318892d
Brian Christian <brchristian@gmail.com>
2020-12-13 10:01:55 -0800

    Update canonical-rails syntax for latest version

--- frontend/config/initializers/canonical_rails.rb
+++ frontend/config/initializers/canonical_rails.rb
@@ -12,5 +12,5 @@ CanonicalRails.setup do |config|
   # Parameter spamming can cause index dilution by creating seemingly different URLs with identical or near-identical content.
   # Unless whitelisted, these parameters will be omitted
 
-  config.whitelisted_parameters = [:keywords, :page, :search, :taxon]
+  config.allowed_parameters = [:keywords, :page, :search, :taxon]
 end
