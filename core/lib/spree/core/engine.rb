a82cac825c0b2a6d46d640eb4a3e7fd7ed20c547
Alessandro Desantis <desa.alessandro@gmail.com>
2021-01-19 14:23:04 +0100

    Avoid loading ActionMailer previews during initialization
    Loading constants during an app's initialization is a deprecated
    technique and will be an error in future versions of Rails.
    
    By moving our custom logic for loading Solidus's previews to an
    `after_initialize` block, we ensure the preview classes are only
    loaded after initialization has completed.

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -80,14 +80,12 @@ module Spree
             caller
           )
         end
-      end
 
-      # Load in mailer previews for apps to use in development.
-      # We need to make sure we call `Preview.all` before requiring our
-      # previews, otherwise any previews the app attempts to add need to be
-      # manually required.
-      if Rails.env.development?
-        initializer "spree.mailer_previews" do
+        # Load in mailer previews for apps to use in development.
+        # We need to make sure we call `Preview.all` before requiring our
+        # previews, otherwise any previews the app attempts to add need to be
+        # manually required.
+        if Rails.env.development?
           ActionMailer::Preview.all
           Dir[root.join("lib/spree/mailer_previews/**/*_preview.rb")].each do |file|
             require_dependency file
