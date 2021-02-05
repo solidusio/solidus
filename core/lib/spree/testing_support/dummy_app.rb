0b30461e76db24d153af00db3a63329528a17ecf
Elia Schito <elia@schito.me>
2021-02-05 18:47:41 +0100

    Don't hack into AM to add mail previews
    Instead just append our AM preview path let the app control wether
    they're enabled or not, without forcing it only on some environments.

--- core/lib/spree/testing_support/dummy_app.rb
+++ core/lib/spree/testing_support/dummy_app.rb
@@ -64,6 +64,7 @@ module DummyApp
 
     config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob" if RAILS_6_OR_ABOVE
     config.action_mailer.preview_path = File.expand_path('dummy_app/mailer_previews', __dir__)
+    config.action_mailer.show_previews = true
     config.active_record.sqlite3.represent_boolean_as_integer = true unless RAILS_6_OR_ABOVE
 
     config.storage_path = Rails.root.join('tmp', 'storage')
