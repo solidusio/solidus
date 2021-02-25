77d70d7dfe38cfa3056986caf1aa6230df611548
Michael Caviness <michael@tcwequipment.com>
2021-02-25 03:07:43 -0800

    Add English variants to select2_local directory (#3895)
    * Add English variants to select2_local directory
    
    Co-authored-by: Michael Caviness <michaelcaviness@MacBook-Pro.localdomain>

---
+++ backend/vendor/assets/javascripts/solidus_admin/select2_locales/select2_locale_en-NZ.js
@@ -0,0 +1,10 @@
+/**
+ * Select2 English NZ translations
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.select2.locales['en-NZ'] = {};
+
+    $.extend($.fn.select2.defaults, $.fn.select2.locales['en-NZ']);
+})(jQuery);
