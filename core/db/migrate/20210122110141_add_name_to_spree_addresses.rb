d56bb08d5ed3584401726256726cefc955067286
andrea longhi <andrea@spaghetticode.it>
2021-02-26 15:34:22 +0100

    Add `name` column to `spree_addresses` table

---
+++ core/db/migrate/20210122110141_add_name_to_spree_addresses.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AddNameToSpreeAddresses < ActiveRecord::Migration[5.2]
+  def up
+    add_column :spree_addresses, :name, :string
+    add_index :spree_addresses, :name
+  end
+
+  def down
+    remove_index :spree_addresses, :name
+    remove_column :spree_addresses, :name
+  end
+end
