dcd96c5b8971e6b433aed6cc32838c61f6cd843e
matheusteixeira <matheusteixeira01@live.com>
2021-02-26 19:03:31 -0300

    Improve deprectation spec

--- core/spec/models/spree/order/number_generator_spec.rb
+++ core/spec/models/spree/order/number_generator_spec.rb
@@ -3,13 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Spree::Order::NumberGenerator do
-  subject(:generator) { described_class.new }
-
   describe '#generate' do
-    context 'when is called on a depracated class' do
+    before do
+      Spree::Deprecation.behavior = behavior
+    end
+
+    context 'when is called on a depracated class without raising' do
+      let(:behavior) { :silence }
+
       it 'calls the method on the new class' do
         expect_any_instance_of(Spree::Core::NumberGenerator).to receive(:generate)
-        generator.generate
+        described_class.new.generate
+      end
+    end
+
+    context 'when is called on a depracated class with raising behavior' do
+      let(:behavior) { :raise }
+
+      it 'raises DeprecationException' do
+        expect { described_class.new }.to raise_error(ActiveSupport::DeprecationException)
       end
     end
   end
