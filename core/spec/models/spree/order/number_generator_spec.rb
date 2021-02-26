a5149a64ed7700cc173f7c1d66ee619c94e2db87
matheusteixeira <matheusteixeira01@live.com>
2021-02-26 19:14:33 -0300

    Remove tests that raise due to SOLIDUS_RAISE_DEPRECATIONS

--- core/spec/models/spree/order/number_generator_spec.rb
+++ core/spec/models/spree/order/number_generator_spec.rb
@@ -4,22 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Spree::Order::NumberGenerator do
   describe '#generate' do
-    before do
-      Spree::Deprecation.behavior = behavior
-    end
-
-    context 'when is called on a depracated class without raising' do
-      let(:behavior) { :silence }
-
-      it 'calls the method on the new class' do
-        expect_any_instance_of(Spree::Core::NumberGenerator).to receive(:generate)
-        described_class.new.generate
-      end
-    end
-
     context 'when is called on a depracated class with raising behavior' do
-      let(:behavior) { :raise }
-
       it 'raises DeprecationException' do
         expect { described_class.new }.to raise_error(ActiveSupport::DeprecationException)
       end
