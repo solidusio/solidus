fb6cce6dc5e2c34cf0fea0ebe1144ec6df3a641e
Connor Ferguson <cpfergus1@gmail.com>
2021-04-14 07:13:55 -0600

    Added a method helper for image specs to DRY code
    Commit History:
    Changed file name to represent module name
    Removed unnecessary line breaks

--- core/spec/models/spree/image_spec.rb
+++ core/spec/models/spree/image_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Spree::Image, type: :model do
+  include ImageSpecHelper
   it_behaves_like 'an attachment' do
     subject { create(:image) }
     let(:attachment_name) { :attachment }
@@ -10,19 +11,17 @@ RSpec.describe Spree::Image, type: :model do
   end
 
   it 'is valid when attachment has allowed content type' do
-    image = build(:image,
-                  attachment: File.open(File.join('lib', 'spree', 'testing_support', 'fixtures', 'blank.jpg')))
+    image = build(:image, attachment: open_image('blank.jpg'))
     expect(image).to be_valid
   end
 
   it 'is not valid when attachment has restricted content type' do
-    image = build(:image,
-                  attachment: File.open(File.join('lib', 'spree', 'testing_support', 'fixtures', 'file.txt')))
+    image = build(:image, attachment: open_image('file.txt'))
     expect(image).to_not be_valid
   end
 
   describe 'attachment details' do
-    let(:image_file) { File.open(File.join('lib', 'spree', 'testing_support', 'fixtures', 'blank.jpg')) }
+    let(:image_file) { open_image('blank.jpg') }
     subject { create(:image, attachment: image_file) }
 
     it 'returns if attachment is present' do
---
+++ core/spec/support/image_spec_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ImageSpecHelper
+  def open_image(image)
+    File.open(File.join('lib', 'spree', 'testing_support', 'fixtures', image))
+  end
+end
