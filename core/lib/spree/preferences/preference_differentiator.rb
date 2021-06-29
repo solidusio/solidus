5e3a6a2211e81f7fb22c12e1d21c9e740055cd51
Marc Busqué <marc@lamarciana.com>
2021-06-29 12:52:29 +0200

    Rename to Preferences::PreferenceDifferentiator

--- core/lib/spree/preferences/preference_differentiator.rb
+++ core/lib/spree/preferences/preference_differentiator.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Spree
-  module Core
-    class PreferenceChangesBetweenSolidusVersions
+  module Preferences
+    class PreferenceDifferentiator
       attr_reader :config_class
 
       def initialize(config_class)
