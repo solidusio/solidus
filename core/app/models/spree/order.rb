solidusio/solidus

commit 94bfaa7a1796cf0dbe45792855e99b2d255af1e5

Replace references to firstname and lastname with name attribute

core/app/models/spree/order.rb
@@ -50,9 +50,8 @@ module Spree
       go_to_state :confirm
     end
 
-    ransack_alias :bill_address_firstname_or_lastname, :bill_address_firstname_or_bill_address_lastname
     self.whitelisted_ransackable_associations = %w[shipments user order_promotions promotions bill_address ship_address line_items]
-    self.whitelisted_ransackable_attributes = %w[bill_address_firstname_or_lastname completed_at created_at email number state payment_state shipment_state total store_id]
+    self.whitelisted_ransackable_attributes = %w[bill_address_name completed_at created_at email number state payment_state shipment_state total store_id]
 
     attr_reader :coupon_code
     attr_accessor :temporary_address

api/spec/requests/spree/api/users_controller_spec.rb
@@ -153,7 +153,7 @@ module Spree
           q: {
             m: 'or',
             email_start: 'distinct_test',
-            firstname_or_lastname_start: 'distinct_test'
+            name_start: 'distinct_test'
           }
         }
         expect(json_response['count']).to eq(1)

backend/app/assets/javascripts/spree/backend/user_picker.js
@@ -28,7 +28,7 @@ $.fn.userAutocomplete = function () {
           q: {
             m: 'or',
             email_start: term,
-            firstname_or_lastname_start: term
+            name_start: term
           }
         };
       },

backend/app/assets/javascripts/spree/backend/views/order/customer_select.js
@@ -34,7 +34,7 @@ Spree.Views.Order.CustomerSelect = Backbone.View.extend({
             q: {
               m: 'or',
               email_start: term,
-              firstname_or_lastname_start: term
+              name_start: term
             }
           }
         },

backend/app/controllers/spree/admin/search_controller.rb
@@ -16,7 +16,7 @@ module Spree
           @users = Spree.user_class.ransack({
             m: 'or',
             email_start: params[:q],
-            firstname_or_lastname_start: params[:q]
+            name_start: params[:q]
           }).result.limit(10)
         end
       end

backend/app/views/spree/admin/orders/index.html.erb
@@ -66,8 +66,8 @@
 
             <div class="col-12 col-xl-6">
               <div class="field">
-                <%= label_tag :q_bill_address_firstname_or_lastname_start, t('spree.name_contains') %>
-                <%= f.text_field :bill_address_firstname_or_lastname_start, size: 25 %>
+                <%= label_tag :q_bill_address_name_start, t('spree.name_contains') %>
+                <%= f.text_field :bill_address_name_start, size: 25 %>
               </div>
             </div>
 

core/app/models/concerns/spree/user_methods.rb
@@ -34,9 +34,9 @@ module Spree
 
       include Spree::RansackableAttributes unless included_modules.include?(Spree::RansackableAttributes)
 
-      ransack_alias :firstname_or_lastname, :addresses_firstname_or_addresses_lastname
+      ransack_alias :name, :addresses_name
       self.whitelisted_ransackable_associations = %w[addresses spree_roles]
-      self.whitelisted_ransackable_attributes = %w[firstname_or_lastname id email created_at]
+      self.whitelisted_ransackable_attributes = %w[name id email created_at]
     end
 
     def wallet

core/app/models/spree/address.rb
@@ -24,9 +24,8 @@ module Spree
 
     DB_ONLY_ATTRS = %w(id updated_at created_at).freeze
     TAXATION_ATTRS = %w(state_id country_id zipcode).freeze
-    LEGACY_NAME_ATTRS = %w(firstname lastname).freeze
 
-    self.whitelisted_ransackable_attributes = %w[firstname lastname]
+    self.whitelisted_ransackable_attributes = %w[name]
 
     scope :with_values, ->(attributes) do
       where(value_attributes(attributes))
@@ -160,29 +159,6 @@ module Spree
 
     def country_iso
       country && country.iso
-    end
-
-    # @return [String] the full name on this address
-    def name
-      Spree::Address::Name.new(
-        read_attribute(:firstname),
-        read_attribute(:lastname)
-      ).value
-    end
-
-    def name=(value)
-      return if value.nil?
-
-      name_from_value = Spree::Address::Name.new(value)
-      write_attribute(:firstname, name_from_value.first_name)
-      write_attribute(:lastname, name_from_value.last_name)
-    end
-
-    def as_json(options = {})
-      super.tap do |hash|
-        hash.except!(*LEGACY_NAME_ATTRS)
-        hash['name'] = name
-      end
     end
   end
 end

commit 0ed8335e5fc81f657a2b4cc78b0419bb51ad74c5

Fix Ransack error when searching for orders by customer name
There's an issue in 3.0 where, if you try to search for an
order by the customer's name, Ransack crashes.

The problem was introduced inadvertently in 94bfaa7[1]. This
Ransackable attribute was supposed to be removed completely,
since it is not a Ransack alias anymore, but an actual DB column
that lives on the `bill_address` association.

[1]: https://github.com/solidusio/solidus/commit/94bfaa7a1796cf0dbe45792855e99b2d255af1e5

core/app/models/spree/order.rb
@@ -51,7 +51,7 @@ module Spree
     end
 
     self.whitelisted_ransackable_associations = %w[shipments user order_promotions promotions bill_address ship_address line_items]
-    self.whitelisted_ransackable_attributes = %w[bill_address_name completed_at created_at email number state payment_state shipment_state total store_id]
+    self.whitelisted_ransackable_attributes = %w[completed_at created_at email number state payment_state shipment_state total store_id]
 
     attr_reader :coupon_code
     attr_accessor :temporary_address