ee83f09582a813e543b8a41df601427a815645d1
Dmitri Borowski <d.borowskij@gmail.com>
2021-11-04 17:08:11 +0300

    #3559 overriding the default configuration

--- core/lib/spree/testing_support/dummy_app.rb
+++ core/lib/spree/testing_support/dummy_app.rb
@@ -145,6 +145,7 @@ Spree.config do |config|
     config.image_attachment_module = 'Spree::Image::PaperclipAttachment'
     config.taxon_attachment_module = 'Spree::Taxon::PaperclipAttachment'
   end
+  config.associate_user_in_authentication_extension = true
 end
 
 # Raise on deprecation warnings
