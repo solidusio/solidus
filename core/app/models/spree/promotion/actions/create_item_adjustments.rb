b274da98c6138d709975b1727e52aff04828dd70
Martin Meyerhoff <mamhoff@gmail.com>
2022-02-28 10:55:05 +0100

    Promotion Rule CreateItemAdjustments: Use in-memory objects
    This changes the function that obtains the line items of an order to
    create adjustments for to use in-memory objects rather than querying the
    database for line items that have no adjustments with a source from the
    current rule.
    
    Promotion rules can easily be preloaded, and there's no need for the DB
    call in the previous code.

--- core/app/models/spree/promotion/actions/create_item_adjustments.rb
+++ core/app/models/spree/promotion/actions/create_item_adjustments.rb
@@ -83,13 +83,8 @@ module Spree
         end
 
         def line_items_to_adjust(promotion, order)
-          excluded_ids = adjustments.
-            where(adjustable_id: order.line_items.pluck(:id), adjustable_type: 'Spree::LineItem').
-            pluck(:adjustable_id).
-            to_set
-
           order.line_items.select do |line_item|
-            !excluded_ids.include?(line_item.id) &&
+            line_item.adjustments.none? { |adjustment| adjustment.source == self } &&
               promotion.line_item_actionable?(order, line_item)
           end
         end
