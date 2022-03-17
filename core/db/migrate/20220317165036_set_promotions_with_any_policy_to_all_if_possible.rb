e09c89f1f5d71534d3c639e66c12b9df41f3a4d5
Martin Meyerhoff <mamhoff@gmail.com>
2022-03-17 18:04:03 +0100

    Update migrations with unnecessary "any" match_policy
    A promotion that has a match_policy of "any", but not more than 1 rule,
    can also have a match_policy of "all" with no change in behavior. This
    will stop a lot of people from getting deprecation warnings.

---
+++ core/db/migrate/20220317165036_set_promotions_with_any_policy_to_all_if_possible.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class SetPromotionsWithAnyPolicyToAllIfPossible < ActiveRecord::Migration[5.2]
+  def up
+    Spree::Promotion.where(match_policy: :any).includes(:promotion_rules).all.each do |promotion|
+      if promotion.promotion_rules.length <= 1
+        promotion.update!(match_policy: :all)
+      end
+    end
+  end
+
+  def down
+    # No-Op
+  end
+end
