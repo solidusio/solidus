b86706f24a6d68e16307f0bad6d5904b8e27a55c
Martin Meyerhoff <mamhoff@gmail.com>
2022-03-18 09:47:14 +0100

    Add RSpec metadata helper "silence_deprecations"
    This allows us to mark a block as testing deprecated code so that it can
    easily be identified and removed in the future.

---
+++ core/lib/spree/testing_support/silence_deprecations.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.around(:each, silence_deprecations: true) do |example|
+    Spree::Deprecation.silence do
+      example.run
+    end
+  end
+end
