f5f01f156666520a76a24633a9ba0e334e20e274
George Mendoza <gsmendoza@gmail.com>
2022-03-24 14:55:54 +0800

    Wrap common Rake namespace in a CommonRakeTasks class

--- core/lib/spree/testing_support/common_rake.rb
+++ core/lib/spree/testing_support/common_rake.rb
@@ -6,35 +6,43 @@ end
 
 require 'generators/spree/dummy/dummy_generator'
 
-namespace :common do
-  task :test_app, :user_class do |_t, args|
-    args.with_defaults(user_class: "Spree::LegacyUser")
-    require ENV['LIB_NAME']
+class CommonRakeTasks
+  include Rake::DSL
 
-    ENV["RAILS_ENV"] = 'test'
+  def initialize
+    namespace :common do
+      task :test_app, :user_class do |_t, args|
+        args.with_defaults(user_class: "Spree::LegacyUser")
+        require ENV['LIB_NAME']
 
-    Spree::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--quiet"]
-    Solidus::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--auto-accept", "--with-authentication=false", "--payment-method=none", "--migrate=false", "--seed=false", "--sample=false", "--quiet", "--user_class=#{args[:user_class]}"]
+        ENV["RAILS_ENV"] = 'test'
 
-    puts "Setting up dummy database..."
+        Spree::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--quiet"]
+        Solidus::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--auto-accept", "--with-authentication=false", "--payment-method=none", "--migrate=false", "--seed=false", "--sample=false", "--quiet", "--user_class=#{args[:user_class]}"]
 
-    sh "bin/rails db:environment:set RAILS_ENV=test"
-    sh "bin/rails db:drop db:create db:migrate VERBOSE=false RAILS_ENV=test"
+        puts "Setting up dummy database..."
 
-    begin
-      generator_namespace = "#{ENV['LIB_NAMESPACE']&.underscore || ENV['LIB_NAME']}"
+        sh "bin/rails db:environment:set RAILS_ENV=test"
+        sh "bin/rails db:drop db:create db:migrate VERBOSE=false RAILS_ENV=test"
 
-      require "generators/#{generator_namespace}/install/install_generator"
-      puts 'Running extension installation generator...'
-      sh "bin/rails generate #{generator_namespace}:install --auto-run-migrations"
-    rescue LoadError
-      # No extension generator to run
+        begin
+          generator_namespace = "#{ENV['LIB_NAMESPACE']&.underscore || ENV['LIB_NAME']}"
+
+          require "generators/#{generator_namespace}/install/install_generator"
+          puts 'Running extension installation generator...'
+          sh "bin/rails generate #{generator_namespace}:install --auto-run-migrations"
+        rescue LoadError
+          # No extension generator to run
+        end
+      end
+
+      task :seed do |_t, _args|
+        puts "Seeding ..."
+
+        sh "bundle exec rake db:seed RAILS_ENV=test"
+      end
     end
   end
-
-  task :seed do |_t, _args|
-    puts "Seeding ..."
-
-    sh "bundle exec rake db:seed RAILS_ENV=test"
-  end
 end
+
+CommonRakeTasks.new
