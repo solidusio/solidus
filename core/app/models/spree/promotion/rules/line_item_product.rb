a2ee6572bdd583df226d209bb56747d27b749224
Martin Meyerhoff <mamhoff@gmail.com>
2022-03-27 09:16:14 +0200

    Add LineItemProduct Rule
    This rule is like "Product", but only applies to line items.

---
+++ core/app/models/spree/promotion/rules/line_item_product.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Spree
+  class Promotion < Spree::Base
+    module Rules
+      # A rule to apply a promotion only to line items with or without a chosen product
+      class LineItemProduct < Spree::PromotionRule
+        MATCH_POLICIES = %w(include exclude)
+
+        has_many :product_promotion_rules,
+                 dependent: :destroy,
+                 foreign_key: :promotion_rule_id,
+                 class_name: "Spree::ProductPromotionRule"
+        has_many :products,
+                 class_name: "Spree::Product",
+                 through: :product_promotion_rules
+
+        preference :match_policy, :string, default: MATCH_POLICIES.first
+
+        def applicable?(promotable)
+          promotable.is_a?(Spree::LineItem)
+        end
+
+        def eligible?(line_item, _options = {})
+          if inverse?
+            !product_ids.include?(line_item.variant.product_id)
+          else
+            product_ids.include?(line_item.variant.product_id)
+          end
+        end
+
+        def product_ids_string
+          product_ids.join(",")
+        end
+
+        def product_ids_string=(product_ids)
+          self.product_ids = product_ids.to_s.split(",").map(&:strip)
+        end
+
+        private
+
+        def inverse?
+          preferred_match_policy == "exclude"
+        end
+      end
+    end
+  end
+end
