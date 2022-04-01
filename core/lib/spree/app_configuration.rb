908bbbc687af3b4939e8a8bd877bded4a4ca9392
Martin Meyerhoff <mamhoff@gmail.com>
2022-04-01 10:43:48 +0200

    Add Promotion Rule "LineItemTaxon"
    This Rule can replace the use of `actionable?` on the taxon promotion
    rule.

--- core/lib/spree/app_configuration.rb
+++ core/lib/spree/app_configuration.rb
@@ -626,6 +626,7 @@ module Spree
           Spree::Promotion::Rules::LineItemProduct
           Spree::Promotion::Rules::OneUsePerUser
           Spree::Promotion::Rules::Taxon
+          Spree::Promotion::Rules::LineItemTaxon
           Spree::Promotion::Rules::NthOrder
           Spree::Promotion::Rules::OptionValue
           Spree::Promotion::Rules::LineItemOptionValue
