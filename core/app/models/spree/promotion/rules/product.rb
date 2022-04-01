4ad9f821991a4d9d869f0317af5684963566597c
Martin Meyerhoff <mamhoff@gmail.com>
2022-04-01 11:09:17 +0200

    Remove "actionable?" from Product rule

--- core/app/models/spree/promotion/rules/product.rb
+++ core/app/models/spree/promotion/rules/product.rb
@@ -56,17 +56,6 @@ module Spree
           eligibility_errors.empty?
         end
 
-        def actionable?(line_item)
-          case preferred_match_policy
-          when 'any', 'all'
-            product_ids.include? line_item.variant.product_id
-          when 'none'
-            product_ids.exclude? line_item.variant.product_id
-          else
-            raise "unexpected match policy: #{preferred_match_policy.inspect}"
-          end
-        end
-
         def product_ids_string
           product_ids.join(',')
         end
