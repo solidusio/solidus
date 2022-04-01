ab5b9bd6243de920c1454abc0bfb6cea0d45ef2a
Martin Meyerhoff <mamhoff@gmail.com>
2022-04-01 10:43:48 +0200

    Add Promotion Rule "LineItemTaxon"
    This Rule can replace the use of `actionable?` on the taxon promotion
    rule.

---
+++ core/app/models/spree/promotion/rules/line_item_taxon.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Spree
+  class Promotion < Spree::Base
+    module Rules
+      class LineItemTaxon < PromotionRule
+        has_many :promotion_rule_taxons, class_name: 'Spree::PromotionRuleTaxon', foreign_key: :promotion_rule_id,
+          dependent: :destroy
+        has_many :taxons, through: :promotion_rule_taxons, class_name: 'Spree::Taxon'
+
+        MATCH_POLICIES = %w(include exclude)
+
+        validates_inclusion_of :preferred_match_policy, in: MATCH_POLICIES
+
+        preference :match_policy, :string, default: MATCH_POLICIES.first
+        def applicable?(promotable)
+          promotable.is_a?(Spree::LineItem)
+        end
+
+        def eligible?(line_item, _options = {})
+          found = Spree::Classification.where(
+            product_id: line_item.variant.product_id,
+            taxon_id: rule_taxon_ids_with_children
+          ).exists?
+
+          case preferred_match_policy
+          when 'include'
+            found
+          when 'exclude'
+            !found
+          else
+            raise "unexpected match policy: #{preferred_match_policy.inspect}"
+          end
+        end
+
+        def taxon_ids_string
+          taxons.pluck(:id).join(',')
+        end
+
+        def taxon_ids_string=(taxon_ids)
+          taxon_ids = taxon_ids.to_s.split(',').map(&:strip)
+          self.taxons = Spree::Taxon.find(taxon_ids)
+        end
+
+        private
+
+        # ids of taxons rules and taxons rules children
+        def rule_taxon_ids_with_children
+          taxons.flat_map { |taxon| taxon.self_and_descendants.ids }.uniq
+        end
+      end
+    end
+  end
+end
---
+++ core/spec/models/spree/promotion/rules/line_item_taxon_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Spree::Promotion::Rules::LineItemTaxon, type: :model do
+  let(:taxon)   { create :taxon, name: 'first' }
+  let(:taxon2)  { create :taxon, name: 'second' }
+  let(:order)   { create :order_with_line_items }
+  let(:product) { order.products.first }
+
+  let(:rule) do
+    described_class.create!(promotion: create(:promotion))
+  end
+
+  describe '#eligible?' do
+    let(:line_item) { order.line_items.first! }
+    let(:order) { create :order_with_line_items }
+    let(:taxon) { create :taxon, name: 'first' }
+
+    context 'with an invalid match policy' do
+      before do
+        rule.preferred_match_policy = 'invalid'
+        rule.save!(validate: false)
+        line_item.product.taxons << taxon
+        rule.taxons << taxon
+      end
+
+      it 'raises' do
+        expect {
+          rule.eligible?(line_item)
+        }.to raise_error('unexpected match policy: "invalid"')
+      end
+    end
+
+    context 'when a product has a taxon of a taxon rule' do
+      before do
+        product.taxons << taxon
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is eligible' do
+        expect(rule).to be_eligible(line_item)
+      end
+    end
+
+    context 'when a product has a taxon child of a taxon rule' do
+      before do
+        taxon.children << taxon2
+        product.taxons << taxon2
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is eligible' do
+        expect(rule).to be_eligible(line_item)
+      end
+
+      context "with 'exclude' match policy" do
+        before do
+          rule.update(preferred_match_policy: :exclude)
+        end
+
+        it "is not eligible" do
+          expect(rule).not_to be_eligible(line_item)
+        end
+      end
+    end
+
+    context 'when a product does not have taxon or child taxon of a taxon rule' do
+      before do
+        product.taxons << taxon2
+        rule.taxons << taxon
+        rule.save!
+      end
+
+      it 'is not eligible' do
+        expect(rule).not_to be_eligible(line_item)
+      end
+
+      context "with 'exclude' match policy" do
+        before do
+          rule.update(preferred_match_policy: :exclude)
+        end
+
+        it "is not eligible" do
+          expect(rule).to be_eligible(line_item)
+        end
+      end
+    end
+  end
+end
