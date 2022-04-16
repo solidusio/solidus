d65fbc82c1fc5de826ce3169b3c8401dfec81d63
John Pitchko <john.pitchko@icloud.com>
2022-04-16 09:35:36 -0600

    Validate email has a domain

--- core/lib/spree/core/validators/email.rb
+++ core/lib/spree/core/validators/email.rb
@@ -13,11 +13,16 @@ module Spree
   #
   class EmailValidator < ActiveModel::EachValidator
     EMAIL_REGEXP = URI::MailTo::EMAIL_REGEXP
+    EMAIL_DOMAIN_REGEXP = /\.[a-z]+/i
 
     def validate_each(record, attribute, value)
-      unless EMAIL_REGEXP.match? value
+      unless valid_email?(value)
         record.errors.add(attribute, :invalid, { value: value }.merge!(options))
       end
+    end
+
+    def valid_email?(value)
+      EMAIL_REGEXP.match?(value) && EMAIL_DOMAIN_REGEXP.match?(value)
     end
   end
 end
