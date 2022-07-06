31dd14f4a503405e3f5de0c6f32c6bbc8eb031b2
Ryan Woods <contact@ryanofwoods.com>
2022-07-06 12:22:11 +0200

    Wrap address_factory_spec in a context
    Improves the readability of the next commit.

--- core/spec/lib/spree/core/testing_support/factories/address_factory_spec.rb
+++ core/spec/lib/spree/core/testing_support/factories/address_factory_spec.rb
@@ -31,17 +31,19 @@ RSpec.describe 'address factory' do
     end
   end
 
-  describe 'when passing in a state and country' do
-    subject { build(:address, country_iso_code: country_iso_code, state_code: state_code) }
+  context 'states and countries' do
+    describe 'when passing in a state and country' do
+      subject { build(:address, country_iso_code: country_iso_code, state_code: state_code) }
 
-    context 'when the country has a state with proper code' do
-      let(:country_iso_code) { "US" }
-      let(:state_code) { "NY" }
+      context 'when the country has a state with proper code' do
+        let(:country_iso_code) { "US" }
+        let(:state_code) { "NY" }
 
-      it 'works' do
-        expect(subject).to be_valid
-        expect(subject.state.abbr).to eq("NY")
-        expect(subject.country.iso).to eq("US")
+        it 'works' do
+          expect(subject).to be_valid
+          expect(subject.state.abbr).to eq("NY")
+          expect(subject.country.iso).to eq("US")
+        end
       end
     end
   end
