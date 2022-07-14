441d7ac6451e43ba2e4d712adb8b428a1ba92e59
George Mendoza <gsmendoza@gmail.com>
2022-07-14 09:58:55 +0800

    Permit ActiveSupport::HashWithIndifferentAccess for safe load
    Spree::PromotionRule accepts ActiveSupport::HashWithIndifferentAccess as
    persistable preferences.
    
    See https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
    
    Affected backend specs:
    
    ```
    Failures:
    
      1) Promotion with option value rule adding an option value rule
         Failure/Error: existing = promotion.rules.map { |rule| rule.class.name }
    
         ActionView::Template::Error:
           Tried to load unspecified class: ActiveSupport::HashWithIndifferentAccess
         # /home/gsmendoza/repos/solidusio/solidus/core/lib/spree/preferences/persistable.rb:18:in `initialize_preference_defaults'
         # ./app/helpers/spree/promotion_rules_helper.rb:6:in `map'
         # ./app/helpers/spree/promotion_rules_helper.rb:6:in `options_for_promotion_rule_types'
         # ./app/views/spree/admin/promotions/_rules.html.erb:10:in `block in __home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions__rules_html_erb___2684429756418516206_286660'
         # ./app/views/spree/admin/promotions/_rules.html.erb:3:in `__home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions__rules_html_erb___2684429756418516206_286660'
         # ./app/views/spree/admin/promotions/edit.html.erb:29:in `__home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions_edit_html_erb___1754085209430652883_286520'
         # ./app/controllers/spree/admin/resource_controller.rb:27:in `block (2 levels) in edit'
         # ./app/controllers/spree/admin/resource_controller.rb:26:in `edit'
         # ------------------
         # --- Caused by: ---
         # Psych::DisallowedClass:
         #   Tried to load unspecified class: ActiveSupport::HashWithIndifferentAccess
         #   /home/gsmendoza/repos/solidusio/solidus/core/lib/spree/preferences/persistable.rb:18:in `initialize_preference_defaults'
    
      2) Promotion with option value rule with an existing option value rule deleting a product
         Failure/Error: existing = promotion.rules.map { |rule| rule.class.name }
    
         ActionView::Template::Error:
           Tried to load unspecified class: ActiveSupport::HashWithIndifferentAccess
         # /home/gsmendoza/repos/solidusio/solidus/core/lib/spree/preferences/persistable.rb:18:in `initialize_preference_defaults'
         # ./app/helpers/spree/promotion_rules_helper.rb:6:in `map'
         # ./app/helpers/spree/promotion_rules_helper.rb:6:in `options_for_promotion_rule_types'
         # ./app/views/spree/admin/promotions/_rules.html.erb:10:in `block in __home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions__rules_html_erb___2684429756418516206_286660'
         # ./app/views/spree/admin/promotions/_rules.html.erb:3:in `__home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions__rules_html_erb___2684429756418516206_286660'
         # ./app/views/spree/admin/promotions/edit.html.erb:29:in `__home_gsmendoza_repos_solidusio_solidus_backend_app_views_spree_admin_promotions_edit_html_erb___1754085209430652883_286520'
         # ./app/controllers/spree/admin/resource_controller.rb:27:in `block (2 levels) in edit'
         # ./app/controllers/spree/admin/resource_controller.rb:26:in `edit'
         # ------------------
         # --- Caused by: ---
         # Psych::DisallowedClass:
         #   Tried to load unspecified class: ActiveSupport::HashWithIndifferentAccess
         #   /home/gsmendoza/repos/solidusio/solidus/core/lib/spree/preferences/persistable.rb:18:in `initialize_preference_defaults'
    
    Finished in 3 minutes 34.5 seconds (files took 2.87 seconds to load)
    779 examples, 2 failures, 1 pending
    
    Failed examples:
    
    rspec ./spec/features/admin/promotions/option_value_rule_spec.rb:18 # Promotion with option value rule adding an option value rule
    rspec ./spec/features/admin/promotions/option_value_rule_spec.rb:78 # Promotion with option value rule with an existing option value rule deleting a product
    
    Randomized with seed 15122
    ```

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -16,7 +16,8 @@ module Spree
       end
 
       config.active_record.yaml_column_permitted_classes ||= []
-      config.active_record.yaml_column_permitted_classes |= [Symbol, BigDecimal]
+      config.active_record.yaml_column_permitted_classes |=
+        [Symbol, BigDecimal, ActiveSupport::HashWithIndifferentAccess]
 
       initializer "spree.environment", before: :load_config_initializers do |app|
         app.config.spree = Spree::Config.environment
