6db139cbf1e3ae04f0c1cb07e804cc09a46ddfb9
Alessandro Desantis <desa.alessandro@gmail.com>
2022-07-20 00:05:32 +0200

    Move legacy guides to a separate repository
    These have been moved to https://github.com/solidusio/legacy-guides,
    because they are going to be replaced by the new guides hosted at
    https://github.com/solidusio/edgeguides pretty soon.

--- guides/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-
-module.exports = merge(common, {
-  plugins: [
-    new MinifyPlugin()
-  ]
-});
