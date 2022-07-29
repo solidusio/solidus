904c62234a6a5c0d581736460f008479593fca1e
Adam Mueller <adam@super.gd>
2022-07-29 13:11:20 -0700

    Add order-level tax support to OrderTax
    We'll need to be able to easily pass back order-level tax data to our
    classes that apply taxes to the order. This adds in an extra array to
    allow for that and reuses the existing Tax::ItemTax class for containing
    the data.
    
    Co-Authored-By: Sung Noh <sung@meundies.com>

--- core/app/models/spree/tax/order_tax.rb
+++ core/app/models/spree/tax/order_tax.rb
@@ -8,13 +8,15 @@ module Spree
     # adjustments on an order.
     #
     # @attr_reader [Integer] order_id the {Spree::Order} these taxes apply to
+    # @attr_reader [Array<Spree::Tax::ItemTax>] order_taxes an array of tax
+    #   data for the order
     # @attr_reader [Array<Spree::Tax::ItemTax>] line_item_taxes an array of
     #   tax data for order's line items
     # @attr_reader [Array<Spree::Tax::ItemTax>] shipment_taxes an array of
     #   tax data for the order's shipments
     class OrderTax
       include ActiveModel::Model
-      attr_accessor :order_id, :line_item_taxes, :shipment_taxes
+      attr_accessor :order_id, :order_taxes, :line_item_taxes, :shipment_taxes
     end
   end
 end
