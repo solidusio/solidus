8aa618b92c9e3437afc4b9731a0031cfbf3bbf51
Elia Schito <elia@schito.me>
2022-10-25 12:32:31 +0200

    Track the corresponding minor branch in SSF

--- core/lib/generators/solidus/install/install_generator/install_frontend.rb
+++ core/lib/generators/solidus/install/install_generator/install_frontend.rb
@@ -49,7 +49,7 @@ module Solidus
           BundlerContext.bundle_cleanly { `bundle add solidus_auth_devise` }
           @generator_context.generate('solidus:auth:install --auto-run-migrations')
         end
-        @generator_context.apply "https://raw.githubusercontent.com/solidusio/solidus_starter_frontend/main/template.rb"
+        @generator_context.apply "https://raw.githubusercontent.com/solidusio/solidus_starter_frontend/v3.2/template.rb"
       end
 
       def auth_present?(installer_adds_auth)
