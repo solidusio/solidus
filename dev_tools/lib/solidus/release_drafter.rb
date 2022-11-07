143e3d2d376feafd5e3ff9c3dcbf063cc80dbf51
Marc Busqué <marc@lamarciana.com>
2022-11-07 16:00:29 +0100

    Automatically update the release draft when merging PRs
    We add a GitHub action to automatically update the next release draft
    [1] whenever a PR is merged.
    
    A PR is categorized under at least one of the changelog categories
    according to its labels [2]. Labels are already automatically added by the
    labeler GitHub action [3]. A PR can be skipped when the `changelog:skip`
    label is added.
    
    The chosen format mimics what we've done in the Changelog [4], so we can
    leverage this work further to automate its generation.
    
    We prepend the draft content with a comment warning about its automatic
    generation and append a link to the list of new commits since the
    previous tag.
    
    The candidate tag is computed like this:
    
    - We take the current tag in the base branch thanks to a custom callable
      workflow that extracts the information from the pipeline.
    - For PRs to master, the following tag is the subsequent minor or major
      version depending on the value of `Spree::VERSION`.
    - The following tag is the next patch version for PRs to branches
      corresponding to supported versions.
    
    We considered using Release Drafter action [5] as an alternative, but it
    reaches GitHub API abuse limit in our repo.
    
    [1] - https://docs.github.com/en/repositories/releasing-projects-on-github/managing-releases-in-a-repository
    [2] - https://github.com/solidusio/solidus/pull/4756
    [3] - https://github.com/actions/labeler
    [4] - https://github.com/solidusio/solidus/blob/0cae150e4f347cd9f232001bb945ecc9974abb29/CHANGELOG.md
    [5] - https://github.com/marketplace/actions/release-drafter

---
+++ dev_tools/lib/solidus/release_drafter.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require_relative 'release_drafter/client'
+require_relative 'release_drafter/builder'
+
+module Solidus
+  # Coordinates updating the release draft on GitHub when a PR is merged.
+  #
+  # @api private
+  class ReleaseDrafter
+    LABEL_PREFIX = 'changelog:'
+
+    LABELS = {
+      "#{LABEL_PREFIX}solidus_core" => 'Solidus Core',
+      "#{LABEL_PREFIX}solidus_backend" => 'Solidus Backend',
+      "#{LABEL_PREFIX}solidus_api" => 'Solidus API',
+      "#{LABEL_PREFIX}solidus_sample" => 'Solidus Sample',
+      "#{LABEL_PREFIX}solidus" => 'Solidus'
+    }.freeze
+
+    SKIP_LABEL = "#{LABEL_PREFIX}skip"
+
+    NO_EDIT_WARNING = <<~TXT
+      <!-- Please, don't edit manually. The content is automatically generated. -->
+    TXT
+
+    def initialize(github_token:, repository:,
+                   client: Client.new(github_token: github_token, repository: repository))
+      @repository = repository
+      @client = client
+    end
+
+    def call(pr_number:, current_tag:, candidate_tag:, branch:)
+      pr = @client.fetch_pr(pr_number: pr_number)
+      pr_labels = pr.labels.map(&:name)
+      matching_labels = LABELS.keys & pr_labels
+      return if pr_labels.include?(SKIP_LABEL) || matching_labels.empty?
+
+      draft = @client.fetch_draft(tag: candidate_tag)
+      Builder.new(draft: draft, categories: LABELS.values, prepend: NO_EDIT_WARNING, append: full_changelog(current_tag, candidate_tag))
+        .then { |builder| add_pr(builder, pr, matching_labels) }
+        .then { |draft| save_release(draft, candidate_tag, branch) }
+    end
+
+    private
+
+    def builder(draft, current_tag, candidate_tag)
+      Builder.new(draft: draft, categories: LABELS.values, prepend: NO_EDIT_WARNING, append: full_changelog(current_tag, candidate_tag))
+    end
+
+    def add_pr(builder, pr, labels)
+      builder.add(
+        number: pr.number,
+        categories: LABELS.values_at(*labels),
+        title: pr.title,
+        user: pr.user.login
+      )
+    end
+
+    def save_release(draft, candidate_tag, branch)
+      if draft.new?
+        @client.create_draft(draft: draft, tag: candidate_tag, branch: branch)
+      else
+        @client.update_draft(draft: draft, tag: candidate_tag, branch: branch)
+      end
+    end
+
+    def full_changelog(current_tag, candidate_tag)
+      <<~TXT
+
+
+        **Full Changelog**: https://github.com/#{@repository}/compare/#{current_tag}...#{candidate_tag}
+      TXT
+    end
+  end
+end
---
+++ dev_tools/spec/lib/solidus/release_drafter_spec.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+require 'ostruct'
+require 'solidus/release_drafter'
+
+RSpec.describe Solidus::ReleaseDrafter do
+  describe '#call' do
+    let(:client) do
+      Class.new do
+        def initialize(prs: {})
+          @prs = prs
+        end
+
+        def fetch_draft(tag:)
+          Solidus::ReleaseDrafter::Draft.empty
+        end
+
+        def fetch_pr(pr_number:)
+          @prs[pr_number]
+        end
+
+        def create_draft(draft:, tag:, branch:)
+          draft
+        end
+
+        def update_draft(draft:, tag:, branch:)
+          draft
+        end
+
+        def add_pr(pr_number:, labels: [])
+          @prs[pr_number] = OpenStruct.new(
+            number: pr_number,
+            title: "PR number #{pr_number}",
+            labels: labels.map { |name| OpenStruct.new(name: name) },
+            user: OpenStruct.new(login: 'alice')
+          )
+        end
+      end.new
+    end
+
+    subject do
+      described_class.new(
+        client: client,
+        github_token: 'dummy',
+        repository: 'fake/solidus'
+      )
+    end
+
+    it 'adds the PR under a single matching label' do
+      client.add_pr(pr_number: 1, labels: ['changelog:solidus_core'])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+      ).to match(/#{Regexp.escape("## Solidus Core\n- PR number 1 #1 (@alice)")}/)
+    end
+
+    it 'adds the PR under more than one matching label' do
+      client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+      ).to match(/#{Regexp.escape("## Solidus Core\n- PR number 1 #1 (@alice)")}.*#{Regexp.escape("## Solidus API\n- PR number 1 #1 (@alice)")}/m)
+    end
+
+    it 'ignores if no matching label is present' do
+      client.add_pr(pr_number: 1, labels: %w[other])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master')
+      ).to be(nil)
+    end
+
+    it 'ignores if the skipping label is present' do
+      client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:skip])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master')
+      ).to be(nil)
+    end
+
+    it 'adds a non-edit warning' do
+      client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+      ).to include("don't edit manually")
+    end
+
+    it 'adds a link to the full Changelog' do
+      client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
+
+      expect(
+        subject.call(pr_number: 1, current_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+      ).to include("**Full Changelog**: https://github.com/fake/solidus/compare/v3.0.0...v3.1.0")
+    end
+  end
+end
