9fec679b00e931b2139f8290b5c36a3a0f30a135
George Mendoza <gsmendoza@gmail.com>
2022-12-19 15:30:48 +0800

    Make install_payment_method easier to move to a payment method template

--- core/lib/generators/solidus/install/install_generator.rb
+++ core/lib/generators/solidus/install/install_generator.rb
@@ -198,21 +198,19 @@ module Solidus
     end
 
     def install_payment_method
+      return unless PAYMENT_METHODS.fetch(@selected_payment_method)
+
       plugin_name = PAYMENT_METHODS.fetch(@selected_payment_method)
-
-      return unless plugin_name
-
       plugin_generator_name = "#{plugin_name}:install"
 
       if bundler_context.dependencies[plugin_name]
         say_status :skipping, "#{plugin_name} is already in the gemfile"
-        return
+      else
+        gem plugin_name
+        run_bundle
+        run "spring stop" if defined?(Spring)
+        generate "#{plugin_generator_name} --skip_migrations=true"
       end
-
-      gem plugin_name
-      run_bundle
-      run "spring stop" if defined?(Spring)
-      generate "#{plugin_generator_name} --skip_migrations=true"
     end
 
     def run_migrations
