67ee6c9a35694501877e12b097dc2204e046fd2d
George Mendoza <gsmendoza@gmail.com>
2022-12-19 14:57:58 +0800

    Use SolidusPaypalCommercePlatform >= 1.a if frontend is StarterFrontend
    Fixes #4788.

--- core/lib/generators/solidus/install/app_templates/payment_method/paypal.rb
+++ core/lib/generators/solidus/install/app_templates/payment_method/paypal.rb
@@ -4,7 +4,7 @@ plugin_generator_name = "#{plugin_name}:install"
 if bundler_context.dependencies[plugin_name]
   say_status :skipping, "#{plugin_name} is already in the gemfile"
 else
-  gem plugin_name
+  gem plugin_name, @selected_frontend == 'solidus_frontend' ? '< 1' : '>= 1.a'
   run_bundle
   run "spring stop" if defined?(Spring)
   generate "#{plugin_generator_name} --skip_migrations=true"
