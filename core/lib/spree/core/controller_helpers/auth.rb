e82312316ebc40cc0ac147c10dafafd8bd6899c6
Marc Busqué <marc@lamarciana.com>
2023-01-16 06:40:51 +0100

    Revert the deprecation of `#redirect_back_or_default` method
    We deprecated `#redirect_back_or_default` method in #4533 [1]. However,
    the original plan was halted because of auth problems in
    solidus_auth_devise. See solidusio/solidus_auth_devise/232 [2] for details.
    
    [1] - https://github.com/solidusio/solidus/pull/4533
    [2] - https://github.com/solidusio/solidus_auth_devise/pull/232

--- core/lib/spree/core/controller_helpers/auth.rb
+++ core/lib/spree/core/controller_helpers/auth.rb
@@ -37,12 +37,6 @@ module Spree
         end
 
         def redirect_back_or_default(default)
-          Spree::Deprecation.warn <<~MSG
-            'Please use #stored_spree_user_location_or when using solidus_auth_devise.
-            Otherwise, please utilize #redirect_back provided in Rails 5+ or
-            #redirect_back_or_to in Rails 7+ instead'
-          MSG
-
           redirect_to(session["spree_user_return_to"] || default)
           session["spree_user_return_to"] = nil
         end
@@ -57,11 +51,6 @@ module Spree
         end
 
         def store_location
-          Spree::Deprecation.warn <<~MSG
-            store_location is being deprecated in solidus 4.0
-            without replacement
-          MSG
-
           Spree::UserLastUrlStorer.new(self).store_location
         end
 
