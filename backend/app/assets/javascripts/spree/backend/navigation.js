79ab2459ca85a4931d34427334f0f87b9bd11459
Massimiliano Lattanzio <massimiliano.lattanzio@gmail.com>
2023-01-26 18:01:25 +0100

    Fix sticky admin nav on long menu
    In Solidus, the admin nav menu becomes sticky after scrolling the page.
    If the menu contains a few entries, everything works fine.
    But if the menu includes some entries and the height exceed the window
    height, since the menu is sticky, the latest entries will be hidden.
    
    This commit applies the sticky only if the menu height does not exceed
    the window height.

--- backend/app/assets/javascripts/spree/backend/navigation.js
+++ backend/app/assets/javascripts/spree/backend/navigation.js
@@ -3,11 +3,21 @@ Spree.ready(function() {
     return $('.admin-nav-header').outerHeight() + $('.admin-nav-menu').outerHeight() + $('.admin-nav-footer').outerHeight();
   };
 
-  var checkSideBarFit = function() {
-    $('.admin-nav').toggleClass('fits', navHeight() < $(window).height());
+  var initStickyNavbar = function() {
+    $(".admin-nav-sticky, .admin-nav").stick_in_parent();
   };
 
-  $(".admin-nav-sticky, .admin-nav").stick_in_parent();
+  var detachStickyNavbar = function() {
+    $(".admin-nav-sticky, .admin-nav").trigger("sticky_kit:detach");
+  };
+
+  var checkSideBarFit = function() {
+    var fits = navHeight() < $(window).height();
+
+    $('.admin-nav').toggleClass('fits', fits);
+
+    fits ? initStickyNavbar() : detachStickyNavbar();
+  };
 
   checkSideBarFit();
   $(window).on('resize', checkSideBarFit);
