7c74da3b4b3ad4d112bb56f76ab1750e4c7861eb
George Mendoza <gsmendoza@gmail.com>
2023-01-30 14:10:04 +0800

    Revert "Fetch solidus_frontend from RubyGems instead of GitHub"
    This reverts commit aaa136f11121560cc903c8a223332e6220b04779.
    
    Since we'll point solidus_frontend to 3.4.0.dev, we should be able to
    fetch the master version of solidus_frontend from GitHub.

--- core/lib/generators/solidus/install/app_templates/frontend/classic.rb
+++ core/lib/generators/solidus/install/app_templates/frontend/classic.rb
@@ -3,7 +3,10 @@ solidus = Bundler.locked_gems.dependencies['solidus']
 if Bundler.locked_gems.dependencies['solidus_frontend']
   say_status :skipping, "solidus_frontend is already in the bundle", :blue
 else
-  bundle_command("add solidus_frontend")
+  # `solidus_frontend` is not sourced from rubygems if the solidus gem was not.
+  github_solidus_frontend = '--github solidusio/solidus_frontend' unless solidus.nil? || solidus.source.is_a?(Bundler::Source::Rubygems)
+
+  bundle_command("add solidus_frontend #{github_solidus_frontend}")
 end
 
 # Disable solidus_bolt installation from solidus_frontend as it can be
