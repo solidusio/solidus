3f091b248814a0e28f81611c73f5e4438898f0a8
Alberto Vena <kennyadsl@gmail.com>
2023-02-08 22:58:33 +0100

    Use a non-cachable reference to retrieve the starter template
    This avoids the template to be cached, so that changes to the starter
    are immediately reflected on new installations.
    
    We are seeing this caching happening in our CI, eg:
    
    https://app.circleci.com/pipelines/github/nebulab/solidus/819/workflows/ec62bd62-29a3-45f0-ac20-50964bb76619/jobs/11334
    
    The format is something similar to: "main@{2023-02-08 18:32:00}", which
    retrieve the content of the specified branch at a specific time. See:
    https://linux.die.net/man/1/git-rev-parse.
    
    Co-authored-by: elia <eliaschito@nebulab.com>

--- core/lib/generators/solidus/install/app_templates/frontend/starter.rb
+++ core/lib/generators/solidus/install/app_templates/frontend/starter.rb
@@ -1,3 +1,8 @@
 apply "#{__dir__}/break_down_solidus_gem.rb"
 run_bundle
-apply 'https://github.com/solidusio/solidus_starter_frontend/raw/main/template.rb'
+
+branch = "main"
+current_timestamp = Time.now.strftime("%F %H:%M:%S")
+uncachable_reference = "#{branch}@{#{current_timestamp}}"
+
+apply URI.encode("https://github.com/solidusio/solidus_starter_frontend/raw/#{uncachable_reference}/template.rb")
