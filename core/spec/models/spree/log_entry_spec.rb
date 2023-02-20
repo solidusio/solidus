2804ce23f895fbecdc853bd6edf47c796fd80dee
Elia Schito <elia@schito.me>
2023-02-20 16:09:40 +0100

    Ensure LogEntry only saves safe data
    This will prevent from saving values that are now allowed in the
    serialized column.

--- core/spec/models/spree/log_entry_spec.rb
+++ core/spec/models/spree/log_entry_spec.rb
@@ -56,13 +56,61 @@ RSpec.describe Spree::LogEntry, type: :model do
 
   describe '#parsed_details=' do
     it 'serializes the provided value to YAML' do
-      value = double("an object", to_yaml: "---\nfoo: bar")
+      log_entry = described_class.new(parsed_details: { "foo" => "bar" })
 
-      log_entry = described_class.new(parsed_details: value)
+      expect(log_entry.details).to eq("---\nfoo: bar\n")
+      expect(log_entry.parsed_details).to eq("foo" => "bar")
+    end
 
-      expect(value).to have_received(:to_yaml)
-      expect(log_entry.details).to eq("---\nfoo: bar")
-      expect(log_entry.parsed_details).to eq({"foo" => "bar"})
+    it 'allows aliases by default' do
+      x = []
+      x << x
+
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = x }.not_to raise_error
+    end
+
+    it 'can disable aliases and raises a meaningful exception when used' do
+      stub_spree_preferences(log_entry_allow_aliases: false)
+      x = []
+      x << x
+
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = x }.to raise_error(described_class::BadAlias, /log_entry_allow_aliases/)
+    end
+
+    it 'can dump ActiveMerchant::Billing::Response instances' do
+      response = ActiveMerchant::Billing::Response.new('success', 'message')
+
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = response }.not_to raise_error
+    end
+
+    it 'can dump ActiveSupport::TimeWithZone instances' do
+      time = Time.zone.now
+
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = time }.not_to raise_error
+    end
+
+    it 'can dump user specified class instances' do
+      stub_spree_preferences(log_entry_permitted_classes: ['Date'])
+
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = Date.new }.not_to raise_error
+    end
+
+    it 'raises a meaningful exception when a disallowed class is found' do
+      log_entry = described_class.new
+
+      expect { log_entry.parsed_details = Date.new }.to raise_error(
+        described_class::DisallowedClass, /log_entry_permitted_classes/
+      )
     end
   end
 end
