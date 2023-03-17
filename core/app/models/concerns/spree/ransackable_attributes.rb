456eb7349d142e19943e0cee020c8330cd16821e
Alberto Vena <kennyadsl@gmail.com>
2023-03-17 11:24:46 +0100

    Remove deprecated ransackable attributes
    Please, use the provided counterpars.
    
    Ref #4853.

--- core/app/models/concerns/spree/ransackable_attributes.rb
+++ core/app/models/concerns/spree/ransackable_attributes.rb
@@ -6,27 +6,6 @@ module Spree::RansackableAttributes
     class_attribute :allowed_ransackable_associations, default: []
     class_attribute :allowed_ransackable_attributes, default: []
     class_attribute :allowed_ransackable_scopes, default: []
-
-    def self.whitelisted_ransackable_associations
-      Spree::Deprecation.deprecation_warning(:whitelisted_ransackable_associations, 'use allowed_ransackable_associations instead')
-      allowed_ransackable_associations
-    end
-
-    def self.whitelisted_ransackable_associations=(new_value)
-      Spree::Deprecation.deprecation_warning(:whitelisted_ransackable_associations=, 'use allowed_ransackable_associations= instead')
-      self.allowed_ransackable_associations = new_value
-    end
-
-    def self.whitelisted_ransackable_attributes
-      Spree::Deprecation.deprecation_warning(:whitelisted_ransackable_attributes, 'use allowed_ransackable_attributes instead')
-      allowed_ransackable_attributes
-    end
-
-    def self.whitelisted_ransackable_attributes=(new_value)
-      Spree::Deprecation.deprecation_warning(:whitelisted_ransackable_attributes=, 'use allowed_ransackable_attributes= instead')
-      self.allowed_ransackable_attributes = new_value
-    end
-
     class_attribute :default_ransackable_attributes
     self.default_ransackable_attributes = %w[id]
   end
