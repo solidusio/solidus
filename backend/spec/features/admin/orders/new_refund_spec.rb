d525168410957f73e3e52740c3f015112e8ab0fc
Senem Soy <senem@super.gd>
2023-04-04 12:00:26 -0700

    Reproduce missing refund log entry bug
    This reproduces the issue mentioned here #4866
    
    Co-authored-by: Benjamin Willems <benjamin@super.gd>

--- backend/spec/features/admin/orders/new_refund_spec.rb
+++ backend/spec/features/admin/orders/new_refund_spec.rb
@@ -19,7 +19,25 @@ RSpec.describe 'New Refund creation', :js do
       click_button 'Refund'
     end
     expect(page).to have_content 'Refund has been successfully created!'
-    expect(page).to have_selector 'td', text: amount
+    within 'tr[data-hook="refunds_row"]' do
+      expect(page).to have_selector 'td', text: amount
+    end
+  end
+
+  it 'creates a new log entry for a refund', pending: 'This is pending because currently no log entry is created for this refund.' do
+    visit spree.new_admin_order_payment_refund_path(order, payment)
+    expect(page).not_to have_selector 'td', text: amount
+    within '.new_refund' do
+      fill_in 'refund_amount', with: amount
+      select reason.name, from: 'Reason'
+      click_button 'Refund'
+    end
+    expect(page).to have_content 'Refund has been successfully created!'
+    within 'tr[data-hook="payments_row"]' do
+      payment_number = find_all('td').first.text
+      click_on payment_number
+      expect(page).to have_selector 'tr.log_entry'
+    end
   end
 
   it 'disables the button at submit' do
