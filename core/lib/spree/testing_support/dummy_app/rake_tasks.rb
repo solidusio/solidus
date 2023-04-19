21cfd6959666ae597b0ed1e56280d6d81ccaceb6
Elia Schito <elia@schito.me>
2023-04-19 18:06:53 +0200

    Drop support for Rails 5 MigrationContext.new signature

--- core/lib/spree/testing_support/dummy_app/rake_tasks.rb
+++ core/lib/spree/testing_support/dummy_app/rake_tasks.rb
@@ -38,14 +38,10 @@ namespace :db do
     # railties:install:migrations and then db:migrate.
     # Migrations should be run one directory at a time
     ActiveRecord::Migrator.migrations_paths.each do |path|
-      if Rails.gem_version >= Gem::Version.new('6.0.0')
-        ActiveRecord::MigrationContext.new(
-          [path],
-          ActiveRecord::SchemaMigration
-        ).migrate
-      else
-        ActiveRecord::MigrationContext.new([path]).migrate
-      end
+      ActiveRecord::MigrationContext.new(
+        [path],
+        ActiveRecord::SchemaMigration
+      ).migrate
     end
 
     ActiveRecord::Base.clear_cache!
