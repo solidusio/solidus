528d3c9c79540ea88ca48138a5635725d605557c
Elia Schito <elia@schito.me>
2023-05-04 17:08:54 +0200

    Control wether to show the new admin with a cookie
    The admin installation need to be run after core routes are set so
    it takes precedence as new routes are added on top.

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 SolidusAdmin::Engine.routes.draw do
-  get '/', to: ->(env) { [200, {}, ['Hello from the new admin!']] }
+  get 'orders', to: ->(env) { [200, {}, ['Hello from the new admin orders section!']] }
 end
