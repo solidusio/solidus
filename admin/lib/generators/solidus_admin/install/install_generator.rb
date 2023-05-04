b8aef61fa36d83c046a65dd1d0a394f4c0411e3f
Elia Schito <elia@schito.me>
2023-05-04 17:08:54 +0200

    Control wether to show the new admin with a cookie
    The admin installation need to be run after core routes are set so
    it takes precedence as new routes are added on top.

--- admin/lib/generators/solidus_admin/install/install_generator.rb
+++ admin/lib/generators/solidus_admin/install/install_generator.rb
@@ -4,7 +4,11 @@ module SolidusAdmin
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def install_solidus_core_support
-        route "mount SolidusAdmin::Engine, at: '/solidus_admin'"
+        route <<~RUBY
+          mount SolidusAdmin::Engine, at: '/admin', constraints: ->(req) {
+            req.cookies['solidus_admin'] == 'true'
+          }
+        RUBY
       end
     end
   end
