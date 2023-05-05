a727675355f13b010e1530f88d69943357641457
Marc Busqué <marc@lamarciana.com>
2023-05-05 16:09:07 +0200

    Prepare example to show how tailwind is used in solidus_admin
    It can be rendered at `/admin/orders`, and we make use of it both in the
    layout and action templates.
    
    We need to isolate the engine so that the right controller is used.

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 SolidusAdmin::Engine.routes.draw do
-  get 'orders', to: ->(env) { [200, {}, ['Hello from the new admin orders section!']] }
+  resources :orders, only: :index
 end
