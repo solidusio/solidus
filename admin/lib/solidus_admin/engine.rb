97e7c84c6f8a604aeb42ec566ed6481df505e613
Marc Busqué <marc@lamarciana.com>
2023-05-05 16:09:07 +0200

    Prepare example to show how tailwind is used in solidus_admin
    It can be rendered at `/admin/orders`, and we make use of it both in the
    layout and action templates.
    
    We need to isolate the engine so that the right controller is used.

--- admin/lib/solidus_admin/engine.rb
+++ admin/lib/solidus_admin/engine.rb
@@ -2,6 +2,8 @@
 
 module SolidusAdmin
   class Engine < ::Rails::Engine
+    isolate_namespace SolidusAdmin
+
     initializer "solidus_admin.assets" do |app|
       app.config.assets.precompile += %w[solidus_admin_manifest]
     end
