4b4d93374bcabad9a3c8752c6e96f4d0de3f6622
Marc Busqué <marc@lamarciana.com>
2023-05-05 16:06:47 +0200

    Prepare assets pipeline for solidus_admin engine
    We just add an empty manifest to the assets pipeline from the
    solidus_admin engine.

--- admin/lib/solidus_admin/engine.rb
+++ admin/lib/solidus_admin/engine.rb
@@ -2,5 +2,8 @@
 
 module SolidusAdmin
   class Engine < ::Rails::Engine
+    initializer "solidus_admin.assets" do |app|
+      app.config.assets.precompile += %w[solidus_admin_manifest]
+    end
   end
 end
