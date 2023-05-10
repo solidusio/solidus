0b8946c9a283ff692c8fea2d993c513148e7df68
Marc Busqué <marc@lamarciana.com>
2023-05-10 05:26:23 +0200

    Backport recent changes to the development tools
    Although it's not required, keeping parity makes them easier to
    maintain. We're backporting:
    
    - 1879d0d6074212fd48524403c5b2162e6fa1be96
    - 6c508a0d3a036fedcf970abd4592189cf3f62a36
    - dc9b9dff5b406002f8b0d2c10e3fad7ba45331f7
    - ecaa92efdf96dfcd32500412ce3685acc1c16103
    - a823c04b36eff71ced1064acffcf658002fe3374
    - b5acc4a8b0f31901d90b05740df55559238eb847
    - c035686bc571998676a8e0657e912ce2dafaf50c

--- dev_tools/spec/lib/solidus/release_drafter_spec.rb
+++ dev_tools/spec/lib/solidus/release_drafter_spec.rb
@@ -50,23 +50,23 @@ RSpec.describe Solidus::ReleaseDrafter do
       client.add_pr(pr_number: 1, labels: ['changelog:solidus_core'])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
-      ).to match(/#{Regexp.escape("## Solidus Core\n- PR number 1 #1 (@alice)")}/)
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main').content
+      ).to match(/#{Regexp.escape("## Solidus Core\n* PR number 1 by @alice in https://github.com/fake/solidus/pull/1")}/)
     end
 
     it 'adds the PR under more than one matching label' do
       client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
-      ).to match(/#{Regexp.escape("## Solidus Core\n- PR number 1 #1 (@alice)")}.*#{Regexp.escape("## Solidus API\n- PR number 1 #1 (@alice)")}/m)
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main').content
+      ).to match(/#{Regexp.escape("## Solidus Core\n* PR number 1 by @alice in https://github.com/fake/solidus/pull/1")}.*#{Regexp.escape("## Solidus API\n* PR number 1 by @alice in https://github.com/fake/solidus/pull/1")}/m)
     end
 
     it 'ignores if no matching label is present' do
       client.add_pr(pr_number: 1, labels: %w[other])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master')
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main')
       ).to be(nil)
     end
 
@@ -74,7 +74,7 @@ RSpec.describe Solidus::ReleaseDrafter do
       client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:skip])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master')
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main')
       ).to be(nil)
     end
 
@@ -82,7 +82,7 @@ RSpec.describe Solidus::ReleaseDrafter do
       client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main').content
       ).to include("don't edit manually")
     end
 
@@ -90,7 +90,7 @@ RSpec.describe Solidus::ReleaseDrafter do
       client.add_pr(pr_number: 1, labels: %w[changelog:solidus_core changelog:solidus_api])
 
       expect(
-        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'master').content
+        subject.call(pr_number: 1, current_diff_source_tag: 'v3.0.0', candidate_tag: 'v3.1.0', branch: 'main').content
       ).to include("**Full Changelog**: https://github.com/fake/solidus/compare/v3.0.0...v3.1.0")
     end
   end
