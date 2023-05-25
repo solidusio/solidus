1da562cc10e91baba7374512ca388f5f381f0c65
Marc Busqué <marc@lamarciana.com>
2023-05-25 10:06:39 +0200

    Dummy main navigation component for Solidus Admin
    This is only to showcase the structure of a Solidus Admin component,
    where all components should inherit from the BaseComponent class.

---
+++ admin/app/components/solidus_admin/base_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module SolidusAdmin
+  # BaseComponent is the base class for all components in Solidus Admin.
+  class BaseComponent < ViewComponent::Base
+  end
+end
