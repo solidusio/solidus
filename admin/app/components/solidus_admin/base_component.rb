d44841001402086868baf8a4e79230e155d99aba
Marc Busqué <marc@lamarciana.com>
2023-05-25 15:49:11 +0200

    Use a configurable collection component for nav items in the admin
    We're transforming the main nav item component into a collection
    component to allow reusing it from within the main nav component.
    
    We're separating the data for a nav item from its view representation.
    A `SolidusAdmin::MainNavItem` represents a nav item, with its title and
    position.
    
    We're using `dry-system` to register the available nav items into the
    `SolidusAdmin::Container` registry. On top of that, host applications
    can register their own ones. The `main_nav` key is used to register
    items in the collection. All of them can be accessed through
    `SolidusAdmin::Container.within_namespace('main_nav')`.
    
    Example of how to register a nav item:
    
    ```ruby
    require "solidus_admin/main_nav_item"
    
    stats = SolidusAdmin::MainNavItem.new(
      title: "Stats",
      position: "15"
    )
    
    SolidusAdmin::Container.register("main_nav.stats", stats)
    ```
    
    Once we're out of the showcase phase, we can pass the URL as a lambda
    taking the request context.

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -3,5 +3,8 @@
 module SolidusAdmin
   # BaseComponent is the base class for all components in Solidus Admin.
   class BaseComponent < ViewComponent::Base
+    def container
+      SolidusAdmin::Container
+    end
   end
 end
