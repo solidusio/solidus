a928e0784453c6a7f63ff4fe52899d5ed97ab9be
Marc Busqué <marc@lamarciana.com>
2023-05-26 14:36:06 +0200

    Inject nested components into parent component initialization in admin
    By following this pattern, overriding the parent component becomes
    easier, as only the initializer is needed.
    
    That also allows to avoid slots and its problem with deeply nested
    structures [1].
    
    [1] - https://github.com/ViewComponent/view_component/discussions/867

--- admin/app/components/solidus_admin/main_nav_component.rb
+++ admin/app/components/solidus_admin/main_nav_component.rb
@@ -3,14 +3,26 @@
 module SolidusAdmin
   # Renders the main navigation of Solidus Admin.
   class MainNavComponent < BaseComponent
+    def initialize(main_nav_item_component: component("main_nav_item"), items: container.within_namespace("main_nav"))
+      @main_nav_item_component = main_nav_item_component
+      @items = items
+      super
+    end
+
     erb_template <<~ERB
       <nav>
         <%=
-          render component("main_nav_item").with_collection(
-            container.all("main_nav").sort_by(&:position)
+          render @main_nav_item_component.with_collection(
+            sorted_items
           )
         %>
       </nav>
     ERB
+
+    private
+
+    def sorted_items
+      @items.sort_by(&:position)
+    end
   end
 end
