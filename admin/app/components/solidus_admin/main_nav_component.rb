0c18a7fcae3ed1ba422555beeed490c0c84be653
Marc Busqué <marc@lamarciana.com>
2023-05-26 11:09:30 +0200

    Allow overriding admin component templates
    Unfortunately, view_component doesn't allow overriding templates from
    the host application by just having the same relative path [1].
    
    For now, we'll use the inline templates feature from v3 [2] so that host
    applications can override only the template while still inheriting the
    rest of view logic.
    
    [1] - https://github.com/ViewComponent/view_component/issues/411
    [2] - https://viewcomponent.org/guide/templates.html#inline

--- admin/app/components/solidus_admin/main_nav_component.rb
+++ admin/app/components/solidus_admin/main_nav_component.rb
@@ -3,5 +3,14 @@
 module SolidusAdmin
   # Renders the main navigation of Solidus Admin.
   class MainNavComponent < BaseComponent
+    erb_template <<~ERB
+      <nav>
+        <%=
+          render component("main_nav_item").with_collection(
+            container.all("main_nav").sort_by(&:position)
+          )
+        %>
+      </nav>
+    ERB
   end
 end
