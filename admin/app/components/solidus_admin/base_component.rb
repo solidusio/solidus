8904313f169641ab1161e3bf1f3fd75f6c146e8e
Marc Busqué <marc@lamarciana.com>
2023-06-05 06:33:53 +0200

    Auto-inject dependencies into components
    By using dry-system's auto_inject feature, we can get rid of the
    boilerplate code that injects dependencies into components.
    
    We create a `SolidusAdmin::Import` constant that can be used to dynamically
    create readers for each declared dependency.
    
    We also register a top level `main_nav_items` dependency to avoid
    calling a private in nature `.within_namespace` method.
    
    [1] - https://dry-rb.org/gems/dry-system/1.0/dependency-auto-injection/

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "solidus_admin/system/import"
+
 module SolidusAdmin
   # BaseComponent is the base class for all components in Solidus Admin.
   class BaseComponent < ViewComponent::Base
