28e91ca4c4a8ef5b56dd4ee13da3011339641241
Marc Busqué <marc@lamarciana.com>
2023-06-05 06:33:53 +0200

    Auto-inject dependencies into components
    By using dry-system's auto_inject feature, we can get rid of the
    boilerplate code that injects dependencies into components.
    
    We create a `SolidusAdmin::Import` constant that can be used to dynamically
    create readers for each declared dependency.
    
    We also register a top level `main_nav_items` dependency to avoid
    calling a private in nature `.within_namespace` method.
    
    [1] - https://dry-rb.org/gems/dry-system/1.0/dependency-auto-injection/

--- admin/app/components/solidus_admin/main_nav_component.rb
+++ admin/app/components/solidus_admin/main_nav_component.rb
@@ -3,16 +3,15 @@
 module SolidusAdmin
   # Renders the main navigation of Solidus Admin.
   class MainNavComponent < BaseComponent
-    def initialize(main_nav_item_component: component("main_nav_item"), items: container.within_namespace("main_nav"))
-      @main_nav_item_component = main_nav_item_component
-      @items = items
-      super
-    end
+    include Import[
+      "main_nav_item_component",
+      items: "main_nav_items"
+    ]
 
     erb_template <<~ERB
       <nav>
         <%=
-          render @main_nav_item_component.with_collection(
+          render main_nav_item_component.with_collection(
             sorted_items
           )
         %>
@@ -22,7 +21,7 @@ module SolidusAdmin
     private
 
     def sorted_items
-      @items.sort_by(&:position)
+      items.sort_by(&:position)
     end
   end
 end
