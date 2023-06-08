4957779675f250fae3060c3acc4f3ff70aba5f24
Elia Schito <elia@schito.me>
2023-06-08 18:06:40 +0200

    Add an identifier helper to the base component
    This will help avoiding explicit references to the stimulus controller
    identifier.

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -7,5 +7,9 @@ module SolidusAdmin
   class BaseComponent < ViewComponent::Base
     include ViewComponent::InlineTemplate
     include SolidusAdmin::ContainerHelper
+
+    def identifier
+      @identifier ||= module_parent.to_s.underscore.dasherize.gsub(%r{/}, '--')
+    end
   end
 end
