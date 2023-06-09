d16b91ab8a39a1475ad169e1faa9f628483fa08f
Elia Schito <elia@schito.me>
2023-06-09 16:34:27 +0200

    Use /admin/account as the solidus_admin testbed
    This page will probably be necessary in order to manage user
    preferences like locale, timezone, etc.

--- admin/app/controllers/solidus_admin/orders_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SolidusAdmin
-  # rubocop:disable Rails/ApplicationController
-  class OrdersController < ActionController::Base
-    layout 'solidus_admin/application'
-  end
-  # rubocop:enable Rails/ApplicationController
-end
