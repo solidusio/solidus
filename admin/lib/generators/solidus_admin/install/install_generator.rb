8a55702f7130a3eb7764f61df4d19a1e671d257c
Marc Busqué <marc@lamarciana.com>
2023-06-13 06:08:28 +0200

    Generate solidus_admin initializer on installation
    It's good to have a solidus_admin initializer on installation, so that
    users can easily configure the admin interface and see what options are
    available.

--- admin/lib/generators/solidus_admin/install/install_generator.rb
+++ admin/lib/generators/solidus_admin/install/install_generator.rb
@@ -3,12 +3,18 @@
 module SolidusAdmin
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('templates', __dir__)
+
       def install_solidus_core_support
         route <<~RUBY
           mount SolidusAdmin::Engine, at: '/admin', constraints: ->(req) {
             req.cookies['solidus_admin'] == 'true'
           }
         RUBY
+      end
+
+      def copy_initializer
+        copy_file "config/initializers/solidus_admin.rb"
       end
 
       def ignore_tailwind_build_files
