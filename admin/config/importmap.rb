f56ece1ea49c684d6078f94f137a24efc754436f
Elia Schito <elia@schito.me>
2023-06-14 18:33:21 +0200

    Add hotwire support to the admin

--- admin/config/importmap.rb
+++ admin/config/importmap.rb
@@ -1,1 +1,7 @@
+# Stimulus & Turbo
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/turbo-rails", to: "turbo.js"
+
 pin "solidus_admin/application", preload: true
+pin_all_from SolidusAdmin::Engine.root.join("app/javascript/solidus_admin/controllers"), under: "solidus_admin/controllers"
