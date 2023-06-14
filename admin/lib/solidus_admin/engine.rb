83352a026c863440a9df3fa5a5d490aeae38b5eb
Elia Schito <elia@schito.me>
2023-06-14 18:26:00 +0200

    Setup an admin specific importmap instance
    - The helper is identical to the original one except for defaults and
      passing a custom importmap (see https://github.com/rails/importmap-rails/pull/187)
    - The reloading configuration is a simplified version of the one in
      importmap-rails, same for the reloader

--- admin/lib/solidus_admin/engine.rb
+++ admin/lib/solidus_admin/engine.rb
@@ -2,6 +2,7 @@
 
 require "view_component"
 require "solidus_admin/container"
+require "solidus_admin/importmap_reloader"
 
 module SolidusAdmin
   class Engine < ::Rails::Engine
@@ -11,8 +12,34 @@ module SolidusAdmin
       require "solidus_admin/configuration"
     end
 
+    initializer "solidus_admin.importmap" do
+      SolidusAdmin::Config.importmap_paths.each { |path| SolidusAdmin.importmap.draw(path) }
+    end
+
+    initializer "solidus_admin.importmap.reloader" do |app|
+      ImportmapReloader.new.tap do |reloader|
+        reloader.execute
+        app.reloaders << reloader
+        app.reloader.to_run { reloader.execute }
+      end
+    end
+
     initializer "solidus_admin.assets" do |app|
       app.config.assets.precompile += %w[solidus_admin_manifest.js]
+    end
+
+    initializer "solidus_admin.importmap.cache_sweeper" do |app|
+      if app.config.importmap.sweep_cache
+        SolidusAdmin.importmap.cache_sweeper(watches: SolidusAdmin::Config.importmap_cache_sweepers)
+
+        ActiveSupport.on_load(:action_controller_base) do
+          before_action { SolidusAdmin.importmap.cache_sweeper.execute_if_updated }
+        end
+      end
+    end
+
+    initializer "solidus_admin.importmap.assets" do |app|
+      app.config.assets.paths << SolidusAdmin::Engine.root.join("app/javascript")
     end
 
     initializer "solidus_admin.main_nav_items_provider" do
