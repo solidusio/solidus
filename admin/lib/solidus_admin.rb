38b7ef50841573d26ce9881f0ddee83e8859cf7e
Elia Schito <elia@schito.me>
2023-06-14 18:26:00 +0200

    Setup an admin specific importmap instance
    - The helper is identical to the original one except for defaults and
      passing a custom importmap (see https://github.com/rails/importmap-rails/pull/187)
    - The reloading configuration is a simplified version of the one in
      importmap-rails, same for the reloader

--- admin/lib/solidus_admin.rb
+++ admin/lib/solidus_admin.rb
@@ -2,3 +2,11 @@
 
 require 'solidus_admin/version'
 require 'solidus_admin/engine'
+
+require 'importmap-rails'
+
+module SolidusAdmin
+  singleton_class.attr_accessor :importmap
+
+  self.importmap = Importmap::Map.new
+end
