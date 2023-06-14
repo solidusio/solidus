7084f89ee8bba7a428ab12c90d49229eab39c0bf
Martin Meyerhoff <mamhoff@gmail.com>
2023-06-14 12:15:17 +0200

    Add "Percent" calculator
    Over time, it turned out that Solidus core has two calculators doing the
    same thing, `FlatPercentItemTotal` as well as `PercentOnLineItem`. This
    calculator has all the features of both (except for the protection
    against overpromoting items, which the adjuster classes do).

---
+++ friendly_promotions/app/models/solidus_friendly_promotions/calculators/percent.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_dependency 'spree/calculator'
+
+module SolidusFriendlyPromotions
+  module Calculators
+    class Percent < Spree::Calculator
+      preference :percent, :decimal, default: 0
+
+      def compute(object)
+        preferred_currency = object.order.currency
+        currency_exponent = ::Money::Currency.find(preferred_currency).exponent
+        (object.amount * preferred_percent / 100).round(currency_exponent)
+      end
+    end
+  end
+end
