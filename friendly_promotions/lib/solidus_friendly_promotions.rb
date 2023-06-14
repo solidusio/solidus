16743bd44a87325258cb7ca5be84830b7917727e
Martin Meyerhoff <mamhoff@gmail.com>
2023-06-14 14:25:13 +0200

    Make sure Solidus is around when running `bin/rake`

--- friendly_promotions/lib/solidus_friendly_promotions.rb
+++ friendly_promotions/lib/solidus_friendly_promotions.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "spree"
 require "solidus_friendly_promotions/configuration"
 require "solidus_friendly_promotions/version"
 require "solidus_friendly_promotions/engine"
