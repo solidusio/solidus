d695da750ebc2d674f73380cacb464c5c88a5579
Elia Schito <elia@schito.me>
2023-06-19 16:43:11 +0200

    Ensure tailwind classes are properly read from components
    - It was missing component .erb templates and .js controllers
    - We're not supposed to use non-ERB templates in the SoliduAdmin
      engine
    - The SolidusAdmin engine has no public directory
    
    Co-Authored-By: Rainer Dema <rainer.dema@gmail.com>

--- admin/lib/generators/solidus_admin/install/templates/config/initializers/solidus_admin.rb
+++ admin/lib/generators/solidus_admin/install/templates/config/initializers/solidus_admin.rb
@@ -8,12 +8,12 @@ SolidusAdmin::Config.configure do |config|
 
   # Add custom paths for TailwindCSS to scan for styles. By default, it already
   # includes the following paths:
-  #
   # - public/solidus_admin/*.html
   # - app/helpers/solidus_admin/**/*.rb
   # - app/assets/javascripts/solidus_admin/**/*.js
   # - app/views/solidus_admin/**/*.{erb,haml,html,slim}
-  # - app/components/solidus_admin/**/*.rb
+  # - app/components/solidus_admin/**/*.{rb,erb,haml,html,slim,js}
+  #
   # config.tailwind_content << Rails.root.join("app/my/custom/path/**.rb")
 
   # Append custom stylesheets to be compiled by TailwindCSS.
