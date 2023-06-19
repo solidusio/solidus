a2b569961064588bd3923f17e6669bc90ec82ff1
Marc Busqué <marc@lamarciana.com>
2023-06-19 16:09:22 +0200

    Remove unused admin auto-injector
    We decided to hold off on this feature in
    https://github.com/solidusio/solidus/pull/5136, so for now we can remove
    the unused code.

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "solidus_admin/system/import"
-
 module SolidusAdmin
   # BaseComponent is the base class for all components in Solidus Admin.
   class BaseComponent < ViewComponent::Base
