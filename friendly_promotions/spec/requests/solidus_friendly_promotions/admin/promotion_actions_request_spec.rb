870af34e8ca73b8c2c6c2b0f6a26362698bb1b31
Martin Meyerhoff <mamhoff@gmail.com>
2023-06-20 11:31:29 +0200

    Import Promotion Actions Controller

---
+++ friendly_promotions/spec/requests/solidus_friendly_promotions/admin/promotion_actions_request_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SolidusFriendlyPromotions::Admin::PromotionActionsController, type: :request do
+  stub_authorization!
+
+  let!(:promotion) { create(:promotion) }
+
+  it "can create a promotion action of a valid type" do
+    post solidus_friendly_promotions.admin_promotion_promotion_actions_path(promotion_id: promotion.id, action_type: "Spree::Promotion::Actions::CreateAdjustment")
+    expect(response).to be_redirect
+    expect(response).to redirect_to spree.edit_admin_promotion_path(promotion)
+    expect(promotion.actions.count).to eq(1)
+  end
+
+  it "can not create a promotion action of an invalid type" do
+    post solidus_friendly_promotions.admin_promotion_promotion_actions_path(promotion_id: promotion.id, action_type: "Spree::InvalidType")
+    expect(response).to be_redirect
+    expect(response).to redirect_to spree.edit_admin_promotion_path(promotion)
+    expect(promotion.rules.count).to eq(0)
+  end
+end
