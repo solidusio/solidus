1dcc6735395f2d898908a69e842e4fdd1da16c05
Martin Meyerhoff <mamhoff@gmail.com>
2023-06-20 16:22:55 +0200

    Add all promotion routes

--- friendly_promotions/config/routes.rb
+++ friendly_promotions/config/routes.rb
@@ -2,6 +2,14 @@
 
 SolidusFriendlyPromotions::Engine.routes.draw do
   namespace :admin do
-    resources :promotions, only: [:index, :new]
+    resources :promotions do
+      resources :promotion_rules
+      resources :promotion_actions
+      resources :promotion_codes, only: [:index, :new, :create]
+      resources :promotion_code_batches, only: [:index, :new, :create] do
+        get '/download', to: "promotion_code_batches#download", defaults: { format: "csv" }
+      end
+    end
+    resources :promotion_categories, except: [:show]
   end
 end
