ff80f8f3db4ca11820d7a76d115e3d2dc170d7fe
Elia Schito <elia@schito.me>
2023-06-20 12:47:00 +0200

    Drop our custom importmap helper
    A new version of importmap-rails has been released that supports
    passing an importmap from outside.

--- admin/app/controllers/solidus_admin/base_controller.rb
+++ admin/app/controllers/solidus_admin/base_controller.rb
@@ -4,6 +4,5 @@ module SolidusAdmin
   class BaseController < Spree::BaseController
     layout 'solidus_admin/application'
     helper 'solidus_admin/container'
-    helper 'solidus_admin/importmap'
   end
 end
