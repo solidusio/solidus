2e23ce41c4cbacf59fd373dde06f7e89bca07960
Elia Schito <elia@schito.me>
2023-06-21 18:05:36 +0200

    Allow admin components to access `spree` routes
    By default other engine namespaces are not available in the component
    view context, so we need to give it explicit access.
    
    Co-Authored-By: Rainer Dema <rainer.dema@gmail.com>

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -9,5 +9,9 @@ module SolidusAdmin
     def stimulus_id
       @stimulus_id ||= self.class.module_parent.to_s.underscore.dasherize.gsub(%r{/}, '--')
     end
+
+    def spree
+      @spree ||= Spree::Core::Engine.routes.url_helpers
+    end
   end
 end
