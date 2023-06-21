ddcf110ff79a13981aea368f697dce0167fb663c
Elia Schito <elia@schito.me>
2023-06-21 18:30:42 +0200

    Extract cell values to helper methods in the product/index component
    This way the template becomes more readable and sets a standard for
    future columns.
    
    Co-Authored-By: Rainer Dema <rainer.dema@gmail.com>

--- admin/app/components/solidus_admin/products/index/component.rb
+++ admin/app/components/solidus_admin/products/index/component.rb
@@ -4,4 +4,21 @@ class SolidusAdmin::Products::Index::Component < SolidusAdmin::BaseComponent
   def initialize(products:)
     @products = products
   end
+
+  # @!visibility private
+
+  def image_column(product)
+    link_to(
+      image_tag(product.gallery.images.first.url(:small), class: 'h-[40px] w-[40px] max-w-[40px]'),
+      spree.edit_admin_product_path(product),
+    )
+  end
+
+  def name_column(product)
+    link_to product.name, spree.edit_admin_product_path(product)
+  end
+
+  def price_column(product)
+    product.master.display_price.to_html
+  end
 end
