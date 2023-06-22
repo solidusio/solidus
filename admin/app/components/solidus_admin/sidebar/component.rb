7c1bd7a72c8009d3bc4ded3bd6b782465ce27431
Marc Busqué <marc@lamarciana.com>
2023-06-22 16:43:17 +0200

    Style store link from the admin sidebar
    A few decisions are made here when it comes to following the provided
    Figma design. TL;DR: Figma, from which we have less control, wins over
    Tailwind, which we can change at any time:
    
    - We start configuring Tailwind to avoid using hardcoded `[style]`
      attributes. We'll continue this whenever we find the need to add
      new styles.
    - Even when both Tailwind and Figma use the same style name and value,
      we still explicitly configure it in Tailwind. This is to avoid future
      discrepancies and to make it easier to change. Example: `fontSize/sm`.
    - When Tailwind and Figma use the same style value but with different
      names, we add the Figma name to Tailwind. Ideally, we should try to
      change Figma to use the same name as Tailwind, but, for now, it'll be
      easy to compare styles in this way, and a rename from the code
      part will be straightforward. Example: `fontSize/tiny`.
    - When Tailwind and Figma use different style values for the same style
      name, we change Tailwind to use the Figma value. Example:
      `fontWeight/bold`, which is `600` for the Figma style and `700` for
      Tailwind.
    - For spacing, we didn't find the need to configure anything different
      from the default Tailwind spacing scale, so we use it as is. That
      keeps things much simpler in something that could be cumbersome to
      think about.
    
    Another rule we followed is that when needing to separate different
    blocks, we use the top margin from the element below.
    
    We also followed a loose rule where we stack the CSS classes in
    different lines, from top to bottom:
    
    - Spacing in relation to other elements: display, flex, etc...
    - Spacing in relation to the element itself: padding, margin, etc...
    - Border styles: color, width, radius...
    - Text styles: font size, color...
    - Font styles: familiy, weight...
    - Background styles: color, image...

--- admin/app/components/solidus_admin/sidebar/component.rb
+++ admin/app/components/solidus_admin/sidebar/component.rb
@@ -3,6 +3,7 @@
 # Renders the sidebar
 class SolidusAdmin::Sidebar::Component < SolidusAdmin::BaseComponent
   def initialize(
+    store:,
     logo_path: SolidusAdmin::Config.logo_path,
     items: container["main_nav_items"],
     item_component: component("sidebar/item")
@@ -10,6 +11,7 @@ class SolidusAdmin::Sidebar::Component < SolidusAdmin::BaseComponent
     @logo_path = logo_path
     @items = items
     @item_component = item_component
+    @store = store
   end
 
   erb_template <<~ERB
@@ -17,10 +19,31 @@ class SolidusAdmin::Sidebar::Component < SolidusAdmin::BaseComponent
       border-r border-r-gray-100
       col-start-1 col-end-2
       lg:col-start-1 lg:col-end-3
+      bg-white
       h-screen
       p-[16px]
     ">
       <%= image_tag @logo_path, alt: "Solidus" %>
+      <%= link_to @store.url,
+            class: "
+              block
+              mt-4 px-2 py-1.5
+              border border-gray-100 rounded-sm shadow-sm
+              bg-arrow-right-up-line bg-right-top bg-no-repeat bg-origin-content
+            " do %>
+        <p class="
+          text-sm text-black
+          font-sans font-bold
+        ">
+          <%= @store.name %>
+        </p>
+        <p class="
+          text-tiny text-gray-500
+          font-sans
+        ">
+          <%= @store.url %>
+        </p>
+      <% end %>
       <nav data-controller="main-nav">
         <%= render @item_component.with_collection(items) %>
       </nav>
