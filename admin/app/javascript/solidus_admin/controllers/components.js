e5bd5e65f3fb3f1592950693bb9e46af65ce31bc
Elia Schito <elia@schito.me>
2023-06-26 16:47:31 +0200

    Don't hardcode the stimulus controller suffix for components

--- admin/app/javascript/solidus_admin/controllers/components.js
+++ admin/app/javascript/solidus_admin/controllers/components.js
@@ -3,19 +3,18 @@ import "@hotwired/stimulus"
 const registeredControllers = {}
 
 // Eager load all controllers registered beneath the `under` path in the import map to the passed application instance.
-export function eagerLoadComponentsFrom(under, application) {
-  const paths = Object.keys(parseImportmapJson()).filter(path => path.match(new RegExp(`^${under}/.*/component$`)))
-  paths.forEach(path => registerControllerFromPath(path, under, application))
+export function eagerLoadComponents(application, { under, suffix }) {
+  const paths = Object.keys(parseImportmapJson()).filter((path) => path.match(new RegExp(`^${under}/.*${suffix}$`)))
+  paths.forEach((path) => registerComponent(path, under, application, suffix))
 }
 
 function parseImportmapJson() {
   return JSON.parse(document.querySelector("script[type=importmap]").text).imports
 }
 
-function registerControllerFromPath(path, under, application) {
+function registerComponent(path, under, application, suffix) {
   const name = path
-    .replace(new RegExp(`^${under}/`), "")
-    .replace(/\/component$/, "")
+    .replace(new RegExp(`^${under}/(.*)${suffix}$`), '$1')
     .replace(/\//g, "--")
     .replace(/_/g, "-")
 
