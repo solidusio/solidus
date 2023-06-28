886d856bcbc4cfadbe631421363b0c63ff4cd92b
Marc Busqué <marc@lamarciana.com>
2023-06-28 12:28:13 +0200

    Set up nested main nav items for the admin
    We need to add both a `children` and a `top_level` attributes:
    
    - `children` allows us to easily retrieve all the children for a given
      item.
    - `top_level` allows us to create different styles for the top level
      items.
    
    `children` can't be used alone, as some of the top level items won't
    have children (e.x. users).
    
    We could have used a `parent` attribute instead of both `children` and
    `top_level`, but that would have made the implementation more complex,
    as we would have to modify the state of the given parent item to
    easily retrieve its children. Instead, thanks to the `#with_children`
    method we make both attributes private in nature.
    
    We take the ocasion to start testing both the `MainNavItem` and the
    item components, as they are now more complex.

--- admin/lib/solidus_admin/providers/main_nav.rb
+++ admin/lib/solidus_admin/providers/main_nav.rb
@@ -8,12 +8,20 @@ module SolidusAdmin
     Container.register_provider("main_nav") do
       start do
         container.namespace("main_nav") do
-          register("orders", MainNavItem.new(key: "orders", icon: "solidus_admin/inbox-line.svg", position: "10"))
-          register("products", MainNavItem.new(key: "products", icon: "solidus_admin/price-tag-3-line.svg", position: "20"))
-          register("promotions", MainNavItem.new(key: "promotions", icon: "solidus_admin/megaphone-line.svg", position: "30"))
-          register("stock", MainNavItem.new(key: "stock", icon: "solidus_admin/stack-line.svg", position: "40"))
-          register("users", MainNavItem.new(key: "users", icon: "solidus_admin/user-line.svg", position: "50"))
-          register("settings", MainNavItem.new(key: "settings", icon: "solidus_admin/settings-line.svg", position: "60"))
+          register("orders", MainNavItem.new(key: "orders", icon: "solidus_admin/inbox-line.svg", position: 10))
+          register(
+            "products",
+            MainNavItem.new(
+              key: "products",
+              icon: "solidus_admin/price-tag-3-line.svg",
+              position: 20
+            )
+              .with_child(key: "option_types", position: 10)
+          )
+          register("promotions", MainNavItem.new(key: "promotions", icon: "solidus_admin/megaphone-line.svg", position: 30))
+          register("stock", MainNavItem.new(key: "stock", icon: "solidus_admin/stack-line.svg", position: 40))
+          register("users", MainNavItem.new(key: "users", icon: "solidus_admin/user-line.svg", position: 50))
+          register("settings", MainNavItem.new(key: "settings", icon: "solidus_admin/settings-line.svg", position: 60))
         end
 
         container.register("main_nav_items") do
