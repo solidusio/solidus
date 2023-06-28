ad80e11766c6142f6c8a5488fd7a486152170229
Marc Busqué <marc@lamarciana.com>
2023-06-28 12:28:13 +0200

    Set up nested main nav items for the admin
    We need to add both a `children` and a `top_level` attributes:
    
    - `children` allows us to easily retrieve all the children for a given
      item.
    - `top_level` allows us to create different styles for the top level
      items.
    
    `children` can't be used alone, as some of the top level items won't
    have children (e.x. users).
    
    We could have used a `parent` attribute instead of both `children` and
    `top_level`, but that would have made the implementation more complex,
    as we would have to modify the state of the given parent item to
    easily retrieve its children. Instead, thanks to the `#with_children`
    method we make both attributes private in nature.
    
    We take the ocasion to start testing both the `MainNavItem` and the
    item components, as they are now more complex.

--- admin/lib/solidus_admin/main_nav_item.rb
+++ admin/lib/solidus_admin/main_nav_item.rb
@@ -1,17 +1,53 @@
 # frozen_string_literal: true
 
 module SolidusAdmin
-  # Encapsulates the data for a main nav item.
+  # Encapsulates the data for a main navigation item.
   class MainNavItem
-    attr_reader :key, :icon, :position
+    # @!attribute [r] key
+    #  @return [String] a unique identifier for this item
+    attr_reader :key
 
-    # @param key [String]
-    # @param icon [String]
-    # @param position [Integer]
-    def initialize(key:, icon:, position:)
+    # @!attribute [r] icon
+    #  @return [String] asset path to an icon for this item
+    attr_reader :icon
+
+    # @!attribute [r] position
+    #  @return [Integer] the position of this item relative to its parent
+    attr_reader :position
+
+    # @api private
+    attr_reader :children, :top_level
+
+    def initialize(key:, position:, icon: nil, children: [], top_level: true)
       @key = key
       @icon = icon
       @position = position
+      @children = children
+      @top_level = top_level
+    end
+
+    # @return [MainNavItem] adds a child to this item (returning a new instance)
+    def with_child(key:, position:, icon: nil, children: [])
+      self.class.new(
+        key: self.key,
+        icon: self.icon,
+        position: self.position,
+        top_level: top_level,
+        children: self.children + [
+          self.class.new(
+            key: key,
+            position: position,
+            icon: icon,
+            children: children,
+            top_level: false
+          )
+        ]
+      )
+    end
+
+    # @return [Boolean] whether this item has any children
+    def children?
+      @children.any?
     end
   end
 end
