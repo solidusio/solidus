b58c885bfb91786108290f6d91c8d4e49aa62de3
Marc Busqué <marc@lamarciana.com>
2023-06-28 12:28:13 +0200

    Set up nested main nav items for the admin
    We need to add both a `children` and a `top_level` attributes:
    
    - `children` allows us to easily retrieve all the children for a given
      item.
    - `top_level` allows us to create different styles for the top level
      items.
    
    `children` can't be used alone, as some of the top level items won't
    have children (e.x. users).
    
    We could have used a `parent` attribute instead of both `children` and
    `top_level`, but that would have made the implementation more complex,
    as we would have to modify the state of the given parent item to
    easily retrieve its children. Instead, thanks to the `#with_children`
    method we make both attributes private in nature.
    
    We take the ocasion to start testing both the `MainNavItem` and the
    item components, as they are now more complex.

--- admin/app/components/solidus_admin/sidebar/item/component.rb
+++ admin/app/components/solidus_admin/sidebar/item/component.rb
@@ -11,4 +11,30 @@ class SolidusAdmin::Sidebar::Item::Component < SolidusAdmin::BaseComponent
   def name
     t(".main_nav.#{@item.key}")
   end
+
+  # Arbitrary Tailwind background images is not working:
+  # https://github.com/tailwindlabs/tailwindcss/discussions/6617
+  def background_image_style_attribute
+    return unless @item.icon
+
+    "style=\"background-image: url('".html_safe +
+      image_path(@item.icon) +
+      "')\"".html_safe
+  end
+
+  def link_level_classes
+    if @item.top_level
+      "text-black font-bold"
+    else
+      "text-gray-600"
+    end
+  end
+
+  def nested_items
+    return unless @item.children?
+
+    tag.nav do
+      render self.class.with_collection(@item.children)
+    end
+  end
 end
