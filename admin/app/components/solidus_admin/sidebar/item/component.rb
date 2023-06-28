ec63197862de6f40444d8541ccc49e1c1fda4ca9
Marc Busqué <marc@lamarciana.com>
2023-06-28 05:50:29 +0200

    Move nav item markup to a template
    It's starting to get larger, and it's easier to read and edit in a
    template.

--- admin/app/components/solidus_admin/sidebar/item/component.rb
+++ admin/app/components/solidus_admin/sidebar/item/component.rb
@@ -2,30 +2,11 @@
 
 # Menu item within a {Sidebar}
 class SolidusAdmin::Sidebar::Item::Component < SolidusAdmin::BaseComponent
-  include ViewComponent::InlineTemplate
-
   with_collection_parameter :item
 
   def initialize(item:)
     @item = item
   end
-
-  # Arbitrary Tailwind background images is not working:
-  # https://github.com/tailwindlabs/tailwindcss/discussions/6617
-  erb_template <<~ERB
-    <div class="
-        py-2 px-3 mb-1
-        bg-no-repeat bg-left bg-origin-content
-      " style="background-image: url('<%= image_path(@item.icon) %>')">
-      <a href="#" class="
-        pl-[1.81rem]
-        text-sm text-black
-        font-sans font-bold
-      ">
-        <%= name %>
-      </a>
-    </div>
-  ERB
 
   def name
     t(".main_nav.#{@item.key}")
