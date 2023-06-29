7681c84ce41057e68bcf1f16e732ff35132b0580
Martin Meyerhoff <mamhoff@gmail.com>
2023-06-29 11:51:56 +0200

    Properly render number with currency forms

--- friendly_promotions/app/javascript/solidus_friendly_promotions.js
+++ friendly_promotions/app/javascript/solidus_friendly_promotions.js
@@ -2,3 +2,7 @@ import "@hotwired/turbo-rails";
 import "solidus_friendly_promotions/controllers";
 
 Turbo.session.drive = false;
+
+document.addEventListener("turbo:frame-load", ({ _target }) => {
+  Spree.initNumberWithCurrency();
+});
