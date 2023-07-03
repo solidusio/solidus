098a18fee6f992760c9e24cff8f2335a75f9bed4
Martin Meyerhoff <mamhoff@gmail.com>
2023-07-03 10:42:08 +0200

    Add Line Item Option Values Controller

--- friendly_promotions/app/assets/config/solidus_friendly_promotions/manifest.js
+++ friendly_promotions/app/assets/config/solidus_friendly_promotions/manifest.js
@@ -3,3 +3,4 @@
 //= link solidus_friendly_promotions/controllers/index.js
 //= link solidus_friendly_promotions/controllers/calculator_tiers_controller.js
 //= link solidus_friendly_promotions/controllers/flash_controller.js
+//= link solidus_friendly_promotions/controllers/product_option_values_controller.js
