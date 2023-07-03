6c6ba82df4a191162addef79d1bbf9d07d9549d1
Elia Schito <elia@schito.me>
2023-07-03 12:32:46 +0200

    Generate component previews along with components

--- admin/spec/generator/solidus_admin/component_generator_spec.rb
+++ admin/spec/generator/solidus_admin/component_generator_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe SolidusAdmin::ComponentGenerator, type: :generator do
       expect(engine_path('app/components/solidus_admin/ui/foo/component.html.erb').read).to start_with(%{<div class="<%= stimulus_id %>"})
       expect(engine_path('app/components/solidus_admin/ui/foo/component.js').read).to include(%{export default class extends Controller})
       expect(engine_path('spec/components/solidus_admin/ui/foo/component_spec.rb').read).to include(%{RSpec.describe SolidusAdmin::UI::Foo::Component})
+      expect(engine_path('spec/components/previews/solidus_admin/ui/foo/component_preview.rb').read).to include(%{class SolidusAdmin::UI::Foo::ComponentPreview < ViewComponent::Preview})
+      expect(engine_path('spec/components/previews/solidus_admin/ui/foo/component_preview/overview.html.erb').read).to include(%{<%= render component.new %>})
     end
   end
 
