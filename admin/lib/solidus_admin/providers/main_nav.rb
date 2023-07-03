648061e14853451c7a0439dcdd8837ce95432fe3
Marc Busqué <marc@lamarciana.com>
2023-07-03 15:42:02 +0200

    Hide container details from the user to configure admin main navigation
    Instead, the entry point is `SolidusAdmin::Config.main_nav`, from which
    they can call `add` to add a new item to the main navigation:
    
    ```ruby
    SolidusAdmin::Config.main_nav do |main_nav|
      main_nav.add(
        key: :my_custom_link,
        route: :my_custom_link_path,
        position: 80
      )
    end
    ```
    
    The `SolidusAdmin::Config::MainNav` class can be further extended to
    provide a richer API for the user to configure the main navigation, like
    adding a `#remove` method or using `after:` parameters instead of
    `position:`. This is left for further thought and discussion.

--- admin/lib/solidus_admin/providers/main_nav.rb
+++ admin/lib/solidus_admin/providers/main_nav.rb
@@ -7,22 +7,56 @@ module SolidusAdmin
   module Providers
     Container.register_provider("main_nav") do
       start do
-        container.namespace("main_nav") do
-          register("orders", MainNavItem.new(key: "orders", route: :orders_path, icon: "solidus_admin/inbox-line.svg", position: 10))
-          register(
-            "products",
-            MainNavItem.new(
+        SolidusAdmin::Config.main_nav do |main_nav|
+          main_nav.add(
+            key: "orders",
+            route: :orders_path,
+            icon: "solidus_admin/inbox-line.svg",
+            position: 10
+          )
+
+          main_nav
+            .add(
               key: "products",
               route: :products_path,
               icon: "solidus_admin/price-tag-3-line.svg",
-              position: 20
+              position: 20,
+              children: [
+                {
+                  key: "option_types",
+                  route: :option_types_path,
+                  position: 10
+                }
+              ]
             )
-              .with_child(key: "option_types", route: :option_types_path, position: 10)
+
+          main_nav.add(
+            key: "promotions",
+            route: :promotions_path,
+            icon: "solidus_admin/megaphone-line.svg",
+            position: 30
           )
-          register("promotions", MainNavItem.new(key: "promotions", route: :promotions_path, icon: "solidus_admin/megaphone-line.svg", position: 30))
-          register("stock", MainNavItem.new(key: "stock", route: :stock_items_path, icon: "solidus_admin/stack-line.svg", position: 40))
-          register("users", MainNavItem.new(key: "users", route: :users_path, icon: "solidus_admin/user-line.svg", position: 50))
-          register("settings", MainNavItem.new(key: "settings", route: :stores_path, icon: "solidus_admin/settings-line.svg", position: 60))
+
+          main_nav.add(
+            key: "stock",
+            route: :stock_items_path,
+            icon: "solidus_admin/stack-line.svg",
+            position: 40
+          )
+
+          main_nav.add(
+            key: "users",
+            route: :users_path,
+            icon: "solidus_admin/user-line.svg",
+            position: 50
+          )
+
+          main_nav.add(
+            key: "settings",
+            route: :stores_path,
+            icon: "solidus_admin/settings-line.svg",
+            position: 60
+          )
         end
 
         container.register("main_nav_items") do
