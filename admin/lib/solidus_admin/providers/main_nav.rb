9f6345d802ed27ae69f71ac66b9c5d147c1fcf46
Marc Busqué <marc@lamarciana.com>
2023-07-03 06:26:00 +0200

    Set up admin's main navigation routes
    We configure the main navigation routes so that the main menu is already
    clickable.
    
    For that, we add a `:route` option to the `SolidusAdmin::MainNavItem`.
    This can be either a symbol or a proc, and it's resolved by the
    components layer when it provides the url helpers as an argument for the
    `#path` method. When `:route` is a symbol, it's used as the name of the
    helper to be called, while when a proc, it's called with the url helpers
    as an argument.
    
    As not all the routes are still available on the new admin, we create a
    `SolidusAdmin::UrlHelpersWithFallbacks` module, which acts as a proxy
    for the url helpers, and falls back to the old admin ones when
    the new ones are not available. When all the work will be done, we can
    remove this module and dispatch the new url helpers directly.
    
    We need to accommodate tests for the new `:route` option, and we also
    need to mock actual items for the sidebar component to avoid errors because
    of the missing routes.
    
    We leave out of this commit mounting the old admin in the dummy
    application, which we'll need for feature specs. We might also want to
    consider using the url helpers with fallbacks everywhere else.

--- admin/lib/solidus_admin/providers/main_nav.rb
+++ admin/lib/solidus_admin/providers/main_nav.rb
@@ -8,20 +8,21 @@ module SolidusAdmin
     Container.register_provider("main_nav") do
       start do
         container.namespace("main_nav") do
-          register("orders", MainNavItem.new(key: "orders", icon: "solidus_admin/inbox-line.svg", position: 10))
+          register("orders", MainNavItem.new(key: "orders", route: :orders_path, icon: "solidus_admin/inbox-line.svg", position: 10))
           register(
             "products",
             MainNavItem.new(
               key: "products",
+              route: :products_path,
               icon: "solidus_admin/price-tag-3-line.svg",
               position: 20
             )
-              .with_child(key: "option_types", position: 10)
+              .with_child(key: "option_types", route: :option_types_path, position: 10)
           )
-          register("promotions", MainNavItem.new(key: "promotions", icon: "solidus_admin/megaphone-line.svg", position: 30))
-          register("stock", MainNavItem.new(key: "stock", icon: "solidus_admin/stack-line.svg", position: 40))
-          register("users", MainNavItem.new(key: "users", icon: "solidus_admin/user-line.svg", position: 50))
-          register("settings", MainNavItem.new(key: "settings", icon: "solidus_admin/settings-line.svg", position: 60))
+          register("promotions", MainNavItem.new(key: "promotions", route: :promotions_path, icon: "solidus_admin/megaphone-line.svg", position: 30))
+          register("stock", MainNavItem.new(key: "stock", route: :stock_items_path, icon: "solidus_admin/stack-line.svg", position: 40))
+          register("users", MainNavItem.new(key: "users", route: :users_path, icon: "solidus_admin/user-line.svg", position: 50))
+          register("settings", MainNavItem.new(key: "settings", route: :stores_path, icon: "solidus_admin/settings-line.svg", position: 60))
         end
 
         container.register("main_nav_items") do
--- admin/spec/solidus_admin/main_nav_item_spec.rb
+++ admin/spec/solidus_admin/main_nav_item_spec.rb
@@ -5,9 +5,9 @@ require "spec_helper"
 RSpec.describe SolidusAdmin::MainNavItem do
   describe "#with_child" do
     it "returns a new instance" do
-      item = described_class.new(key: "foo", position: 1)
+      item = described_class.new(key: "foo", route: :foo_path, position: 1)
 
-      result = item.with_child(key: "bar", position: 1)
+      result = item.with_child(key: "bar", route: :bar_path, position: 1)
 
       aggregate_failures do
         expect(result).to be_a(described_class)
@@ -16,12 +16,13 @@ RSpec.describe SolidusAdmin::MainNavItem do
     end
 
     it "keeps the item attributes" do
-      item = described_class.new(key: "foo", position: 1, icon: "icon", top_level: true)
+      item = described_class.new(key: "foo", route: :foo_path, position: 1, icon: "icon", top_level: true)
 
-      result = item.with_child(key: "bar", position: 1)
+      result = item.with_child(key: "bar", route: :bar_path, position: 1)
 
       aggregate_failures do
         expect(result.key).to eq("foo")
+        expect(result.route).to eq(:foo_path)
         expect(result.position).to eq(1)
         expect(result.icon).to eq("icon")
         expect(result.top_level).to be(true)
@@ -30,18 +31,18 @@ RSpec.describe SolidusAdmin::MainNavItem do
 
     it "adds a child to the item" do
       item = described_class.new(
-        key: "foo", position: 1, children: [described_class.new(key: "bar", position: 1)]
+        key: "foo", route: :foo_path, position: 1, children: [described_class.new(key: "bar", route: :bar_path, position: 1)]
       )
 
-      result = item.with_child(key: "baz", position: 1)
+      result = item.with_child(key: "baz", route: :baz_path, position: 1)
 
       expect(result.children.count).to be(2)
     end
 
     it "sets child as not top level" do
-      item = described_class.new(key: "foo", position: 1)
+      item = described_class.new(key: "foo", route: :foo_path, position: 1)
 
-      result = item.with_child(key: "bar", position: 1)
+      result = item.with_child(key: "bar", route: :bar_path, position: 1)
 
       expect(result.children.first.top_level).to be(false)
     end
@@ -49,17 +50,45 @@ RSpec.describe SolidusAdmin::MainNavItem do
 
   describe "#children?" do
     it "returns false when there are no children" do
-      item = described_class.new(key: "foo", position: 1)
+      item = described_class.new(key: "foo", route: :foo_path, position: 1)
 
       expect(item.children?).to be(false)
     end
 
     it "returns true when there are children" do
       item = described_class.new(
-        key: "foo", position: 1, children: [described_class.new(key: "bar", position: 1)]
+        key: "foo", route: :foo_path, position: 1, children: [described_class.new(key: "bar", route: :bar_path, position: 1)]
       )
 
       expect(item.children?).to be(true)
+    end
+  end
+
+  describe "#path" do
+    context "when the route is a symbol" do
+      it "calls that method on the url_helpers" do
+        item = described_class.new(key: "foo", route: :foo_path, position: 1)
+        url_helpers = Module.new do
+          def self.foo_path
+            "/foo"
+          end
+        end
+
+        expect(item.path(url_helpers)).to eq("/foo")
+      end
+    end
+
+    context "when the route is a Proc" do
+      it "yields the url_helpers to it" do
+        item = described_class.new(key: "foo", route: -> { _1.foo_path }, position: 1)
+        url_helpers = Module.new do
+          def self.foo_path
+            "/foo"
+          end
+        end
+
+        expect(item.path(url_helpers)).to eq("/foo")
+      end
     end
   end
 end
