93380c4dad054ec87a7de21c5ccf7cb11a4115a4
Elia Schito <elia@schito.me>
2023-07-05 19:39:32 +0200

    Provide columns as an array to the ui/table component initializer

--- admin/app/components/solidus_admin/ui/table/component.rb
+++ admin/app/components/solidus_admin/ui/table/component.rb
@@ -1,23 +1,10 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
-  def initialize(rows:, model_class:)
+  def initialize(rows:, model_class:, columns: [])
     @rows = rows
     @model_class = model_class
-    @columns = []
-  end
-
-  def self.build_for(rows, **args, &block)
-    new(rows: rows, **args).tap(&block)
-  end
-
-  def column(name, content = nil, class_name: nil, &block)
-    raise ArgumentError, "content and block are mutually exclusive" if content && block
-
-    content ||= block # if a block is passed, use it as the content
-    content ||= name if name.is_a?(Symbol) # if a symbol is passed, use it as the method name
-
-    @columns << Column.new(name: name, content: content, class_name: class_name)
+    @columns = columns.map { Column.new(**_1) }
   end
 
   def render_cell(cell)
@@ -53,6 +40,6 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
     render_cell(cell)
   end
 
-  Column = Struct.new(:name, :content, :class_name, keyword_init: true)
+  Column = Struct.new(:header, :data, :class_name, keyword_init: true)
   private_constant :Column
 end
