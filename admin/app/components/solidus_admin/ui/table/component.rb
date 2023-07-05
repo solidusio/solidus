430ebbf00837dc3da7572d2fa10ac5beb55a76ca
Elia Schito <elia@schito.me>
2023-07-05 19:26:32 +0200

    Normalize cell content generation in the ui/table component
    - add special support for Symbol, and Proc in header and data cells
    - add support for returning component instances as content

--- admin/app/components/solidus_admin/ui/table/component.rb
+++ admin/app/components/solidus_admin/ui/table/component.rb
@@ -11,14 +11,46 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
     new(rows: rows, **args).tap(&block)
   end
 
-  def column(name, class_name: nil, &block)
-    name =
-      case name
-      when Symbol then @model_class.human_attribute_name(name)
-      else name
+  def column(name, content = nil, class_name: nil, &block)
+    raise ArgumentError, "content and block are mutually exclusive" if content && block
+
+    content ||= block # if a block is passed, use it as the content
+    content ||= name if name.is_a?(Symbol) # if a symbol is passed, use it as the method name
+
+    @columns << Column.new(name: name, content: content, class_name: class_name)
+  end
+
+  def render_cell(cell)
+    # Allow component instances as cell content
+    if cell.respond_to?(:render_in)
+      cell.render_in(self)
+    else
+      cell
+    end
+  end
+
+  def render_header_cell(cell)
+    cell =
+      case cell
+      when Symbol
+        @model_class.human_attribute_name(cell)
+      when Proc
+        cell.call
       end
 
-    @columns << Column.new(name: name, content: block, class_name: class_name)
+    render_cell(cell)
+  end
+
+  def render_data_cell(cell, data)
+    cell =
+      case cell
+      when Symbol
+        data.public_send(cell)
+      when Proc
+        cell.call(data)
+      end
+
+    render_cell(cell)
   end
 
   Column = Struct.new(:name, :content, :class_name, keyword_init: true)
