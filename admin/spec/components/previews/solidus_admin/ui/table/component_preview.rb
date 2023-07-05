a29fab90a8448505ea681cf9006890b3cd7cbde3
Elia Schito <elia@schito.me>
2023-07-05 19:39:32 +0200

    Provide columns as an array to the ui/table component initializer

--- admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
+++ admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
@@ -14,14 +14,18 @@ class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
   def simple
     model_class = Spree::Product
     rows = Array.new(10) do |n|
-      model_class.new(name: "Product #{n}", price: n * 10.0, available_on: n.days.ago)
+      model_class.new(id: n, name: "Product #{n}", price: n * 10.0, available_on: n.days.ago)
     end
 
-    render component("ui/table").new(rows: rows, model_class: model_class).tap { |t|
-      t.column(:id)
-      t.column(:name) { _1.name }
-      t.column(-> { "Availability at #{Time.current}" }, -> { "#{time_ago_in_words _1.available_on} ago" })
-      t.column(-> { component("ui/badge").new(name: "$$$") }, -> { component("ui/badge").new(name: _1.display_price, color: :green) })
-    }
+    render component("ui/table").new(
+      rows: rows,
+      model_class: model_class,
+      columns: [
+        { header: :id, data: :id },
+        { header: :name, data: -> { _1.name } },
+        { header: -> { "Availability at #{Time.current}" }, data: -> { "#{time_ago_in_words _1.available_on} ago" } },
+        { header: -> { component("ui/badge").new(name: "$$$") }, data: -> { component("ui/badge").new(name: _1.display_price, color: :green) } },
+      ]
+    )
   end
 end
