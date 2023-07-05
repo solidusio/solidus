ac7e8df05db98f652a917453344f53cc0cbd67a2
Elia Schito <elia@schito.me>
2023-07-05 17:33:01 +0200

    Keep all preview related generic helpers into a module
    - adds `current_component`
    - adds a `helpers` reference to all helpers available in SolidusAdmin::BaseController
    - delegates missing methods to `helpers`
    - adds all standard action_view helpers to the preview controller templates

--- admin/spec/components/previews/solidus_admin/ui/badge/component_preview.rb
+++ admin/spec/components/previews/solidus_admin/ui/badge/component_preview.rb
@@ -2,28 +2,27 @@
 
 # @component "ui/badge"
 class SolidusAdmin::UI::Badge::ComponentPreview < ViewComponent::Preview
-  layout "solidus_admin/preview"
-  include SolidusAdmin::ContainerHelper
+  include SolidusAdmin::Preview
 
   # @param name [String]
   def overview(name: "Label")
-    render_with_template(locals: { name: name, component: component("ui/badge") })
+    render_with_template(locals: { name: name })
   end
 
   # @param name [String]
   # @param color select :color_options
   # @param size select :size_options
   def playground(name: "Label", color: :green, size: :m)
-    render component("ui/badge").new(name: name, color: color, size: size)
+    render current_component.new(name: name, color: color, size: size)
   end
 
   private
 
   def size_options
-    component('ui/badge')::SIZES.keys
+    current_component::SIZES.keys
   end
 
   def color_options
-    component('ui/badge')::COLORS.keys
+    current_component::COLORS.keys
   end
 end
