aeda743dbea9a6e5ba4a7ffb4d1defea3a48cfc1
Elia Schito <elia@schito.me>
2023-07-05 19:26:32 +0200

    Normalize cell content generation in the ui/table component
    - add special support for Symbol, and Proc in header and data cells
    - add support for returning component instances as content

--- admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
+++ admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
@@ -5,9 +5,10 @@ class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
   include SolidusAdmin::Preview
 
   # Render a simple table with 10 products.
-  # - The first column is the product `#name` attribute, and uses a **symbol** for both the content and the header
-  # - The second column is the product `#available_on` attribute, and uses **a block returning strings** for both the content and the header
-  # - The third column is the product `#price` attribute, uses **blocks returning component instances** for both the content and the header
+  # - The `:id` column is the product `#id` attribute, and uses a **symbol** for both the content and the header
+  # - The `:name` column is the product `#name` attribute, and uses **a block returning strings** for the content and a symbol for the header
+  # - The `:available_on` column is the product `#available_on` attribute, and uses **a block returning strings** for both the content and the header
+  # - The `:price` column shows the product `#price` attribute in a badge component, uses **blocks returning component instances** for both the content and the header
   #
   # All these ways to header and data cells can be mixed and matched.
   def simple
@@ -17,9 +18,10 @@ class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
     end
 
     render component("ui/table").new(rows: rows, model_class: model_class).tap { |t|
+      t.column(:id)
       t.column(:name) { _1.name }
-      t.column(:available_on) { "#{time_ago_in_words _1.available_on} ago" }
-      t.column(:price) { component("ui/badge").new(name: _1.display_price, color: :green) }
+      t.column(-> { "Availability at #{Time.current}" }, -> { "#{time_ago_in_words _1.available_on} ago" })
+      t.column(-> { component("ui/badge").new(name: "$$$") }, -> { component("ui/badge").new(name: _1.display_price, color: :green) })
     }
   end
 end
