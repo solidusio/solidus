9baeaf9e07c42d8c3775629592afd0210273dd34
Elia Schito <elia@schito.me>
2023-07-10 12:25:49 +0200

    Do not modify core routes in the base component specs
    This will interfere with the default navigation relying on legacy
    backend routes used to create links to section not already covered
    by SolidusAdmin.

--- admin/spec/components/solidus_admin/base_component_spec.rb
+++ admin/spec/components/solidus_admin/base_component_spec.rb
@@ -5,14 +5,10 @@ require "spec_helper"
 RSpec.describe SolidusAdmin::BaseComponent, type: :component do
   describe "#spree" do
     it "gives access to spree routing helpers" do
-      # The spree/core routes start as empty, so we need to add a route to test
-      Spree::Core::Engine.routes.draw { get '/foo/bar', to: 'foo#bar', as: :foo_bar }
-
+      allow(Spree::Core::Engine.routes.url_helpers).to receive(:foo_path).and_return("/foo/bar")
       component = described_class.new
 
-      expect(component.spree.foo_bar_path).to eq("/foo/bar")
-    ensure
-      Spree::Core::Engine.routes.clear!
+      expect(component.spree.foo_path).to eq("/foo/bar")
     end
   end
 
