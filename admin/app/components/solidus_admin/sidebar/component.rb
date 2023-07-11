525c5ff27c3099e321348612148301948669bca9
Marc Busqué <marc@lamarciana.com>
2023-07-11 15:28:29 +0200

    Add account navigation component for the new admin
    It's not still fully functional, as authentication is not yet
    in place. For now, we use dummy data to show the component.
    
    We're also changing the layout so the sidebar container occupies the full
    height of the viewport. That fixes an issue where the sidebar wasn't
    filling it due to being in a shorter container.

--- admin/app/components/solidus_admin/sidebar/component.rb
+++ admin/app/components/solidus_admin/sidebar/component.rb
@@ -6,11 +6,13 @@ class SolidusAdmin::Sidebar::Component < SolidusAdmin::BaseComponent
     store:,
     logo_path: SolidusAdmin::Config.logo_path,
     items: container["main_nav_items"],
-    item_component: component("sidebar/item")
+    item_component: component("sidebar/item"),
+    account_nav_component: component("sidebar/account_nav")
   )
     @logo_path = logo_path
     @items = items
     @item_component = item_component
+    @account_nav_component = account_nav_component
     @store = store
   end
 
