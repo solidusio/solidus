fe2e269e4b203dae4d600b6c5bd6369e63d4bf13
Marc Busqué <marc@lamarciana.com>
2023-07-11 15:28:29 +0200

    Add account navigation component for the new admin
    It's not still fully functional, as authentication is not yet
    in place. For now, we use dummy data to show the component.
    
    We're also changing the layout so the sidebar container occupies the full
    height of the viewport. That fixes an issue where the sidebar wasn't
    filling it due to being in a shorter container.

---
+++ admin/spec/components/solidus_admin/sidebar/account_nav/component_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SolidusAdmin::Sidebar::AccountNav::Component, type: :component do
+  it "renders the overview preview" do
+    render_preview(:overview)
+  end
+
+  it "renders link to the account" do
+    component = described_class.new(
+      account_path: "/admin/account"
+    )
+
+    render_inline(component)
+
+    expect(page).to have_link("Account", href: "/admin/account")
+  end
+
+  it "renders link to logout" do
+    component = described_class.new(
+      logout_path: "/admin/logout"
+    )
+
+    render_inline(component)
+
+    expect(page).to have_link("Logout", href: "/admin/logout")
+  end
+
+  it "renders user lanel" do
+    component = described_class.new(
+      user_label: "Alice"
+    )
+
+    render_inline(component)
+
+    expect(page).to have_content("Alice")
+  end
+end
