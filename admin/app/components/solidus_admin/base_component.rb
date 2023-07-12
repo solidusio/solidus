d3dbd6f0483898e64da5f0251d9d12a9da6047c5
Alberto Vena <kennyadsl@gmail.com>
2023-07-12 12:38:30 +0200

    Extract sidebar item's icon to a proper helper
    We are going to use this icon_tag a lot so it might
    make sense to extract it into a common place.

--- admin/app/components/solidus_admin/base_component.rb
+++ admin/app/components/solidus_admin/base_component.rb
@@ -5,6 +5,23 @@ module SolidusAdmin
   class BaseComponent < ViewComponent::Base
     include SolidusAdmin::ContainerHelper
 
+    # Renders a remixincon svg.
+    #
+    # @param name [String] the name of the icon
+    # @option attrs [String] :class the class to add to the svg
+    # @return [String] the svg tag
+    # @see https://remixicon.com/
+    def icon_tag(name, **attrs)
+      href = image_path("solidus_admin/remixicon.symbol.svg") + "#ri-#{name}"
+      tag.svg(
+        class: attrs[:class],
+      ) do
+        tag.use(
+          "xlink:href": href
+        )
+      end
+    end
+
     def self.stimulus_id
       @stimulus_id ||= name.underscore
         .sub(/^solidus_admin\/(.*)\/component$/, '\1')
