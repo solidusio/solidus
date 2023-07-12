badfd93531ab0330c6a043a0c632aacd7d86fadb
Elia Schito <elia@schito.me>
2023-07-12 19:39:39 +0200

    Add delete, discontinue, and activate actions to the products/index component

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -2,5 +2,11 @@
 
 SolidusAdmin::Engine.routes.draw do
   resource :account, only: :show
-  resources :products, only: :index
+  resources :products, only: :index do
+    collection do
+      delete :destroy
+      put :discontinue
+      put :activate
+    end
+  end
 end
