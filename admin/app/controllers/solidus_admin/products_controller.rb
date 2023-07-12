377e549edb84ce3b23fbc37d3e9aa49e4646d041
Elia Schito <elia@schito.me>
2023-07-12 19:39:39 +0200

    Add delete, discontinue, and activate actions to the products/index component

--- admin/app/controllers/solidus_admin/products_controller.rb
+++ admin/app/controllers/solidus_admin/products_controller.rb
@@ -8,5 +8,46 @@ module SolidusAdmin
         per_page: SolidusAdmin::Config[:products_per_page]
       )
     end
+
+    def destroy
+      @products = Spree::Product.where(id: params[:id])
+
+      Spree::Product.transaction do
+        @products.discard_all
+      end
+
+      flash[:notice] = t('.success')
+      redirect_to products_path, status: :see_other
+    end
+
+    def discontinue
+      @products = Spree::Product.where(id: params[:id])
+
+      Spree::Product.transaction do
+        @products
+          .update_all(discontinue_on: Time.current)
+      end
+
+      flash[:notice] = t('.success')
+      redirect_to products_path, status: :see_other
+    end
+
+    def activate
+      @products = Spree::Product.where(id: params[:id])
+
+      Spree::Product.transaction do
+        @products
+          .where.not(discontinue_on: nil)
+          .update_all(discontinue_on: nil)
+
+        @products
+          .where("available_on <= ?", Time.current)
+          .or(@products.where(available_on: nil))
+          .update_all(discontinue_on: nil)
+      end
+
+      flash[:notice] = t('.success')
+      redirect_to products_path, status: :see_other
+    end
   end
 end
