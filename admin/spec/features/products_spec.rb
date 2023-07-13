153a769c4bdac7115b50d491e6c7079d57d11c9f
Marc Busqué <marc@lamarciana.com>
2023-07-13 13:28:31 +0200

    Add automatic testing for accessibility in the new admin
    We use the axe set of gems [1] to test for automatic testing of accessibility
    issues in the new admin. This commit adds the necessary gems and configuration
    to the admin Gemfile and spec_helper.rb files. It also adds a test to the
    products listing.
    
    As accessibility testing requires javascript, which makes tests slow,
    we'll need to be selective about where we add these tests.
    
    [1] - https://github.com/dequelabs/axe-core-gems
    
    Closes #5132

--- admin/spec/features/products_spec.rb
+++ admin/spec/features/products_spec.rb
@@ -3,12 +3,13 @@
 require 'spec_helper'
 
 describe "Products", type: :feature do
-  it "lists products" do
+  it "lists products", :js do
     create(:product, name: "Just a product", price: 19.99)
 
     visit "/admin/products"
 
     expect(page).to have_content("Just a product")
     expect(page).to have_content("$19.99")
+    expect(page).to be_axe_clean
   end
 end
