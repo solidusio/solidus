b094512a1c1dd48f5aaa8e20c88631115b546dc5
Rainer Dema <rainer.dema@gmail.com>
2023-07-13 19:58:55 +0200

    Add `geared_pagination` gem
    This commit adds the geared_pagination gem to the Gemfile, enabling
    geared pagination for the admin interface. It also includes the
    GearedPagination::Controller module in the base controller to provide
    pagination functionality.

--- admin/app/controllers/solidus_admin/base_controller.rb
+++ admin/app/controllers/solidus_admin/base_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'geared_pagination'
+
 module SolidusAdmin
   class BaseController < Spree::BaseController
+    include ::GearedPagination::Controller
+
     layout 'solidus_admin/application'
     helper 'solidus_admin/container'
     helper 'solidus_admin/layout'
