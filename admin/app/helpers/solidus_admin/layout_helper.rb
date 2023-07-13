851267fe5026184e58d77d917f4c16d9ea08d9c5
Marc Busqué <marc@lamarciana.com>
2023-07-13 11:53:45 +0200

    Fix HTML tag not specifying language in the admin
    This fixes WCAG 2.1's SC 3.1.1 (Language of Page) criteria [1].
    
    [1] - https://www.w3.org/WAI/WCAG21/Understanding/language-of-page

--- admin/app/helpers/solidus_admin/layout_helper.rb
+++ admin/app/helpers/solidus_admin/layout_helper.rb
@@ -3,6 +3,11 @@
 module SolidusAdmin
   # Helpers for the admin layout
   module LayoutHelper
+    # @return [Symbol]
+    def current_locale(backend: I18n)
+      backend.locale
+    end
+
     # @param store_name [String]
     # @param controller_name [String]
     # @return [String] HTML title
