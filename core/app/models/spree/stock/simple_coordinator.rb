6355850a34ac50b0223fe8126aca2e0002c90d11
Ben A. Morgan <ben@benmorgan.io>
2023-07-13 21:48:23 -0600

    reorganize simple stock coordinator for improve debugging

--- core/app/models/spree/stock/simple_coordinator.rb
+++ core/app/models/spree/stock/simple_coordinator.rb
@@ -22,39 +22,65 @@ module Spree
     class SimpleCoordinator
       attr_reader :order
 
+      # @api private
+      attr_reader :inventory_units, :splitters, :stock_locations,
+        :filtered_stock_locations, :inventory_units_by_variant, :desired,
+        :availability, :allocator, :packages
+
       def initialize(order, inventory_units = nil)
         @order = order
         @inventory_units =
           inventory_units || Spree::Config.stock.inventory_unit_builder_class.new(order).units
         @splitters = Spree::Config.environment.stock_splitters
 
-        filtered_stock_locations = Spree::Config.stock.location_filter_class.new(Spree::StockLocation.all, @order).filter
+        @filtered_stock_locations = Spree::Config.stock.location_filter_class.new(load_stock_locations, order).filter
         sorted_stock_locations = Spree::Config.stock.location_sorter_class.new(filtered_stock_locations).sort
         @stock_locations = sorted_stock_locations
 
         @inventory_units_by_variant = @inventory_units.group_by(&:variant)
-        @desired = Spree::StockQuantities.new(@inventory_units_by_variant.transform_values(&:count))
+        @desired = Spree::StockQuantities.new(inventory_units_by_variant.transform_values(&:count))
         @availability = Spree::Stock::Availability.new(
-          variants: @desired.variants,
-          stock_locations: @stock_locations
+          variants: desired.variants,
+          stock_locations: stock_locations
         )
 
-        @allocator = Spree::Config.stock.allocator_class.new(@availability)
+        @allocator = Spree::Config.stock.allocator_class.new(availability)
       end
 
       def shipments
-        @shipments ||= build_shipments
+        @shipments ||= begin
+          @packages = build_packages
+          shipments = build_shipments
+
+          # Make sure we don't add the proposed shipments to the order
+          order.shipments = order.shipments - shipments
+
+          shipments
+        end
       end
 
       private
 
+      def load_stock_locations
+        Spree::StockLocation.all
+      end
+
       def build_shipments
+        # Turn the Stock::Packages into a Shipment with rates
+        packages.map do |package|
+          shipment = package.shipment = package.to_shipment
+          shipment.shipping_rates = Spree::Config.stock.estimator_class.new.shipping_rates(package)
+          shipment
+        end
+      end
+
+      def build_packages
         # Allocate any available on hand inventory and remaining desired inventory from backorders
-        on_hand_packages, backordered_packages, leftover = @allocator.allocate_inventory(@desired)
+        on_hand_packages, backordered_packages, leftover = allocator.allocate_inventory(desired)
 
         raise Spree::Order::InsufficientStock.new(items: leftover.quantities) unless leftover.empty?
 
-        packages = @stock_locations.map do |stock_location|
+        packages = stock_locations.map do |stock_location|
           # Combine on_hand and backorders into a single package per-location
           on_hand = on_hand_packages[stock_location.id] || Spree::StockQuantities.new
           backordered = backordered_packages[stock_location.id] || Spree::StockQuantities.new
@@ -71,19 +97,7 @@ module Spree
         end.compact
 
         # Split the packages
-        packages = split_packages(packages)
-
-        # Turn the Stock::Packages into a Shipment with rates
-        shipments = packages.map do |package|
-          shipment = package.shipment = package.to_shipment
-          shipment.shipping_rates = Spree::Config.stock.estimator_class.new.shipping_rates(package)
-          shipment
-        end
-
-        # Make sure we don't add the proposed shipments to the order
-        order.shipments = order.shipments - shipments
-
-        shipments
+        split_packages(packages)
       end
 
       def split_packages(initial_packages)
@@ -96,7 +110,7 @@ module Spree
       def get_units(quantities)
         # Change our raw quantities back into inventory units
         quantities.flat_map do |variant, quantity|
-          @inventory_units_by_variant[variant].shift(quantity)
+          inventory_units_by_variant[variant].shift(quantity)
         end
       end
     end
