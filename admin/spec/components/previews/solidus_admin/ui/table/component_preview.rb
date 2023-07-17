f3ca403bc8057a8b21c9a57b0aab64ce4845a406
Rainer Dema <rainer.dema@gmail.com>
2023-07-17 08:49:14 +0200

    Integrate `ui/pagination` component with `ui/table` component
    The SolidusAdmin::UI::Table::Component now accepts a Geared Pagination
    page object, and a pagination component class as parameters in its
    initializer.
    
    It also includes the rendering of the table footer where the pagination
    component is used.

--- admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
+++ admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
@@ -4,7 +4,7 @@
 class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
   include SolidusAdmin::Preview
 
-  # Render a simple table with 10 products.
+  # Render a simple table with 10 products and pagination.
   # - The `:id` column is the product `#id` attribute, and uses a **symbol** for both the content and the header
   # - The `:name` column is the product `#name` attribute, and uses **a block returning strings** for the content and a symbol for the header
   # - The `:available_on` column is the product `#available_on` attribute, and uses **a block returning strings** for both the content and the header
@@ -17,11 +17,16 @@ class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
       model_class.new(id: n, name: "Product #{n}", price: n * 10.0, available_on: n.days.ago)
     end
 
+    page = Struct.new(:records, :number, :next_param, :first?, :last?).new(rows, 1, '#', true, false)
+
+    page.records.define_singleton_method(:model) do
+      model_class
+    end
+
     render component("ui/table").new(
-      rows: rows,
-      model_class: model_class,
+      page: page,
       columns: [
-        { header: :id, data: :id },
+        { header: :id, data: -> { _1.id.to_s } },
         { header: :name, data: -> { _1.name } },
         { header: -> { "Availability at #{Time.current}" }, data: -> { "#{time_ago_in_words _1.available_on} ago" } },
         { header: -> { component("ui/badge").new(name: "$$$") }, data: -> { component("ui/badge").new(name: _1.display_price, color: :green) } },
