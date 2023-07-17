459f822ea9b34fad7d132394a07eed86b434a82b
Rainer Dema <rainer.dema@gmail.com>
2023-07-17 08:49:23 +0200

    Reallocate Pagination Component into Table Component Module
    The component `SolidusAdmin::UI::Pagination::Component` has been moved
    to `SolidusAdmin::UI::Table::Pagination::Component`.
    
    This refactoring reflects the close relationship between the Pagination
    component and the Table component, and it highlights the Pagination
    component's role in providing pagination functionality specifically
    for tables.

--- admin/spec/components/solidus_admin/ui/table/pagination/component_spec.rb
+++ admin/spec/components/solidus_admin/ui/table/pagination/component_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-RSpec.describe SolidusAdmin::UI::Pagination::Component, type: :component do
+RSpec.describe SolidusAdmin::UI::Table::Pagination::Component, type: :component do
   it "renders the overview preview" do
     render_preview(:overview)
   end
