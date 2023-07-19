7d4430e43ac1f938ddb6aca9621ff244148886a8
Elia Schito <elia@schito.me>
2023-07-19 14:51:00 +0200

    Keep translation files organized per controller
    Since we're leveraging relative lookup for translations in controllers,
    we can keep them organized so that we can see all translations related
    to a controller at a glance.
    
    The title key has been added because `solidus_admin.products` is the
    base scope for all translations of the products_controller.

--- admin/app/helpers/solidus_admin/layout_helper.rb
+++ admin/app/helpers/solidus_admin/layout_helper.rb
@@ -11,8 +11,8 @@ module SolidusAdmin
     # @param store_name [String]
     # @param controller_name [String]
     # @return [String] HTML title
-    def solidus_admin_title(store_name: current_store.name, controller_name: controller.controller_name)
-      "#{store_name} - #{t("solidus_admin.#{controller_name}")}"
+    def solidus_admin_title(store_name: current_store.name, controller_name: self.controller_name)
+      "#{store_name} - #{t("solidus_admin.#{controller_name}.title")}"
     end
   end
 end
