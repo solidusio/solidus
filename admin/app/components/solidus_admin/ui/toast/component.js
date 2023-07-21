fa687256a3a5acff45cea1be5b09ac993bad0f97
Marc Busqué <marc@lamarciana.com>
2023-07-21 14:38:35 +0200

    Make the toast component accessible
    We mark it as a polite dialog. When rendered, we move focus to the
    close button so it can be quickly dismissed with the keyboard.

--- admin/app/components/solidus_admin/ui/toast/component.js
+++ admin/app/components/solidus_admin/ui/toast/component.js
@@ -1,8 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['closeButton']
   static classes = ['closing']
   static values = { transition: Number }
+
+  connect () {
+    // Give focus to the close button
+    this.closeButtonTarget.focus();
+  }
 
   close () {
     this.element.classList.add(...this.closingClasses);
