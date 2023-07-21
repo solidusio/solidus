61dbd3d00f624a155ab1241041137fa7e9aec210
Marc Busqué <marc@lamarciana.com>
2023-07-21 12:25:32 +0200

    Use the global icon helper for icons in the toast component

--- admin/app/components/solidus_admin/ui/toast/component.rb
+++ admin/app/components/solidus_admin/ui/toast/component.rb
@@ -15,15 +15,4 @@ class SolidusAdmin::UI::Toast::Component < SolidusAdmin::BaseComponent
     @icon = icon
     @scheme = scheme.to_sym
   end
-
-  def icon_tag(icon, class_names: nil)
-    href = image_path("solidus_admin/remixicon.symbol.svg") + "#ri-#{icon}"
-    tag.svg(
-      class: "w-[1.125rem] h-[1.125rem] fill-current #{class_names}",
-    ) do
-      tag.use(
-        "xlink:href": href
-      )
-    end
-  end
 end
