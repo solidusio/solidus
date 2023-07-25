a469442fa7e91ddfb080bf501040b3378ba50837
Martin Meyerhoff <mamhoff@gmail.com>
2023-07-25 10:23:41 +0200

    Allow Shipment discounter to also discount rates
    Because we've abstracted away the type of adjustment, the
    `AdjustShipment` action can now also discount shipping rates.

--- friendly_promotions/app/models/solidus_friendly_promotions/actions/adjust_shipment.rb
+++ friendly_promotions/app/models/solidus_friendly_promotions/actions/adjust_shipment.rb
@@ -4,7 +4,7 @@ module SolidusFriendlyPromotions
   module Actions
     class AdjustShipment < PromotionAction
       def can_discount?(object)
-        object.is_a? Spree::Shipment
+        object.is_a?(Spree::Shipment) || object.is_a?(Spree::ShippingRate)
       end
 
       def available_calculators
--- friendly_promotions/spec/models/solidus_friendly_promotions/actions/adjust_shipment_spec.rb
+++ friendly_promotions/spec/models/solidus_friendly_promotions/actions/adjust_shipment_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe SolidusFriendlyPromotions::Actions::AdjustShipment do
 
     it { is_expected.to eq("Discount matching shipments") }
   end
+
+  describe "#can_discount?" do
+    subject { action.can_discount?(promotable) }
+
+    context "with a line item" do
+      let(:promotable) { Spree::LineItem.new }
+
+      it { is_expected.to be false }
+    end
+
+    context "with a shipment" do
+      let(:promotable) { Spree::Shipment.new }
+
+      it { is_expected.to be true }
+    end
+
+    context "with a shipping rate" do
+      let(:promotable) { Spree::ShippingRate.new }
+
+      it { is_expected.to be true }
+    end
+  end
 end
