a6bbd04ff50b6934b877d9e9c90c189164397f23
Martin Meyerhoff <mamhoff@gmail.com>
2023-07-26 14:06:21 +0200

    Add timestamps to shipping rate discounts

--- friendly_promotions/db/migrate/20230725074235_create_shipping_rate_discounts.rb
+++ friendly_promotions/db/migrate/20230725074235_create_shipping_rate_discounts.rb
@@ -5,6 +5,8 @@ class CreateShippingRateDiscounts < ActiveRecord::Migration[7.0]
       t.references :shipping_rate, type: :integer, null: false, foreign_key: { to_table: :spree_shipping_rates }
       t.decimal :amount, precision: 10, scale: 2, null: false
       t.string :label, null: false
+
+      t.timestamps
     end
   end
 end
