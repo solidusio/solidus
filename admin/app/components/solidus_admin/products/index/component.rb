9c3f2957d80614efcfb1c2e2dccad568154583ce
Elia Schito <elia@schito.me>
2023-07-26 16:15:19 +0200

    Move product/index columns and batch actions to the component class

--- admin/app/components/solidus_admin/products/index/component.rb
+++ admin/app/components/solidus_admin/products/index/component.rb
@@ -24,47 +24,106 @@ class SolidusAdmin::Products::Index::Component < SolidusAdmin::BaseComponent
     @page.last? ? nil : solidus_admin.url_for(host: request.host, port: request.port, **request.params, page: @page.next_param)
   end
 
-  def image_column(product)
-    image = product.gallery.images.first or return
-
-    link_to(
-      image_tag(image.url(:small), class: 'h-10 w-10 max-w-min rounded border border-gray-100', alt: product.name),
-      spree.edit_admin_product_path(product),
-      class: 'inline-flex overflow-hidden',
-      tabindex: -1,
-    )
+  def batch_actions
+    [
+      {
+        display_name: t('.batch_actions.delete'),
+        action: solidus_admin.products_path,
+        method: :delete,
+        icon: 'delete-bin-7-line',
+      },
+      {
+        display_name: t('.batch_actions.discontinue'),
+        action: solidus_admin.discontinue_products_path,
+        method: :put,
+        icon: 'pause-circle-line',
+      },
+      {
+        display_name: t('.batch_actions.activate'),
+        action: solidus_admin.activate_products_path,
+        method: :put,
+        icon: 'play-circle-line',
+      },
+    ]
   end
 
-  def name_column(product)
-    link_to product.name, spree.edit_admin_product_path(product)
+  def columns
+    [
+      image_column,
+      name_column,
+      status_column,
+      price_column,
+      stock_column,
+    ]
   end
 
-  def status_column(product)
-    if product.available?
-      @badge_component.new(name: t('.status.available'), color: :green)
-    else
-      @badge_component.new(name: t('.status.discontinued'), color: :red)
-    end
-  end
+  def image_column
+    {
+      class_name: "w-[72px]",
+      header: tag.span('aria-label': t('.product_image'), role: 'text'),
+      data: ->(product) do
+        image = product.gallery.images.first or return
 
-  def stock_column(product)
-    stock_info =
-      case (on_hand = product.total_on_hand)
-      when Float::INFINITY
-        content_tag :span, t('.stock.in_stock', on_hand: t('.stock.infinity')), class: 'text-forest'
-      when 1..Float::INFINITY
-        content_tag :span, t('.stock.in_stock', on_hand: on_hand), class: 'text-forest'
-      else
-        content_tag :span, t('.stock.in_stock', on_hand: on_hand), class: 'text-red-500'
+        link_to(
+          image_tag(image.url(:small), class: 'h-10 w-10 max-w-min rounded border border-gray-100', alt: product.name),
+          spree.edit_admin_product_path(product),
+          class: 'inline-flex overflow-hidden',
+          tabindex: -1,
+        )
       end
-
-    variant_info =
-      t('.for_variants', count: product.variants.count)
-
-    safe_join([stock_info, variant_info], ' ')
+    }
   end
 
-  def price_column(product)
-    content_tag :div, product.master.display_price.to_html
+  def name_column
+    {
+      header: :name,
+      data: ->(product) do
+        link_to product.name, spree.edit_admin_product_path(product)
+      end
+    }
+  end
+
+  def status_column
+    {
+      header: :status,
+      data: ->(product) do
+        if product.available?
+          @badge_component.new(name: t('.status.available'), color: :green)
+        else
+          @badge_component.new(name: t('.status.discontinued'), color: :red)
+        end
+      end
+    }
+  end
+
+  def stock_column
+    {
+      header: :stock,
+      data: ->(product) do
+        stock_info =
+          case (on_hand = product.total_on_hand)
+          when Float::INFINITY
+            content_tag :span, t('.stock.in_stock', on_hand: t('.stock.infinity')), class: 'text-forest'
+          when 1..Float::INFINITY
+            content_tag :span, t('.stock.in_stock', on_hand: on_hand), class: 'text-forest'
+          else
+            content_tag :span, t('.stock.in_stock', on_hand: on_hand), class: 'text-red-500'
+          end
+
+        variant_info =
+          t('.for_variants', count: product.variants.count)
+
+        content_tag :div, safe_join([stock_info, variant_info], ' ')
+      end
+    }
+  end
+
+  def price_column
+    {
+      header: :price,
+      data: ->(product) do
+        content_tag :div, product.master.display_price.to_html
+      end
+    }
   end
 end
