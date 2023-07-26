27ef88dca6e4eee24b96c438094ca870257972f0
Elia Schito <elia@schito.me>
2023-07-26 19:40:06 +0200

    Reimplement the pagination as `ui/button` components
    - Switch some ARIA attributes to semantic HTML
      - added `rel="prev"` and `rel="next"`
      - added text visible only to screen-readers
      - use aria-disabled

--- admin/spec/components/solidus_admin/ui/button/component_spec.rb
+++ admin/spec/components/solidus_admin/ui/button/component_spec.rb
@@ -3,7 +3,9 @@
 require "spec_helper"
 
 RSpec.describe SolidusAdmin::UI::Button::Component, type: :component do
-  it "renders the overview preview" do
+  it "renders previews" do
+    render_preview(:playground)
     render_preview(:overview)
+    render_preview(:group)
   end
 end
