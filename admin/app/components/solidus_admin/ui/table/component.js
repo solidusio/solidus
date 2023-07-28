a8818aeab66ba583a48f3938c0028cc7d434f045
Elia Schito <elia@schito.me>
2023-07-28 12:34:22 +0200

    Turn ui/table modes into stimulus values

--- admin/app/components/solidus_admin/ui/table/component.js
+++ admin/app/components/solidus_admin/ui/table/component.js
@@ -3,25 +3,28 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["checkbox", "headerCheckbox", "batchToolbar", "scopesToolbar", "defaultHeader", "batchHeader", "selectedRowsCount"]
   static classes = ["selectedRow"]
+  static values = {
+    mode: { type: String, default: "scopes" },
+  }
 
   connect() {
-    this.mode = "default"
+    this.modeValue = "scopes"
 
     this.render()
   }
 
   selectRow(event) {
     if (this.checkboxTargets.some((checkbox) => checkbox.checked)) {
-      this.mode = "batch"
+      this.modeValue = "batch"
     } else {
-      this.mode = "default"
+      this.modeValue = "scopes"
     }
 
     this.render()
   }
 
   selectAllRows(event) {
-    this.mode = event.target.checked ? "batch" : "default"
+    this.modeValue = event.target.checked ? "batch" : "scopes"
     this.checkboxTargets.forEach((checkbox) => (checkbox.checked = event.target.checked))
 
     this.render()
@@ -30,10 +33,10 @@ export default class extends Controller {
   render() {
     const selectedRows = this.checkboxTargets.filter((checkbox) => checkbox.checked)
 
-    this.batchToolbarTarget.toggleAttribute("hidden", this.mode !== "batch")
-    this.batchHeaderTarget.toggleAttribute("hidden", this.mode !== "batch")
-    this.scopesToolbarTarget.toggleAttribute("hidden", this.mode !== "default")
-    this.defaultHeaderTarget.toggleAttribute("hidden", this.mode !== "default")
+    this.batchToolbarTarget.toggleAttribute("hidden", this.modeValue !== "batch")
+    this.batchHeaderTarget.toggleAttribute("hidden", this.modeValue !== "batch")
+    this.scopesToolbarTarget.toggleAttribute("hidden", this.modeValue !== "scopes")
+    this.defaultHeaderTarget.toggleAttribute("hidden", this.modeValue !== "scopes")
 
     // Update the rows background color
     this.checkboxTargets.filter((checkbox) => checkbox.closest('tr').classList.toggle(this.selectedRowClass, checkbox.checked))
