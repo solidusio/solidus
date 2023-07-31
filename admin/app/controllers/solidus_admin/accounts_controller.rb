5de28ac4b9ca8b78b2b1dafa93ae6c2c11845c29
Elia Schito <elia@schito.me>
2023-07-31 17:51:45 +0200

    Use the current admin user page as the account path
    This let's us remove the dummy page we previously added to show the
    logout link.

--- admin/app/controllers/solidus_admin/accounts_controller.rb
+++ admin/app/controllers/solidus_admin/accounts_controller.rb
@@ -2,5 +2,8 @@
 
 module SolidusAdmin
   class AccountsController < SolidusAdmin::BaseController
+    def show
+      redirect_to spree.edit_admin_user_path(current_solidus_admin_user)
+    end
   end
 end
