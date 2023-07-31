c347a76b17284989cbbdd3256462d8379d9e933f
Elia Schito <elia@schito.me>
2023-07-31 14:44:14 +0200

    Decouple the ui/table component from the pagination implementation
    Still provide pagination support out of the box without explicitly
    needing to render a different component from outside.
    
    Shifting the responsibility of providing page links and the correct
    data to the ui/table component makes everything more linear.
    
    Co-Authored-By: Rainer Dema <rainer.dema@gmail.com>

--- admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
+++ admin/spec/components/previews/solidus_admin/ui/table/component_preview.rb
@@ -12,17 +12,20 @@ class SolidusAdmin::UI::Table::ComponentPreview < ViewComponent::Preview
   #
   # All these ways to header and data cells can be mixed and matched.
   def simple
-    model_class = Spree::Product
-    rows = Array.new(10) do |n|
-      model_class.new(id: n, name: "Product #{n}", price: n * 10.0, available_on: n.days.ago)
-    end
-
-    page = Struct.new(:records, :number, :next_param, :first?, :last?).new(rows, 1, '#', true, false)
-
-    page.records.define_singleton_method(:model) do
-      model_class
-    end
-
-    render_with_template(locals: { page: page, rows: rows })
+    render current_component.new(
+      model_class: Spree::Product,
+      rows: Array.new(10) {
+        Spree::Product.new(id: n, name: "Product #{n}", price: n * 10.0, available_on: n.days.ago)
+      },
+      columns: [
+        { header: :id, data: -> { _1.id.to_s } },
+        { header: :name, data: :name },
+        { header: -> { "Availability at #{Time.current}" }, data: -> { "#{time_ago_in_words _1.available_on} ago" } },
+        { header: -> { component("ui/badge").new(name: "$$$") }, data: -> { component("ui/badge").new(name: _1.display_price, color: :green) } },
+        { header: "Generated at", data: Time.current.to_s },
+      ],
+      prev_page_link: nil,
+      next_page_link: '#2',
+    )
   end
 end
