bc9b85258e277c738be44234a9bb5f33bb634da9
Elia Schito <elia@schito.me>
2023-07-31 17:27:50 +0200

    Use the ui/icon component to render the sidebar arrow

--- admin/app/components/solidus_admin/sidebar/component.rb
+++ admin/app/components/solidus_admin/sidebar/component.rb
@@ -6,14 +6,17 @@ class SolidusAdmin::Sidebar::Component < SolidusAdmin::BaseComponent
     store:,
     logo_path: SolidusAdmin::Config.logo_path,
     items: container["main_nav_items"],
+    icon_component: component("ui/icon"),
     item_component: component("sidebar/item"),
     account_nav_component: component("sidebar/account_nav")
   )
     @logo_path = logo_path
     @items = items
+    @store = store
+
+    @icon_component = icon_component
     @item_component = item_component
     @account_nav_component = account_nav_component
-    @store = store
   end
 
   def items
