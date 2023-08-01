960e3d058e00a7a9c48d1f203fdf491bee72b6b3
Elia Schito <elia@schito.me>
2023-08-01 15:04:35 +0200

    Add a helper to stub authorization when visiting backend pages
    For some reason the core authorization helper is not working.

--- admin/spec/features/accounts_spec.rb
+++ admin/spec/features/accounts_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe "Accounts", type: :feature do
   it "shows account info" do
     user = create(:admin_user, email: 'admin@example.com')
+    stub_authorization! user
     sign_in user
 
     visit "/admin/account"
