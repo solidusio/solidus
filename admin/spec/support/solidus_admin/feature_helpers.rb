806a5f9b2e6a7f62a5609408ca693853cfa9a9b1
Elia Schito <elia@schito.me>
2023-08-01 15:04:35 +0200

    Add a helper to stub authorization when visiting backend pages
    For some reason the core authorization helper is not working.

--- admin/spec/support/solidus_admin/feature_helpers.rb
+++ admin/spec/support/solidus_admin/feature_helpers.rb
@@ -5,5 +5,16 @@ module SolidusAdmin
     def sign_in(user)
       allow_any_instance_of(SolidusAdmin::BaseController).to receive(:spree_current_user).and_return(user)
     end
+
+    def stub_authorization!(user)
+      ability = Spree::Ability.new(user)
+      if block_given?
+        yield ability
+      else
+        ability.can :manage, :all
+      end
+      allow_any_instance_of(SolidusAdmin::BaseController).to receive(:current_ability).and_return(ability)
+      allow_any_instance_of(Spree::Admin::BaseController).to receive(:current_ability).and_return(ability)
+    end
   end
 end
