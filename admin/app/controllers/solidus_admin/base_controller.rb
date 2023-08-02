d747571f9514e3bfed32aeefc1a774cceed8a1e4
Elia Schito <elia@schito.me>
2023-08-02 17:32:47 +0200

    Add support for changing locale in the sidebar/account_nav component

--- admin/app/controllers/solidus_admin/base_controller.rb
+++ admin/app/controllers/solidus_admin/base_controller.rb
@@ -7,13 +7,27 @@ module SolidusAdmin
     include ActiveStorage::SetCurrent
     include ::SolidusAdmin::Auth
     include Spree::Core::ControllerHelpers::Store
+    include Spree::Admin::SetsUserLanguageLocaleKey
 
     include SolidusAdmin::AuthAdapters::Backend if defined?(Spree::Backend)
 
     include ::GearedPagination::Controller
 
+    before_action :set_locale
+
     layout 'solidus_admin/application'
     helper 'solidus_admin/container'
     helper 'solidus_admin/layout'
+
+    private
+
+    def set_locale
+      if params[:switch_to_locale].to_s != session[set_user_language_locale_key].to_s
+        session[set_user_language_locale_key] = params[:switch_to_locale]
+        flash[:notice] = t('spree.locale_changed')
+      end
+
+      I18n.locale = session[set_user_language_locale_key] ? session[set_user_language_locale_key].to_sym : I18n.default_locale
+    end
   end
 end
