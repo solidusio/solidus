c559574767759c880e28382e624aead8edadfe6b
Elia Schito <elia@schito.me>
2023-08-03 18:19:50 +0200

    Extract the locale helpers to a dedicated namespace

--- admin/app/controllers/solidus_admin/base_controller.rb
+++ admin/app/controllers/solidus_admin/base_controller.rb
@@ -6,28 +6,14 @@ module SolidusAdmin
   class BaseController < ApplicationController
     include ActiveStorage::SetCurrent
     include Spree::Core::ControllerHelpers::Store
-    include Spree::Admin::SetsUserLanguageLocaleKey
     include GearedPagination::Controller
 
     include SolidusAdmin::ControllerHelpers::Auth
+    include SolidusAdmin::ControllerHelpers::Locale
     include SolidusAdmin::AuthAdapters::Backend if defined?(Spree::Backend)
-
-
-    before_action :set_locale
 
     layout 'solidus_admin/application'
     helper 'solidus_admin/container'
     helper 'solidus_admin/layout'
-
-    private
-
-    def set_locale
-      if params[:switch_to_locale].to_s != session[set_user_language_locale_key].to_s
-        session[set_user_language_locale_key] = params[:switch_to_locale]
-        flash[:notice] = t('spree.locale_changed')
-      end
-
-      I18n.locale = session[set_user_language_locale_key] ? session[set_user_language_locale_key].to_sym : I18n.default_locale
-    end
   end
 end
