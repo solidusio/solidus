6381d8a5d5d51f6eaafdbc7a962316a7b478682b
Elia Schito <elia@schito.me>
2023-08-03 18:21:37 +0200

    Don't override the admin locale session key
    The previous implementation was writing on the session key if the
    requested locale was empty and the session key was set.

--- admin/app/controllers/solidus_admin/controller_helpers/locale.rb
+++ admin/app/controllers/solidus_admin/controller_helpers/locale.rb
@@ -6,16 +6,27 @@ module SolidusAdmin::ControllerHelpers::Locale
 
   included do
     before_action :set_locale
+    before_action :update_user_locale
   end
 
   private
 
-  def set_locale
-    if params[:switch_to_locale].to_s != session[set_user_language_locale_key].to_s
-      session[set_user_language_locale_key] = params[:switch_to_locale]
-      flash[:notice] = t('spree.locale_changed')
-    end
+  def update_user_locale
+    requested_locale = params[:switch_to_locale] or return
 
-    I18n.locale = session[set_user_language_locale_key] ? session[set_user_language_locale_key].to_sym : I18n.default_locale
+    if requested_locale.to_sym != user_locale
+      session[set_user_language_locale_key] = requested_locale
+
+      flash[:notice] = t('spree.locale_changed')
+      redirect_to url_for(request.params.except(:switch_to_locale))
+    end
+  end
+
+  def user_locale
+    session[set_user_language_locale_key] || I18n.default_locale
+  end
+
+  def set_locale
+    I18n.locale = user_locale
   end
 end
