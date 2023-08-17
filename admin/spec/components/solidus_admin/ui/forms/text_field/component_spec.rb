d47fff936a78772a2afb57156105c8e56caf4e67
Marc Busqué <marc@lamarciana.com>
2023-08-17 11:23:29 +0200

    Add text_field component to solidus_admin
    This component is used to render a text field in a form. It leverages
    the `type` attribute to also render different input fields, although we
    might want to specialize in the future.
    
    It comes in three sizes: small, medium and large. It also supports
    rendering a label and a hint, as well as field error messages.
    
    It needs to be rendered in the context of a block yielded by one of
    Rails' form helpers, such as `form_for` or `form_with`. This is to
    leverage the automatic inferrence of the `name`, `id` and `for`
    attributes, therefore avoiding boilerplate. When the given form builder
    is bound to a model instance, the error messages will be automatically
    extracted. Otherwise, an explicit errors hash needs to be passed.
    
    Ref. #5329

---
+++ admin/spec/components/solidus_admin/ui/forms/text_field/component_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SolidusAdmin::UI::Forms::TextField::Component, type: :component do
+  it "renders the overview preview" do
+    render_preview(:overview)
+    render_preview(:playground)
+  end
+
+  describe "#initialize" do
+    it "uses given errors when form is bound to a model" do
+      form = double("form", object: double("model", errors: {}))
+
+      component = described_class.new(form: form, field: :name, errors: { name: ["can't be blank"] })
+
+      expect(component.errors?).to be(true)
+    end
+
+    it "uses model errors when form is bound to a model and they are not given" do
+      form = double("form", object: double("model", errors: { name: ["can't be blank"] }))
+
+      component = described_class.new(form: form, field: :name)
+
+      expect(component.errors?).to be(true)
+    end
+
+    it "uses given errors when form is not bound to a model" do
+      form = double("form", object: nil)
+
+      component = described_class.new(form: form, field: :name, errors: { name: ["can't be blank"] })
+
+      expect(component.errors?).to be(true)
+    end
+
+    it "raises an error when form is not bound to a model and errors are not given" do
+      form = double("form", object: nil)
+
+      expect { described_class.new(form: form, field: :name) }.to raise_error(ArgumentError)
+    end
+  end
+end
