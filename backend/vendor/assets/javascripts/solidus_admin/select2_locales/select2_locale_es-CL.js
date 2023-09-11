28ae3e2800aa746ebf420407d504263387ae8bd5
MauricioTRP <92993580+MauricioTRP@users.noreply.github.com>
2023-09-11 14:52:07 -0300

    Added es-CL select2_locale js
    Added missing select2_local es-CL

---
+++ backend/vendor/assets/javascripts/solidus_admin/select2_locales/select2_locale_es-CL.js
@@ -0,0 +1,10 @@
+/**
+ * Select2 Chilean Spanish translations
+ */
+(function ($) {
+  "use strict";
+
+  $.fn.select2.locales['es-CL'] = {};
+
+  $.extend($.fn.select2.defaults, $.fn.select2.locales['es-CL']);
+})(jQuery);
