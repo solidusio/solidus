51310d7fe4fe518f57ec23a691e21258b1670cbc
Elia Schito <elia@schito.me>
2023-09-13 18:06:19 +0200

    Add the layout and basic buttons for the products/show component

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -2,7 +2,7 @@
 
 SolidusAdmin::Engine.routes.draw do
   resource :account, only: :show
-  resources :products, only: :index do
+  resources :products, only: [:index, :show, :edit, :update] do
     collection do
       delete :destroy
       put :discontinue
