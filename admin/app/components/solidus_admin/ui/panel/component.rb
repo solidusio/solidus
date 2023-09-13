9bf84949de0573a4b6de2893c791f23c694c871e
Elia Schito <elia@schito.me>
2023-09-13 17:23:36 +0200

    Support only a single action as a link in the ui/panel component

--- admin/app/components/solidus_admin/ui/panel/component.rb
+++ admin/app/components/solidus_admin/ui/panel/component.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Panel::Component < SolidusAdmin::BaseComponent
-  renders_one :actions
+  renders_one :action, ->(name:, href:, icon: 'add-box-fill', **args) {
+    link_to(
+      icon_tag(icon, class: 'w-[1.4em] h-[1.4em]') + name,
+      href,
+      **args,
+      class: 'flex gap-1 hover:underline'
+    )
+  }
 
   # @param title [String] the title of the panel
   # @param title_hint [String] the title hint of the panel
