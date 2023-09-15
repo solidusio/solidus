002c2f30ca8b40fcb84f666028faf3c30a08e764
Elia Schito <elia@schito.me>
2023-09-15 15:58:45 +0200

    Remove unused form harness

--- admin/spec/components/solidus_admin/ui/forms/fieldset/component_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-RSpec.describe SolidusAdmin::UI::Forms::Fieldset::Component, type: :component do
-  it "renders the overview preview" do
-    render_preview(:overview)
-  end
-end
