f6d9cf234bfcb05f6ead222233e90b2c58924c7c
Elia Schito <elia@schito.me>
2023-09-18 12:25:28 +0200

    Reimplement text_field, selext, and text_area with the input component

--- admin/app/components/solidus_admin/ui/forms/field/component.rb
+++ admin/app/components/solidus_admin/ui/forms/field/component.rb
@@ -1,11 +1,72 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Forms::Field::Component < SolidusAdmin::BaseComponent
-  def initialize(label:, hint: nil, tip: nil, error: nil, **attributes)
+  def initialize(label:, hint: nil, tip: nil, error: nil, input_attributes: nil, **attributes)
     @label = label
     @hint = hint
     @tip = tip
-    @error = error
+    @error = [error] if error.present?
     @attributes = attributes
+    @input_attributes = input_attributes
+
+    raise ArgumentError, "provide either a block or input_attributes" if content? && input_attributes
+  end
+
+  def self.text_field(form, method, hint: nil, tip: nil, size: :m, **attributes)
+    errors = form.object.errors.messages_for(method).presence
+
+    new(
+      label: form.object.class.human_attribute_name(method),
+      hint: hint,
+      tip: tip,
+      error: errors,
+      input_attributes: {
+        name: "#{form.object_name}[#{method}]",
+        tag: :input,
+        size: size,
+        value: form.object.public_send(method),
+        error: (errors.to_sentence.capitalize if errors),
+        **attributes,
+      }
+    )
+  end
+
+  def self.select(form, method, choices, hint: nil, tip: nil, size: :m, **attributes)
+    errors = form.object.errors.messages_for(method).presence
+
+    new(
+      label: form.object.class.human_attribute_name(method),
+      hint: hint,
+      tip: tip,
+      error: errors,
+      input_attributes: {
+        name: "#{form.object_name}[#{method}]",
+        tag: :select,
+        choices: choices,
+        size: size,
+        value: form.object.public_send(method),
+        error: (errors.to_sentence.capitalize if errors),
+        **attributes,
+      }
+    )
+  end
+
+  def self.text_area(form, method, hint: nil, tip: nil, size: :m, **attributes)
+    errors = form.object.errors.messages_for(method).presence
+
+    new(
+      label: form.object.class.human_attribute_name(method),
+      hint: hint,
+      tip: tip,
+      error: errors,
+      input_attributes: {
+        name: "#{form.object_name}[#{method}]",
+        size: size,
+        tag: :textarea,
+        value: form.object.public_send(method),
+        error: (errors.to_sentence.capitalize if errors),
+        **attributes,
+      }
+    )
   end
 end
