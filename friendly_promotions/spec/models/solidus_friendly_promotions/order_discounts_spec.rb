889fd135abcec5fa8ae245ede81a31280ce6ca31
Martin Meyerhoff <mamhoff@gmail.com>
2023-09-29 13:45:18 +0200

    Remove unused OrderDiscounts class
    The responsibilities for this class are now in the Discountable::*
    classes.

--- friendly_promotions/spec/models/solidus_friendly_promotions/order_discounts_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe SolidusFriendlyPromotions::OrderDiscounts do
-  it { is_expected.to respond_to :order_id }
-  it { is_expected.to respond_to :line_item_discounts }
-  it { is_expected.to respond_to :shipment_discounts }
-  it { is_expected.to respond_to :shipping_rate_discounts }
-end
