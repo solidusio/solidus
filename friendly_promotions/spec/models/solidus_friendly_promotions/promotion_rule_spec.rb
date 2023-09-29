6b75dcf4e2e41539c42721e9e3156c4053262f86
Martin Meyerhoff <mamhoff@gmail.com>
2023-09-29 17:53:40 +0200

    Fix standardrb error in promotion rule spec

--- friendly_promotions/spec/models/solidus_friendly_promotions/promotion_rule_spec.rb
+++ friendly_promotions/spec/models/solidus_friendly_promotions/promotion_rule_spec.rb
@@ -3,11 +3,16 @@
 require "spec_helper"
 
 RSpec.describe SolidusFriendlyPromotions::PromotionRule do
-  class BadTestRule < SolidusFriendlyPromotions::PromotionRule; end
+  let(:bad_test_rule_class) { Class.new(SolidusFriendlyPromotions::PromotionRule) }
+  let(:test_rule_class) do
+    Class.new(SolidusFriendlyPromotions::PromotionRule) do
+      def self.model_name
+        ActiveModel::Name.new(self, nil, "test_rule")
+      end
 
-  class TestRule < SolidusFriendlyPromotions::PromotionRule
-    def eligible?(_promotable, _options = {})
-      true
+      def eligible?(_promotable, _options = {})
+        true
+      end
     end
   end
 
@@ -18,21 +23,22 @@ RSpec.describe SolidusFriendlyPromotions::PromotionRule do
   end
 
   it "forces developer to implement eligible? method" do
-    expect { BadTestRule.new.eligible?("promotable") }.to raise_error NotImplementedError
+    expect { bad_test_rule_class.new.eligible?("promotable") }.to raise_error NotImplementedError
+    expect { test_rule_class.new.eligible?("promotable") }.not_to raise_error NotImplementedError
   end
 
   it "validates unique rules for a promotion" do
-    promotion_one = TestRule.new
+    promotion_one = test_rule_class.new
     promotion_one.promotion_id = 1
     promotion_one.save
 
-    promotion_two = TestRule.new
+    promotion_two = test_rule_class.new
     promotion_two.promotion_id = 1
     expect(promotion_two).not_to be_valid
   end
 
   it "generates its own partial path" do
-    rule = TestRule.new
+    rule = test_rule_class.new
     expect(rule.to_partial_path).to eq "solidus_friendly_promotions/admin/promotion_rules/rules/test_rule"
   end
 end
