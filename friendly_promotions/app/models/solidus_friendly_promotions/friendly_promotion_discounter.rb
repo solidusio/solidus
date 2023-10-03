3a7955c6e6e804b9cb5d514a84597c6ea7f33f4a
Martin Meyerhoff <mamhoff@gmail.com>
2023-10-03 11:39:11 +0200

    Extract promotion loading from promo discounter

--- friendly_promotions/app/models/solidus_friendly_promotions/friendly_promotion_discounter.rb
+++ friendly_promotions/app/models/solidus_friendly_promotions/friendly_promotion_discounter.rb
@@ -6,6 +6,7 @@ module SolidusFriendlyPromotions
 
     def initialize(order)
       @order = order
+      possible_promotions = PromotionLoader.new(order: order).call
       @promotions = PromotionEligibility.new(promotable: order, possible_promotions: possible_promotions).call
     end
 
@@ -54,43 +55,6 @@ module SolidusFriendlyPromotions
         chosen_item_discounts = SolidusFriendlyPromotions.config.discount_chooser_class.new(rate).call(discounts)
         [rate, chosen_item_discounts]
       end
-    end
-
-    def possible_promotions
-      promos = connected_order_promotions | sale_promotions
-      promos.flat_map(&:actions).group_by(&:preload_relations).each do |preload_relations, actions|
-        preload(records: actions, associations: preload_relations)
-      end
-      promos.flat_map(&:rules).group_by(&:preload_relations).each do |preload_relations, rules|
-        preload(records: rules, associations: preload_relations)
-      end
-      promos.reject { |promotion| promotion.usage_limit_exceeded?(excluded_orders: [order]) }
-    end
-
-    def preload(records:, associations:)
-      ActiveRecord::Associations::Preloader.new(records: records, associations: associations).call
-    end
-
-    def connected_order_promotions
-      eligible_connected_promotion_ids = order.friendly_order_promotions.select do |order_promotion|
-        order_promotion.promotion_code.nil? || !order_promotion.promotion_code.usage_limit_exceeded?(excluded_orders: [order])
-      end.map(&:promotion_id)
-      order.friendly_promotions.active(reference_time).where(id: eligible_connected_promotion_ids).includes(promotion_includes)
-    end
-
-    def sale_promotions
-      SolidusFriendlyPromotions::Promotion.where(apply_automatically: true).active(reference_time).includes(promotion_includes)
-    end
-
-    def reference_time
-      order.completed_at || Time.current
-    end
-
-    def promotion_includes
-      [
-        :rules,
-        :actions
-      ]
     end
   end
 end
---
+++ friendly_promotions/spec/models/solidus_friendly_promotions/promotion_loader_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe SolidusFriendlyPromotions::PromotionLoader do
+  describe "selecting promotions" do
+    subject { described_class.new(order: order).call }
+
+    let(:order) { create(:order) }
+
+    let!(:active_promotion) { create(:friendly_promotion, :with_adjustable_action, apply_automatically: true) }
+    let!(:inactive_promotion) do
+      create(:friendly_promotion, :with_adjustable_action, expires_at: 2.days.ago, apply_automatically: true)
+    end
+    let!(:connectable_promotion) { create(:friendly_promotion, :with_adjustable_action) }
+    let!(:connectable_inactive_promotion) do
+      create(:friendly_promotion, :with_adjustable_action, expires_at: 2.days.ago)
+    end
+
+    context "no promo is connected to the order" do
+      it "returns only active promotions" do
+        expect(subject).to eq([active_promotion])
+      end
+    end
+
+    context "an active promo is connected to the order" do
+      before do
+        order.friendly_promotions << connectable_promotion
+      end
+
+      it "checks active and connected promotions" do
+        expect(subject).to include(active_promotion, connectable_promotion)
+      end
+    end
+
+    context "an inactive promo is connected to the order" do
+      before do
+        order.friendly_promotions << connectable_inactive_promotion
+      end
+
+      it "does not check connected inactive promotions" do
+        expect(subject).not_to include(connectable_inactive_promotion)
+        expect(subject).to eq([active_promotion])
+      end
+    end
+  end
+
+  context "promotions in the past" do
+    let(:order) { create(:order, completed_at: 7.days.ago) }
+    let(:currently_active_promotion) { create(:friendly_promotion, :with_adjustable_action, starts_at: 1.hour.ago) }
+    let(:past_promotion) { create(:friendly_promotion, :with_adjustable_action, starts_at: 1.year.ago, expires_at: 11.months.ago) }
+    let(:order_promotion) { create(:friendly_promotion, :with_adjustable_action, starts_at: 8.days.ago, expires_at: 6.days.ago) }
+
+    before do
+      order.friendly_promotions << past_promotion
+      order.friendly_promotions << order_promotion
+      order.friendly_promotions << currently_active_promotion
+    end
+
+    subject { described_class.new(order: order).call }
+
+    it "only evaluates the past promotion that was active when the order was completed" do
+      expect(subject).to eq([order_promotion])
+    end
+  end
+end
