0195e3fb56e7e4380713e69cc0f706e6fec0c10d
Martin Meyerhoff <mamhoff@gmail.com>
2023-10-06 14:50:25 +0200

    Add stimulus-rails as a dependency
    We need stimulus-rails.

--- friendly_promotions/lib/solidus_friendly_promotions.rb
+++ friendly_promotions/lib/solidus_friendly_promotions.rb
@@ -3,6 +3,7 @@
 require "spree"
 require "turbo-rails"
 require "importmap-rails"
+require "stimulus-rails"
 require "solidus_friendly_promotions/configuration"
 require "solidus_friendly_promotions/version"
 require "solidus_friendly_promotions/engine"
