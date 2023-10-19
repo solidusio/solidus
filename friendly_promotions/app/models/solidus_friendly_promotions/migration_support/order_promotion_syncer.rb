f5392507fff6300d9887fa5c1826990db4d9ae2a
Martin Meyerhoff <mamhoff@gmail.com>
2023-10-19 12:25:14 +0200

    Allow syncing friendly order promotions to spree order promotions
    If anything goes wrong while migrating, this commit allows us to easily
    roll back.

--- friendly_promotions/app/models/solidus_friendly_promotions/migration_support/order_promotion_syncer.rb
+++ friendly_promotions/app/models/solidus_friendly_promotions/migration_support/order_promotion_syncer.rb
@@ -10,6 +10,13 @@ module SolidusFriendlyPromotions
       end
 
       def call
+        sync_spree_order_promotions_to_friendly_order_promotions
+        sync_friendly_order_promotions_to_spree_order_promotions
+      end
+
+      private
+
+      def sync_spree_order_promotions_to_friendly_order_promotions
         order.order_promotions.each do |spree_order_promotion|
           friendly_promotion = SolidusFriendlyPromotions::Promotion.find_by(
             original_promotion_id: spree_order_promotion.promotion.id
@@ -23,6 +30,22 @@ module SolidusFriendlyPromotions
           order.friendly_order_promotions.find_or_create_by!(
             promotion: friendly_promotion,
             promotion_code: friendly_promotion_code
+          )
+        end
+      end
+
+      def sync_friendly_order_promotions_to_spree_order_promotions
+        order.friendly_order_promotions.each do |friendly_order_promotion|
+          spree_promotion = friendly_order_promotion.promotion.original_promotion
+          next unless spree_promotion
+          if friendly_order_promotion.promotion_code
+            spree_promotion_code = spree_promotion.promotion_codes.find_by(
+              value: friendly_order_promotion.promotion_code.value
+            )
+          end
+          order.order_promotions.find_or_create_by!(
+            promotion: spree_promotion,
+            promotion_code: spree_promotion_code
           )
         end
       end
--- friendly_promotions/spec/models/solidus_friendly_promotions/migration_support/order_promotion_syncer_spec.rb
+++ friendly_promotions/spec/models/solidus_friendly_promotions/migration_support/order_promotion_syncer_spec.rb
@@ -59,4 +59,46 @@ RSpec.describe SolidusFriendlyPromotions::MigrationSupport::OrderPromotionSyncer
       end
     end
   end
+
+  context "when there are frienly order promotions" do
+    let(:spree_promotion) { nil }
+    let(:friendly_promotion) { create(:friendly_promotion, original_promotion: spree_promotion) }
+    let(:friendly_promotion_code) { nil }
+
+    before do
+      order.friendly_order_promotions.create(
+        promotion: friendly_promotion,
+        promotion_code: friendly_promotion_code
+      )
+    end
+
+    it "does not create order promotions" do
+      expect { subject }.not_to change { order.order_promotions.length }
+    end
+
+    context "when there is a corresponding  promotion" do
+      let(:spree_promotion) { create(:promotion) }
+      let!(:spree_promotion_code) { nil }
+
+      it "creates a spree order promotion" do
+        expect { subject }.to change { order.order_promotions.length }.by(1)
+        expect(order.order_promotions.first.promotion).to eq(spree_promotion)
+      end
+
+      it "does not create a friendly order promotion twice" do
+        described_class.new(order: order).call
+        expect { subject }.not_to change { order.order_promotions.length }
+      end
+
+      context "with a promotion code" do
+        let(:friendly_promotion_code) { create(:friendly_promotion_code, promotion: friendly_promotion) }
+        let!(:spree_promotion_code) { create(:promotion_code, promotion: spree_promotion, value: friendly_promotion_code.value) }
+
+        it "does creates a friendly order promotion with the corresponding code" do
+          expect { subject }.to change { order.order_promotions.length }.by(1)
+          expect(order.order_promotions.first.promotion_code).to eq(spree_promotion_code)
+        end
+      end
+    end
+  end
 end
