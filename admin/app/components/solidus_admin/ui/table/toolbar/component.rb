53b7783c8f66ed4fa2e93cb4b93677843647a6b5
Rainer Dema <rainer.dema@gmail.com>
2023-10-23 18:12:25 +0200

    Support params in `ui/table/toolbar` component
    Updated the `SolidusAdmin::UI::Table::Toolbar::Component` to accept
    and render additional parameters.

--- admin/app/components/solidus_admin/ui/table/toolbar/component.rb
+++ admin/app/components/solidus_admin/ui/table/toolbar/component.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Table::Toolbar::Component < SolidusAdmin::BaseComponent
-  erb_template <<~ERB
-    <div class="
-      h-14 p-2 bg-white border-b border-gray-100
-      justify-start items-center gap-2
-      visible:flex hidden:hidden
-      rounded-t-lg
-    ">
-      <%= content %>
-    </div>
-  ERB
+  def initialize(**options)
+    @options = options
+  end
+
+  def call
+    tag.div(
+      content,
+      **@options,
+      class: "
+        h-14 p-2 bg-white border-b border-gray-100
+        justify-start items-center gap-2
+        visible:flex hidden:hidden
+        rounded-t-lg
+        #{@options[:class]}
+      "
+    )
+  end
 end
