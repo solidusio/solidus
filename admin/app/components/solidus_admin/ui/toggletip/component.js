2a631bcc2d8034a455e559cc34f4348d7d51dc1c
Elia Schito <elia@schito.me>
2023-10-24 16:06:25 +0200

    Add some dynamic positioning to the `ui/toggletip` component

--- admin/app/components/solidus_admin/ui/toggletip/component.js
+++ admin/app/components/solidus_admin/ui/toggletip/component.js
@@ -2,10 +2,11 @@ import { Controller } from '@hotwired/stimulus'
 import { useClickOutside } from 'stimulus-use'
 
 export default class extends Controller {
-  static targets = ['button', 'bubble', 'content']
+  static targets = ['bubble']
 
   connect () {
     useClickOutside(this)
+    this.open = false
   }
 
   clickOutside () {
@@ -13,14 +14,31 @@ export default class extends Controller {
   }
 
   toggle (e) {
-    this.element.open = !this.element.open
+    e.preventDefault()
+    this.open = !this.open
+    this.render()
   }
 
   open () {
-    this.element.open = true
+    this.open = true
+    this.render()
   }
 
   close () {
-    this.element.open = false
+    this.open = false
+    this.render()
+  }
+
+  render() {
+    const needsPositioning = this.open && !this.element.open
+    this.element.open = this.open
+
+    if (needsPositioning) {
+      const bubbleRect = this.bubbleTarget.getBoundingClientRect()
+      if (bubbleRect.right  > window.innerWidth) this.bubbleTarget.style.left = `${window.innerWidth - bubbleRect.width}px`
+      if (bubbleRect.bottom > window.innerHeight) this.bubbleTarget.style.top = `${window.innerHeight - bubbleRect.height}px`
+      if (bubbleRect.left < 0) this.bubbleTarget.style.left = '0px'
+      if (bubbleRect.top < 0) this.bubbleTarget.style.top = '0px'
+    }
   }
 }
