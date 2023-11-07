b58646bc012d3de4111d43426fa8a1c9700b33df
Rainer Dema <rainer.dema@gmail.com>
2023-11-07 16:09:27 +0100

    Implement server-side state preloading for address form
    Enhance the address form's state field by ensuring the correct state is selected
    during asynchronous loading.
    This adjustment addresses a timing issue where Stimulus did not populate the
    state select promptly, leading to mismatched selections.

--- admin/app/components/solidus_admin/ui/forms/address/component.js
+++ admin/app/components/solidus_admin/ui/forms/address/component.js
@@ -3,10 +3,6 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ["country", "state"]
 
-  connect() {
-    this.loadStates()
-  }
-
   loadStates() {
     const countryId = this.countryTarget.value
 
