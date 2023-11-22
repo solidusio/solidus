aacc79783e692468d09dcaef16afe2bd87f09663
Rainer Dema <rainer.dema@gmail.com>
2023-11-22 11:11:21 +0100

    [wip] Add `orders/show/shipment` component

--- admin/app/components/solidus_admin/orders/show/shipment/component.rb
+++ admin/app/components/solidus_admin/orders/show/shipment/component.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::Orders::Show::Shipment::Component < SolidusAdmin::BaseComponent
-  def initialize(shipment:)
+  def initialize(shipment:, index:)
     @shipment = shipment
+    @index = index
+  end
+
+  def manifest
+    Spree::ShippingManifest.new(
+      inventory_units: @shipment.inventory_units.where(carton_id: nil),
+    ).items.sort_by { |item| item.line_item.created_at }
   end
 end
