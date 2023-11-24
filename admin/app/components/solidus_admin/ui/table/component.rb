34faee9fcc13150083afc2d9d2dcaa3968586a19
Elia Schito <elia@schito.me>
2023-11-24 13:05:08 +0100

    Refactor `ui/table` to use the internal objects instead of proxy ivars

--- admin/app/components/solidus_admin/ui/table/component.rb
+++ admin/app/components/solidus_admin/ui/table/component.rb
@@ -7,36 +7,53 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
   Scope = Struct.new(:name, :label, :default, keyword_init: true)
   private_constant :BatchAction, :Column, :Filter, :Scope
 
-  Data = Struct.new(:rows, :class, :url, :prev, :next, :columns, :fade, :batch_actions, keyword_init: true) # rubocop:disable Lint/StructNewOverride
-  Search = Struct.new(:name, :value, :url, :searchbar_key, :filters, :scopes, keyword_init: true)
+  class Data < Struct.new(:rows, :class, :url, :prev, :next, :columns, :fade, :batch_actions, keyword_init: true) # rubocop:disable Lint/StructNewOverride,Style/StructInheritance
+    def initialize(**args)
+      super
+
+      self.columns = columns.map { |column| Column.new(wrap: false, **column) }
+      self.batch_actions = batch_actions.to_a.map { |action| BatchAction.new(**action) }
+    end
+
+    def plural_name
+      self[:class].model_name.human.pluralize if self[:class]
+    end
+  end
+
+  class Search < Struct.new(:name, :value, :url, :searchbar_key, :filters, :scopes, keyword_init: true) # rubocop:disable Style/StructInheritance
+    def initialize(**args)
+      super
+
+      self.filters = filters.to_a.map { |filter| Filter.new(**filter) }
+      self.scopes = scopes.to_a.map { |scope| Scope.new(**scope) }
+    end
+
+    def current_scope
+      scopes.find { |scope| scope.name.to_s == value[:scope].presence } || default_scope
+    end
+
+    def default_scope
+      scopes.find(&:default)
+    end
+
+    def scope_param_name
+      "#{name}[scope]"
+    end
+
+    def searchbar_param_name
+      "#{name}[#{searchbar_key}]"
+    end
+
+    def value
+      super || {}
+    end
+  end
 
   def initialize(id:, data:, search: nil)
     @id = id
     @data = Data.new(**data)
+    @data.columns.unshift selectable_column if @data.batch_actions.present?
     @search = Search.new(**search)
-
-    # Data
-    @columns = @data.columns.map { Column.new(wrap: true, **_1) }
-    @columns.unshift selectable_column if @data.batch_actions.present?
-    @batch_actions = @data.batch_actions&.map { BatchAction.new(**_1) }
-    @model_class = data[:class]
-    @rows = @data.rows
-    @row_fade = @data.fade
-    @row_url = @data.url
-    @prev_page_link = @data.prev
-    @next_page_link = @data.next
-
-    # Search
-    @filters = @search.filters.map { Filter.new(**_1) }
-    @scopes = @search.scopes.map { Scope.new(**_1) }
-    @search_param = @search.name
-    @search_params = @search.value
-    @search_key = @search.searchbar_key
-    @search_url = @search.url
-  end
-
-  def resource_plural_name
-    @model_class.model_name.human.pluralize
   end
 
   def selectable_column
@@ -95,7 +112,7 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
   def render_ransack_filter_dropdown(filter, index)
     render component("ui/table/ransack_filter").new(
       presentation: filter.presentation,
-      search_param: @search_param,
+      search_param: @search.name,
       combinator: filter.combinator,
       attribute: filter.attribute,
       predicate: filter.predicate,
@@ -107,7 +124,7 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
 
   def render_header_cell(cell, **attrs)
     cell = cell.call if cell.respond_to?(:call)
-    cell = @model_class.human_attribute_name(cell) if cell.is_a?(Symbol)
+    cell = @data[:class].human_attribute_name(cell) if cell.is_a?(Symbol)
     cell = cell.render_in(self) if cell.respond_to?(:render_in)
 
     content_tag(:th, cell, class: %{
@@ -135,14 +152,10 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
   end
 
   def current_scope_name
-    @current_scope_name ||= params.dig(@search_param, :scope).presence || @scopes.find(&:default)&.name&.to_s
-  end
-
-  def scope_param_name
-    @scope_param_name ||= "#{@search_param}[scope]"
+    @search.current_scope.name
   end
 
   def initial_mode
-    @initial_mode ||= params.dig(@search_param, @search_key) ? "search" : "scopes"
+    @initial_mode ||= @search.value[@search.searchbar_key] ? "search" : "scopes"
   end
 end
