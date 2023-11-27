6b2c6ed66b957859896aeedf7256c9641ca2e4e4
Elia Schito <elia@schito.me>
2023-11-27 16:14:02 +0100

    Allow columns to be defined as simple symbols
    When both the header and the data are defaulting to an AR attribute
    passing just the symbol should suffice.

--- admin/app/components/solidus_admin/ui/table/component.rb
+++ admin/app/components/solidus_admin/ui/table/component.rb
@@ -12,7 +12,9 @@ class SolidusAdmin::UI::Table::Component < SolidusAdmin::BaseComponent
     def initialize(**args)
       super
 
-      self.columns = columns.map { |column| Column.new(wrap: false, **column) }
+      self.columns = columns.map do |column|
+        column.is_a?(Symbol) ? Column.new(wrap: false, header: column, data: column) : Column.new(wrap: false, **column)
+      end
       self.batch_actions = batch_actions.to_a.map { |action| BatchAction.new(**action) }
     end
 
