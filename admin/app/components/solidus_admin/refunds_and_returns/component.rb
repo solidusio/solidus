2df44f1143f42ee004c00384c11338bb2265eb1d
Rainer Dema <rainer.dema@gmail.com>
2023-12-05 17:48:50 +0100

    Add `store credit reasons` index page with dedicated actions

--- admin/app/components/solidus_admin/refunds_and_returns/component.rb
+++ admin/app/components/solidus_admin/refunds_and_returns/component.rb
@@ -9,12 +9,12 @@ class SolidusAdmin::RefundsAndReturns::Component < SolidusAdmin::BaseComponent
   end
 
   def tabs
-    {}
     {
       Spree::RefundReason => solidus_admin.refund_reasons_path,
       Spree::ReimbursementType => solidus_admin.reimbursement_types_path,
       Spree::ReturnReason => solidus_admin.return_reasons_path,
       Spree::AdjustmentReason => solidus_admin.adjustment_reasons_path,
+      Spree::StoreCreditReason => solidus_admin.store_credit_reasons_path,
     }
   end
 end
---
+++ admin/app/components/solidus_admin/store_credit_reasons/index/component.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class SolidusAdmin::StoreCreditReasons::Index::Component < SolidusAdmin::BaseComponent
+  include SolidusAdmin::Layout::PageHelpers
+
+  def initialize(page:)
+    @page = page
+  end
+
+  def title
+    Spree::StoreCreditReason.model_name.human.pluralize
+  end
+
+  def prev_page_path
+    solidus_admin.url_for(**request.params, page: @page.number - 1, only_path: true) unless @page.first?
+  end
+
+  def next_page_path
+    solidus_admin.url_for(**request.params, page: @page.next_param, only_path: true) unless @page.last?
+  end
+
+  def batch_actions
+    [
+      {
+        display_name: t('.batch_actions.delete'),
+        action: solidus_admin.store_credit_reasons_path,
+        method: :delete,
+        icon: 'delete-bin-7-line',
+      },
+    ]
+  end
+
+  def filters
+    []
+  end
+
+  def scopes
+    []
+  end
+
+  def columns
+    [
+      :name,
+      {
+        header: :active,
+        data: ->(store_credit_reason) do
+          store_credit_reason.active? ? component('ui/badge').yes : component('ui/badge').no
+        end
+      },
+    ]
+  end
+end
