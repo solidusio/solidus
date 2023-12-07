3f588efadbbf9ee658834a7e1f7dbdd566e7bd32
Rainer Dema <rainer.dema@gmail.com>
2023-12-07 14:16:25 +0100

    Introude new edit and update action for stock item records
    Co-Authored-By: Elia Schito <elia@schito.me>

--- admin/app/controllers/solidus_admin/stock_items_controller.rb
+++ admin/app/controllers/solidus_admin/stock_items_controller.rb
@@ -3,6 +3,8 @@
 module SolidusAdmin
   class StockItemsController < SolidusAdmin::BaseController
     include SolidusAdmin::ControllerHelpers::Search
+    before_action :load_stock_items, only: [:index, :edit, :update]
+    before_action :load_stock_item, only: [:edit, :update]
 
     search_scope(:all, default: true) { _1 }
     search_scope(:back_orderable) { _1.where(backorderable: true) }
@@ -11,16 +13,46 @@ module SolidusAdmin
     search_scope(:in_stock) { _1.where('count_on_hand > 0') }
 
     def index
-      stock_items = apply_search_to(
-        Spree::StockItem.order(created_at: :desc, id: :desc),
-        param: :q,
-      )
-
-      set_page_and_extract_portion_from(stock_items)
-
       respond_to do |format|
         format.html { render component('stock_items/index').new(page: @page) }
       end
+    end
+
+    def edit
+    end
+
+    def update
+      quantity_adjustment = params[:quantity_adjustment].to_i
+      @stock_item.assign_attributes(stock_item_params.except(:page, :q))
+      @stock_item.stock_movements.build(quantity: quantity_adjustment, originator: current_solidus_admin_user)
+
+      if @stock_item.save
+        redirect_to solidus_admin.stock_items_path, status: :see_other
+      else
+      end
+    end
+
+    private
+
+    def load_stock_items
+      @stock_items = apply_search_to(
+        Spree::StockItem.reorder(nil),
+        param: :q,
+      )
+
+      set_page_and_extract_portion_from(@stock_items, ordered_by: {
+        variant_id: :desc,
+        stock_location_id: :desc,
+        id: :desc,
+      })
+    end
+
+    def load_stock_item
+      @stock_item = Spree::StockItem.find(params[:id])
+    end
+
+    def stock_item_params
+      params.require(:stock_item).permit(:backorderable, :page, :q)
     end
   end
 end
