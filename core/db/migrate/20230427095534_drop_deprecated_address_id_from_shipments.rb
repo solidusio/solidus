af2dc5a9b9a0ff5ebe93b54c9a99eb94b0876812
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2023-12-19 12:05:57 +0100

    Fix down migration
    `bin/rails db:rollback`
    
    fails with
    
    ```
    Caused by:
    ArgumentError: wrong number of arguments (given 2, expected 3)
    ```
    This commit is fixing the rollback

--- core/db/migrate/20230427095534_drop_deprecated_address_id_from_shipments.rb
+++ core/db/migrate/20230427095534_drop_deprecated_address_id_from_shipments.rb
@@ -5,7 +5,7 @@ class DropDeprecatedAddressIdFromShipments < ActiveRecord::Migration[5.2]
   end
 
   def down
-    add_column :spree_shipments, :deprecated_address_id
+    add_column :spree_shipments, :deprecated_address_id, :integer
     add_index :spree_shipments, :deprecated_address_id, name: :index_spree_shipments_on_deprecated_address_id
   end
 end
