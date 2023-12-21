19e0678f60f0354e67ca233a44459af1cf83dfbc
Rainer Dema <rainer.dema@gmail.com>
2023-12-21 11:07:27 +0100

    Removed unnecessary semicolon in `orders/show` component

--- admin/app/components/solidus_admin/orders/show/component.js
+++ admin/app/components/solidus_admin/orders/show/component.js
@@ -2,6 +2,6 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   closeMenus() {
-    this.element.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+    this.element.querySelectorAll('details').forEach(details => details.removeAttribute('open'))
   }
 }
