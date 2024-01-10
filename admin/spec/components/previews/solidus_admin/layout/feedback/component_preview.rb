d4c83e3aced349017d54496a65731790e81a93cd
Elia Schito <elia@schito.me>
2024-01-10 16:33:15 +0100

    Remove previews for non-UI components that didn't add much value
    The coverage shouldn't drop after this change.

--- admin/spec/components/previews/solidus_admin/layout/feedback/component_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SolidusAdmin::Layout::Feedback::ComponentPreview < ViewComponent::Preview
-  include SolidusAdmin::Preview
-
-  def overview
-    render_with_template
-  end
-end
