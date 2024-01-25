ac4eb8dd46bc3b114ebc608cc39a24c21f5876b3
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-25 15:08:02 +0100

    Environment Extension: Allow default value
    This extends the `add_class_set` command to accept a default value.
    Useful for, well, default values.

--- core/lib/spree/core/environment_extension.rb
+++ core/lib/spree/core/environment_extension.rb
@@ -8,10 +8,10 @@ module Spree
       extend ActiveSupport::Concern
 
       class_methods do
-        def add_class_set(name)
+        def add_class_set(name, default: [])
           define_method(name) do
             set = instance_variable_get("@#{name}")
-            set ||= send("#{name}=", [])
+            set ||= send("#{name}=", default)
             set
           end
 
--- core/spec/lib/spree/core/environment_extension_spec.rb
+++ core/spec/lib/spree/core/environment_extension_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe Spree::Core::EnvironmentExtension do
   subject! { base.include(described_class).new }
 
   describe '.add_class_set' do
+    let(:class_one) { String }
+    let(:class_two) { Array }
+    let(:class_three) { Hash }
+
     context 'with a class set named "foo"' do
       before { base.add_class_set('foo') }
-
-      let(:class_one) { String }
-      let(:class_two) { Array }
-      let(:class_three) { Hash }
 
       describe '#foo' do
         it { respond_to?(:foo) }
@@ -29,6 +29,16 @@ RSpec.describe Spree::Core::EnvironmentExtension do
         it { expect(subject.foo).to include(class_one) }
         it { expect(subject.foo).to include(class_two) }
         it { expect(subject.foo).not_to include(class_three) }
+      end
+    end
+
+    context "with a default value of [class_one]" do
+      before { base.add_class_set('foo', default: [class_one]) }
+
+      describe '#foo' do
+        it { respond_to?(:foo) }
+        it { expect(subject.foo).to include(class_one) }
+        it { expect(subject.foo).to be_kind_of Spree::Core::ClassConstantizer::Set }
       end
     end
   end
