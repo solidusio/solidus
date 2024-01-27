88253985c488ee742a013921f70e8b8f49abd3d1
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-27 13:14:25 +0100

    Move PercentOnLineItem calculator factory to legacy_promotions

--- core/lib/spree/testing_support/factories/calculator_factory.rb
+++ core/lib/spree/testing_support/factories/calculator_factory.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :calculator, aliases: [:flat_rate_calculator], class: 'Spree::Calculator::FlatRate' do
+  factory :flat_rate_calculator, aliases: [:calculator], class: 'Spree::Calculator::FlatRate' do
     preferred_amount { 10.0 }
   end
 
@@ -21,9 +21,5 @@ FactoryBot.define do
 
   factory :shipping_no_amount_calculator, class: 'Spree::Calculator::Shipping::FlatRate' do
     preferred_amount { 0 }
-  end
-
-  factory :percent_on_item_calculator, class: 'Spree::Calculator::PercentOnLineItem' do
-    preferred_percent { 10 }
   end
 end
