e6a11c73fb214dae945d70c4210ed8c2d9091e3f
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-27 13:05:14 +0100

    Move completed order with promotion factory to legacy_promotions

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -54,25 +54,6 @@ FactoryBot.define do
         order.recalculate
       end
 
-      factory :completed_order_with_promotion do
-        transient do
-          completed_at { Time.current }
-          promotion { nil }
-        end
-
-        after(:create) do |order, evaluator|
-          promotion = evaluator.promotion || create(:promotion, code: "test")
-          promotion_code = promotion.codes.first || create(:promotion_code, promotion: promotion)
-
-          promotion.activate(order: order, promotion_code: promotion_code)
-          order.order_promotions.create!(promotion: promotion, promotion_code: promotion_code)
-
-          # Complete the order after the promotion has been activated
-          order.update_column(:completed_at, evaluator.completed_at)
-          order.update_column(:state, "complete")
-        end
-      end
-
       factory :order_ready_to_complete do
         state { 'confirm' }
         payment_state { 'checkout' }
