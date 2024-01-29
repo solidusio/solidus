545a0e85d6ce8b3de99db710d5d13a25e4bbcc88
Martin Meyerhoff <mamhoff@gmail.com>
2024-01-29 15:41:21 +0100

    Add promotion-specific configuration to Spree::Core::PromotionConfiguration
    We want to be able to move all promotion-related things out of
    `solidus_core` in PR #5634. However, Spree::AppConfiguration also does
    all the promotion-specific things, and we can't move the app
    configuration out of core.
    
    In #5658, we've added a promotion configuration object as a nucleus for
    core's promotion system's configuration,
    `Spree::Core::PromotionConfiguration`. This implements all the
    promotion-specific configuration preferences from
    `Spree::AppConfiguration` there.

--- core/spec/lib/spree/core/promotion_configuration_spec.rb
+++ core/spec/lib/spree/core/promotion_configuration_spec.rb
@@ -3,8 +3,26 @@
 require "rails_helper"
 
 RSpec.describe Spree::Core::PromotionConfiguration do
+  subject(:config) { described_class.new }
+
+  it "uses base searcher class by default" do
+    expect(config.promotion_chooser_class).to eq Spree::PromotionChooser
+  end
+
+  it "uses order adjustments recalculator class by default" do
+    expect(config.promotion_adjuster_class).to eq Spree::Promotion::OrderAdjustmentsRecalculator
+  end
+
+  it "uses promotion handler coupon class by default" do
+    expect(config.coupon_code_handler_class).to eq Spree::PromotionHandler::Coupon
+  end
+
+  it "uses promotion handler shipping class by default" do
+    expect(config.shipping_promotion_handler_class).to eq Spree::PromotionHandler::Shipping
+  end
+
   describe "#calculators" do
-    subject { described_class.new.calculators[promotion_action] }
+    subject { config.calculators[promotion_action] }
 
     context "for Spree::Promotion::Actions::CreateAdjustment" do
       let(:promotion_action) { Spree::Promotion::Actions::CreateAdjustment }
