e29983fc60d7a99c3080f1f78577aad82864e59b
Martin Meyerhoff <mamhoff@gmail.com>
2024-02-17 21:28:01 +0100

    Add a Null promotion configuration
    We need a promotion configuration when solidus_legacy_promotions is out
    of core. This provides it.

---
+++ core/lib/spree/core/null_promotion_configuration.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Spree
+  module Core
+    class NullPromotionConfiguration < Spree::Preferences::Configuration
+      # promotion_adjuster_class allows extensions to provide their own Promotion Adjuster
+      class_name_attribute :promotion_adjuster_class, default: 'Spree::NullPromotionAdjuster'
+
+      # Allows providing a different coupon code handler.
+      # @!attribute [rw] coupon_code_handler_class
+      # @see Spree::NullPromotionHandler
+      # @return [Class] an object that conforms to the API of
+      #   the standard coupon code handler class
+      #   Spree::NullPromotionHandler.
+      class_name_attribute :coupon_code_handler_class, default: 'Spree::NullPromotionHandler'
+    end
+  end
+end
