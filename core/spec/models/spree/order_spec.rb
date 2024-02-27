3e557003b18b85234bc5290354abe3042a538059
Martin Meyerhoff <mamhoff@gmail.com>
2024-02-27 10:52:47 +0100

    Move promotion-related behaviour on empty! to gem
    This should probably in the future be implemented in an Omnes hook.

--- core/spec/models/spree/order_spec.rb
+++ core/spec/models/spree/order_spec.rb
@@ -6,14 +6,6 @@ RSpec.describe Spree::Order, type: :model do
   let(:store) { create(:store) }
   let(:user) { create(:user, email: "solidus@example.com") }
   let(:order) { create(:order, user: user, store: store) }
-  let(:promotion) do
-    FactoryBot.create(
-      :promotion,
-      :with_order_adjustment,
-      code: "discount"
-    )
-  end
-  let(:code) { promotion.codes.first }
 
   context '#store' do
     it { is_expected.to respond_to(:store) }
@@ -249,15 +241,13 @@ RSpec.describe Spree::Order, type: :model do
     before do
       create(:line_item, order: order)
       create(:shipment, order: order)
-      create(:adjustment, adjustable: order, order: order)
-      promotion.activate(order: order, promotion_code: code)
+      create(:adjustment, source: nil, adjustable: order, order: order)
       order.recalculate
 
       # Make sure we are asserting changes
       expect(order.line_items).not_to be_empty
       expect(order.shipments).not_to be_empty
       expect(order.adjustments).not_to be_empty
-      expect(order.promotions).not_to be_empty
       expect(order.item_total).not_to eq 0
       expect(order.item_count).not_to eq 0
       expect(order.shipment_total).not_to eq 0
@@ -269,7 +259,6 @@ RSpec.describe Spree::Order, type: :model do
       expect(order.line_items).to be_empty
       expect(order.shipments).to be_empty
       expect(order.adjustments).to be_empty
-      expect(order.promotions).to be_empty
       expect(order.item_total).to eq 0
       expect(order.item_count).to eq 0
       expect(order.shipment_total).to eq 0
--- legacy_promotions/spec/models/spree/order_spec.rb
+++ legacy_promotions/spec/models/spree/order_spec.rb
@@ -16,4 +16,46 @@ RSpec.describe Spree::Order do
       order.apply_shipping_promotions
     end
   end
+
+  context "empty!" do
+    let!(:order) { create(:order) }
+    let(:promotion) do
+      FactoryBot.create(
+        :promotion,
+        :with_order_adjustment,
+        code: "discount"
+      )
+    end
+    let(:code) { promotion.codes.first }
+
+    before do
+      create(:line_item, order: order)
+      create(:shipment, order: order)
+      create(:adjustment, adjustable: order, order: order)
+      promotion.activate(order: order, promotion_code: code)
+      order.recalculate
+
+      # Make sure we are asserting changes
+      expect(order.line_items).not_to be_empty
+      expect(order.shipments).not_to be_empty
+      expect(order.adjustments).not_to be_empty
+      expect(order.promotions).not_to be_empty
+      expect(order.item_total).not_to eq 0
+      expect(order.item_count).not_to eq 0
+      expect(order.shipment_total).not_to eq 0
+      expect(order.adjustment_total).not_to eq 0
+    end
+
+    it "clears out line items, adjustments and update totals" do
+      order.empty!
+      expect(order.line_items).to be_empty
+      expect(order.shipments).to be_empty
+      expect(order.adjustments).to be_empty
+      expect(order.promotions).to be_empty
+      expect(order.item_total).to eq 0
+      expect(order.item_count).to eq 0
+      expect(order.shipment_total).to eq 0
+      expect(order.adjustment_total).to eq 0
+    end
+  end
 end
