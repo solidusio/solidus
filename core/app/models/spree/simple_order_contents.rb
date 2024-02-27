1376fe3fee5012d4971c5f13a1b1120fbfbe235b
Martin Meyerhoff <mamhoff@gmail.com>
2024-02-27 13:33:27 +0100

    Rubocop fixes

--- core/app/models/spree/simple_order_contents.rb
+++ core/app/models/spree/simple_order_contents.rb
@@ -110,7 +110,7 @@ module Spree
     def grab_line_item_by_variant(variant, raise_error = false, options = {})
       line_item = order.find_line_item_by_variant(variant, options)
 
-      if !line_item.present? && raise_error
+      if line_item.blank? && raise_error
         raise ActiveRecord::RecordNotFound, "Line item not found for variant #{variant.sku}"
       end
 
