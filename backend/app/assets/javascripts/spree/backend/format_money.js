e81048f5c34c4cfeceb85908a3ca2d3076ba6393
Martin Meyerhoff <mamhoff@gmail.com>
2024-03-06 22:56:11 +0100

    Fix JS locale data for release of Money 6.19
    The money gem in its latest iteration added a space between value and
    unit of the Swiss Franc. Our heuristic in `_js_locale_data.html.erb`
    can't deal with that, so we add a third branch to it: If the money gem
    has a format wish, we'll respect it, but replace its abbreviations with
    the one the `accounting.js` library prefers.
    
    (cherry picked from commit f19e6fcab59db4887ba46c36ee6e2616c06d3a46)

--- backend/app/assets/javascripts/spree/backend/format_money.js
+++ backend/app/assets/javascripts/spree/backend/format_money.js
@@ -7,5 +7,5 @@ Spree.formatMoney = function(amount, currency) {
   var thousand = Spree.t('currency_delimiter');
   var decimal = Spree.t('currency_separator');
 
-  return accounting.formatMoney(amount, currencyInfo[0], currencyInfo[1], thousand, decimal, currencyInfo[2]);
+  return accounting.formatMoney(amount, currencyInfo[0], currencyInfo[1], thousand, decimal, currencyInfo[2]).trim();
 }
