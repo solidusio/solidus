44e01f5c0b4c25958459c64eb734ad8170477200
Martin Meyerhoff <mamhoff@gmail.com>
2024-03-08 17:10:00 +0100

    Move promotion helpers into solidus_legacy_promotions
    Note that the promotion rules helper does not live under
    app/helpers/spree/admin, but the promotion helper does. Keeping it just
    the same.

---
+++ legacy_promotions/spec/helpers/promotion_rules_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Spree::PromotionRulesHelper, type: :helper do
+  it "does not include existing rules in options" do
+    promotion = Spree::Promotion.new
+    promotion.promotion_rules << Spree::Promotion::Rules::ItemTotal.new
+
+    options = helper.options_for_promotion_rule_types(promotion)
+    expect(options).not_to match(/ItemTotal/)
+  end
+end
