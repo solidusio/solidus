0ad5cb7c9d0f82c00c971a9ba11d9e17120df36e
Loic <lginoux@gmail.com>
2024-04-10 12:57:02 +0200

    Create a shipping category via new admin UI

--- admin/app/controllers/solidus_admin/shipping_categories_controller.rb
+++ admin/app/controllers/solidus_admin/shipping_categories_controller.rb
@@ -4,13 +4,47 @@ module SolidusAdmin
   class ShippingCategoriesController < SolidusAdmin::BaseController
     include SolidusAdmin::ControllerHelpers::Search
 
-    def index
-      shipping_categories = apply_search_to(
-        Spree::ShippingCategory.order(id: :desc),
-        param: :q,
-      )
+    def new
+      @shipping_category = Spree::ShippingCategory.new
 
-      set_page_and_extract_portion_from(shipping_categories)
+      set_index_page
+
+      respond_to do |format|
+        format.html { render component('shipping_categories/new').new(page: @page, shipping_category: @shipping_category) }
+      end
+    end
+
+    def create
+      @shipping_category = Spree::ShippingCategory.new(shipping_category_params)
+
+      if @shipping_category.save
+        respond_to do |format|
+          flash[:notice] = t('.success')
+
+          format.html do
+            redirect_to solidus_admin.shipping_categories_path, status: :see_other
+          end
+
+          format.turbo_stream do
+            # we need to explicitly write the refresh tag for now.
+            # See https://github.com/hotwired/turbo-rails/issues/579
+            render turbo_stream: '<turbo-stream action="refresh" />'
+          end
+        end
+      else
+        set_index_page
+
+        respond_to do |format|
+          format.html do
+            page_component = component('shipping_categories/new').new(page: @page, shipping_category: @shipping_category)
+            render page_component, status: :unprocessable_entity
+          end
+        end
+      end
+    end
+
+    def index
+      set_index_page
 
       respond_to do |format|
         format.html { render component('shipping_categories/index').new(page: @page) }
@@ -34,7 +68,16 @@ module SolidusAdmin
     end
 
     def shipping_category_params
-      params.require(:shipping_category).permit(:shipping_category_id, permitted_shipping_category_attributes)
+      params.require(:shipping_category).permit(:name)
+    end
+
+    def set_index_page
+      shipping_categories = apply_search_to(
+        Spree::ShippingCategory.order(id: :desc),
+        param: :q,
+      )
+
+      set_page_and_extract_portion_from(shipping_categories)
     end
   end
 end
