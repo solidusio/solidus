9f99b212faa3f4461e50df59635fb5959e35ba7f
Martin Meyerhoff <mamhoff@gmail.com>
2024-05-10 12:35:27 +0200

    Convert product picker to Web Component
    Much easier than having to initialize manually.

--- friendly_promotions/app/assets/config/solidus_friendly_promotions/manifest.js
+++ friendly_promotions/app/assets/config/solidus_friendly_promotions/manifest.js
@@ -5,3 +5,4 @@
 //= link solidus_friendly_promotions/controllers/flash_controller.js
 //= link solidus_friendly_promotions/controllers/product_option_values_controller.js
 //= link solidus_friendly_promotions/jquery/option_value_picker.js
+//= link solidus_friendly_promotions/web_components/product_picker.js
--- friendly_promotions/config/importmap.rb
+++ friendly_promotions/config/importmap.rb
@@ -8,5 +8,7 @@ pin_all_from SolidusFriendlyPromotions::Engine.root.join("app/javascript/solidus
   under: "solidus_friendly_promotions/controllers"
 pin_all_from SolidusFriendlyPromotions::Engine.root.join("app/javascript/solidus_friendly_promotions/jquery"),
   under: "solidus_friendly_promotions/jquery"
+pin_all_from SolidusFriendlyPromotions::Engine.root.join("app/javascript/solidus_friendly_promotions/web_components"),
+  under: "solidus_friendly_promotions/web_components"
 
 pin "solidus_friendly_promotions", to: "solidus_friendly_promotions.js", preload: true
