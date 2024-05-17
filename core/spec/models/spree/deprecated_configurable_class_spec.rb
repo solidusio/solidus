eccba68f60b589b08053d40d8de41d3b41f3457d
Martin Meyerhoff <mamhoff@gmail.com>
2024-05-17 14:07:28 +0200

    Add a deprecated configurable class
    This class will act as a stand-in for those classes that are currently
    configurable via Solidus' `AppConfiguration` class, but are only used
    from *within* the legacy promotion system. We have delegated these
    extension points to `Spree::Config.promotions`, so the
    `Spree::Core::NullPromotionConfiguration` needs to implement methods for
    them, but I don't see a lot of sense in providing Null implementations
    for things that aren't even needed.

---
+++ core/spec/models/spree/deprecated_configurable_class_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Spree::DeprecatedConfigurableClass do
+  let(:deprecator) { Spree.deprecator }
+
+  before do
+    allow(deprecator).to receive(:warn)
+  end
+
+  it "warns when a method is called" do
+    described_class.new.some_method
+
+    expect(deprecator).to have_received(:warn).with(/It appears you are using Solidus' Legacy promotion system/).at_least(:once)
+  end
+
+  it "can be instantiated with any arguments" do
+    described_class.new(:foo, :bar).some_method
+
+    expect(deprecator).to have_received(:warn).with(/It appears you are using Solidus' Legacy promotion system/).at_least(:once)
+  end
+
+  it "can take method chains" do
+    described_class.new.foo.bar.baz
+
+    expect(deprecator).to have_received(:warn).with(/It appears you are using Solidus' Legacy promotion system/).at_least(:once)
+  end
+end
