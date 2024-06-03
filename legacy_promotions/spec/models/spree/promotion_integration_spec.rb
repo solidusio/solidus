f749b7876610f2f9739aedc7292ecb851da1e0d7
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-03 12:29:45 +0200

    Move integration specs from distributed amount unit spec
    These specs test the entire promotion system, not just this unit.

--- legacy_promotions/spec/models/spree/promotion_integration_spec.rb
+++ legacy_promotions/spec/models/spree/promotion_integration_spec.rb
@@ -82,4 +82,49 @@ RSpec.describe "Legacy promotion system" do
       end
     end
   end
+
+  describe "distributing amount across line items" do
+    let(:calculator) { Spree::Calculator::DistributedAmount.new }
+    let(:promotion) {
+      create :promotion,
+        name: '15 spread'
+    }
+    let(:order) {
+      create :completed_order_with_promotion,
+        promotion: promotion,
+        line_items_attributes: [{ price: 20 }, { price: 30 }, { price: 100 }]
+    }
+
+    before do
+      calculator.preferred_amount = 15
+      Spree::Promotion::Actions::CreateItemAdjustments.create!(calculator: calculator, promotion: promotion)
+      order.recalculate
+    end
+
+    it 'correctly distributes the entire discount' do
+      expect(order.promo_total).to eq(-15)
+      expect(order.line_items.map(&:adjustment_total)).to eq([-2, -3, -10])
+    end
+
+    context 'with product promotion rule' do
+      let(:first_product) { order.line_items.first.product }
+
+      before do
+        rule = Spree::Promotion::Rules::Product.create!(
+          promotion: promotion,
+          product_promotion_rules: [
+            Spree::ProductPromotionRule.new(product: first_product),
+          ],
+        )
+        promotion.rules << rule
+        promotion.save!
+        order.recalculate
+      end
+
+      it 'still distributes the entire discount' do
+        expect(order.promo_total).to eq(-15)
+        expect(order.line_items.map(&:adjustment_total)).to eq([-15, 0, 0])
+      end
+    end
+  end
 end
