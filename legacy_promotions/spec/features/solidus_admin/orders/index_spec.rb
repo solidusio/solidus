79c11951f8a60058f7799c0a11d4a89943d19961
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-06 16:10:49 +0200

    Create custom orders index component for solidus_legacy_promotions
    The new admin allows orders to be filtered by promotion name. However,
    with the legacy promotion system extracted into an extension, we can't
    rely on `Spree::Promotion` to exist in `solidus_admin`. In order to get
    around this issue, we create a new index component for the admin orders
    page that includes the filter, and register it in an initializer.
    
    This also adds a spec for filtering by promotion name.

---
+++ legacy_promotions/spec/features/solidus_admin/orders/index_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Orders", type: :feature, solidus_admin: true do
+  let(:promotion) { create(:promotion, name: "10OFF") }
+  let!(:order_with_promotion) { create(:completed_order_with_promotion, number: "R123456789", promotion: promotion) }
+  let!(:order_without_promotion) { create(:completed_order_with_totals, number: "R987654321") }
+
+  before { sign_in create(:admin_user, email: "admin@example.com") }
+
+  it "lists products", :js do
+    visit "/admin/orders"
+    find("button[aria-label=Filter]").click
+    within("div[role=search]") do
+      expect(page).to have_content("Promotions")
+      find(:xpath, "//summary[normalize-space(text())='Promotions']").click
+    end
+    check "10OFF"
+    expect(page).to have_content("R123456789")
+    expect(page).not_to have_content("R987654321")
+    expect(page).to be_axe_clean
+  end
+end
