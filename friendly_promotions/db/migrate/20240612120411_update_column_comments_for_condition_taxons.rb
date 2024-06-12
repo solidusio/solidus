e4b185edbfd8981bbcb46d59524949a2e0a4e652
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-12 14:10:24 +0200

    Update table column for condition taxons

---
+++ friendly_promotions/db/migrate/20240612120411_update_column_comments_for_condition_taxons.rb
@@ -0,0 +1,24 @@
+class UpdateColumnCommentsForConditionTaxons < ActiveRecord::Migration[7.1]
+  def up
+    if connection.supports_comments?
+      change_table_comment(:friendly_condition_taxons, friendly_condition_taxons_table_comment)
+      change_column_comment(:friendly_condition_taxons, :condition_id, condition_id_comment)
+    end
+  end
+
+  private
+
+  def friendly_condition_taxons_table_comment
+    <<~COMMENT
+      Join table between conditions and taxons. Only used if the promotion condition's type is SolidusFriendlyPromotion::Conditions::Taxon or
+      SolidusFriendlyPromotion::Conditions::LineItemTaxon. Represents those taxons that the promotion condition matches on using its any/all/none
+      or include/exclude match policy.
+    COMMENT
+  end
+
+  def condition_id_comment
+    <<~COMMENT
+      Foreign key to the friendly_conditions table.
+    COMMENT
+  end
+end
