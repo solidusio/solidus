f790e7379b092a5d5191d18e85b40e065ad66ad9
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-12 13:50:45 +0200

    Update table comments for friendly_conditions

---
+++ friendly_promotions/db/migrate/20240612114132_update_column_comments_for_conditions.rb
@@ -0,0 +1,30 @@
+class UpdateColumnCommentsForConditions < ActiveRecord::Migration[7.1]
+  def up
+    if connection.supports_comments?
+      change_table_comment(:friendly_conditions, friendly_conditions_table_comment)
+      change_column_comment(:friendly_conditions, :benefit_id, benefit_id_comment)
+      change_column_comment(:friendly_conditions, :preferences, preferences_comment)
+    end
+  end
+
+  private
+
+  def friendly_conditions_table_comment
+    <<~COMMENT
+      Represents a promotion benefit condition. A promotion benefit may have many conditions, which determine whether the benefit is eligible.
+      All conditions must be eligible for the benefit to be eligible. If there are no conditions, the benefit is always eligible.
+    COMMENT
+  end
+
+  def benefit_id_comment
+    <<~COMMENT
+      Foreign key to the friendly_benefits table.
+    COMMENT
+  end
+
+  def preferences_comment
+    <<~COMMENT
+      Preferences for this promotion condition. Serialized YAML column with preferences for this promotion condition.
+    COMMENT
+  end
+end
