46888aa3a64548a6747089ba110ce910651b1fc0
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-12 09:09:50 +0200

    Add order filtering spec
    I'm getting a code coverage issue because the `ensure_js_is_ready`
    helper is only used in the `solidus_leagcy_promotions`. Adding a spec
    here for filtering by payment state.

--- admin/spec/features/orders/index_spec.rb
+++ admin/spec/features/orders/index_spec.rb
@@ -15,4 +15,26 @@ describe "Orders", type: :feature do
     expect(page).to have_content("$19.99")
     expect(page).to be_axe_clean
   end
+
+  it "Filters product list", :js do
+    create(:completed_order_with_pending_payment, number: "R123456789", total: 19.99)
+    create(:order_ready_to_ship, number: "R987654321", total: 29.99)
+
+    visit "/admin/orders"
+
+    ensure_js_is_ready
+
+    click_button "Filter"
+
+    within("div[role=search]") do
+      expect(page).to have_content("Payment State")
+      find(:xpath, "//summary[normalize-space(text())='Payment State']").click
+    end
+    check "Balance Due"
+    expect(page).to have_content("R123456789")
+    expect(page).not_to have_content("R987654321")
+
+    # TODO: Fix colors of the orange pills. They don't have enough contrast.
+    # expect(page).to be_axe_clean
+  end
 end
