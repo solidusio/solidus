d92e4cdd517b501a502a5d685129f130dbc3871f
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-12 14:03:29 +0200

    Update column comments for friendly_condition_products

---
+++ friendly_promotions/db/migrate/20240612115749_update_column_comments_for_condition_products.rb
@@ -0,0 +1,22 @@
+class UpdateColumnCommentsForConditionProducts < ActiveRecord::Migration[7.1]
+  def up
+    if connection.supports_comments?
+      change_table_comment(:friendly_condition_products, friendly_condition_products_table_comment)
+      change_column_comment(:friendly_condition_products, :condition_id, condition_id_comment)
+    end
+  end
+
+  private
+
+  def friendly_condition_products_table_comment
+    <<~COMMENT
+      Join table between promotion conditions and products.
+    COMMENT
+  end
+
+  def condition_id_comment
+    <<~COMMENT
+      Foreign key to the friendly_conditions table.
+    COMMENT
+  end
+end
