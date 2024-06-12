aff229d1ef5c6728d801efabb7dce20c9d34bc88
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-12 14:14:07 +0200

    Update column comments for condition users

---
+++ friendly_promotions/db/migrate/20240612121049_update_column_comments_for_condition_users.rb
@@ -0,0 +1,23 @@
+class UpdateColumnCommentsForConditionUsers < ActiveRecord::Migration[7.1]
+  def up
+    if connection.supports_comments?
+      change_table_comment(:friendly_condition_users, friendly_condition_users_table_comment)
+      change_column_comment(:friendly_condition_users, :condition_id, condition_id_comment)
+    end
+  end
+
+  private
+
+  def friendly_condition_users_table_comment
+    <<~COMMENT
+      Join table between promotion conditions and users. Used with promotion conditions of type "SolidusFriendlyPromotions::Conditions::User".
+      An entry here indicates that a promotion is eligible for the user ID specified here.
+    COMMENT
+  end
+
+  def condition_id_comment
+    <<~COMMENT
+      Foreign key to the friendly_conditions table.
+    COMMENT
+  end
+end
