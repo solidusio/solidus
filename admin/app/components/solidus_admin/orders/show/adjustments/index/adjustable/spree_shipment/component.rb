7f201b6c28d95f01bc4da99aa034c016afea80f3
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-14 09:26:58 +0200

    Use dynamic dispatch for adjustables as well
    This allows us to remove a bunch of untested (and untestable) code.
    
    Especially the `else` block in `figcaption_for_adjustable` is impossible
    to reach, as only orders, line items and shipments are adjustable. If
    people have their own adjustable objects, they can now just add a
    component.

---
+++ admin/app/components/solidus_admin/orders/show/adjustments/index/adjustable/spree_shipment/component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class SolidusAdmin::Orders::Show::Adjustments::Index::Adjustable::SpreeShipment::Component < SolidusAdmin::Orders::Show::Adjustments::Index::Adjustable::Component
+  def caption
+    "#{t('spree.shipment')} ##{adjustable.number}"
+  end
+
+  def detail
+    link_to(
+      adjustable.shipping_method.name,
+      spree.edit_admin_shipping_method_path(adjustable.shipping_method),
+      class: "body-link"
+    )
+  end
+end
--- admin/app/components/solidus_admin/orders/show/adjustments/index/component.rb
+++ admin/app/components/solidus_admin/orders/show/adjustments/index/component.rb
@@ -73,10 +73,9 @@ class SolidusAdmin::Orders::Show::Adjustments::Index::Component < SolidusAdmin::
         header: :adjustable,
         col: { class: 'w-56' },
         data: ->(adjustment) {
-          tag.figure(safe_join([
-            render(component("ui/thumbnail").for(adjustment.adjustable, class: "basis-10")),
-            figcaption_for_adjustable(adjustment),
-          ]), class: "flex items-center gap-2")
+          component_name = adjustment.adjustable&.class&.table_name&.singularize
+          component_key = ["orders/show/adjustments/index/adjustable", component_name].compact.join("/")
+          render component(component_key).new(adjustment)
         }
       },
       {
@@ -140,58 +139,5 @@ class SolidusAdmin::Orders::Show::Adjustments::Index::Component < SolidusAdmin::
         end
       },
     ]
-  end
-
-  def icon_thumbnail(name)
-    render component("ui/thumbnail").new(src: svg_data_uri(icon_tag(name)))
-  end
-
-  def svg_data_uri(data)
-    "data:image/svg+xml;base64,#{Base64.strict_encode64(data)}"
-  end
-
-  def figcaption_for_adjustable(adjustment)
-    # ["Spree::LineItem", "Spree::Order", "Spree::Shipment"]
-    record = adjustment.adjustable
-    record_class = adjustment.adjustable_type&.constantize
-
-    case record || record_class
-    when Spree::LineItem
-      variant = record.variant
-      options_text = variant.options_text.presence
-
-      description = options_text || variant.sku
-      detail = link_to(variant.product.name, solidus_admin.product_path(record.variant.product), class: "body-link")
-    when Spree::Order
-      description = "#{Spree::Order.model_name.human} ##{record.number}"
-      detail = record.display_total
-    when Spree::Shipment
-       description = "#{t('spree.shipment')} ##{record.number}"
-       detail = link_to(record.shipping_method.name, spree.edit_admin_shipping_method_path(record.shipping_method), class: "body-link")
-    when nil
-      # noop
-    else
-      name_method = [:display_name, :name, :number].find { record.respond_to? _1 } if record
-      price_method = [:display_amount, :display_total, :display_cost].find { record.respond_to? _1 } if record
-
-      description = record_class.model_name.human
-      description = "#{description} - #{record.public_send(name_method)}" if name_method
-
-      # attempt creating a link
-      url_options = [:admin, record, :edit, { only_path: true }]
-      url = begin; spree.url_for(url_options); rescue NoMethodError => e; logger.error(e.to_s); nil end
-
-      description = link_to(description, url, class: "body-link") if url
-      detail = record.public_send(price_method) if price_method
-    end
-
-    thumbnail_caption(description, detail)
-  end
-
-  def thumbnail_caption(first_line, second_line)
-    tag.figcaption(safe_join([
-      tag.div(first_line || NBSP, class: 'text-black body-small whitespace-nowrap text-ellipsis overflow-hidden'),
-      tag.div(second_line || NBSP, class: 'text-gray-500 body-small whitespace-nowrap text-ellipsis overflow-hidden')
-    ]), class: "flex flex-col gap-0 max-w-[15rem]")
   end
 end
