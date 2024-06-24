e699705a3e1dafa33bcf8fe6cfdbdf96ae38865b
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-24 14:47:10 +0200

    Fix order recalculator spec
    In https://github.com/friendlycart/solidus_friendly_promotions/pull/130,
    we changed the default of
    SolidusFriendlyPromotions.config.sync_order_promotions, but failed to
    adjust this spec accordingly.

--- friendly_promotions/spec/models/spree/order_recalculator_spec.rb
+++ friendly_promotions/spec/models/spree/order_recalculator_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Spree::Config.order_recalculator_class do
   subject { described_class.new(order).recalculate }
 
   it "calls the order promotion syncer" do
-    expect_any_instance_of(SolidusFriendlyPromotions::MigrationSupport::OrderPromotionSyncer).to receive(:call)
+    expect(SolidusFriendlyPromotions::MigrationSupport::OrderPromotionSyncer).not_to receive(:new)
     subject
   end
 
   context "if config option is set to false" do
     around do |example|
-      SolidusFriendlyPromotions.config.sync_order_promotions = false
-      example.run
       SolidusFriendlyPromotions.config.sync_order_promotions = true
+      example.run
+      SolidusFriendlyPromotions.config.sync_order_promotions = false
     end
 
     it "does not call the order promotion syncer" do
-      expect(SolidusFriendlyPromotions::MigrationSupport::OrderPromotionSyncer).not_to receive(:new)
+      expect_any_instance_of(SolidusFriendlyPromotions::MigrationSupport::OrderPromotionSyncer).to receive(:call)
       subject
     end
   end
