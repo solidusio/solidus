42d8bd67888ae7f0d133db069553d62c103c1639
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-27 12:26:54 +0200

    Product/LineItemProduct: Test product_ids_string getter and setter

--- promotions/spec/models/solidus_promotions/conditions/product_spec.rb
+++ promotions/spec/models/solidus_promotions/conditions/product_spec.rb
@@ -211,4 +211,20 @@ RSpec.describe SolidusPromotions::Conditions::Product, type: :model do
       it { is_expected.to be_falsey }
     end
   end
+
+  describe "#product_ids_string" do
+    it "returns a string of product ids" do
+      condition.products = [create(:product), create(:product)]
+      expect(condition.product_ids_string).to eq("#{condition.products[0].id},#{condition.products[1].id}")
+    end
+  end
+
+  describe "#product_ids_string=" do
+    it "sets products based on a string of product ids" do
+      product_one = create(:product)
+      product_two = create(:product)
+      condition.product_ids_string = "#{product_one.id},#{product_two.id}"
+      expect(condition.products).to eq([product_one, product_two])
+    end
+  end
 end
