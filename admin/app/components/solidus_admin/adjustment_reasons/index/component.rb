b5b04938741e0c64e6296cdf1cffdceb0e2d6f1a
Madeline Collier <madeline@super.gd>
2024-08-07 16:03:35 +0200

    Create adjustment reasons via new admin UI modal

--- admin/app/components/solidus_admin/adjustment_reasons/index/component.rb
+++ admin/app/components/solidus_admin/adjustment_reasons/index/component.rb
@@ -13,6 +13,20 @@ class SolidusAdmin::AdjustmentReasons::Index::Component < SolidusAdmin::RefundsA
     :name_or_code_cont
   end
 
+  def page_actions
+    render component("ui/button").new(
+      tag: :a,
+      text: t('.add'),
+      href: solidus_admin.new_adjustment_reason_path, data: { turbo_frame: :new_adjustment_reason_modal },
+      icon: "add-line",
+      class: "align-self-end w-full",
+    )
+  end
+
+  def turbo_frames
+    %w[new_adjustment_reason_modal]
+  end
+
   def row_url(adjustment_reason)
     spree.edit_admin_adjustment_reason_path(adjustment_reason)
   end
--- admin/spec/features/adjustment_reasons_spec.rb
+++ admin/spec/features/adjustment_reasons_spec.rb
@@ -19,4 +19,44 @@ describe "Adjustment Reasons", :js, type: :feature do
     expect(Spree::AdjustmentReason.count).to eq(0)
     expect(page).to be_axe_clean
   end
+
+  context "when creating a new adjustment reason" do
+    let(:query) { "?page=1&q%5Bname_or_code_cont%5D=new" }
+
+    before do
+      visit "/admin/adjustment_reasons#{query}"
+      click_on "Add new"
+      expect(page).to have_content("New Adjustment Reason")
+      expect(page).to be_axe_clean
+    end
+
+    it "opens a modal" do
+      expect(page).to have_selector("dialog")
+      within("dialog") { click_on "Cancel" }
+      expect(page).not_to have_selector("dialog")
+      expect(page.current_url).to include(query)
+    end
+
+    context "with valid data" do
+      it "successfully creates a new adjustment reason, keeping page and q params" do
+        fill_in "Name", with: "New Reason"
+        fill_in "Code", with: "1234"
+
+        click_on "Add Adjustment Reason"
+
+        expect(page).to have_content("Adjustment reason was successfully created.")
+        expect(Spree::AdjustmentReason.find_by(name: "New Reason")).to be_present
+        expect(page.current_url).to include(query)
+      end
+    end
+
+    context "with invalid data" do
+      it "fails to create a new adjustment reason, keeping page and q params" do
+        click_on "Add Adjustment Reason"
+
+        expect(page).to have_content("can't be blank").twice
+        expect(page.current_url).to include(query)
+      end
+    end
+  end
 end
