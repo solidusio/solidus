f2bfd0653b8a66894fc59296ff3532655e9e2313
Martin Meyerhoff <mamhoff@gmail.com>
2024-08-09 08:37:10 +0200

    Namespace JS under `backend`
    This is to indicate clearly that this JS is used for the backend UI.

--- promotions/app/javascript/backend/solidus_promotions/controllers/index.js
+++ promotions/app/javascript/backend/solidus_promotions/controllers/index.js
@@ -1,8 +1,8 @@
-import { application } from "solidus_promotions/controllers/application";
+import { application } from "backend/solidus_promotions/controllers/application";
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom(
-  "solidus_promotions/controllers",
+  "backend/solidus_promotions/controllers",
   application
 );
--- promotions/config/importmap.rb
+++ promotions/config/importmap.rb
@@ -4,11 +4,11 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from SolidusPromotions::Engine.root.join("app/javascript/solidus_promotions/controllers"),
-  under: "solidus_promotions/controllers"
-pin_all_from SolidusPromotions::Engine.root.join("app/javascript/solidus_promotions/jquery"),
-  under: "solidus_promotions/jquery"
-pin_all_from SolidusPromotions::Engine.root.join("app/javascript/solidus_promotions/web_components"),
-  under: "solidus_promotions/web_components"
+pin_all_from SolidusPromotions::Engine.root.join("app/javascript/backend/solidus_promotions/controllers"),
+  under: "backend/solidus_promotions/controllers"
+pin_all_from SolidusPromotions::Engine.root.join("app/javascript/backend/solidus_promotions/jquery"),
+  under: "backend/solidus_promotions/jquery"
+pin_all_from SolidusPromotions::Engine.root.join("app/javascript/backend/solidus_promotions/web_components"),
+  under: "backend/solidus_promotions/web_components"
 
-pin "solidus_promotions", to: "solidus_promotions.js", preload: true
+pin "backend/solidus_promotions", to: "backend/solidus_promotions.js", preload: true
