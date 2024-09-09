3bb092ec02b5d6f8de728c34a4c09d193fd21786
Madeline Collier <madeline@super.gd>
2024-09-09 16:59:33 +0200

    Display last_sign_in_at instead of created_at
    The new users admin interface now shows the last sign in date instead of
    the date the user was created. It will range between "Never", "Less than
    a minute ago", "3 days ago", "2 months ago" etc. It uses the built-in
    logic provided by `ActionView::Helpers::DateHelper#time_ago_in_words`.
    
    The specs are a bit tricky and stubby due to the need to keep them
    general and agnostic of whatever is being used for the
    `Spree.user_class`.

--- admin/app/components/solidus_admin/users/index/component.rb
+++ admin/app/components/solidus_admin/users/index/component.rb
@@ -88,9 +88,24 @@ class SolidusAdmin::Users::Index::Component < SolidusAdmin::UsersAndRoles::Compo
         data: -> { _1.display_lifetime_value.to_html },
       },
       {
-        header: :created_at,
-        data: ->(user) { l(user.created_at.to_date, format: :long) },
+        header: :last_active,
+        data: ->(user) { last_login(user) },
       },
     ]
+  end
+
+  private
+
+  # @todo add logic to display "Invitation sent" when the user has not yet
+  #   accepted the invitation and filled out account details. To be implemented
+  #   in conjunction with the invitation logic.
+  def last_login(user)
+    return t('.last_login.never') if user.try(:last_sign_in_at).blank?
+
+    t(
+      '.last_login.login_time_ago',
+      # @note The second `.try` is only here for the specs to work.
+      last_login_time: time_ago_in_words(user.try(:last_sign_in_at))
+    ).capitalize
   end
 end
--- admin/spec/features/users_spec.rb
+++ admin/spec/features/users_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 describe "Users", :js, type: :feature do
-  before { sign_in create(:admin_user, email: 'admin@example.com') }
+  before { sign_in create(:admin_user, email: "admin@example.com") }
 
   it "lists users and allows deleting them" do
     create(:user, email: "customer@example.com")
@@ -11,6 +11,9 @@ describe "Users", :js, type: :feature do
     create(:user, :with_orders, email: "customer-with-order@example.com")
 
     visit "/admin/users"
+    expect(page).to have_content("Last active")
+    expect(page).to have_content("Never")
+
     expect(page).to have_content("customer@example.com")
     expect(page).to have_content("admin-2@example.com")
     expect(page).to have_content("customer-with-order@example.com")
@@ -32,5 +35,21 @@ describe "Users", :js, type: :feature do
     expect(page).to have_content("Users were successfully removed.")
     expect(page).not_to have_content("customer@example.com")
     expect(Spree.user_class.count).to eq(3)
+  end
+
+  context "when a user has recently signed in" do
+    let(:sign_in_date) { DateTime.now }
+
+    before do
+      allow_any_instance_of(Spree.user_class).to receive(:try).with(:email).and_call_original
+      allow_any_instance_of(Spree.user_class).to receive(:try).with(:last_sign_in_at).and_return(sign_in_date)
+    end
+
+    it "lists the last time they were active" do
+      visit "/admin/users"
+      expect(page).to have_content("Last active")
+      expect(page).to have_content("Less than a minute ago")
+      expect(page).not_to have_content("Never")
+    end
   end
 end
