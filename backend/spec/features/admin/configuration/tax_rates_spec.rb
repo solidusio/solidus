7f7f27557365d5ad7fc1e4741218e87c8aa84fd8
Alessandro Lepore <alessandro@juul.com>
2024-10-03 12:30:15 +0200

    Format date with #to_fs
    The #to_s(format) method was deprecated and removed from Rails 7.1.
    This is causing `ActionView::Template::Error wrong number of arguments (given 1, expected 0)`
    when a tax rate with an expiration date is displayed.

--- backend/spec/features/admin/configuration/tax_rates_spec.rb
+++ backend/spec/features/admin/configuration/tax_rates_spec.rb
@@ -25,6 +25,7 @@ describe "Tax Rates", type: :feature do
     click_link "Tax Rates"
     click_link "New Tax Rate"
     fill_in "Rate", with: "0.05"
+    fill_in "Expiration Date", with: "2050-01-01"
     click_button "Create"
     expect(page).to have_content("Tax Rate has been successfully created!")
   end
