75970042615a2f39eeefc60231c3b4412103918f
Chris Todorov <chris@super.gd>
2024-10-10 14:14:50 -0700

    Add filtering by store to orders index component
    In the legacy admin the order search supported a filter by store. This
    behaviour is not in the new admin yet, so we're introducing it with this
    change.
    
    Co-authored-by: Harmony Evangelina <harmony@super.gd>
    Co-authored-by: Tom Van Manen <tom@super.gd>

--- admin/spec/features/orders/index_spec.rb
+++ admin/spec/features/orders/index_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe "Orders", type: :feature do
   before { sign_in create(:admin_user, email: 'admin@example.com') }
 
-  it "lists products", :js do
+  it "lists orders", :js do
     create(:order, number: "R123456789", total: 19.99)
 
     visit "/admin/orders"
@@ -14,5 +14,32 @@ describe "Orders", type: :feature do
     expect(page).to have_content("R123456789")
     expect(page).to have_content("$19.99")
     expect(page).to be_axe_clean
+  end
+
+  context "with multiple stores", :js do
+    let!(:order_in_default_store) { create :order }
+    let(:another_store) { create :store, name: "Another Store" }
+    let!(:order_in_another_store) { create :order, store: another_store }
+
+    it "can filter orders by store" do
+      visit solidus_admin.orders_path
+
+      click_on "In Progress"
+
+      expect(page).to have_content(order_in_default_store.number)
+      expect(page).to have_content(order_in_another_store.number)
+
+      click_on "Filter"
+
+      within("div[role='search']") do
+        find('details', text: "Store").click
+        expect(page).to have_content("Another Store")
+
+        find('label', text: "Another Store").click
+      end
+
+      expect(page).to have_content(order_in_another_store.number)
+      expect(page).to_not have_content(order_in_default_store.number)
+    end
   end
 end
