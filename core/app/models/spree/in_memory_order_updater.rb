5aaaac4478487a0134a3ede047c21569874e873c
benjamin wil <benjamin@super.gd>
2024-10-11 13:56:49 -0700

    Add `persist` flag to `#recalculate`
    We want our new in-memory order updater to be able to persist or not
    persist changes to the order record.
    
    This is a first step in ensuring we don't need to write to the database
    using the order updater. Clearly we have more work to do to ensure this
    functions like the existing updater.
    
    Co-authored-by: Adam Mueller <adam@super.gd>
    Co-authored-by: Andrew Stewart <andrew@super.gd>
    Co-authored-by: Harmony Evangelina <harmony@super.gd>
    Co-authored-by: Jared Norman <jared@super.gd>
    Co-authored-by: Nick Van Doorn <nick@super.gd>
    Co-authored-by: Noah Silvera <noah@super.gd>
    Co-authored-by: Senem Soy <senem@super.gd>
    Co-authored-by: Sofia Besenski <sofia@super.gd>
    Co-authored-by: Tom Van Manen <tom@super.gd>

--- core/app/models/spree/in_memory_order_updater.rb
+++ core/app/models/spree/in_memory_order_updater.rb
@@ -16,7 +16,7 @@ module Spree
     # This method should never do anything to the Order that results in a save call on the
     # object with callbacks (otherwise you will end up in an infinite recursion as the
     # associations try to save and then in turn try to call +update!+ again.)
-    def recalculate
+    def recalculate(persist: true)
       order.transaction do
         recalculate_item_count
         update_shipment_amounts
@@ -28,7 +28,8 @@ module Spree
         end
 
         Spree::Bus.publish :order_recalculated, order: order
-        persist_totals
+
+        persist_totals if persist
       end
     end
     alias_method :update, :recalculate
