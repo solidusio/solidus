d46369f7d7f9246665fcc6830654c6e56d0a78fb
Madeline Collier <madeline@super.gd>
2024-10-11 17:02:58 +0200

    Handle states_required? in admin address component
    Previously, this component would just disable the state_id field when
    the selected country did not require states, but this meant that the
    user would never be able to self-fill the state_name field, which is
    something that some stores can require with the
    `Spree::Config[:address_requires_state]` preference.
    
    The component now takes this into account and will swap between a
    state_id dropdown select field, or a blank state_name text input
    depending on the country the user has selected.
    
    This brings this new admin component up to par with the previous backend
    address forms.

--- admin/app/components/solidus_admin/ui/forms/address/component.js
+++ admin/app/components/solidus_admin/ui/forms/address/component.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["country", "state"]
+  static targets = ["country", "state", "stateName", "stateWrapper", "stateNameWrapper"]
 
   loadStates() {
     const countryId = this.countryTarget.value
@@ -13,22 +13,47 @@ export default class extends Controller {
       })
   }
 
-  updateStateOptions(data) {
-    const stateSelect = this.stateTarget
+  updateStateOptions(states) {
+    if (states.length === 0) {
+      // Show state name text input if no states to choose from.
+      this.toggleStateFields(false)
+    } else {
+      // Show state select dropdown.
+      this.toggleStateFields(true)
+      this.populateStateSelect(states)
+    }
+  }
 
+  toggleStateFields(showSelect) {
+    const stateWrapper = this.stateWrapperTarget
+    const stateNameWrapper = this.stateNameWrapperTarget
+    const stateSelect = this.stateTarget
+    const stateName = this.stateNameTarget
+
+
+    if (showSelect) {
+      // Show state select dropdown.
+      stateSelect.disabled = false
+      stateName.value = ""
+      stateWrapper.classList.remove('hidden')
+      stateNameWrapper.classList.add('hidden')
+    } else {
+      // Show state name text input if no states to choose from.
+      stateSelect.disabled = true
+      stateWrapper.classList.add("hidden")
+      stateNameWrapper.classList.remove("hidden")
+    }
+  }
+
+  populateStateSelect(states) {
+    const stateSelect = this.stateTarget
     stateSelect.innerHTML = ""
 
-    if (data.length === 0) {
-      stateSelect.disabled = true
-    } else {
-      stateSelect.disabled = false
-
-      data.forEach((state) => {
-        const option = document.createElement("option")
-        option.value = state.id
-        option.innerText = state.name
-        stateSelect.appendChild(option)
-      })
-    }
+    states.forEach((state) => {
+      const option = document.createElement("option")
+      option.value = state.id
+      option.innerText = state.name
+      stateSelect.appendChild(option)
+    })
   }
 }
