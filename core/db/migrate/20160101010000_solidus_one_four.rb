d339e8413cfa76c3cbfb3fcc616571fcae0828cd
Martin Meyerhoff <mamhoff@gmail.com>
2024-11-13 17:00:28 +0100

    Move Line Item Actions to solidus_legacy_promotions
    This table belongs to the legacy promotion system and was,
    unfortunately, missed in the original extraction.

--- core/db/migrate/20160101010000_solidus_one_four.rb
+++ core/db/migrate/20160101010000_solidus_one_four.rb
@@ -197,16 +197,6 @@ class SolidusOneFour < ActiveRecord::Migration[5.0]
       t.index ["variant_id"], name: "index_inventory_units_on_variant_id"
     end
 
-    create_table "spree_line_item_actions", force: :cascade do |t|
-      t.integer "line_item_id", null: false
-      t.integer "action_id", null: false
-      t.integer "quantity", default: 0
-      t.datetime "created_at", precision: 6
-      t.datetime "updated_at", precision: 6
-      t.index ["action_id"], name: "index_spree_line_item_actions_on_action_id"
-      t.index ["line_item_id"], name: "index_spree_line_item_actions_on_line_item_id"
-    end
-
     create_table "spree_line_items", force: :cascade do |t|
       t.integer "variant_id"
       t.integer "order_id"
