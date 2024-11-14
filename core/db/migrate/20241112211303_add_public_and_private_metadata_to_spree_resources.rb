e5398a25f0bff378520567f0103c63dad7af454b
Mayur Shah <shahmayur001@gmail.com>
2024-11-15 02:53:42 +0530

    Updated migration to add more models

--- core/db/migrate/20241112211303_add_public_and_private_metadata_to_spree_resources.rb
+++ core/db/migrate/20241112211303_add_public_and_private_metadata_to_spree_resources.rb
@@ -11,6 +11,16 @@ class AddPublicAndPrivateMetadataToSpreeResources < ActiveRecord::Migration[7.0]
       spree_stock_movements
       spree_addresses
       spree_refunds
+      spree_products
+      spree_customer_returns
+      spree_stock_locations
+      spree_store_credit_events
+      spree_stores
+      spree_taxonomies
+      spree_taxons
+      spree_variants
+      spree_users
+      spree_return_authorizations
     ].each do |table_name|
       change_table table_name do |t|
         # Check if the database supports jsonb for efficient querying
