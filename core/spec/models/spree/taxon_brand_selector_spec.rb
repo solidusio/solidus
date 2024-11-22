eeb60c9959050dd05111b18f3e4c3afaeabda22f
Mayur <shahmayur001@gmail.com>
2024-11-22 16:57:34 +0530

    Added more test cases

--- core/spec/models/spree/taxon_brand_selector_spec.rb
+++ core/spec/models/spree/taxon_brand_selector_spec.rb
@@ -15,5 +15,32 @@ RSpec.describe Spree::TaxonBrandSelector, type: :model do
         expect(subject.call).to eq(taxon)
       end
     end
+
+    context "when the product has multiple taxons under the 'Brands' taxonomy" do
+      let(:taxon_b) { create(:taxon, taxonomy: taxonomy, name: "Brand B") }
+      before { product.taxons << taxon_b }
+
+      it "returns the first taxon under 'Brands'" do
+        expect(subject.call).to eq(taxon)
+      end
+    end
+
+    context "when the product does not have a taxon under the 'Brands' taxonomy" do
+      let(:other_taxonomy) { create(:taxonomy, name: "Categories") }
+      let(:other_taxon) { create(:taxon, taxonomy: other_taxonomy, name: "Category A") }
+      let(:product) { create(:product, taxons: [other_taxon]) }
+
+      it "returns nil" do
+        expect(subject.call).to be_nil
+      end
+    end
+
+    context "when the product has no taxons" do
+      let(:product) { create(:product) }
+
+      it "returns nil" do
+        expect(subject.call).to be_nil
+      end
+    end
   end
 end
