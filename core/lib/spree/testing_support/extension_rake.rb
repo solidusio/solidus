c13efb779ed002b96daf72ca7545405762885948
Martin Meyerhoff <mamhoff@gmail.com>
2024-12-01 12:56:42 +0100

    Test app task: Allow passing in user class
    Without this, every extension's test app always has `Spree::LegacyUser`
    configured. Mostly, that's fine, but for solidus_auth_devise, it'd be
    nicer if the generated spree.rb file would have `Spree.user_class =
    "Spree::User"` rather than `Spree::LegacyUser`.
    
    (cherry picked from commit 520efc0631bdf34c46f205aa63e632fabe7246db)

--- core/lib/spree/testing_support/extension_rake.rb
+++ core/lib/spree/testing_support/extension_rake.rb
@@ -4,8 +4,8 @@ require 'spree/testing_support/common_rake'
 
 desc "Generates a dummy app for testing an extension"
 namespace :extension do
-  task :test_app, [:user_class] do |_t, _args|
+  task :test_app, [:user_class] do |_t, args|
     Spree::DummyGeneratorHelper.inject_extension_requirements = true
-    Rake::Task['common:test_app'].invoke
+    Rake::Task['common:test_app'].invoke(args[:user_class])
   end
 end
