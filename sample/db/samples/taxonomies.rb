bb4338287ee7417501d962ae7611e2683b0b0b75
Mayur Shah <shahmayur001@gmail.com>
2024-12-02 23:24:18 +0530

    Fixed store validation issue

--- sample/db/samples/taxonomies.rb
+++ sample/db/samples/taxonomies.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-taxonomies = [
-  { name: "Categories" },
-  { name: "Brand" }
-]
+store = Spree::Store.where(code: 'sample-store').first
 
+taxonomies = [
+  { name: "Categories", store:  },
+  { name: "Brands", store: }
+]
 taxonomies.each do |taxonomy_attrs|
-  Spree::Taxonomy.create!(taxonomy_attrs)
+  Spree::Taxonomy.find_or_create_by!(taxonomy_attrs)
 end
