01325f4ea4273dbabf42d233db270e0fc57b3ec9
Madeline Collier <madeline@super.gd>
2024-12-05 13:27:37 +0100

    Add product properties create/edit flow to admin
    This builds off of the excellent work started in
    https://github.com/solidusio/solidus/pull/5885 and in
    https://github.com/solidusio/solidus/pull/5926 to build out the
    create/edit/update flow for product properties in the new admin.
    
    I've added a request spec that was missing and tweaked a few other
    things.

--- admin/spec/features/properties_spec.rb
+++ admin/spec/features/properties_spec.rb
@@ -21,4 +21,89 @@ describe "Properties", :js, type: :feature do
     expect(page).not_to have_content("Type prop")
     expect(Spree::Property.count).to eq(1)
   end
+
+  context "creating a new property" do
+    it "creates a new product property" do
+      visit "/admin/properties"
+      click_on "Add new"
+
+      fill_in "Name", with: "Color"
+      fill_in "Presentation", with: "Cool Color"
+      click_on "Add Property"
+
+      expect(page).to have_content("Property was successfully created.")
+      expect(page).to have_content("Color")
+      expect(page).to have_content("Cool Color")
+      expect(Spree::Property.count).to eq(1)
+    end
+
+    it "shows validation errors" do
+      visit "/admin/properties"
+      click_on "Add new"
+
+      fill_in "Name", with: ""
+      click_on "Add Property"
+
+      expect(page).to have_content("can't be blank")
+      expect(Spree::Property.count).to eq(0)
+    end
+  end
+
+  context "editing an existing property" do
+    let!(:property) { create(:property, name: "Color", presentation: "Cool Color") }
+
+    it "updates the property" do
+      visit "/admin/properties"
+      find_row("Color").click
+
+      fill_in "Name", with: "Size"
+      fill_in "Presentation", with: "Cool Size"
+      click_on "Update Property"
+
+      expect(page).to have_content("Property was successfully updated.")
+      expect(page).to have_content("Size")
+      expect(page).to have_content("Cool Size")
+      expect(Spree::Property.count).to eq(1)
+    end
+
+    it "shows validation errors" do
+      visit "/admin/properties"
+      find_row("Color").click
+
+      fill_in "Name", with: ""
+      click_on "Update Property"
+
+      expect(page).to have_content("can't be blank")
+      expect(Spree::Property.count).to eq(1)
+    end
+  end
+
+  context "editing an existing property" do
+    let!(:property) { create(:property, name: "Color", presentation: "Cool Color") }
+
+    it "updates the property" do
+      visit "/admin/properties"
+      find_row("Color").click
+
+      fill_in "Name", with: "Size"
+      fill_in "Presentation", with: "Cool Size"
+      click_on "Update Property"
+
+      expect(page).to have_content("Property was successfully updated.")
+      expect(page).to have_content("Size")
+      expect(page).to have_content("Cool Size")
+      expect(Spree::Property.count).to eq(1)
+    end
+
+    it "shows validation errors" do
+      visit "/admin/properties"
+      find_row("Color").click
+
+      fill_in "Name", with: ""
+      click_on "Update Property"
+
+      expect(page).to have_content("can't be blank")
+      expect(Spree::Property.count).to eq(1)
+    end
+  end
 end
