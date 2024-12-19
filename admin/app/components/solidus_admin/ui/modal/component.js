b83936303883bb4b66f3ee61a710a10cb64c1b9c
Thomas von Deyen <thomas@vondeyen.com>
2024-12-19 18:18:02 +0100

    feat(admin/ui/modal): Add close action
    This removes the dialog element from the dom after closing it.
    Combined with the new pages index controller we can open and close
    modals without leaving the page. Much faster and less round trips.

--- admin/app/components/solidus_admin/ui/modal/component.js
+++ admin/app/components/solidus_admin/ui/modal/component.js
@@ -3,5 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.showModal()
+    this.element.addEventListener("close", () => this.removeModal())
+  }
+
+  close() {
+    this.element.close()
+  }
+
+  removeModal() {
+    this.element.remove()
   }
 }
