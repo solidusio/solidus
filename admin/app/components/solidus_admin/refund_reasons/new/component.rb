83f637e8f754e34f61a877f11014c20f2aac237a
Thomas von Deyen <thomas@vondeyen.com>
2024-12-20 21:53:39 +0100

    Admin refund reasons: Load modal with turbo frame

--- admin/app/components/solidus_admin/refund_reasons/new/component.rb
+++ admin/app/components/solidus_admin/refund_reasons/new/component.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
-class SolidusAdmin::RefundReasons::New::Component < SolidusAdmin::BaseComponent
-  def initialize(page:, refund_reason:)
-    @page = page
-    @refund_reason = refund_reason
-  end
-
-  def form_id
-    dom_id(@refund_reason, "#{stimulus_id}_new_refund_reason_form")
-  end
+class SolidusAdmin::RefundReasons::New::Component < SolidusAdmin::Resources::New::Component
 end
