560c28fe5efd4ddade2623a81d0f249ec342f581
Martin Meyerhoff <mamhoff@gmail.com>
2024-12-21 15:56:41 +0100

    Legacy Promotions: Use Flickwerk for patches
    This should stop us from loading order, line items, adjustment,
    calculators, the order updater, product, and shipment on app startup.

--- legacy_promotions/app/patches/models/solidus_legacy_promotions/spree_order_updater_patch.rb
+++ legacy_promotions/app/patches/models/solidus_legacy_promotions/spree_order_updater_patch.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SolidusLegacyPromotions
-  module SpreeOrderUpdaterDecorator
+  module SpreeOrderUpdaterPatch
     def update_adjustment_total
       recalculate_adjustments
 
