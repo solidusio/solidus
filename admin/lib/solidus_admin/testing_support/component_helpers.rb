30dcf9ed8ec597d55d175d71196a843b7c83c5cf
Martin Meyerhoff <mamhoff@gmail.com>
2025-01-03 17:32:34 +0100

    [SolidusAdmin] Fix mock_component helper
    The mock component in the ComponentHelpers module was not a real
    constant, which messes with ViewComponent's expectations about what
    render_inline is given. Using `stub_const` in the helper allows us to
    give it an actual name, and view_component > 3.21.0 will work for us.
    
    The helper is only used in the base component spec. I could have spent
    more time giving it an optional block, but this solution is the most
    straightforward.

--- admin/lib/solidus_admin/testing_support/component_helpers.rb
+++ admin/lib/solidus_admin/testing_support/component_helpers.rb
@@ -12,15 +12,9 @@ module SolidusAdmin
       #      "Rendered"
       #    end
       #  end
-      def mock_component(&definition)
-        location = caller(1, 1).first
-        component_class = Class.new(SolidusAdmin::BaseComponent)
-        # ViewComponent will complain if we don't fake a class name:
-        # @see https://github.com/ViewComponent/view_component/blob/5decd07842c48cbad82527daefa3fe9c65a4226a/lib/view_component/base.rb#L371
-        component_class.define_singleton_method(:name) { "Foo" }
-        component_class.define_singleton_method(:to_s) { "#{name} (#{location})" }
-        component_class.class_eval(&definition) if definition
-        component_class
+      def mock_component(class_name = "Foo::Component", &definition)
+        component_class = stub_const(class_name, Class.new(described_class, &definition))
+        component_class.new
       end
     end
   end
