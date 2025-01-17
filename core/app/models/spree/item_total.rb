c261747d5ceba381607ac6419494c47bcaf8e335
Jared Norman <jared@super.gd>
2025-01-17 14:17:33 -0800

    Refactor line item total calculations
    While working on the in-memory updater in #5872, we found the need to
    change how item totals were being calculated, so that we could mark
    adjustments for destruction without actually destroying them, while
    still keeping tax adjustments intact. This change is completely
    backwards-compatible with the current OrderUpdater, so to reduce the
    scope of our PR, we wanted to make this change separately.
    
    Since the OrderUpdater is already very large, this helps reduce its
    responsibilities and makes it easier to test this behaviour. We don't
    see it as necessary to make this a configurable class, but this change
    leaves that option open in the future.
    
    Co-authored-by: Adam Mueller <adam@super.gd>
    Co-authored-by: Alistair Norman <alistair@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>
    Co-authored-by: Harmony Bouvier <harmony@super.gd>
    Co-authored-by: Sofia Besenski <sofia@super.gd>
    Co-authored-by: benjamin wil <benjamin@super.gd>

---
+++ core/app/models/spree/item_total.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Spree::ItemTotal
+  def initialize(item)
+    @item = item
+  end
+
+  def recalculate!
+    tax_adjustments = item.adjustments.select do |adjustment|
+      adjustment.tax? && !adjustment.marked_for_destruction?
+    end
+
+    # Included tax adjustments are those which are included in the price.
+    # These ones should not affect the eventual total price.
+    #
+    # Additional tax adjustments are the opposite, affecting the final total.
+    item.included_tax_total   = tax_adjustments.select(&:included?).sum(&:amount)
+    item.additional_tax_total = tax_adjustments.reject(&:included?).sum(&:amount)
+
+    item.adjustment_total = item.adjustments.reject { |adjustment|
+      adjustment.marked_for_destruction? || adjustment.included?
+    }.sum(&:amount)
+  end
+
+  private
+
+  attr_reader :item
+end
