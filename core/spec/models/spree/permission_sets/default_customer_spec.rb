0695366948ba6b9826ade58ebfd01a56911d40d2
Martin Meyerhoff <mamhoff@gmail.com>
2025-01-23 12:58:08 +0100

    Move default customer spec from spec/lib to spec/models
    We had two spec files for this functionality.

--- core/spec/models/spree/permission_sets/default_customer_spec.rb
+++ core/spec/models/spree/permission_sets/default_customer_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe Spree::PermissionSets::DefaultCustomer do
       expect(described_class.category).to eq(:default_customer)
     end
   end
+
+  context 'as Guest User' do
+    context 'for Order' do
+      context 'guest_token is empty string' do
+        let(:ability) { Spree::Ability.new(nil) }
+        let(:resource) { build(:order) }
+        let(:token) { '' }
+
+        it 'should not be allowed to read or update the order' do
+          allow(resource).to receive_messages(guest_token: '')
+
+          expect(ability).to_not be_able_to(:show, resource, token)
+          expect(ability).to_not be_able_to(:show, resource, token)
+        end
+      end
+    end
+  end
 end
