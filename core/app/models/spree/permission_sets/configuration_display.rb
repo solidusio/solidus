af449d9a815c9c19f1164dacd8908084dd1ceb4e
Martin Meyerhoff <mamhoff@gmail.com>
2025-01-23 11:35:12 +0100

    Fix Rubocop offenses

--- core/app/models/spree/permission_sets/configuration_display.rb
+++ core/app/models/spree/permission_sets/configuration_display.rb
@@ -33,20 +33,20 @@ module Spree
       end
 
       def activate!
-          can [:read, :admin], Spree::TaxCategory
-          can [:read, :admin], Spree::TaxRate
-          can [:read, :admin], Spree::Zone
-          can [:read, :admin], Spree::Country
-          can [:read, :admin], Spree::State
-          can [:read, :admin], Spree::PaymentMethod
-          can [:read, :admin], Spree::Taxonomy
-          can [:read, :admin], Spree::ShippingMethod
-          can [:read, :admin], Spree::ShippingCategory
-          can [:read, :admin], Spree::StockLocation
-          can [:read, :admin], Spree::StockMovement
-          can [:read, :admin], Spree::RefundReason
-          can [:read, :admin], Spree::ReimbursementType
-          can [:read, :admin], Spree::ReturnReason
+        can [:read, :admin], Spree::TaxCategory
+        can [:read, :admin], Spree::TaxRate
+        can [:read, :admin], Spree::Zone
+        can [:read, :admin], Spree::Country
+        can [:read, :admin], Spree::State
+        can [:read, :admin], Spree::PaymentMethod
+        can [:read, :admin], Spree::Taxonomy
+        can [:read, :admin], Spree::ShippingMethod
+        can [:read, :admin], Spree::ShippingCategory
+        can [:read, :admin], Spree::StockLocation
+        can [:read, :admin], Spree::StockMovement
+        can [:read, :admin], Spree::RefundReason
+        can [:read, :admin], Spree::ReimbursementType
+        can [:read, :admin], Spree::ReturnReason
       end
     end
   end
--- core/app/models/spree/permission_sets/user_management.rb
+++ core/app/models/spree/permission_sets/user_management.rb
@@ -25,7 +25,7 @@ module Spree
       def activate!
         can [:admin, :read, :create, :update, :save_in_address_book, :remove_from_address_book, :addresses, :orders, :items], Spree.user_class
 
-        # Note: This does not work with accessible_by.
+        # NOTE: This does not work with accessible_by.
         # See https://github.com/solidusio/solidus/pull/1263
         can :update_email, Spree.user_class do |user|
           user.spree_roles.none?
