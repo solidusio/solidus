e2b4c057ee86b033dd4fadb50a57e815bc9baba2
Eugene Chaikin <nutaksho@gmail.com>
2025-02-13 14:31:00 +0100

    Fix rubocop offence

--- admin/app/helpers/solidus_admin/void_elements_helper.rb
+++ admin/app/helpers/solidus_admin/void_elements_helper.rb
@@ -5,9 +5,9 @@ module SolidusAdmin
     # https://github.com/rails/rails/blob/194d697036c61af0caa66de5659721ded2478ce9/actionview/lib/action_view/helpers/tag_helper.rb#L84
     HTML_VOID_ELEMENTS = %i(area base br col embed hr img input keygen link meta source track wbr)
 
-    # @param el [Symbol]
-    def void_element?(el)
-      HTML_VOID_ELEMENTS.include?(el)
+    # @param element [Symbol]
+    def void_element?(element)
+      HTML_VOID_ELEMENTS.include?(element)
     end
   end
 end
