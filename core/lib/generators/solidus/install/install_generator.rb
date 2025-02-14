e760610bbab41c7ae4cc3c1d445bd2dc65557aeb
Martin Meyerhoff <mamhoff@gmail.com>
2025-02-14 14:10:36 +0100

    Revert "Merge pull request #6122 from mamhoff/create-manifest-js-in-generated-apps"
    This reverts commit bec42f64b9008dda63dc89c69de68fdc0076f293, reversing
    changes made to 37a5c8a3182e58185a2ec5691a47721c320ad764.

--- core/lib/generators/solidus/install/install_generator.rb
+++ core/lib/generators/solidus/install/install_generator.rb
@@ -121,9 +121,6 @@ module Solidus
 
     def add_files
       template 'config/initializers/spree.rb.tt', 'config/initializers/spree.rb'
-      if Rails.version >= "8.0"
-        template "app/assets/config/manifest.js", "app/assets/config/manifest.js", force: true
-      end
     end
 
     def install_file_attachment
