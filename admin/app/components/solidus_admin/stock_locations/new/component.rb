f5c5170f1ce111210de150ffc42489508ff4c0d4
Eugene Chaikin <nutaksho@gmail.com>
2025-02-25 12:57:24 +0100

    wip

--- admin/app/components/solidus_admin/stock_locations/new/component.rb
+++ admin/app/components/solidus_admin/stock_locations/new/component.rb
@@ -3,4 +3,14 @@
 class SolidusAdmin::StockLocations::New::Component < SolidusAdmin::Resources::New::Component
   include SolidusAdmin::Layout::PageHelpers
 
+  def initialize(*args)
+    super(*args)
+    ensure_country
+  end
+
+  private
+
+  def ensure_country
+    @stock_location.country.blank? && @stock_location.country = Spree::Country.default
+  end
 end
