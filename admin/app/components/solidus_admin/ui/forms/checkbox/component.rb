cca5b91941f5413a47a95d3a2a9af9d1ffa11a07
Eugene Chaikin <nutaksho@gmail.com>
2025-02-28 15:42:44 +0100

    Refactor ui/forms/checkbox component to include common form elements
    Motivation:
    We have a couple of places throughout the admin where checkboxes
    are used as part of a form, with all the same elements being reused
     to construct a checkbox field:
     - label with some caption;
     - hidden input field to support sending a "0" value in request;
     - optional toggletip hint;
    Few places however lacked the hidden input, therefore the form was
    not submitting the "0" value correctly.
    
    This updated component builds a reusable form checkbox, that
    encapsulates all the needed layout and logic for a form checkbox to
     be displayed and work properly, with an interface to customise it
     to each form needs.

--- admin/app/components/solidus_admin/ui/forms/checkbox/component.rb
+++ admin/app/components/solidus_admin/ui/forms/checkbox/component.rb
@@ -1,42 +1,35 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Forms::Checkbox::Component < SolidusAdmin::BaseComponent
-  SIZES = {
-    s: 'w-4 h-4',
-    m: 'w-5 h-5',
+  FONT_WEIGHTS = {
+    normal: 'font-normal',
+    semibold: 'font-semibold',
   }.freeze
 
-  def initialize(size: :m, **attributes)
-    @size = size
-    @attributes = attributes
+  FONT_SIZES = {
+    xs: 'text-xs',
+    s: 'text-sm',
+  }.freeze
+
+  renders_one :caption, ->(text:, weight: :normal, size: :s, **options) do
+    tag.span(
+      text,
+      class: "
+        #{FONT_WEIGHTS.fetch(weight)}
+        #{FONT_SIZES.fetch(size)}
+        #{options.delete(:classes)}
+      ",
+      **options
+    )
   end
 
-  def call
-    tag.input(
-      type: 'checkbox',
-      class: "
-        #{SIZES.fetch(@size)}
-        form-checkbox
-        cursor-pointer
-        disabled:cursor-not-allowed
+  renders_one :hint, ->(text:, position: :above) do
+    render component("ui/toggletip").new(text:, position:)
+  end
 
-        bg-white rounded border border-2 border-gray-300
-        hover:border-gray-700
-        focus:ring focus:ring-gray-300 focus:ring-0.5 focus:bg-white focus:ring-offset-0
-        active:ring active:ring-gray-300 active:ring-0.5
-        disabled:border-gray-300
-
-        indeterminate:border-gray-700 indeterminate:bg-gray-700 indeterminate:text-white
-        indeterminate:hover:border-gray-500 indeterminate:hover:bg-gray-500
-        indeterminate:focus:bg-gray-700
-        indeterminate:disabled:border-gray-300 indeterminate:disabled:bg-gray-300
-
-        checked:border-gray-700 checked:bg-gray-700 checked:text-white
-        checked:hover:border-gray-500 checked:hover:bg-gray-500
-        checked:focus:bg-gray-700
-        checked:disabled:border-gray-300 checked:disabled:bg-gray-300
-      ",
-      **@attributes,
-    )
+  def initialize(object_name:, method:, checked:, **attributes)
+    @name = "#{object_name}[#{method}]"
+    @checked = !!checked
+    @attributes = attributes
   end
 end
