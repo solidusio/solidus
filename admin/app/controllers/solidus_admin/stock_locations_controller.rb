fabd710a77deb949e111642299012899011583d2
Eugene Chaikin <nutaksho@gmail.com>
2025-02-28 12:26:24 +0100

    Update stock locations controller
    As a common resource controller make it inherit from
    ResourcesController.

--- admin/app/controllers/solidus_admin/stock_locations_controller.rb
+++ admin/app/controllers/solidus_admin/stock_locations_controller.rb
@@ -1,35 +1,11 @@
 # frozen_string_literal: true
 
 module SolidusAdmin
-  class StockLocationsController < SolidusAdmin::BaseController
-    include SolidusAdmin::ControllerHelpers::Search
-
-    def index
-      stock_locations = apply_search_to(
-        Spree::StockLocation.order(id: :desc),
-        param: :q,
-      )
-
-      set_page_and_extract_portion_from(stock_locations)
-
-      respond_to do |format|
-        format.html { render component('stock_locations/index').new(page: @page) }
-      end
-    end
-
-    def destroy
-      @stock_locations = Spree::StockLocation.where(id: params[:id])
-
-      Spree::StockLocation.transaction { @stock_locations.destroy_all }
-
-      flash[:notice] = t('.success')
-      redirect_back_or_to stock_locations_path, status: :see_other
-    end
-
+  class StockLocationsController < SolidusAdmin::ResourcesController
     private
 
-    def stock_location_params
-      params.require(:stock_location).permit(:stock_location_id, permitted_stock_location_attributes)
-    end
+    def resource_class = Spree::StockLocation
+
+    def resources_collection = Spree::StockLocation.unscoped
   end
 end
