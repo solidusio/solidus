6c34fdd1ceb54caab03075f69da3124b567a67b2
Senem Soy <senem@super.gd>
2025-02-28 14:19:13 -0800

    Don't persist line item on promotion application
    This is in service of the in-memory order updater.
    
    Co-authored-by: Adam <adam@super.gd>
    Co-authored-by: Alistair <alistair@super.gd>
    Co-authored-by: An <andrew@super.gd>
    Co-authored-by: Benjamin <benjamin@super.gd>
    Co-authored-by: Chris <chris@super.gd>
    Co-authored-by: Harmony <harmony@super.gd>
    Co-authored-by: Jared <jared@super.gd>
    Co-authored-by: Nick <nick@super.gd>
    Co-authored-by: Noah <noah@super.gd>
    Co-authored-by: Senem <senem@super.gd>
    Co-authored-by: Sofia <sofia@super.gd>

--- promotions/app/models/solidus_promotions/benefits/create_discounted_item.rb
+++ promotions/app/models/solidus_promotions/benefits/create_discounted_item.rb
@@ -17,9 +17,7 @@ module SolidusPromotions
       end
 
       def remove_from(order)
-        line_item = find_item(order)
-        # The enemy
-        order.line_items.destroy(line_item)
+        find_item(order)&.mark_for_destruction
       end
 
       private
@@ -28,9 +26,8 @@ module SolidusPromotions
         order.line_items.detect { |line_item| line_item.managed_by_order_benefit == self }
       end
 
-      # The enemy - create_item
       def create_item(order)
-        order.line_items.create!(quantity: determine_item_quantity(order), variant: variant, managed_by_order_benefit: self)
+        order.line_items.build(quantity: determine_item_quantity(order), variant: variant, managed_by_order_benefit: self)
       end
 
       def determine_item_quantity(order)
