297d3c5cab9a29b2d172ea72e59fcc0ebb802828
Ilia Shavrin <shavrin.ip@gmail.com>
2025-03-11 15:57:26 +0100

    Fixed migrations so you can rollback them all

--- core/db/migrate/20180416083007_add_apply_to_all_to_variant_property_rule.rb
+++ core/db/migrate/20180416083007_add_apply_to_all_to_variant_property_rule.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AddApplyToAllToVariantPropertyRule < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :spree_variant_property_rules, :apply_to_all, :boolean, default: false, null: false
     change_column :spree_variant_property_rules, :apply_to_all, :boolean, default: true
   end
