9a655be5b3aa79a142c783e6fc6c714fa98458f3
Chris Todorov <chris@super.gd>
2025-03-14 14:18:26 -0700

    Mark adjustments for destruction instead of destroying
    See code comments and spec changes. This is similar to how we handled
    this in the legacy promotion system.
    
    Co-authored-by: Adam <adam@super.gd>
    Co-authored-by: Alistair <alistair@super.gd>
    Co-authored-by: An <andrew@super.gd>
    Co-authored-by: Benjamin <benjamin@super.gd>
    Co-authored-by: Harmony <harmony@super.gd>
    Co-authored-by: Jared <jared@super.gd>
    Co-authored-by: Nick <nick@super.gd>
    Co-authored-by: Senem <senem@super.gd>
    Co-authored-by: Sofia <sofia@super.g

--- promotions/app/models/solidus_promotions/order_adjuster.rb
+++ promotions/app/models/solidus_promotions/order_adjuster.rb
@@ -19,7 +19,6 @@ module SolidusPromotions
       # TODO: - enemy identified
       discounted_order = DiscountOrder.new(order, promotions, dry_run: dry_run).call
 
-      # TODO: - enemy identified
       PersistDiscountedOrder.new(discounted_order).call unless dry_run
 
       order.reset_current_discounts
