e0a348693a928b6db0f0fd7e2caff105d47594a6
Eugene Chaikin <nutaksho@gmail.com>
2025-03-17 18:27:37 +0100

    Add `include_name_field` flag to address component
    With default `true`, this flag allows to control whether to
    show "Name" field on the form (will be used for stock
    location form where "Name" is the only field not used).

--- admin/app/components/solidus_admin/ui/forms/address/component.rb
+++ admin/app/components/solidus_admin/ui/forms/address/component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::UI::Forms::Address::Component < SolidusAdmin::BaseComponent
-  def initialize(addressable:, fieldset_name:, disabled: false)
+  def initialize(addressable:, fieldset_name:, disabled: false, include_name_field: true)
     @addressable = addressable
     @fieldset_name = fieldset_name
     @disabled = disabled
+    @include_name_field = include_name_field
   end
 
   def state_options
