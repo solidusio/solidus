7d00ce0e6567d1efbe181866cf284308f3ff8142
Eugene Chaikin <nutaksho@gmail.com>
2025-03-19 19:29:07 +0100

    Create shortcuts for stimulus data attributes

---
+++ admin/app/helpers/solidus_admin/stimulus_helper.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Simple shorthands and helpers for stimulus data attributes to avoid writing clumsy interpolations with `stimulus_id`
+#  Before: "data-#{stimulus_id}-target": "wrapper"
+#  After: stimulus_target("wrapper")
+
+module SolidusAdmin
+  module StimulusHelper
+    def stimulus_controller
+      { "data-controller": stimulus_id }
+    end
+
+    def stimulus_action(action, on: nil)
+      action_construct = []
+      action_construct << "#{on}->" if on.present?
+      action_construct << "#{stimulus_id}##{action}"
+
+      { "data-action": action_construct.join }
+    end
+
+    def stimulus_target(target)
+      { "data-#{stimulus_id}-target": target }
+    end
+
+    def stimulus_value(name:, value:)
+      { "data-#{stimulus_id}-#{name}-value": value }
+    end
+  end
+end
