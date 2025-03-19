8d5b5debc42bf2dc97e15300da226b6f96598dcb
Eugene Chaikin <nutaksho@gmail.com>
2025-03-19 14:15:39 +0100

    Create `.submit` helper method for buttons

--- admin/spec/components/solidus_admin/ui/button/component_spec.rb
+++ admin/spec/components/solidus_admin/ui/button/component_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe SolidusAdmin::UI::Button::Component, type: :component do
     render_preview(:overview)
     render_preview(:group)
   end
+
+  describe ".submit" do
+    let(:component) { described_class.submit(resource:) }
+
+    context "for a new resource" do
+      let(:resource) { build(:zone) }
+
+      it "renders correct submit button" do
+        render_inline(component)
+        expect(page).to have_content("Add Zone")
+      end
+    end
+
+    context "for an existing resource" do
+      let(:resource) { create(:zone) }
+
+      it "renders correct submit button" do
+        render_inline(component)
+        expect(page).to have_content("Update Zone")
+      end
+    end
+  end
 end
