a8302cc0e77aede2e4d203663193f1c97c32af48
Eugene Chaikin <nutaksho@gmail.com>
2025-03-19 15:07:57 +0100

    Define `title` of the modal in form component

--- admin/app/components/solidus_admin/zones/form/component.rb
+++ admin/app/components/solidus_admin/zones/form/component.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class SolidusAdmin::Zones::Form::Component < SolidusAdmin::BaseComponent
-  def initialize(zone:, title:, form_url:, form_id:)
+  def initialize(zone:, form_url:, form_id:)
     @zone = zone
-    @title = title
     @form_url = form_url
     @form_id = form_id
+  end
+
+  def title
+    @zone.new_record? ? t(".title.new") : t(".title.edit")
   end
 end
