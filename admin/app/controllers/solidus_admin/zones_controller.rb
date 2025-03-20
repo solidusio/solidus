a51fb8ca83874873a2bf3db37e44e7ca3482d23c
Eugene Chaikin <nutaksho@gmail.com>
2025-03-20 19:18:50 +0100

    Avoid N+1 when loading zones index page

--- admin/app/controllers/solidus_admin/zones_controller.rb
+++ admin/app/controllers/solidus_admin/zones_controller.rb
@@ -6,6 +6,8 @@ module SolidusAdmin
 
     def resource_class = Spree::Zone
 
+    def resources_collection = Spree::Zone.includes(zone_members: :zoneable)
+
     def permitted_resource_params
       params.require(:zone).permit(:name, :description, state_ids: [], country_ids: [])
     end
