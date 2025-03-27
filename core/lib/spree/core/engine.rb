38a9d36fced65ad2f45df6c2bee94dd023f2fd29
benjamin wil <benjamin@super.gd>
2025-03-27 13:53:52 -0700

    Move OrderMailerSubscriber#send_confirmation_email
    We're moving towards having a separate subscriber for each type of
    transaction email. This commit ensures that `OrderMailerSubscriber`
    actions are all moved to subscriber classes that more accurately
    describe the mailer action being taken.
    
    Now, the old `OrderMailerSubscriber` does nothing and can be removed in
    the next version of Solidus.
    
    In subsequent commits we can now add additional subscribers for:
    
    - Carton shipped transactional emails
    - Short ship transactional emails
    - Order cancellation transactionals emails
    
    Co-authored-by: Senem Soy <senem@super.gd>

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -71,7 +71,7 @@ module Spree
             reimbursement_errored
           ].each { |event_name| Spree::Bus.register(event_name) }
 
-          Spree::OrderMailerSubscriber.new.subscribe_to(Spree::Bus)
+          Spree::OrderConfirmationMailerSubscriber.new.subscribe_to(Spree::Bus)
           Spree::ReimbursementMailerSubscriber.new.subscribe_to(Spree::Bus)
         end
       end
--- core/spec/subscribers/spree/order_mailer_subscriber_spec.rb
+++ core/spec/subscribers/spree/order_mailer_subscriber_spec.rb
@@ -12,29 +12,15 @@ RSpec.describe Spree::OrderMailerSubscriber do
     described_class.new.subscribe_to(bus)
   end
 
-  describe 'on :on_order_finalized' do
-    it 'sends confirmation email' do
-      order = create(:order, confirmation_delivered: false)
+  describe "#send_confirmation_email" do
+    subject { described_class.new.send_confirmation_email({}) }
 
-      expect(Spree::OrderMailer).to receive(:confirm_email).and_call_original
-
-      bus.publish(:order_finalized, order:)
-    end
-
-    it 'marks the order as having the confirmation email delivered' do
-      order = create(:order, confirmation_delivered: false)
-
-      bus.publish(:order_finalized, order:)
-
-      expect(order.confirmation_delivered).to be(true)
-    end
-
-    it "doesn't send confirmation email if already sent" do
-      order = build(:order, confirmation_delivered: true)
-
-      expect(Spree::OrderMailer).not_to receive(:confirm_email)
-
-      bus.publish(:order_finalized, order:)
+    it "results in a deprecation warning" do
+      if ENV["SOLIDUS_RAISE_DEPRECATIONS"]
+        expect { subject }.to raise_error(ActiveSupport::DeprecationException)
+      else
+        expect(subject).to eq nil
+      end
     end
   end
 
