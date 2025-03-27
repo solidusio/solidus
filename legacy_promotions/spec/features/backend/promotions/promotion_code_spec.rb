c390ca8bd592f0f4380f1b1047705cd0a7c45fbd
Martin Meyerhoff <mamhoff@gmail.com>
2025-03-27 16:18:42 +0100

    Do not test Rails disable-with behavior
    We can just test that the buttons have the necessary attribute for the
    behaviour.
    
    These tests had become flaky.
    
    (cherry picked from commit 52791fe72b9b70acbb6f5b4adf7da43788682ca7)

--- legacy_promotions/spec/features/backend/promotions/promotion_code_spec.rb
+++ legacy_promotions/spec/features/backend/promotions/promotion_code_spec.rb
@@ -19,13 +19,8 @@ RSpec.feature "Promotion Codes" do
       expect(page).to have_content "Promotion Code has been successfully created!"
     end
 
-    it "disables the button at submit", :js do
-      page.execute_script "$('form').submit(function(e) { e.preventDefault()})"
-
-      fill_in "Value", with: "XYZ"
-      click_button "Create"
-
-      expect(page).to have_button("Create", disabled: true)
+    it "disables the button at submit" do
+      expect(page).to have_css("input[type='submit'][data-disable-with='Create']")
     end
   end
 end
