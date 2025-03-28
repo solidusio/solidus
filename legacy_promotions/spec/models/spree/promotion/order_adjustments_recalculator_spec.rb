4ef539061140993b67d7fe942294d7d1070cf4c8
Sofia Besenski <sofia@super.gd>
2025-03-28 14:45:01 -0700

    Test InMemoryOrder updater in legacy promotions
    Integration level test using the InMemoryOrderUpdater to ensure we are
    not persisting changes during the promotion recalculations.
    
    Co-authored-by: Chris <chris@super.gd>
    Co-authored-by: Benjamin <benjamin@super.gd>
    Co-authored-by: An <andrew@super.gd>
    Co-authored-by: Jared <jared@super.gd>

--- legacy_promotions/spec/models/spree/promotion/order_adjustments_recalculator_spec.rb
+++ legacy_promotions/spec/models/spree/promotion/order_adjustments_recalculator_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'db-query-matchers'
 
 RSpec.describe Spree::Promotion::OrderAdjustmentsRecalculator do
   subject { described_class.new(order).call(persist:) }
