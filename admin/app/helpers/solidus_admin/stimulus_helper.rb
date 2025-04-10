4bd0a95edaaa6ff46c1d179802f3f69c5f9842c7
Eugene Chaikin <nutaksho@gmail.com>
2025-04-10 16:43:59 +0200

    Explicitly disable offending cop
    There's some bug with Rubocop that even though `on` is
    specified as allowed parameter name, rubocop still complains

--- admin/app/helpers/solidus_admin/stimulus_helper.rb
+++ admin/app/helpers/solidus_admin/stimulus_helper.rb
@@ -10,7 +10,7 @@ module SolidusAdmin
       { "data-controller": stimulus_id }
     end
 
-    def stimulus_action(action, on: nil)
+    def stimulus_action(action, on: nil) # rubocop:disable Naming/MethodParameterName
       action_construct = []
       action_construct << "#{on}->" if on.present?
       action_construct << "#{stimulus_id}##{action}"
