6ba13d788c8f0d832a224b9232322a0bba8441d8
Mayur Shah <shahmayur001@gmail.com>
2025-04-10 16:08:00 +0530

    Add user group assignment upon signup in UsersController
    - Implemented the `set_user_group` method in `Spree::Api::UsersController`.
    - This method assigns the default user group to a new user if the current store has the `enforce_group_upon_signup` setting enabled.
    - Ensures that new users are automatically added to the specified user group during signup.

--- api/app/controllers/spree/api/users_controller.rb
+++ api/app/controllers/spree/api/users_controller.rb
@@ -31,6 +31,8 @@ class Spree::Api::UsersController < Spree::Api::BaseController
     @user = user_class.new(permitted_user_params)
 
     if @user.save
+      set_user_group if current_store&.enforce_group_upon_signup
+
       respond_with(@user, status: 201, default_template: :show)
     else
       invalid_resource!(@user)
@@ -84,6 +86,17 @@ class Spree::Api::UsersController < Spree::Api::BaseController
       super | [:email]
     else
       super
+    end
+  end
+
+  # Sets the user group for a user if they don't have one assigned
+  # This method checks if there's a user (@user) and if they don't have a user group on sign up
+  # If these conditions are met, it assigns the default cart user group from the current store
+  # If enforce_group_upon_signup is enabled on the store settings
+  def set_user_group
+    if @user && @user.user_group.nil?
+      user_group = current_store.default_cart_user_group
+      @user.update(user_group: user_group) if user_group
     end
   end
 end
