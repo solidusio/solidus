4a0e2e94d4113440e3c5b9719145de1b17d79163
Alistair Norman <alistair@super.gd>
2025-04-17 11:42:31 -0700

    Change migration version to 7.0
    This migration previously couldn't be run on Rails < 7.2. We still
    support 7.0 and 7.1.
    
    Co-Authored-By: Benjamin Willems <benjamin@super.gd>
    Co-Authored-By: Senem Soy <senem@super.gd>
    Co-Authored-By: Sofia Besenski <sofia@super.gd>

--- core/db/migrate/20250214094207_add_reverse_charge_status_to_store.rb
+++ core/db/migrate/20250214094207_add_reverse_charge_status_to_store.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddReverseChargeStatusToStore < ActiveRecord::Migration[7.2]
+class AddReverseChargeStatusToStore < ActiveRecord::Migration[7.0]
   def change
     add_column :spree_stores, :reverse_charge_status, :integer, default: 0, null: false,
                 comment: "Enum values: 0 = disabled, 1 = enabled, 2 = not_validated"
