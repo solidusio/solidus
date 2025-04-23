eb96bc715099bbda92cec0011473fd3bf5964e11
Eugene Chaikin <nutaksho@gmail.com>
2025-04-23 14:08:36 +0200

    Add components for store page form

---
+++ admin/app/components/solidus_admin/stores/edit/component.rb
@@ -0,0 +1,3 @@
+class SolidusAdmin::Stores::Edit::Component < SolidusAdmin::Resources::Edit::Component
+  include SolidusAdmin::Layout::PageHelpers
+end
---
+++ admin/app/components/solidus_admin/stores/new/component.rb
@@ -0,0 +1,3 @@
+class SolidusAdmin::Stores::New::Component < SolidusAdmin::Resources::New::Component
+  include SolidusAdmin::Layout::PageHelpers
+end
