e6653dd3c490412fe956b2ce89941bdd6a5ca7f9
Eugene Chaikin <nutaksho@gmail.com>
2025-04-24 10:03:59 +0200

    Populate store form page with fields

--- admin/app/components/solidus_admin/stores/form/component.rb
+++ admin/app/components/solidus_admin/stores/form/component.rb
@@ -8,4 +8,10 @@ class SolidusAdmin::Stores::Form::Component < SolidusAdmin::BaseComponent
     @id = id
     @url = url
   end
+
+  def available_locales
+    Spree.i18n_available_locales.map do |locale|
+      [I18n.t('spree.i18n.this_file_language', locale: locale, default: locale.to_s, fallback: false), locale]
+    end.sort
+  end
 end
