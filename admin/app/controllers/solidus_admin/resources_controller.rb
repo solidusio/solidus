f872dfc0f657dca0f71093ea0bccd52e36ca6eaa
Eugene Chaikin <nutaksho@gmail.com>
2025-05-08 15:41:46 +0200

    Rename resource form turbo frame
    Since stock location form will not be in a modal, change the
    turbo frame name to be more general :resource_form

--- admin/app/controllers/solidus_admin/resources_controller.rb
+++ admin/app/controllers/solidus_admin/resources_controller.rb
@@ -126,7 +126,7 @@ module SolidusAdmin
           render page_component, status: :unprocessable_entity
         end
         format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(:resource_modal, page_component),
+          render turbo_stream: turbo_stream.replace(resource_form_frame, page_component),
             status: :unprocessable_entity
         end
       end
@@ -147,6 +147,10 @@ module SolidusAdmin
 
     def after_destroy_path
       solidus_admin.send("#{plural_resource_name}_path", **search_filter_params)
+    end
+
+    def resource_form_frame
+      :resource_form
     end
   end
 end
