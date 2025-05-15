10e510a2f9a2bd56cb81c170ab1e9965e46391b6
Eugene Chaikin <nutaksho@gmail.com>
2025-05-15 12:01:51 +0200

    Inherit from resources controller

--- admin/app/controllers/solidus_admin/option_types_controller.rb
+++ admin/app/controllers/solidus_admin/option_types_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 module SolidusAdmin
-  class OptionTypesController < SolidusAdmin::BaseController
-    include SolidusAdmin::ControllerHelpers::Search
-    include SolidusAdmin::Moveable
+  class OptionTypesController < SolidusAdmin::ResourcesController
+    before_action :load_option_type, only: [:move]
 
-    def index
-      option_types = apply_search_to(
-        Spree::OptionType.all,
-        param: :q
-      )
-
-      set_page_and_extract_portion_from(option_types)
+    def move
+      @option_type.insert_at(params[:position].to_i)
 
       respond_to do |format|
-        format.html { render component("option_types/index").new(page: @page) }
+        format.js { head :no_content }
       end
     end
 
-    def destroy
-      @option_types = Spree::OptionType.where(id: params[:id])
+    private
 
-      Spree::OptionType.transaction { @option_types.destroy_all }
+    def resource_class = Spree::OptionType
 
-      flash[:notice] = t(".success")
-      redirect_back_or_to option_types_path, status: :see_other
+    def permitted_resource_params
+      params.require(:option_type).permit(:name, :presentation)
+    end
+
+    def resources_collection = Spree::OptionType.unscoped
+
+    def load_option_type
+      @option_type = Spree::OptionType.find(params[:id])
+      authorize! action_name, @option_type
     end
   end
 end
