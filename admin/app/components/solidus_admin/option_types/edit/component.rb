cde78527908aba9731eeef2d28f4abbf47620674
Eugene Chaikin <nutaksho@gmail.com>
2025-05-16 14:52:13 +0200

    Add table for option values

--- admin/app/components/solidus_admin/option_types/edit/component.rb
+++ admin/app/components/solidus_admin/option_types/edit/component.rb
@@ -2,4 +2,48 @@
 
 class SolidusAdmin::OptionTypes::Edit::Component < SolidusAdmin::Resources::Edit::Component
   include SolidusAdmin::Layout::PageHelpers
+
+  def render_option_values
+    render component("ui/table").new(
+      id: stimulus_id,
+      data: {
+        class: Spree::OptionValue,
+        rows: @option_type.option_values,
+        columns: option_value_columns,
+        batch_actions: [
+          {
+            label: t('.panels.option_values.batch_actions.delete'),
+            action: solidus_admin.option_values_path,
+            method: :delete,
+            icon: 'delete-bin-7-line',
+            require_confirmation: true,
+          },
+        ]
+      },
+      sortable: {
+        url: ->(option_value) { solidus_admin.move_option_value_path(option_value) },
+        param: "position",
+      },
+      embedded: true,
+    )
+  end
+
+  def option_value_columns
+    [
+      {
+        header: :name,
+        data: ->(option_value) do
+          link_to option_value.name, "#",
+            class: 'body-link'
+        end
+      },
+      {
+        header: :presentation,
+        data: ->(option_value) do
+          link_to option_value.presentation, "#",
+            class: 'body-link'
+        end
+      },
+    ]
+  end
 end
