bd470e11d08768be54ce24b0078c783255a869b4
Eugene Chaikin <nutaksho@gmail.com>
2025-05-21 15:35:28 +0200

    Cleanup controllers with moveable concern

--- admin/app/controllers/solidus_admin/option_types_controller.rb
+++ admin/app/controllers/solidus_admin/option_types_controller.rb
@@ -2,15 +2,7 @@
 
 module SolidusAdmin
   class OptionTypesController < SolidusAdmin::ResourcesController
-    before_action :load_option_type, only: [:move]
-
-    def move
-      @option_type.insert_at(params[:position].to_i)
-
-      respond_to do |format|
-        format.js { head :no_content }
-      end
-    end
+    include SolidusAdmin::Moveable
 
     private
 
@@ -28,11 +20,6 @@ module SolidusAdmin
 
     def resources_sorting_options
       { position: :asc }
-    end
-
-    def load_option_type
-      @option_type = Spree::OptionType.find(params[:id])
-      authorize! action_name, @option_type
     end
   end
 end
