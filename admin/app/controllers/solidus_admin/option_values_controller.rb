fe47e01d29c399581582f764cae56cf0c24fbe8f
Eugene Chaikin <nutaksho@gmail.com>
2025-05-21 15:01:53 +0200

    Enable sorting of existing option values

--- admin/app/controllers/solidus_admin/option_values_controller.rb
+++ admin/app/controllers/solidus_admin/option_values_controller.rb
@@ -2,9 +2,23 @@
 
 module SolidusAdmin
   class OptionValuesController < SolidusAdmin::ResourcesController
+    before_action :load_option_value, only: [:move]
+
+    def move
+      @option_value.insert_at(params[:position].to_i)
+
+      respond_to do |format|
+        format.js { head :no_content }
+      end
+    end
 
     private
 
     def resource_class = Spree::OptionValue
+
+    def load_option_value
+      @option_value = Spree::OptionValue.find(params[:id])
+      authorize! action_name, @option_value
+    end
   end
 end
