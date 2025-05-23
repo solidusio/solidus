fcf1bf429a2e05f6251b4a9245a3beb7ad5b7e1f
Eugene Chaikin <nutaksho@gmail.com>
2025-05-23 13:06:10 +0200

    Add feature tests

--- admin/spec/features/option_types_spec.rb
+++ admin/spec/features/option_types_spec.rb
@@ -21,4 +21,69 @@ describe "Option Types", :js, type: :feature do
     expect(page).not_to have_content("color")
     expect(Spree::OptionType.count).to eq(1)
   end
+
+  it "allows to create option types/values" do
+    visit "/admin/option_types"
+    click_on "Add new"
+
+    fill_in "Name", with: "clothing-color"
+    fill_in "Presentation", with: "Color"
+    click_on "Add Option Type"
+
+    expect(page).to have_current_path(/\/admin\/option_types\/\d+\/edit/)
+    click_on "Add new"
+
+    within("dialog") do
+      fill_in "Name", with: "blue"
+      fill_in "Presentation", with: "Blue"
+      click_on "Add Option Value"
+    end
+
+    expect(page).to have_content("Option value was successfully created.")
+  end
+
+  it "allows to update option types" do
+    create(:option_type, name: "color", presentation: "Color")
+    visit "/admin/option_types"
+    click_on "Color"
+
+    fill_in "Name", with: "clt-colour"
+    fill_in "Presentation", with: "Colour"
+    within("header") { click_on "Save" }
+
+    expect(page).to have_current_path("/admin/option_types")
+    expect(page).to have_content("Option type was successfully updated.")
+    expect(page).to have_content("clt-colour")
+    expect(page).to have_content("Colour")
+    expect(page).not_to have_content("color")
+    expect(page).not_to have_content("Color")
+  end
+
+  it "allows to edit and destroy option values" do
+    create(:option_type, name: "color", presentation: "Color").tap do |option_type|
+      option_type.option_values = [
+        create(:option_value, name: "blue", presentation: "Blue"),
+        create(:option_value, name: "green", presentation: "Green"),
+      ]
+    end
+
+    visit "/admin/option_types"
+    click_on "Color"
+
+    click_on "Blue"
+    within("dialog") do
+      fill_in "Name", with: "sky-blue"
+      fill_in "Presentation", with: "Sky Blue"
+    end
+
+    click_on "Update Option Value"
+    expect(page).to have_content("Option value was successfully updated.")
+    expect(find("table")).to have_content("sky-blue")
+    expect(find("table")).to have_content("Sky Blue")
+
+    select_row("green")
+    accept_confirm("Are you sure you want to delete 1 option value?") { click_on "Delete" }
+    expect(page).to have_content("Option values were successfully removed.")
+    expect(page).not_to have_content("green")
+  end
 end
