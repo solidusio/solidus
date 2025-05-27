540ad6a3e2b5f262f0922a23f7992a3f99bafc5c
Eugene Chaikin <nutaksho@gmail.com>
2025-05-27 10:25:07 +0200

    Add missing specs for other sortables

--- admin/spec/features/taxonomies_spec.rb
+++ admin/spec/features/taxonomies_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require "solidus_admin/testing_support/shared_examples/moveable"
 
 describe "Taxonomies", :js, type: :feature do
   before { sign_in create(:admin_user, email: 'admin@example.com') }
@@ -20,5 +21,11 @@ describe "Taxonomies", :js, type: :feature do
     expect(page).to have_content("Taxonomies were successfully removed.")
     expect(page).not_to have_content("Categories")
     expect(Spree::Taxonomy.count).to eq(1)
+  end
+
+  it_behaves_like "features: sortable" do
+    let(:factory) { :taxonomy }
+    let(:displayed_attribute) { :name }
+    let(:path) { solidus_admin.taxonomies_path }
   end
 end
