8d2aaaa165b3429d05af0cd76d1f998ed382da7b
Martin Meyerhoff <mamhoff@gmail.com>
2025-05-28 15:02:52 +0200

    AdjustmentReason: Add :dependent option
    We should not delete adjustment reasons if there are adjustments created
    with them.

--- core/app/models/spree/adjustment_reason.rb
+++ core/app/models/spree/adjustment_reason.rb
@@ -2,7 +2,7 @@
 
 module Spree
   class AdjustmentReason < Spree::Base
-    has_many :adjustments, inverse_of: :adjustment_reason
+    has_many :adjustments, inverse_of: :adjustment_reason, dependent: :restrict_with_error
 
     validates :name, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
     validates :code, presence: true, uniqueness: { case_sensitive: false, allow_blank: true }
