378e325973b0c0bb52ec3387d7185d23bef6fe0a
Martin Meyerhoff <mamhoff@gmail.com>
2025-05-28 17:06:38 +0200

    PermissionSet: Add dependent: :destroy
    When a permission set is destroyed, the join table entries to
    spree_roles should also be destroyed.

--- core/app/models/spree/permission_set.rb
+++ core/app/models/spree/permission_set.rb
@@ -2,7 +2,7 @@
 
 module Spree
   class PermissionSet < Spree::Base
-    has_many :role_permissions
+    has_many :role_permissions, dependent: :destroy
     has_many :roles, through: :role_permissions
     validates :name, :set, :privilege, :category, presence: true
     scope :display_permissions, -> { where(privilege: "display") }
