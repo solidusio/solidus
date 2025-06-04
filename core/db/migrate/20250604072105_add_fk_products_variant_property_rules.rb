b8389484185eb0708d70ed9ec736c087dd5c9c9e
Martin Meyerhoff <mamhoff@gmail.com>
2025-06-04 09:24:13 +0200

    Add foreign key constraint to variant property rules
    This model belongs to a product, and is useless without it. Remove the
    `optional` property from the `belongs_to` declaration and add a foreign
    key.
    
    This will also delete useless variant property rules.

---
+++ core/db/migrate/20250604072105_add_fk_products_variant_property_rules.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class AddFkProductsVariantPropertyRules < ActiveRecord::Migration[7.0]
+  def up
+    # Uncomment the following code to remove orphaned records if this migration fails
+    #
+    # say_with_time "Removing orphaned variant property rules (no corresponding product)" do
+    #   Spree::VariantPropertyRule.left_joins(:product).where(spree_products: { id: nil }).delete_all
+    # end
+
+    add_foreign_key :spree_variant_property_rules, :spree_products, column: :product_id, null: false
+  rescue ActiveRecord::StatementInvalid => e
+    if e.cause.is_a?(PG::ForeignKeyViolation) || e.cause.is_a?(Mysql2::Error) || e.cause.is_a?(SQLite3::ConstraintException)
+      Rails.logger.warn <<~MSG
+        ⚠️ Foreign key constraint failed when adding :spree_variant_property_rules => :spree_products.
+        To fix this:
+          1. Uncomment the code that removes orphaned records.
+          2. Rerun the migration.
+        Offending error: #{e.cause.class} - #{e.cause.message}
+      MSG
+    end
+    raise
+  end
+
+  def down
+    remove_foreign_key :spree_variant_property_rules, :spree_products, column: :product_id, null: false
+  end
+end
