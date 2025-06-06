5e0ef349bb6626b720a29482b8539718076e3621
Eugene Chaikin <nutaksho@gmail.com>
2025-06-06 17:43:46 +0200

    Support flash[:alert] = <String>
    Passing strings as messages to `flash[]` is more common than using other
    primitives, and users deciding to set flash[:alert] might not be aware
    of the Hash structure that is required to correctly construct and
    display a UI alert from flash[:alert]. So instead of letting it fail
    because of the type mismatch (when calling #slice on a string instead
    of a hash), we can add a fallback option to treat given string as a body
    of the alert message and construct a `danger` type alert with default
    title.

--- admin/app/components/solidus_admin/layout/flashes/alerts/component.rb
+++ admin/app/components/solidus_admin/layout/flashes/alerts/component.rb
@@ -3,7 +3,17 @@
 class SolidusAdmin::Layout::Flashes::Alerts::Component < SolidusAdmin::BaseComponent
   attr_reader :alerts
 
+  # Construct alert flashes like:
+  #   flash[:alert] = { <alert_type>: { title: "", description: "" } }
+  # See +SolidusAdmin::UI::Alert::Component::SCHEMES+ for available alert types.
+  #
+  # If a string is passed to flash[:alert], we treat it is a body of the alert message and fall back to +danger+ type
+  # and default title (see +SolidusAdmin::UI::Alert::Component+).
   def initialize(alerts:)
-    @alerts = alerts
+    if alerts.is_a?(String)
+      alerts = { danger: { description: alerts } }
+    end
+
+    @alerts = alerts.slice(*SolidusAdmin::UI::Alert::Component::SCHEMES.keys)
   end
 end
