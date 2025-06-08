e604fcfb0ed546e8ade500344a8fb8bd48049f02
Martin Meyerhoff <mamhoff@gmail.com>
2025-06-08 11:37:26 +0200

    Percent calculator: Allow prices
    Prices have their currency on them, we don't need the order. Price
    calculation will, however, pass in some options to `compute`, so let's
    account for that.

--- promotions/spec/models/solidus_promotions/calculators/percent_spec.rb
+++ promotions/spec/models/solidus_promotions/calculators/percent_spec.rb
@@ -4,10 +4,13 @@ require "rails_helper"
 require "shared_examples/calculator_shared_examples"
 
 RSpec.describe SolidusPromotions::Calculators::Percent, type: :model do
-  context "compute" do
-    let(:currency) { "USD" }
-    let(:order) { double(currency: currency) }
-    let(:line_item) { double("Spree::LineItem", discountable_amount: 100, order: order) }
+  it_behaves_like "a calculator with a description"
+
+  let(:currency) { "USD" }
+  let(:order) { double(currency: currency) }
+
+  context "compute_line_item" do
+    let(:line_item) { mock_model("Spree::LineItem", discountable_amount: 100, order: order) }
 
     before { subject.preferred_percent = 15 }
 
@@ -16,5 +19,23 @@ RSpec.describe SolidusPromotions::Calculators::Percent, type: :model do
     end
   end
 
-  it_behaves_like "a calculator with a description"
+  describe "compute_shipment" do
+    let(:shipment) { mock_model(Spree::Shipment, amount: 110, discountable_amount: 100, order: order) }
+
+    before { subject.preferred_percent = 15 }
+
+    it "computes based on item price and quantity" do
+      expect(subject.compute(shipment)).to eq 15
+    end
+  end
+
+  describe "compute_price" do
+    let(:price) { mock_model(Spree::Price, amount: 110, discountable_amount: 100, currency: "USD") }
+
+    before { subject.preferred_percent = 15 }
+
+    it "computes based on item price and quantity" do
+      expect(subject.compute(price, { order: order })).to eq 15
+    end
+  end
 end
