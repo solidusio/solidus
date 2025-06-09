cf6cf649c31e6fd7cb3cf55787e566224d2ca0c1
Eugene Chaikin <nutaksho@gmail.com>
2025-06-09 18:12:28 +0200

    Delegate methods
    Delegate option type methods #name, #presentation, #option_values for
    easier access in the view.

--- core/app/models/spree/product_option_type.rb
+++ core/app/models/spree/product_option_type.rb
@@ -5,5 +5,7 @@ module Spree
     belongs_to :product, class_name: 'Spree::Product', inverse_of: :product_option_types, touch: true
     belongs_to :option_type, class_name: 'Spree::OptionType', inverse_of: :product_option_types
     acts_as_list scope: :product
+
+    delegate :name, :presentation, :option_values, to: :option_type
   end
 end
