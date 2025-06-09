a6d9d007748817ef2b9e467066fc1e7a8bdd06a2
Eugene Chaikin <nutaksho@gmail.com>
2025-06-09 22:02:04 +0200

    Add feature specs

--- admin/spec/features/product_spec.rb
+++ admin/spec/features/product_spec.rb
@@ -52,4 +52,69 @@ describe "Product", type: :feature do
     expect(page).to have_content("Name can't be blank")
     expect(page).to be_axe_clean
   end
+
+  describe "option types", :js do
+    before do
+      create(:option_type, name: "clothing-size", presentation: "Size").tap do |option_type|
+        option_type.option_values << [
+          create(:option_value, name: "S", presentation: "Small"),
+          create(:option_value, name: "M", presentation: "Medium")
+        ]
+      end
+
+      create(:option_type, name: "clothing-color", presentation: "Color").tap do |option_type|
+        option_type.option_values << [
+          create(:option_value, name: "brown", presentation: "Brown"),
+          create(:option_value, name: "red", presentation: "Red")
+        ]
+      end
+    end
+
+    let!(:product) { create(:product, name: "Just a product", slug: 'just-a-prod', price: 19.99) }
+
+    it "updates option types" do
+      visit "/admin/products/just-a-prod"
+      solidus_select(%w[clothing-size:Size clothing-color:Color], from: "Option Types")
+      options_panel = panel(title: "Options")
+      within(options_panel) { click_on "Save" }
+
+      expect(options_panel).to have_content("clothing-size:Size")
+      expect(options_panel).to have_content("S:Small")
+      expect(options_panel).to have_content("M:Medium")
+      expect(options_panel).to have_content("clothing-color:Color")
+      expect(options_panel).to have_content("brown:Brown")
+      expect(options_panel).to have_content("red:Red")
+
+      solidus_unselect(%w[clothing-size:Size clothing-color:Color], from: "Option Types")
+      within(options_panel) { click_on "Save" }
+
+      expect(options_panel).not_to have_content("clothing-size:Size")
+      expect(options_panel).not_to have_content("S:Small")
+      expect(options_panel).not_to have_content("M:Medium")
+      expect(options_panel).not_to have_content("clothing-color:Color")
+      expect(options_panel).not_to have_content("brown:Brown")
+      expect(options_panel).not_to have_content("red:Red")
+    end
+
+    context "clicking on Edit" do
+      it "leads to option type edit page" do
+        option_type = create(:option_type)
+        product.option_types << option_type
+        visit "/admin/products/just-a-prod"
+
+        pending("option types new UI is in progress")
+        within(panel(title: "Options")) { click_on "Edit" }
+        expect(page).to have_current_path("/admin/option_types/#{option_type.id}/edit")
+      end
+    end
+
+    context "clicking on Manage option types" do
+      it "leads to option types index page" do
+        visit "/admin/products/just-a-prod"
+
+        within(panel(title: "Options")) { click_on "Manage option types" }
+        expect(page).to have_current_path("/admin/option_types")
+      end
+    end
+  end
 end
