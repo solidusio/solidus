c7481e3fa638111dd438d4fc2f95d54de7269c6c
Martin Meyerhoff <mamhoff@gmail.com>
2025-06-19 15:54:17 +0200

    Handle failing foreign key additions
    We've added some foreign key constraints recently, and have since come
    up with a nicer way of guiding users towards fixing their data
    intentionally.
    
    This change gives them a nice error message and allows them to change
    the migration to remove offending records.
    
    Co-Authored-By: thomas@vondeyen.com

--- core/db/migrate/20250530102541_add_addressbook_foreign_key.rb
+++ core/db/migrate/20250530102541_add_addressbook_foreign_key.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 class AddAddressbookForeignKey < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    # Uncomment the following code to remove orphaned records if this migration fails
+    #
+    # say_with_time "Removing orphaned address book entries (no corresponding address)" do
+    #   Spree::UserAddress.left_joins(:address).where(spree_addresses: { id: nil }).delete_all
+    # end
+
     add_foreign_key :spree_user_addresses, :spree_addresses, column: :address_id, null: false
+  rescue ActiveRecord::StatementInvalid => e
+    if e.cause.is_a?(PG::ForeignKeyViolation) || e.cause.is_a?(Mysql2::Error) || e.cause.is_a?(SQLite3::ConstraintException)
+      Rails.logger.warn <<~MSG
+        ⚠️ Foreign key constraint failed when adding :spree_user_addresses => :spree_addresses.
+        To fix this:
+          1. Uncomment the code that removes orphaned records.
+          2. Rerun the migration.
+        Offending error: #{e.cause.class} - #{e.cause.message}
+      MSG
+    end
+    raise
+  end
+
+  def down
+    remove_foreign_key :spree_user_addresses, :spree_addresses, column: :address_id, null: false
   end
 end
