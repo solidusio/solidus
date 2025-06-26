20bd425c89a68c8b53332d559268dfde4e270c9a
Eugene Chaikin <nutaksho@gmail.com>
2025-06-26 15:16:12 +0200

    TaxRates controller inherits from ResourcesController

--- admin/app/controllers/solidus_admin/tax_rates_controller.rb
+++ admin/app/controllers/solidus_admin/tax_rates_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SolidusAdmin
-  class TaxRatesController < SolidusAdmin::BaseController
+  class TaxRatesController < SolidusAdmin::ResourcesController
     include SolidusAdmin::ControllerHelpers::Search
 
     def index
@@ -28,8 +28,13 @@ module SolidusAdmin
 
     private
 
-    def tax_rate_params
-      params.require(:tax_rate).permit(:tax_rate_id, permitted_tax_rate_attributes)
+    def resource_class = Spree::TaxRate
+
+    def resources_sorting_options = { created_at: :desc, id: :desc }
+
+    def permitted_resource_params
+      params.require(:tax_rate).permit(:name, :zone_id, :show_rate_in_label, :calculator_type, :amount, :level,
+        :included_in_price, :starts_at, :expires_at, tax_category_ids: [])
     end
   end
 end
