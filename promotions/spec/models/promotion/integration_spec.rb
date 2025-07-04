ee4b5494fd036987eab568827f1e3aab5e09f5e0
Adam Mueller <adam@super.gd>
2025-07-04 15:47:52 -0700

    fixup! Test in-memory order updater with new promotions

--- promotions/spec/models/promotion/integration_spec.rb
+++ promotions/spec/models/promotion/integration_spec.rb
@@ -6,6 +6,15 @@ require "solidus_promotions/promotion_migrator"
 
 RSpec.describe "Promotion System" do
   describe "with the default order recalculator config" do
+    around do |example|
+      prev_recalculator_class = Spree::Config.order_recalculator_class
+      Spree::Config.order_recalculator_class = Spree::OrderUpdater
+
+      example.run
+
+      Spree::Config.order_recalculator_class = prev_recalculator_class
+    end
+
     it_behaves_like "a successfully integrated promotion system"
   end
 end
