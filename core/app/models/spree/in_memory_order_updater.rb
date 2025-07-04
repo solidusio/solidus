d5858cb608eb7e14c34cfa8f52bddabdddffcff3
Chris Todorov <chris@super.gd>
2025-07-04 14:09:17 -0700

    fixup! Don't log when persisting

--- core/app/models/spree/in_memory_order_updater.rb
+++ core/app/models/spree/in_memory_order_updater.rb
@@ -24,25 +24,36 @@ module Spree
     # object with callbacks (otherwise you will end up in an infinite recursion as the
     # associations try to save and then in turn try to call +update!+ again.)
     def recalculate(persist: true)
-      monitor = log_manipulative_queries && !persist ?
+      monitor = log_manipulative_queries ?
         Spree::ManipulativeQueryMonitor :
         proc { |&block| block.call }
 
-      monitor.call do
-        order.transaction do
+      order.transaction do
+        monitor.call do
           recalculate_item_count
+        end
+
+        if persist
           update_shipment_amounts(persist:)
           update_totals(persist:)
+        else
+          monitor.call do
+            update_shipment_amounts(persist:)
+            update_totals(persist:)
+          end
+        end
+
+        monitor.call do
           if order.completed?
             recalculate_payment_state
             update_shipments
             recalculate_shipment_state
           end
-
-          Spree::Bus.publish(:order_recalculated, order:)
-
-          persist_totals if persist
         end
+
+        Spree::Bus.publish(:order_recalculated, order:)
+
+        persist_totals if persist
       end
     end
     alias_method :update, :recalculate
