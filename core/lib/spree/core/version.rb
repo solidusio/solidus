cc85f98f32c4f473b7693a87b93689d6665a92d7
tvdeyen <tvdeyen@users.noreply.github.com>
2025-09-09 08:14:12 +0000

    Post-release chores after having released Solidus v4.6.0
    This code has been automatically generated by our 'Prepare post-release' GitHub
    action.
    
    Make sure that:
    
    - [ ] The new release has been published, along with its tag. See https://github.com/solidusio/solidus/releases/tag/v4.6.0
    - [ ] The corresponding patch branch exists. See https://github.com/solidusio/solidus/tree/v4.6
    - [ ] The corresponding backport-v4.6 label exists. See https://github.com/solidusio/solidus/labels

--- core/lib/spree/core/version.rb
+++ core/lib/spree/core/version.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Spree
-  VERSION = "4.6.0"
+  VERSION = "4.7.0.dev"
 
   def self.solidus_version = VERSION
 
   def self.minimum_required_rails_version = "7.0"
 
-  def self.previous_solidus_minor_version = "4.5"
+  def self.previous_solidus_minor_version = "4.6"
 
   def self.solidus_gem_version = Gem::Version.new(solidus_version)
 end
