595daddc34efdb1d07e10bcd6b7adaae26795199
Martin Meyerhoff <mamhoff@gmail.com>
2025-10-21 08:14:55 +0200

    Refactor: Use preferences DSL for stock configuration
    We use this DSL in a lot of other places, and it's quite readable.

--- core/lib/spree/core/stock_configuration.rb
+++ core/lib/spree/core/stock_configuration.rb
@@ -2,55 +2,15 @@
 
 module Spree
   module Core
-    class StockConfiguration
-      attr_writer :coordinator_class
-      attr_writer :estimator_class
-      attr_writer :location_filter_class
-      attr_writer :location_sorter_class
-      attr_writer :allocator_class
-      attr_writer :inventory_unit_builder_class
-      attr_writer :availability_validator_class
-      attr_writer :inventory_validator_class
-
-      def coordinator_class
-        @coordinator_class ||= '::Spree::Stock::SimpleCoordinator'
-        @coordinator_class.constantize
-      end
-
-      def estimator_class
-        @estimator_class ||= '::Spree::Stock::Estimator'
-        @estimator_class.constantize
-      end
-
-      def location_filter_class
-        @location_filter_class ||= '::Spree::Stock::LocationFilter::Active'
-        @location_filter_class.constantize
-      end
-
-      def location_sorter_class
-        @location_sorter_class ||= '::Spree::Stock::LocationSorter::Unsorted'
-        @location_sorter_class.constantize
-      end
-
-      def allocator_class
-        @allocator_class ||= '::Spree::Stock::Allocator::OnHandFirst'
-        @allocator_class.constantize
-      end
-
-      def inventory_unit_builder_class
-        @inventory_unit_builder_class ||= '::Spree::Stock::InventoryUnitBuilder'
-        @inventory_unit_builder_class.constantize
-      end
-
-      def availability_validator_class
-        @availability_validator_class ||= '::Spree::Stock::AvailabilityValidator'
-        @availability_validator_class.constantize
-      end
- 
-      def inventory_validator_class
-        @inventory_validator_class ||= '::Spree::Stock::InventoryValidator'
-        @inventory_validator_class.constantize
-      end
+    class StockConfiguration < Spree::Preferences::Configuration
+      class_name_attribute :coordinator_class, default: "::Spree::Stock::SimpleCoordinator"
+      class_name_attribute :estimator_class, default: "::Spree::Stock::Estimator"
+      class_name_attribute :location_filter_class, default: "::Spree::Stock::LocationFilter::Active"
+      class_name_attribute :location_sorter_class, default: "::Spree::Stock::LocationSorter::Unsorted"
+      class_name_attribute :allocator_class, default: "::Spree::Stock::Allocator::OnHandFirst"
+      class_name_attribute :inventory_unit_builder_class, default: "::Spree::Stock::InventoryUnitBuilder"
+      class_name_attribute :availability_validator_class, default: "::Spree::Stock::AvailabilityValidator"
+      class_name_attribute :inventory_validator_class, default: "::Spree::Stock::InventoryValidator"
     end
   end
 end
