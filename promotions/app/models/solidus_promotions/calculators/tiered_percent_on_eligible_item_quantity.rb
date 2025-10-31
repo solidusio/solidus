5319913110e6385f7a4bb8411fcf3632717b253a
Martin Meyerhoff <mamhoff@gmail.com>
2025-10-31 22:33:41 +0100

    TieredPercentOnEligibleItemQuantity: Allow discounting shipping
    Prior to this, this did allow discounting shipping as well, but
    implemented the parent class. Now we're not doing that, but instead
    doing what the calculator does normally: Apply a percentage based on the
    sum of quantities of applicable line items.

--- promotions/app/models/solidus_promotions/calculators/tiered_percent_on_eligible_item_quantity.rb
+++ promotions/app/models/solidus_promotions/calculators/tiered_percent_on_eligible_item_quantity.rb
@@ -4,23 +4,30 @@ require_dependency "spree/calculator"
 
 module SolidusPromotions
   module Calculators
-    class TieredPercentOnEligibleItemQuantity < SolidusPromotions::Calculators::TieredPercent
+    class TieredPercentOnEligibleItemQuantity < Spree::Calculator
+      include PromotionCalculator
+
+      preference :base_percent, :decimal, default: Spree::ZERO
       preference :tiers, :hash, default: { 10 => 5 }
+      preference :currency, :string, default: -> { Spree::Config[:currency] }
 
       before_validation :transform_preferred_tiers
 
-      def compute_line_item(line_item)
-        order = line_item.order
+      def compute_item(item)
+        order = item.order
 
         _base, percent = preferred_tiers.sort.reverse.detect do |value, _|
           eligible_line_items_quantity_total(order) >= value
         end
         if preferred_currency.casecmp(order.currency).zero?
-          round_to_currency(line_item.discountable_amount * (percent || preferred_base_percent) / 100, preferred_currency)
+          round_to_currency(item.discountable_amount * (percent || preferred_base_percent) / 100, preferred_currency)
         else
           Spree::ZERO
         end
       end
+      alias_method :compute_shipment, :compute_item
+      alias_method :compute_shipping_rate, :compute_item
+      alias_method :compute_line_item, :compute_item
 
       private
 
