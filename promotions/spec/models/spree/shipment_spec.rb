d66c202affc168b2d2c8f102f1efa8ab1447bb51
Martin Meyerhoff <mamhoff@gmail.com>
2025-11-16 13:53:27 +0100

    Add Discountable module to shipments

--- promotions/spec/models/spree/shipment_spec.rb
+++ promotions/spec/models/spree/shipment_spec.rb
@@ -36,4 +36,122 @@ RSpec.describe Spree::Shipment do
       end
     end
   end
+
+  describe "#discounts_by_lanes" do
+    let(:tax_rate) { create(:tax_rate) }
+    let(:pre_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :pre) }
+    let(:post_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :post) }
+    let(:shipment) { Spree::Shipment.new(adjustments:) }
+    let(:adjustments) { [tax_adjustment, pre_lane_adjustment, post_lane_adjustment] }
+    let(:tax_adjustment) { Spree::Adjustment.new(source: tax_rate, amount: 2) }
+    let(:pre_lane_adjustment) { Spree::Adjustment.new(source: pre_lane_promotion.benefits.first) }
+    let(:post_lane_adjustment) { Spree::Adjustment.new(source: post_lane_promotion.benefits.first) }
+
+    subject { shipment.discounts_by_lanes(lanes) }
+
+    context "if lanes is empty" do
+      let(:lanes) { [] }
+      it { is_expected.to be_empty }
+    end
+
+    context "if lanes is all lanes" do
+      let(:lanes) { SolidusPromotions::Promotion.ordered_lanes }
+
+      it { is_expected.to contain_exactly(pre_lane_adjustment, post_lane_adjustment) }
+    end
+
+    context "if lanes is only pre lane" do
+      let(:lanes) { [:pre] }
+
+      it { is_expected.to contain_exactly(pre_lane_adjustment) }
+    end
+
+    context "if lanes is only default lane" do
+      let(:lanes) { [:default] }
+
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe "#previous_lane_discounts" do
+    let(:order) { Spree::Order.new }
+    let(:tax_rate) { create(:tax_rate) }
+    let(:pre_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :pre) }
+    let(:post_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :post) }
+    let(:shipment) { Spree::Shipment.new(adjustments:, order:) }
+    let(:adjustments) { [tax_adjustment, pre_lane_adjustment, post_lane_adjustment] }
+    let(:tax_adjustment) { Spree::Adjustment.new(source: tax_rate, amount: 2) }
+    let(:pre_lane_adjustment) { Spree::Adjustment.new(source: pre_lane_promotion.benefits.first) }
+    let(:post_lane_adjustment) { Spree::Adjustment.new(source: post_lane_promotion.benefits.first) }
+
+    subject { shipment.previous_lane_discounts }
+
+    it "raises unless we're doing a promotion calculation" do
+      expect { subject }.to raise_exception(SolidusPromotions::NotCalculatingPromotions)
+    end
+
+    context "while calculating promotions" do
+      around do |example|
+        SolidusPromotions::Promotion.within_lane(lane) do
+          example.run
+        end
+      end
+
+      let(:lane) { "pre" }
+      it { is_expected.to be_empty }
+
+      context "if lane is default" do
+        let(:lane) { "default" }
+
+        it { is_expected.to contain_exactly(pre_lane_adjustment) }
+      end
+
+      context "if lane is post" do
+        let(:lane) { "post" }
+
+        it { is_expected.to contain_exactly(pre_lane_adjustment) }
+      end
+    end
+  end
+
+  describe "#current_lane_discounts" do
+    let(:order) { Spree::Order.new }
+    let(:tax_rate) { create(:tax_rate) }
+    let(:pre_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :pre) }
+    let(:post_lane_promotion) { create(:solidus_promotion, :with_adjustable_benefit, lane: :post) }
+    let(:shipment) { Spree::Shipment.new(adjustments:, order:) }
+    let(:adjustments) { [tax_adjustment, pre_lane_adjustment, post_lane_adjustment] }
+    let(:tax_adjustment) { Spree::Adjustment.new(source: tax_rate, amount: 2) }
+    let(:pre_lane_adjustment) { Spree::Adjustment.new(source: pre_lane_promotion.benefits.first) }
+    let(:post_lane_adjustment) { Spree::Adjustment.new(source: post_lane_promotion.benefits.first) }
+
+    subject { shipment.current_lane_discounts }
+
+    it "raises unless we're doing a promotion calculation" do
+      expect { subject }.to raise_exception(SolidusPromotions::NotCalculatingPromotions)
+    end
+
+    context "while calculating promotions" do
+      around do |example|
+        SolidusPromotions::Promotion.within_lane(lane) do
+          example.run
+        end
+      end
+
+      let(:lane) { "pre" }
+      it { is_expected.to contain_exactly(pre_lane_adjustment) }
+
+      context "if lane is default" do
+        let(:lane) { "default" }
+
+        it { is_expected.to be_empty }
+      end
+
+      context "if lane is post" do
+        let(:lane) { "post" }
+
+        it { is_expected.to contain_exactly(post_lane_adjustment) }
+      end
+    end
+  end
 end
