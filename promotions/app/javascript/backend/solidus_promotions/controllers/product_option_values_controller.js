ad046f75f6b461158b3a58ff3077b1e30315ca18
Martin Meyerhoff <mamhoff@gmail.com>
2025-11-17 10:54:50 +0100

    Promotions UI: Make option value picker depend on product ID
    When creating an option value condition, we only want to have option
    values that fit the product in question. This commit makes it such that
    the option value picker re-initializes when the selected product
    changes.
    
    (cherry picked from commit b45afe7c2761dd75b9ce4cfae61d1c8c7bd6a690)

--- promotions/app/javascript/backend/solidus_promotions/controllers/product_option_values_controller.js
+++ promotions/app/javascript/backend/solidus_promotions/controllers/product_option_values_controller.js
@@ -6,8 +6,6 @@ export default class extends Controller {
   connect() {
     this.wrapperClass =
       this.data.get("wrapperClass") || "promo-condition-option-value";
-
-    this.element.querySelectorAll("." + this.wrapperClass).forEach((element) => this.buildSelects(element))
   }
 
   add_row(event) {
@@ -15,7 +13,6 @@ export default class extends Controller {
 
     var content = this.templateTarget.innerHTML;
     this.linksTarget.insertAdjacentHTML("beforebegin", content);
-    this.buildSelects(this.linksTarget.previousElementSibling)
   }
 
   propagate_product_id_to_value_input(event) {
@@ -24,7 +21,9 @@ export default class extends Controller {
     // we first need to greedily match all other square brackets
     const regEx = /(\[.*\])\[.*?\]$/;
     let wrapper = event.target.closest("." + this.wrapperClass);
-    let optionValuesInput = wrapper.querySelector(".option-values-select[type='hidden']");
+    let optionValuesInput = wrapper.querySelector("[is=option-value-picker]");
+    optionValuesInput.dataset.productId = event.target.value;
+    optionValuesInput.value = "";
     optionValuesInput.name = optionValuesInput.name.replace(
       regEx,
       `$1[${event.target.value}]`
@@ -36,27 +35,5 @@ export default class extends Controller {
 
     let wrapper = event.target.closest("." + this.wrapperClass);
     wrapper.remove();
-  }
-
-  // helper functions
-
-  buildSelects(wrapper) {
-    let productSelect = wrapper.querySelector(".product-select")
-    let optionValueSelect = wrapper.querySelector(".option-values-select[type='hidden']")
-    this.buildProductSelect(productSelect)
-    $(optionValueSelect).optionValueAutocomplete({ productSelect });
-  }
-
-  buildProductSelect(productSelect) {
-    var jQueryProductSelect = $(productSelect)
-    jQueryProductSelect.productAutocomplete({
-      multiple: false,
-    })
-    // capture the jQuery "change" event and re-emit it as DOM event "select2Change"
-    // so that Stimulus can capture it
-    jQueryProductSelect.on('change', function () {
-      let event = new Event('select2Change', { bubbles: true }) // fire a native event
-      productSelect.dispatchEvent(event);
-    });
   }
 }
--- promotions/app/javascript/backend/solidus_promotions/web_components/product_picker.js
+++ promotions/app/javascript/backend/solidus_promotions/web_components/product_picker.js
@@ -2,6 +2,10 @@ class ProductPicker extends HTMLInputElement {
   connectedCallback() {
     const multiple = this.dataset.multiple !== "false";
     $(this).productAutocomplete({ multiple });
+    $(this).on("change", (_) => {
+      let event = new Event('select2Change', { bubbles: true }) // fire a native event
+      this.dispatchEvent(event)
+    })
   }
 }
 
