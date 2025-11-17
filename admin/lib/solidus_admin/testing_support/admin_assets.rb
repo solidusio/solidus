23417483a8bc87ecba17903e36ccb2c4b7785b54
Martin Meyerhoff <mamhoff@gmail.com>
2025-11-17 19:20:37 +0100

    Disable Rubocop Rails/Exit locally
    This cop makes a lot of sense for the part of the app that runs
    permanently, but both of these cases are instances where the code being
    aborted is run as a one-off from the command line.

--- admin/lib/solidus_admin/testing_support/admin_assets.rb
+++ admin/lib/solidus_admin/testing_support/admin_assets.rb
@@ -3,7 +3,9 @@
 RSpec.configure do |config|
   config.when_first_matching_example_defined(solidus_admin: true) do
     config.before(:suite) do
-      system('bin/rails solidus_admin:tailwindcss:build') or abort 'Failed to build Tailwind CSS'
+      # rubocop:disable Rails/Exit
+      system("bin/rails solidus_admin:tailwindcss:build") or abort "Failed to build Tailwind CSS"
+      # rubocop:enable Rails/Exit
       Rails.application.precompiled_assets
     end
   end
