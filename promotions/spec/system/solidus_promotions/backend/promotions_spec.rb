b0eda8bad499d5c058d842780be962ec5def1fe5
Martin Meyerhoff <mamhoff@gmail.com>
2025-11-17 12:41:13 +0100

    Fix appearance and specs for option value condition

--- promotions/spec/system/solidus_promotions/backend/promotions_spec.rb
+++ promotions/spec/system/solidus_promotions/backend/promotions_spec.rb
@@ -168,20 +168,21 @@ RSpec.feature "Promotions admin" do
     let!(:option_value) { create(:option_value) }
     let!(:variant) { create(:variant, product: product, option_values: [option_value]) }
 
-    it "allows adding a line_item_with_options condition", :js do
+    it "allows adding an option value condition", :js do
       visit solidus_promotions.edit_admin_promotion_path(promotion_with_benefit)
       click_link "Add Condition"
-      select("Line Item Option Value(s)", from: "Condition Type")
+      select("Line Item variant matches option values", from: "Condition Type")
       click_button "Add"
-      expect(page).to have_content("Line Item Option Value(s)")
+      expect(page).to have_content("Line Item variant matches option values")
       click_link "Add product"
       within(".promo-condition-option-value") do
-        # find('.select2-focusser').set(product.name)
-        # find('.select2-input').set(option_value.name)
+        targetted_select2_search(product.name, from: "#s2id_ov-product-picker-0")
+        targetted_select2_search(option_value.name, from: "#s2id_option-value-picker-0")
       end
       within("#benefits_adjust_line_item_#{promotion_with_benefit.benefits.first.id}_conditions") do
-        find('input[type="submit"]', wait: 5).click
+        click_button("Update")
       end
+      expect(page).to have_content("Condition has been successfully updated!")
     end
   end
 end
