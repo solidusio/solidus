0368520ef8ebb616ec47448655c1abf39639f32a
Martin Meyerhoff <mamhoff@gmail.com>
2025-12-02 10:11:03 +0100

    Add Spree::Shipment#discounted_amount
    This is the same code as in Spree::LineItem. We'll move it to a module
    soon.

--- promotions/app/patches/models/solidus_promotions/shipment_patch.rb
+++ promotions/app/patches/models/solidus_promotions/shipment_patch.rb
@@ -9,6 +9,24 @@ module SolidusPromotions
       shipping_rates.each(&:reset_current_discounts)
     end
 
+    def discounted_amount
+      amount + previous_lanes_discounts.sum(&:amount)
+    end
+
+    private
+
+    def previous_lanes_discounts
+      discounts_by_lanes(PromotionLane.previous_lanes)
+    end
+
+    def discounts_by_lanes(lanes)
+      adjustments.select do |adjustment|
+        !adjustment.marked_for_destruction? &&
+          adjustment.source_type == "SolidusPromotions::Benefit" &&
+          adjustment.source.promotion.lane.in?(lanes)
+      end
+    end
+
     Spree::Shipment.prepend self
   end
 end
