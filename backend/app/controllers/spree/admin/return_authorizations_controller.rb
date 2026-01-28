a570cc048203306d845eb78f0858f2beac3fd1f8
Chris Todorov <chris@super.gd>
2026-01-27 16:06:21 -0800

    Associate return items with return authorization
    This change ensures that return items built for a new return
    authorization are correctly associated with it. This will ensure that
    the display_* methods use the currency from the return authorization,
    instead of the default store currency.
    
    Co-authored-by: Alistair Norman <alistair@super.gd>

--- backend/app/controllers/spree/admin/return_authorizations_controller.rb
+++ backend/app/controllers/spree/admin/return_authorizations_controller.rb
@@ -40,10 +40,13 @@ module Spree
         associated_inventory_units = @return_authorization.return_items.map(&:inventory_unit)
         unassociated_inventory_units = all_inventory_units - associated_inventory_units
 
-        new_return_items = unassociated_inventory_units.map do |new_unit|
-          Spree::ReturnItem.new(inventory_unit: new_unit).tap(&:set_default_amount)
+        unassociated_inventory_units.each do |new_unit|
+          @return_authorization.return_items.
+            build(inventory_unit: new_unit).
+            tap(&:set_default_amount)
         end
-        @form_return_items = (@return_authorization.return_items + new_return_items).sort_by(&:inventory_unit_id)
+
+        @form_return_items = @return_authorization.return_items.sort_by(&:inventory_unit_id)
       end
 
       def load_reimbursement_types
