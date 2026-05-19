0f7b3d5b9a3763df1c5484bd3008a2dd21bb852e
Chris Todorov <chris@super.gd>
2026-05-19 16:03:01 -0700

    Fix double render error in StoreController
    We don't need to render when using `fresh_when`, as it will either
    render the partial or return a not modified status code.
    
    Co-authored-by: Adam Mueller <adam@super.gd>
    Co-authored-by: Alistair Norman <alistair@super.gd>
    Co-authored-by: Jared Norman <jared@super.gd>
    Co-authored-by: Noah Silvera <noah@super.gd>
    Co-authored-by: Senem Soy <senem@super.gd>
    Co-authored-by: Sofia Besenski <sofia@super.gd>

--- starter_frontend/templates/app/controllers/store_controller.rb
+++ starter_frontend/templates/app/controllers/store_controller.rb
@@ -14,8 +14,9 @@ class StoreController < Spree::BaseController
   end
 
   def cart_link
-    render partial: "shared/cart/link_to_cart"
-    fresh_when(etag: current_order, template: "shared/cart/_link_to_cart")
+    unless fresh_when(etag: current_order, template: "shared/cart/_link_to_cart")
+      render partial: "shared/cart/link_to_cart"
+    end
   end
 
   private
