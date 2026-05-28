57e326223804b5402c31b11f1924602fe7938b1b
Chris Todorov <chris@super.gd>
2026-05-28 11:12:38 -0700

    Skip no-unused-var check for JS script file
    This component doesn't conform to the "module" ES source type we have
    configured in eslint, so the use of the functions is not correctly
    detected. There are indeed some unused functions here, but that's out of
    scope for this change and should be refactored in a separate PR.
    
    Co-authored-by: Adam Mueller <adam@super.gd>
    Co-authored-by: Sofia Besenski <sofia@super.gd>
    Co-authored-by: Noah Silvera <noah@super.gd>

--- starter_frontend/templates/app/assets/javascripts/product_selection_component.js
+++ starter_frontend/templates/app/assets/javascripts/product_selection_component.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 document.addEventListener("DOMContentLoaded", function () {
   const optionTypeSelector = document.querySelectorAll(".selection-items");
   for (var i = 0; i < optionTypeSelector.length; i++) {
