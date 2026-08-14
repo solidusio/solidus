ef99146fa0449152604cbfa39897680e005d192e
Chris Todorov <chris@super.gd>
2026-08-14 14:03:16 -0700

    Use pagination config in admin orders/products controllers
    This passes the new config when paginating orders and products in the
    Solidus admin. These two controllers explicitly call the
    `set_and_extract_portion_from` helper so they need to receive this
    argument explicitly, otherwise the default configuration from the geared
    pagination will be used.
    
    Co-authored-by: Senem Soy <senem@super.gd>

--- admin/spec/requests/solidus_admin/orders_spec.rb
+++ admin/spec/requests/solidus_admin/orders_spec.rb
@@ -10,6 +10,41 @@ RSpec.describe "SolidusAdmin::OrdersController", type: :request do
     allow_any_instance_of(SolidusAdmin::BaseController).to receive(:spree_current_user).and_return(admin_user)
   end
 
+  describe "GET #index" do
+    around do |example|
+      original_per_page = SolidusAdmin::Config[:pagination_ratios_per_page]
+      SolidusAdmin::Config[:pagination_ratios_per_page] = [1, 2, 3, 4]
+      example.run
+      SolidusAdmin::Config[:pagination_ratios_per_page] = original_per_page
+    end
+
+    it "paginates resources on first page based on pagination config" do
+      orders = create_list(:completed_order_with_totals, 4)
+      orders.sort_by!(&:completed_at).reverse!
+
+      get solidus_admin.orders_path
+      expect(response).to have_http_status(:ok)
+
+      expect(response.body).to include(orders[0].number)
+      expect(response.body).to_not include(orders[1].number)
+      expect(response.body).to_not include(orders[2].number)
+      expect(response.body).to_not include(orders[3].number)
+    end
+
+    it "paginates resources on second page based on pagination config" do
+      orders = create_list(:completed_order_with_totals, 4)
+      orders.sort_by!(&:completed_at).reverse!
+
+      get solidus_admin.orders_path(params: {page: 2})
+      expect(response).to have_http_status(:ok)
+
+      expect(response.body).to_not include(orders[0].number)
+      expect(response.body).to include(orders[1].number)
+      expect(response.body).to include(orders[2].number)
+      expect(response.body).to_not include(orders[3].number)
+    end
+  end
+
   describe "GET #show" do
     let(:order) { create(:completed_order_with_totals, line_items_count: 3) }
 
