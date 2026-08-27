d15691c12591106bf7b80914d8f2f7677f5fbb26
Martin Meyerhoff <mamhoff@gmail.com>
2026-08-27 10:10:50 +0000

    Load discarded promotions in benefit association
    Benefits need access to their promotion even when it's discarded to
    properly handle cleanup and checks. This matches the pattern used in
    OrderPromotion and PromotionCode associations.
    
    (cherry picked from commit d57b042ea9b4825d1381ada62d44c650a0dc51ca)
    
    # Conflicts:
    #	promotions/app/models/solidus_promotions/benefit.rb
    #	promotions/spec/models/spree/line_item_spec.rb

--- promotions/app/models/solidus_promotions/promotion.rb
+++ promotions/app/models/solidus_promotions/promotion.rb
@@ -12,7 +12,7 @@ module SolidusPromotions
       optional: true,
       inverse_of: :promotions
     belongs_to :original_promotion, class_name: "Spree::Promotion", optional: true
-    has_many :benefits, class_name: "SolidusPromotions::Benefit", dependent: :destroy
+    has_many :benefits, class_name: "SolidusPromotions::Benefit", dependent: :destroy, inverse_of: :promotion
     has_many :conditions, through: :benefits
     has_many :codes, class_name: "SolidusPromotions::PromotionCode", dependent: :destroy, inverse_of: :promotion
     has_many :code_batches, class_name: "SolidusPromotions::PromotionCodeBatch", dependent: :destroy
