058b7780086efba7fe70ef948e4cf5c09b26f1f1
benjamin wil <hey@benjaminwil.info>
2026-09-14 17:27:30 -0700

    Inherit `TaxRatesController#index` and `#destroy`
    There is nothing special about the these actions that require an
    action definition here. We can use the inherited `ResourcesController`.

--- admin/app/controllers/solidus_admin/tax_rates_controller.rb
+++ admin/app/controllers/solidus_admin/tax_rates_controller.rb
@@ -4,28 +4,6 @@ module SolidusAdmin
   class TaxRatesController < SolidusAdmin::ResourcesController
     include SolidusAdmin::ControllerHelpers::Search
 
-    def index
-      tax_rates = apply_search_to(
-        Spree::TaxRate.order(created_at: :desc, id: :desc),
-        param: :q
-      )
-
-      set_page_and_extract_portion_from(tax_rates)
-
-      respond_to do |format|
-        format.html { render component("tax_rates/index").new(page: @page) }
-      end
-    end
-
-    def destroy
-      @tax_rates = Spree::TaxRate.where(id: params[:id])
-
-      Spree::TaxRate.transaction { @tax_rates.destroy_all }
-
-      flash[:notice] = t(".success")
-      redirect_back_or_to tax_rates_path, status: :see_other
-    end
-
     private
 
     def resource_class = Spree::TaxRate
