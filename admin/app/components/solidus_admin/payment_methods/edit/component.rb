04f9aa93b5b87c6457f7908bf0554a742f2020eb
benjamin wil <hey@benjaminwil.info>
2026-09-15 13:51:18 -0700

    Resolve issues with payment methods forms, i18n
    The HTML IDs were incorrect. While I was making this change I saw
    opportunities to reduce the complexity of the component templates
    and reduce the need generic translation strings at the component
    level.

--- admin/app/components/solidus_admin/payment_methods/edit/component.rb
+++ admin/app/components/solidus_admin/payment_methods/edit/component.rb
@@ -2,4 +2,6 @@
 
 class SolidusAdmin::PaymentMethods::Edit::Component < SolidusAdmin::Resources::Edit::Component
   include SolidusAdmin::Layout::PageHelpers
+
+  def back_url = solidus_admin.payment_methods_path
 end
--- admin/app/components/solidus_admin/payment_methods/new/component.rb
+++ admin/app/components/solidus_admin/payment_methods/new/component.rb
@@ -2,4 +2,6 @@
 
 class SolidusAdmin::PaymentMethods::New::Component < SolidusAdmin::Resources::New::Component
   include SolidusAdmin::Layout::PageHelpers
+
+  def back_url = solidus_admin.payment_methods_path
 end
