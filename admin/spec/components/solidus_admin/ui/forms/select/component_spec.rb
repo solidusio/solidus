559ecce16ec3f8cd7ce3d828119eb3004a0ec69c
benjamin wil <hey@benjaminwil.info>
2026-09-25 15:37:57 -0700

    Make select component preview less flaky
    Hard-coded IDs make life bad sometimes. This also happens to improve the
    readability of these previews, in my opinion.

--- admin/spec/components/solidus_admin/ui/forms/select/component_spec.rb
+++ admin/spec/components/solidus_admin/ui/forms/select/component_spec.rb
@@ -3,6 +3,13 @@
 require "spec_helper"
 
 RSpec.describe SolidusAdmin::UI::Forms::Select::Component, type: :component do
+  before do
+    country = create :country
+    create :state, country:, state_code: "AL"
+    create :state, country:, state_code: "AK"
+    create :state, country:, state_code: "AZ"
+  end
+
   it "renders the overview preview" do
     render_preview(:overview)
   end
